f7755e60f3661e7d705e83b76f68ee5b68a1f1e6
Blair Christensen <blair@uchicago.edu>
2004-02-27 19:29:13 +0000

    + Add in initial JDBC (hardcoded!) support + Create pseudorandom sessionIDs instead of assigning '1' to everyone.
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk/grouper@5 bc24b658-cf39-4249-b827-e3526eaaeb42

--- java/src/edu/internet2/middleware/grouper/Grouper.java
+++ java/src/edu/internet2/middleware/grouper/Grouper.java
@@ -1,23 +1,57 @@
 /*
- * $Id: Grouper.java,v 1.1.1.1 2004-02-27 17:55:52 blair Exp $
+ * $Id: Grouper.java,v 1.2 2004-02-27 19:29:13 blair Exp $
  */
 
 // XXX package edu.internet2.middleware.mace.grouper;
 
+import java.sql.*;
+
 public class Grouper {
 
-  private int     sessionID;
-  private String  cred;
+  private Connection  con;
+  private int         sessionID;
+  private String      cred;
+
+  // XXX Ugh.  Make this configurable.
+  private String      url = "jdbc:postgresql://fifty6th:5432/wm_logs";
 
   public Grouper() {
+    con       = null;
     sessionID = -1;
     cred      = null;
   }
 
   public int Session_start (String cred) {
     // XXX Assert parameters
-    this.cred       = cred;
-    this.sessionID  = 1;    // XXX
+
+    // We blindly trust the credentials we are passed.
+    this.cred           = cred;
+
+    // And now we need a session id.
+    // ???  Whether this is the best way to approach the problem is
+    //      another matter.
+    java.util.Random r  = new java.util.Random();
+    // XXX Well that is certainly arbitrary.
+    this.sessionID      = Math.abs( r.nextInt( 65535 ) );
+
+    // XXX Ugh.  Where to put this? 
+    try {
+      // XXX Ugh.  Make this configurable.
+      String driver = "org.postgresql.Driver";
+      Class.forName(driver).newInstance( );
+      // XXX Ugh.  Make this configurable.
+      con = DriverManager.getConnection(url, "wm_logs", "");
+    }
+    catch( Exception e ) {
+      // XXX Specify *what* JDBC driver.
+      System.out.println("Failed to load JDBC driver.");
+      // XXX  And I should probably do something else in this
+      //      case other than just return an invalid session id.
+      // XXX  And I should document somewhere what valid session
+      //      ids are -- once I know myself.
+      return -1;
+    }
+
     return this.sessionID;
   }
 
@@ -47,8 +81,15 @@ public class Grouper {
     }
     this.cred       = null;
     this.sessionID  = -1;
+
+    // ???  Should this be within a 'finally'?  And/Or should it go
+    //      elsewhere?
+    if (con != null) {
+      try { con.close( ); }
+      catch( Exception e ) { e.printStackTrace( ); }
+    }
+
     return true;
   }
-
 }
 
