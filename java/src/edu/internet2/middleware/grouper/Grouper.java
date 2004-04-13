f3bdaf2b8e4ebfd9508295053d142f2c405700f5
Blair Christensen <blair@uchicago.edu>
2004-04-13 21:27:39 +0000

    add initialize() and destroy() instance methods
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk/grouper@32 bc24b658-cf39-4249-b827-e3526eaaeb42

--- java/src/edu/internet2/middleware/grouper/Grouper.java
+++ java/src/edu/internet2/middleware/grouper/Grouper.java
@@ -8,56 +8,41 @@ import java.util.Properties;
  * Provides a Grouper environment.
  *
  * @author blair christensen.
- * @version $Id: Grouper.java,v 1.7 2004-04-12 04:14:36 blair Exp $
+ * @version $Id: Grouper.java,v 1.8 2004-04-13 21:27:39 blair Exp $
  */
 public class Grouper {
 
   /*
-    - Read configuration
-    - Establish database connection
-      - XXX Or should this be per session?
     - Read and cache (in a Map?)
       - XXX types
       - XXX typeDefs
       - XXX fields
   */
 
-
-  /* these remnants have got to go */
-  private Connection  con;
-  private int         sessionID;
-  private String      cred;
-
-  private Properties  conf     = new Properties();
-  private String      confFile = "grouper.cf";
-
-  private String jdbcDriver;
-  private String jdbcPassword;
-  private String jdbcUrl;
-  private String jdbcUsername;
+  private static final Properties conf     = new Properties();
+  private static final String     confFile = "grouper.cf";
 
   /**
-   * Create Grouper environment.
-   * <p>
-   * Instantiating a Grouper object will read in the Grouper
-   * configuration file (grouper.cf), which determines where the groups
-   * registry is located, how to contact the groups registry, and what
-   * implementations of the Grouper interfaces to use.
+   * Create {@link Grouper} environment.
    */
   public Grouper() {
-    con       = null;
-    sessionID = -1;
-    cred      = null;
 
+  }
+
+  /**
+   * Initializes {@link Grouper} environment.
+   * <p>
+   * <ul>
+   *  <li>Reads configuration</li>
+   *  <li>Starts executive {@link GrouperSession} session used for
+   *      bootstrapping and verifying other sessions.</li>
+   * </ul>
+   */
+  public void initialize() {
     try {
       FileInputStream in = new FileInputStream(confFile);
       try {
         conf.load(in);
-        // XXX Try, try, try...
-        jdbcDriver   = conf.getProperty("jdbc.driver");
-        jdbcPassword = conf.getProperty("jdbc.password");
-        jdbcUrl      = conf.getProperty("jdbc.url");
-        jdbcUsername = conf.getProperty("jdbc.username");
       } catch (IOException e) {
         System.err.println("Unable to read '" + confFile + "'");
       }
@@ -66,11 +51,16 @@ public class Grouper {
     }
   }
 
-  /*
-    - XXX Do we need another method to reload the configuration, reconnect
-          to the database, and reread/recache types, etc.?
-      - How would this work?  Perhaps leave this alone for now.
-  */
+  /**
+   * Destroys {@link Grouper} environment.
+   * <p>
+   * <ul>
+   *  <li>Stops executive {@link GrouperSession} session used for
+   *      bootstrapping and verifying other sessions.</li>
+   * </ul>
+   */ 
+  public void destroy() {
+  }
 
 }
 
