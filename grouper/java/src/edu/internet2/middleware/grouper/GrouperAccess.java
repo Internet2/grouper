b441e7d5c063456606dbdb74ec32913cdc511a28
Blair Christensen <blair@uchicago.edu>
2004-04-27 05:24:41 +0000

    A few more (ugly) details regarding the privilege interface and its default implementation.
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@40 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper/java/src/edu/internet2/middleware/grouper/GrouperAccess.java
+++ grouper/java/src/edu/internet2/middleware/grouper/GrouperAccess.java
@@ -2,12 +2,50 @@ package edu.internet2.middleware.directory.grouper;
 
 /** 
  * {@link Grouper} Privilege Interface.
+ * <p>
+ * <ul>
+ *  <li>XXX We may want/need a method that returns all of a user's
+ *      {@link GrouperMember}'s privileges.</li>
+ * </ul>
  *
  * @author  blair christensen.
- * @version $Id: GrouperAccess.java,v 1.2 2004-04-11 03:15:18 blair Exp $
+ * @version $Id: GrouperAccess.java,v 1.3 2004-04-27 05:24:41 blair Exp $
  */
 public interface GrouperPrivilege {
-  public boolean assign(GrouperGroup g, String priv);
-  public boolean has(GrouperGroup g, String priv);
+  /**
+   * Grant privilege on a {@link Grouper} group.
+   * <p>
+   * See implementations for more information.
+   *
+   * @param g     {@link GrouperGroup} to grant privilege on.
+   * @param m     {@link GrouperMember} to grant privilege to.
+   * @param priv  Privilege type to grant.
+   */
+  public void grant(GrouperGroup g, GrouperMember m, String priv);
+
+  /**
+   * Revoke privilege on a {@link Grouper} group.
+   * <p>
+   * See implementations for more information.
+   *
+   * @param g     {@link GrouperGroup} to revoke privilege on.
+   * @param m     {@link GrouperMember} to revoke privilege from.
+   * @param priv  Privilege type to revoke.
+   */
+  public void revoke(GrouperGroup g, GrouperMember m, String priv);
+
+  /**
+   * Verify whether a {@link GrouperMember} has a specified privilege
+   * on a {@link GrouperGroup}.
+   * <p>
+   * See implementations for more information.
+   *
+   * @param g     {@link GrouperGroup} to check privileges on.
+   * @param m     {@link GrouperMember} to check privileges for.
+   * @param priv  Privilege type to check for.
+   * @return  Returns true if the {@link GrouperMember} has privilege
+   *          <i>priv</i> on the {@link GrouperGroup}.
+   */
+  public boolean has(GrouperGroup g, GrouperMember m, String priv);
 }
 
--- grouper/java/src/edu/internet2/middleware/grouper/GrouperAccessImpl.java
+++ grouper/java/src/edu/internet2/middleware/grouper/GrouperAccessImpl.java
@@ -4,14 +4,57 @@ package edu.internet2.middleware.directory.grouper;
  * Default implementation of the {@link GrouperPrivilege} interface.
  *
  * @author  blair christensen.
- * @version $Id: GrouperAccessImpl.java,v 1.1 2004-04-11 22:55:35 blair Exp $
+ * @version $Id: GrouperAccessImpl.java,v 1.2 2004-04-27 05:24:41 blair Exp $
  */
 public class InternalGrouperPrivilege implements GrouperPrivilege {
-  public boolean assign(GrouperGroup g, String priv) {
-    return false;
+  /**
+   * Create a {@link GrouperPrivilege} that handles all privileges
+   * internal to grouper.
+   */
+  public InternalGrouperPrivilege(GrouperSession s) {
+    // Nothing -- Yet
   }
 
-  public boolean has(GrouperGroup g, String priv) {
+  /**
+   * Grant privilege on a {@link Grouper} group.
+   * <p>
+   * <ul>
+   *  <li>XXX Confirm that subjectID has appropriate privilege to
+   *      grant "priv" to "m".</li>
+   *  <li>XXX Update <i>grouper_membership</i> table as apropriate.</li>
+   *  <li>XXX Update <i>grouper_via</i> table as apropriate.</li>
+   * </ul>
+   */
+  public void grant(GrouperGroup g, GrouperMember m, String priv) {
+    // Nothing -- Yet
+  }
+
+  /**
+   * Revoke privilege on a {@link Grouper} group.
+   * <p>
+   * <ul>
+   *  <li>XXX Confirm that subjectID has appropriate privilege to
+   *      revoke "priv" from "m".</li>
+   *  <li>XXX Update <i>grouper_membership</i> table as apropriate.</li>
+   *  <li>XXX Update <i>grouper_via</i> table as apropriate.</li>
+   * </ul>
+   */
+  public void revoke(GrouperGroup g, GrouperMember m, String priv) {
+    // Nothing -- Yet
+  }
+
+  /**
+   * Verify whether a {@link GrouperMember} has a specified privilege
+   * on a {@link GrouperGroup}.
+   * <p>
+   * <ul>
+   *  <li>XXX Confirm that subjectID has appropriate privilege to 
+   *      verify "m"'s privileges.</li>
+   *  <li>XXX Do I need to differentiate between "user" level checks
+   *      and "system" level checks?</li>
+   * </ul>
+   */
+  public boolean has(GrouperGroup g, GrouperMember m, String priv) {
     return false;
   }
 }
