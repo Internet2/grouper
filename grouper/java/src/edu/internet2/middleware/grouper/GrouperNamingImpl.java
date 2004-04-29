e24685f3eee93b9e6afc09b2e8e2d5adfb7adb8b
Blair Christensen <blair@uchicago.edu>
2004-04-29 16:14:44 +0000

    javadoc and some tentative actions to perform
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@63 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper/java/src/edu/internet2/middleware/grouper/GrouperNamingImpl.java
+++ grouper/java/src/edu/internet2/middleware/grouper/GrouperNamingImpl.java
@@ -7,17 +7,44 @@ import  java.util.List;
  * Default implementation of the {@link GrouperNaming} interface.
  *
  * @author  blair christensen.
- * @version $Id: GrouperNamingImpl.java,v 1.2 2004-04-29 03:43:58 blair Exp $
+ * @version $Id: GrouperNamingImpl.java,v 1.3 2004-04-29 16:14:44 blair Exp $
  */
 public class InternalGrouperNaming implements GrouperNaming {
+
+  private GrouperSession intSess = null;
+
   public InternalGrouperNaming(GrouperSession s) {
-    // Nothing -- Yet
+    // Internal reference to the session we are using.
+    intSess = s;
   }
 
+  /**
+   * List of all stems that can be created by a <i>subjectID</i>.
+   * <p>
+   * <ul>
+   *  <li>XXX Fetch and return <i>GrouperCreator:*</i> memberships
+   *      from the <i>grouper_membership</i> table?</li>
+   *  <li>XXX Add to docs/examples/.</li>
+   * </ul>
+   * 
+   */
   public List allowedStems() {
     return null;
   }
 
+  /**
+   * Verifies whether the given stem can be created by a
+   * <i>subjectID</i>.
+   * <p>
+   * <ul>
+   *  <li>XXX Fetch <i>GrouperCreator:*</i> memberships from the
+   *      <i>grouper_membership</i> table?</li>
+   *  <li>Determine whether <i>subjectID</i> is allowed to create
+   *      <i>stem</i>.</li>
+   *  <li>XXX Add to docs/examples/.</li>
+   * </ul>
+   *
+   */
   public boolean allowedStems(String stem) {
     return false;
   }
