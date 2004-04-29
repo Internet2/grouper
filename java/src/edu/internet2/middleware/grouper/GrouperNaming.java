9029a19076f0eb6dde5a8366e7d4a0864b76f850
Blair Christensen <blair@uchicago.edu>
2004-04-29 16:14:08 +0000

    some basic javadoc
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk/grouper@62 bc24b658-cf39-4249-b827-e3526eaaeb42

--- java/src/edu/internet2/middleware/grouper/GrouperNaming.java
+++ java/src/edu/internet2/middleware/grouper/GrouperNaming.java
@@ -6,10 +6,23 @@ import java.util.List;
  * {@link Grouper} Naming Interface.
  *
  * @author  blair christensen.
- * @version $Id: GrouperNaming.java,v 1.1 2004-04-11 03:13:44 blair Exp $
+ * @version $Id: GrouperNaming.java,v 1.2 2004-04-29 16:14:08 blair Exp $
  */
 public interface GrouperNaming {
+  /**
+   * List of all stems that can be created by a <i>subjectID</i>.
+   * 
+   * @return  List of all stems that can be created. 
+   */
   public List allowedStems();
+
+  /**
+   * Verifies whether the given stem can be created by a
+   * <i>subjectID</i>.
+   *
+   * @param   stem  The stem to verify.
+   * @return  True if allowed to create <i>stem</i>.
+   */
   public boolean allowedStems(String stem);
 }
 
