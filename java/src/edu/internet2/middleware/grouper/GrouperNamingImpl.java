d9897ce2979a836bd5ec9bc51080f62ae0bb37c8
Blair Christensen <blair@uchicago.edu>
2004-04-29 03:43:58 +0000

    + import grouper.* + Add stub constructors
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk/grouper@50 bc24b658-cf39-4249-b827-e3526eaaeb42

--- java/src/edu/internet2/middleware/grouper/GrouperNamingImpl.java
+++ java/src/edu/internet2/middleware/grouper/GrouperNamingImpl.java
@@ -1,14 +1,19 @@
 package edu.internet2.middleware.directory.grouper;
 
-import java.util.List;
+import  edu.internet2.middleware.directory.grouper.*;
+import  java.util.List;
 
 /** 
  * Default implementation of the {@link GrouperNaming} interface.
  *
  * @author  blair christensen.
- * @version $Id: GrouperNamingImpl.java,v 1.1 2004-04-11 22:55:35 blair Exp $
+ * @version $Id: GrouperNamingImpl.java,v 1.2 2004-04-29 03:43:58 blair Exp $
  */
 public class InternalGrouperNaming implements GrouperNaming {
+  public InternalGrouperNaming(GrouperSession s) {
+    // Nothing -- Yet
+  }
+
   public List allowedStems() {
     return null;
   }
