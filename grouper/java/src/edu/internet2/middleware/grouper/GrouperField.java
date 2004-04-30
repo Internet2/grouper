4d308452271acc2ad86d676390516b9fd996bf9d
Blair Christensen <blair@uchicago.edu>
2004-04-30 18:55:17 +0000

    Grouper type, field, and typedef classes
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@83 bc24b658-cf39-4249-b827-e3526eaaeb42

---
+++ grouper/java/src/edu/internet2/middleware/grouper/GrouperField.java
@@ -0,0 +1,19 @@
+package edu.internet2.middleware.directory.grouper;
+
+/** 
+ * Class representing a field within a {@link GrouperGroup}.
+ *
+ * @author blair christensen.
+ * @version $Id: GrouperField.java,v 1.1 2004-04-30 18:55:17 blair Exp $
+ */
+public class GrouperField {
+
+  /**
+   * Create a {@link GrouperField} object.
+   */
+  public GrouperField() {
+    // Nothing -- Yet
+  }
+
+}
+
