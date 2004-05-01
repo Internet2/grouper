26308c8e83fb870fe86788724ab0a17b2dd6a6d2
Blair Christensen <blair@uchicago.edu>
2004-05-01 16:57:16 +0000

    Stem and Descriptor stub classes
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@85 bc24b658-cf39-4249-b827-e3526eaaeb42

---
+++ grouper/java/src/edu/internet2/middleware/grouper/GrouperDescriptor.java
@@ -0,0 +1,20 @@
+package edu.internet2.middleware.directory.grouper;
+
+/** 
+ * Class representing the descriptor portion of a {@link GrouperGroup}
+ * name.
+ *
+ * @author blair christensen.
+ * @version $Id: GrouperDescriptor.java,v 1.1 2004-05-01 16:57:16 blair Exp $
+ */
+public class GrouperDescriptor {
+
+  /**
+   * Create a {@link GrouperDescriptor} object.
+   */
+  public GrouperDescriptor() {
+    // Nothing -- Yet
+  }
+
+}
+
---
+++ grouper/java/src/edu/internet2/middleware/grouper/GrouperStem.java
@@ -0,0 +1,19 @@
+package edu.internet2.middleware.directory.grouper;
+
+/** 
+ * Class representing the stem portion of a {@link GrouperGroup} name.
+ *
+ * @author blair christensen.
+ * @version $Id: GrouperStem.java,v 1.1 2004-05-01 16:57:16 blair Exp $
+ */
+public class GrouperStem {
+
+  /**
+   * Create a {@link GrouperStem} object.
+   */
+  public GrouperStem() {
+    // Nothing -- Yet
+  }
+
+}
+
