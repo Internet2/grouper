a30e045a6a2ae1a3f6c993293fdc6440884ec503
Blair Christensen <blair@uchicago.edu>
2004-05-24 21:36:07 +0000

    start making the tests marginally useful
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@101 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper/java/tests/GrouperTest.java
+++ grouper/java/tests/GrouperTest.java
@@ -1,5 +1,5 @@
 /*
- * $Id: GrouperTest.java,v 1.2 2004-03-25 01:40:37 blair Exp $
+ * $Id: GrouperTest.java,v 1.3 2004-05-24 21:36:07 blair Exp $
  */
 
 import edu.internet2.middleware.directory.grouper.*;
@@ -7,46 +7,18 @@ import junit.framework.*;
 
 public class GrouperTest extends TestCase {
 
-  private boolean rv;
-  private int     sessionID;
-  public  Grouper g;
+  public Grouper        G;
 
   public GrouperTest(String name) {
     super(name);
   }
 
-  protected void setUp () {
-    rv        = false;
-    sessionID = -1;
-    g         = new Grouper();
+  public void testSession () {
+    G = new Grouper();
+    G.initialize();
+    G.destroy();
+    // XXX Assert?
   }
 
-  public void testInstantiate () {
-    assertNotNull(g);
-  }
-
-  public void testSession_start () {
-    assertNotNull(g);
-    sessionID = g.Session_start("TestUser");
-    // XXX assertTrue(sessionID.is_int());
-    assertTrue(sessionID != -1);
-    assertTrue(sessionID >   0);
-  }
-
-  public void testSession_end () {
-    assertNotNull(g);
-    rv = g.Session_end(sessionID);
-    assertFalse(rv);
-  }
-
-  public void testSession_start_and_end () {
-    assertNotNull(g);
-    sessionID = g.Session_start("TestUser");
-    //assertTrue(sessionID.is_int());
-    assertTrue(sessionID != -1);
-    assertTrue(sessionID >   0);
-    rv = g.Session_end(sessionID);
-    assertTrue(rv);
-  }
 }
 
