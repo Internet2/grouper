d77e1cde741dc4ad437ba940c6a2653fb059797f
Blair Christensen <blair@uchicago.edu>
2004-08-03 01:07:34 +0000

    protected, not private
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@152 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper/java/src/edu/internet2/middleware/grouper/GrouperType.java
+++ grouper/java/src/edu/internet2/middleware/grouper/GrouperType.java
@@ -4,16 +4,16 @@ package edu.internet2.middleware.directory.grouper;
  * Class representing a type of {@link GrouperGroup}.
  *
  * @author  blair christensen.
- * @version $Id: GrouperType.java,v 1.1 2004-07-26 17:03:49 blair Exp $
+ * @version $Id: GrouperType.java,v 1.2 2004-08-03 01:07:34 blair Exp $
  */
-public class GrouperGroupType {
+public class GrouperType {
 
   private int groupType = 0;
 
   /**
-   * Create a {@link GrouperGroupType} object.
+   * Create a {@link GrouperType} object.
    */
-  public GrouperGroupType() {
+  public GrouperType() {
     // Nothing -- Yet
   }
 
@@ -21,10 +21,11 @@ public class GrouperGroupType {
    * Below for Hibernate
    */
 
-  private int getGroupType() {
+  protected int getGroupType() {
     return this.groupType;
   }
 
+  // XXX Do we really want to allow write access? 
   private void setGroupType(int groupType) {
     this.groupType = groupType;
   }
