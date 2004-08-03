c795536449ae1f99b691e8d59ad08a533b357de5
Blair Christensen <blair@uchicago.edu>
2004-08-03 01:09:52 +0000

    + Implements Serializable (for Hibernate) + Overrides equals() and hashCode() (for Hibernate) + protected, not private
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk/grouper@153 bc24b658-cf39-4249-b827-e3526eaaeb42

--- java/src/edu/internet2/middleware/grouper/GrouperTypeDef.java
+++ java/src/edu/internet2/middleware/grouper/GrouperTypeDef.java
@@ -1,12 +1,14 @@
 package edu.internet2.middleware.directory.grouper;
 
+import  java.io.Serializable;
+
 /** 
- * Class representing a type definition for a {@link GrouperGroup}.
+ * Class representing a type definition for a {@link Grouper}.
  *
  * @author  blair christensen.
- * @version $Id: GrouperTypeDef.java,v 1.1 2004-07-26 17:03:49 blair Exp $
+ * @version $Id: GrouperTypeDef.java,v 1.2 2004-08-03 01:09:52 blair Exp $
  */
-public class GrouperGroupTypeDef {
+public class GrouperTypeDef implements Serializable {
 
   private int     groupType;
   private String  groupField;
@@ -16,21 +18,21 @@ public class GrouperGroupTypeDef {
    */
 
   /**
-   * Create a {@link GrouperGroupTypeDef} object.
+   * Create a {@link GrouperTypeDef} object.
    * <p>
    * XXX Is this class needed?  Or do {@link GrouperField} and 
    *     {@link GrouperType} provide everything this class might be
    *     needed for?
    */
-  public GrouperGroupTypeDef() {
+  public GrouperTypeDef() {
     // Nothing -- Yet
   }
 
   /*
    * Below for Hibernate
    */
-  
-  private int getGroupType() {
+
+  protected int getGroupType() {
     return this.groupType;
   }
 
@@ -38,12 +40,25 @@ public class GrouperGroupTypeDef {
     this.groupType = groupType;
   }
 
-  private String getGroupField() {
+  protected String getGroupField() {
     return this.groupField;
   }
 
   private void setGroupField(String groupField) {
     this.groupField = groupField;
+  }
+
+  // XXX Simplistic!  And probably wrong!
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    return false;
+  }
+
+  // XXX Is this wise?  Correct?  Sufficient?
+  public int hashCode() {
+    return this.getGroupType() + java.lang.Math.abs( this.getGroupField().hashCode() ); 
   }
 
 }
