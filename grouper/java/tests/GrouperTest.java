1107039ee38aa6379a3538c9760a7a340fe774da
Blair Christensen <blair@uchicago.edu>
2004-08-25 20:06:08 +0000

    add copyright/license blurb
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@185 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper/java/tests/GrouperTest.java
+++ grouper/java/tests/GrouperTest.java
@@ -1,5 +1,18 @@
+/* 
+ * Copyright (C) 2004 TODO
+ * All Rights Reserved. 
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted only as authorized by the Academic
+ * Free License version 2.1.
+ *
+ * A copy of this license is available in the file LICENSE in the
+ * top-level directory of the distribution or, alternatively, at
+ * <http://www.opensource.org/licenses/afl-2.1.php>
+ */
+
 /*
- * $Id: GrouperTest.java,v 1.23 2004-08-19 19:27:41 blair Exp $
+ * $Id: GrouperTest.java,v 1.24 2004-08-25 20:06:08 blair Exp $
  */
 
 package test.edu.internet2.middleware.grouper;
