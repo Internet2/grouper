70276f5f25a92cfe1a7a8e55cef926bbcd79fa59
Blair Christensen <blair@uchicago.edu>
2004-11-12 20:32:57 +0000

    put stub and somewhat start testing groups again
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@325 bc24b658-cf39-4249-b827-e3526eaaeb42

---
+++ grouper/java/tests/test/edu/internet2/middleware/grouper/TestGroupsAdd.java
@@ -0,0 +1,60 @@
+/* 
+ * Copyright (C) 2004 Internet2
+ * Copyright (C) 2004 The University Of Chicago
+ * All Rights Reserved. 
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted only as authorized by the Academic
+ * Free License version 2.1.
+ *
+ * A copy of this license is available in the file LICENSE in the
+ * top-level directory of the distribution or, alternatively, at
+ * <http://www.opensource.org/licenses/afl-2.1.php>
+ */
+
+/*
+ * $Id: TestGroupsAdd.java,v 1.1 2004-11-12 20:32:57 blair Exp $
+ */
+
+package test.edu.internet2.middleware.grouper;
+
+import  edu.internet2.middleware.grouper.*;
+import  edu.internet2.middleware.subject.*;
+import  junit.framework.*;
+
+
+public class TestGroups extends TestCase {
+
+  public TestGroups(String name) {
+    super(name);
+  }
+
+  protected void setUp () {
+    // Nothing -- Yet
+  }
+
+  protected void tearDown () {
+    // Nothing -- Yet
+  }
+
+
+  /*
+   * TESTS
+   */
+  
+
+  // Does Group exist?  No.
+  public void testGroupExistFalse() {
+    Grouper         G     = new Grouper();
+    GrouperSession  s     = new GrouperSession();
+    Subject         subj  = GrouperSubject.lookup( Grouper.config("member.system"), "person" );
+    s.start(subj);
+    GrouperGroup g = GrouperGroup.load(s, "stem.0", "descriptor.0");
+    // Confirm that group doesn't exist
+    Assert.assertFalse( g.exists() );
+    // We're done
+    s.stop();
+  }
+
+}
+
