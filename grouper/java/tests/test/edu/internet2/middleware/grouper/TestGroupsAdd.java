9cb8cde7dbbecca65d4155128b282665618f8324
Blair Christensen <blair@uchicago.edu>
2004-11-18 19:20:37 +0000

    rework group create|exists tests
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@361 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper/java/tests/test/edu/internet2/middleware/grouper/TestGroupsAdd.java
+++ grouper/java/tests/test/edu/internet2/middleware/grouper/TestGroupsAdd.java
@@ -13,7 +13,7 @@
  */
 
 /*
- * $Id: TestGroupsAdd.java,v 1.3 2004-11-15 18:22:19 blair Exp $
+ * $Id: TestGroupsAdd.java,v 1.4 2004-11-18 19:20:37 blair Exp $
  */
 
 package test.edu.internet2.middleware.grouper;
@@ -25,10 +25,14 @@ import  junit.framework.*;
 
 public class TestGroups extends TestCase {
 
-  private String stem   = "stem.0";
+  private String stem0  = "stem.0";
   private String stem1  = "stem.1";
-  private String desc   = "desc.0";
+  private String stem2  = "stem.2";
+  private String desc0  = "desc.0";
   private String desc1  = "desc.1";
+  private String desc2  = "desc.2";
+  
+  private String klass  = "edu.internet2.middleware.grouper.GrouperGroup";
 
   public TestGroups(String name) {
     super(name);
@@ -49,70 +53,100 @@ public class TestGroups extends TestCase {
   
 
   // Fetch a non-existent group.
-  public void testGroupExistFalse() {
+  public void testGroupsExistFalse() {
     Grouper         G     = new Grouper();
     GrouperSession  s     = new GrouperSession();
     Subject         subj  = GrouperSubject.lookup( Grouper.config("member.system"), "person" );
     s.start(subj);
-    GrouperGroup    g     = GrouperGroup.load(s, stem, desc);
-    // Confirm that group doesn't exist
-    Assert.assertFalse( g.exists() );
+    // Confirm that groups don't exist
+    GrouperGroup    grp0  = GrouperGroup.load(s, stem1, desc0);
+    Assert.assertFalse( grp0.exists() );
+    GrouperGroup    grp1  = GrouperGroup.load(s, stem1, desc1);
+    Assert.assertFalse( grp1.exists() );
+    GrouperGroup    grp2  = GrouperGroup.load(s, stem1, desc2);
+    Assert.assertFalse( grp2.exists() );
     // We're done
     s.stop();
   }
 
   // Create a group
-  public void testCreateGroup() {
+  public void testCreateGroups() {
     Grouper         G     = new Grouper();
     GrouperSession  s     = new GrouperSession();
     Subject         subj  = GrouperSubject.lookup( Grouper.config("member.system"), "person" );
     s.start(subj);
-    // Create the group
-    GrouperGroup grp      = GrouperGroup.create(s, stem, desc);
-    Assert.assertNotNull(grp);
-    String klass          = "edu.internet2.middleware.grouper.GrouperGroup";
-    Assert.assertTrue( klass.equals( grp.getClass().getName() ) );
-    Assert.assertTrue( grp.exists() );
-    Assert.assertNotNull( grp.type() );
-    Assert.assertNotNull( grp.attribute("stem") );
-    Assert.assertTrue( grp.attribute("stem").value().equals(stem) );
-    Assert.assertNotNull( grp.attribute("descriptor") );
-    Assert.assertTrue( grp.attribute("descriptor").value().equals(desc) );
+    // Create the groups
+    // Group 0
+    GrouperGroup    grp0  = GrouperGroup.create(s, stem0, desc0);
+    Assert.assertNotNull(grp0);
+    Assert.assertTrue( klass.equals( grp0.getClass().getName() ) );
+    Assert.assertTrue( grp0.exists() );
+    Assert.assertNotNull( grp0.type() );
+    Assert.assertNotNull( grp0.attribute("stem") );
+    Assert.assertTrue( grp0.attribute("stem").value().equals(stem0) );
+    Assert.assertNotNull( grp0.attribute("descriptor") );
+    Assert.assertTrue( grp0.attribute("descriptor").value().equals(desc0) );
+    // Group 1
+    GrouperGroup    grp1  = GrouperGroup.create(s, stem1, desc1);
+    Assert.assertNotNull(grp1);
+    Assert.assertTrue( klass.equals( grp1.getClass().getName() ) );
+    Assert.assertTrue( grp1.exists() );
+    Assert.assertNotNull( grp1.type() );
+    Assert.assertNotNull( grp1.attribute("stem") );
+    Assert.assertTrue( grp1.attribute("stem").value().equals(stem1) );
+    Assert.assertNotNull( grp1.attribute("descriptor") );
+    Assert.assertTrue( grp1.attribute("descriptor").value().equals(desc1) );
+    // Group 2
+    GrouperGroup    grp2  = GrouperGroup.create(s, stem2, desc2);
+    Assert.assertNotNull(grp2);
+    Assert.assertTrue( klass.equals( grp2.getClass().getName() ) );
+    Assert.assertTrue( grp2.exists() );
+    Assert.assertNotNull( grp2.type() );
+    Assert.assertNotNull( grp2.attribute("stem") );
+    Assert.assertTrue( grp2.attribute("stem").value().equals(stem2) );
+    Assert.assertNotNull( grp2.attribute("descriptor") );
+    Assert.assertTrue( grp2.attribute("descriptor").value().equals(desc2) );
     // We're done
     s.stop();
   }
 
   // Fetch a valid group
-  public void testFetchValidGroup() {
+  public void testFetchValidGroups() {
     Grouper         G     = new Grouper();
     GrouperSession  s     = new GrouperSession();
     Subject         subj  = GrouperSubject.lookup( Grouper.config("member.system"), "person" );
     s.start(subj);
-    // Fetch the group
-    GrouperGroup    grp   = GrouperGroup.load(s, stem, desc);
-    Assert.assertNotNull(grp);
-    String klass          = "edu.internet2.middleware.grouper.GrouperGroup";
-    Assert.assertTrue( klass.equals( grp.getClass().getName() ) );
-    Assert.assertTrue( grp.exists() );
-    Assert.assertNotNull( grp.type() );
-    Assert.assertNotNull( grp.attribute("stem") );
-    Assert.assertTrue( grp.attribute("stem").value().equals(stem) );
-    Assert.assertNotNull( grp.attribute("descriptor") );
-    Assert.assertTrue( grp.attribute("descriptor").value().equals(desc) );
-    // We're done
-    s.stop();
-  }
-
-  public void testFetchInvalidGroup() {
-    Grouper         G     = new Grouper();
-    GrouperSession  s     = new GrouperSession();
-    Subject         subj  = GrouperSubject.lookup( Grouper.config("member.system"), "person" );
-    s.start(subj);
-    // Fetch the group
-    GrouperGroup grp      = GrouperGroup.load(s, stem1, desc1);
-    // TODO This will fail (or rather, should be changed) if I start
-    //      returning null rather than a desolate object
-    Assert.assertFalse( grp.exists() );
+    // Fetch the groups
+    // Group 0
+    GrouperGroup    grp0  = GrouperGroup.load(s, stem0, desc0);
+    Assert.assertNotNull(grp0);
+    Assert.assertTrue( klass.equals( grp0.getClass().getName() ) );
+    Assert.assertTrue( grp0.exists() );
+    Assert.assertNotNull( grp0.type() );
+    Assert.assertNotNull( grp0.attribute("stem") );
+    Assert.assertTrue( grp0.attribute("stem").value().equals(stem0) );
+    Assert.assertNotNull( grp0.attribute("descriptor") );
+    Assert.assertTrue( grp0.attribute("descriptor").value().equals(desc0) );
+    // Group 1
+    GrouperGroup    grp1  = GrouperGroup.load(s, stem1, desc1);
+    Assert.assertNotNull(grp1);
+    Assert.assertTrue( klass.equals( grp1.getClass().getName() ) );
+    Assert.assertTrue( grp1.exists() );
+    Assert.assertNotNull( grp1.type() );
+    Assert.assertNotNull( grp1.attribute("stem") );
+    Assert.assertTrue( grp1.attribute("stem").value().equals(stem1) );
+    Assert.assertNotNull( grp1.attribute("descriptor") );
+    Assert.assertTrue( grp1.attribute("descriptor").value().equals(desc1) );
+    // Group 2
+    GrouperGroup    grp2  = GrouperGroup.load(s, stem2, desc2);
+    Assert.assertNotNull(grp2);
+    Assert.assertTrue( klass.equals( grp2.getClass().getName() ) );
+    Assert.assertTrue( grp2.exists() );
+    Assert.assertNotNull( grp2.type() );
+    Assert.assertNotNull( grp2.attribute("stem") );
+    Assert.assertTrue( grp2.attribute("stem").value().equals(stem2) );
+    Assert.assertNotNull( grp2.attribute("descriptor") );
+    Assert.assertTrue( grp1.attribute("descriptor").value().equals(desc1) );
     // We're done
     s.stop();
   }
