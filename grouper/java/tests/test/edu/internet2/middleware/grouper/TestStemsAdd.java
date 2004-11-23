0ca6cc970a503acb07fdb6776fa33ff4e2fb3cb8
Blair Christensen <blair@uchicago.edu>
2004-11-23 02:06:11 +0000

    forgot to check in tests
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@414 bc24b658-cf39-4249-b827-e3526eaaeb42

---
+++ grouper/java/tests/test/edu/internet2/middleware/grouper/TestStemsAdd.java
@@ -0,0 +1,214 @@
+/* 
+ * Copyright (C) 2004 Internet2
+ * Copyright (C) 2004 The University Of Chicago
+ * All Rights Reserved. 
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted only as authorized by the Academic
+ * Free License version 2.1.
+ *
+ * A copy of this license is available in the file LICENSE in the
+ * top-level directory of the distribution or, alternatively, at
+ * <http://www.opensource.org/licenses/afl-2.1.php>
+ */
+
+/*
+ * $Id: TestStemsAdd.java,v 1.1 2004-11-23 02:06:11 blair Exp $
+ */
+
+package test.edu.internet2.middleware.grouper;
+
+import  edu.internet2.middleware.grouper.*;
+import  edu.internet2.middleware.subject.*;
+import  junit.framework.*;
+
+
+public class TestNamespaces extends TestCase {
+
+  private String stem0  = null;
+  private String stem1  = null;
+  private String stem2  = null;
+  private String stem00 = "stem.0";
+  private String desc0  = "stem.0";
+  private String desc1  = "stem.1";
+  private String desc2  = "stem.2";
+  private String desc00 = "stem.0.0";
+  
+  private String klass  = "edu.internet2.middleware.grouper.GrouperGroup";
+  private String type   = "naming";
+
+
+  public TestNamespaces(String name) {
+    super(name);
+  }
+
+  protected void setUp () {
+    // Nothing -- Yet
+  }
+
+  protected void tearDown () {
+    // Nothing -- Yet
+  }
+
+
+  /*
+   * TESTS
+   */
+  
+
+  // Fetch a non-existent namespaces
+  public void testGroupsExistFalse() {
+    GrouperSession  s     = new GrouperSession();
+    Subject         subj  = GrouperSubject.lookup( Grouper.config("member.system"), "person" );
+    s.start(subj);
+    // Confirm that namespaces don't exist
+    GrouperGroup    ns0   = GrouperGroup.load(s, stem0, desc0, type);
+    Assert.assertNotNull(ns0);
+    Assert.assertFalse( ns0.exists() );
+    GrouperGroup    ns1   = GrouperGroup.load(s, stem1, desc1, type);
+    Assert.assertNotNull(ns1);
+    Assert.assertFalse( ns1.exists() );
+    GrouperGroup    ns2   = GrouperGroup.load(s, stem2, desc2, type);
+    Assert.assertNotNull(ns2);
+    Assert.assertFalse( ns2.exists() );
+    GrouperGroup    ns00  = GrouperGroup.load(s, stem00, desc00, type);
+    Assert.assertNotNull(ns00);
+    Assert.assertFalse( ns00.exists() );
+    // We're done
+    s.stop();
+  }
+
+  // Create namespaces
+  public void testCreateGroups() {
+    GrouperSession  s     = new GrouperSession();
+    Subject         subj  = GrouperSubject.lookup( Grouper.config("member.system"), "person" );
+    s.start(subj);
+    // Create the namespaces
+    // ns0
+    GrouperGroup ns0 = GrouperGroup.create(s, stem0, desc0, type);
+    Assert.assertNotNull(ns0);
+    Assert.assertTrue( klass.equals( ns0.getClass().getName() ) );
+    Assert.assertTrue( ns0.exists() );
+    Assert.assertNotNull( ns0.type() );
+    Assert.assertTrue( ns0.type().equals(type) ); 
+    // FIXME Shouldn't this be null?
+    Assert.assertNotNull( ns0.attribute("stem") );
+    // FIXME *shrug*
+    // Assert.assertTrue( ns0.attribute("stem").value().equals(stem0) );
+    Assert.assertNotNull( ns0.attribute("descriptor") );
+    Assert.assertTrue( ns0.attribute("descriptor").value().equals(desc0) );
+
+    // ns1
+    GrouperGroup ns1 = GrouperGroup.create(s, stem1, desc1, type);
+    Assert.assertNotNull(ns1);
+    Assert.assertTrue( klass.equals( ns1.getClass().getName() ) );
+    Assert.assertTrue( ns1.exists() );
+    Assert.assertNotNull( ns1.type() );
+    Assert.assertTrue( ns1.type().equals(type) ); 
+    // FIXME Shouldn't this be null?
+    Assert.assertNotNull( ns1.attribute("stem") );
+    // FIXME *shrug*
+    // Assert.assertTrue( ns1.attribute("stem").value().equals(stem1) );
+    Assert.assertNotNull( ns1.attribute("descriptor") );
+    Assert.assertTrue( ns1.attribute("descriptor").value().equals(desc1) );
+
+    // ns2
+    GrouperGroup ns2 = GrouperGroup.create(s, stem2, desc2, type);
+    Assert.assertNotNull(ns2);
+    Assert.assertTrue( klass.equals( ns2.getClass().getName() ) );
+    Assert.assertTrue( ns2.exists() );
+    Assert.assertNotNull( ns2.type() );
+    Assert.assertTrue( ns2.type().equals(type) ); 
+    // FIXME Shouldn't this be null?
+    Assert.assertNotNull( ns2.attribute("stem") );
+    // FIXME *shrug*
+    // Assert.assertTrue( ns2.attribute("stem").value().equals(stem2) );
+    Assert.assertNotNull( ns2.attribute("descriptor") );
+    Assert.assertTrue( ns2.attribute("descriptor").value().equals(desc2) );
+
+    // ns00
+    GrouperGroup ns00 = GrouperGroup.create(s, stem00, desc00, type);
+    Assert.assertNotNull(ns00);
+    Assert.assertTrue( klass.equals( ns00.getClass().getName() ) );
+    Assert.assertTrue( ns00.exists() );
+    Assert.assertNotNull( ns00.type() );
+    Assert.assertTrue( ns00.type().equals(type) ); 
+    Assert.assertNotNull( ns00.attribute("stem") );
+    Assert.assertTrue( ns00.attribute("stem").value().equals(stem00) );
+    Assert.assertNotNull( ns00.attribute("descriptor") );
+    Assert.assertTrue( ns00.attribute("descriptor").value().equals(desc00) );
+
+    // We're done
+    s.stop();
+  }
+
+  // Fetch valid namespaces
+  public void testFetchValidNamespaces() {
+    GrouperSession  s     = new GrouperSession();
+    Subject         subj  = GrouperSubject.lookup( Grouper.config("member.system"), "person" );
+    s.start(subj);
+    // Fetch the namespaces
+    // ns0
+    GrouperGroup ns0 = GrouperGroup.load(s, stem0, desc0);
+    Assert.assertNotNull(ns0);
+    Assert.assertTrue( klass.equals( ns0.getClass().getName() ) );
+    // FIXME Assert.assertTrue( ns0.exists() );
+    // FIXME Assert.assertNotNull( ns0.type() );
+    // FIXME Assert.assertTrue( ns0.type().equals(type) ); 
+    // FIXME Shouldn't this be null?
+    // Assert.assertNotNull( ns0.attribute("stem") );
+    // FIXME *shrug*
+    // Assert.assertTrue( ns0.attribute("stem").value().equals(stem0) );
+    // FIXME Assert.assertNotNull( ns0.attribute("descriptor") );
+    // FIXME Assert.assertTrue( ns0.attribute("descriptor").value().equals(desc0) );
+
+    // ns1
+    GrouperGroup ns1 = GrouperGroup.load(s, stem1, desc1);
+    Assert.assertNotNull(ns1);
+    Assert.assertTrue( klass.equals( ns1.getClass().getName() ) );
+    // FIXME Assert.assertTrue( ns1.exists() );
+    // FIXME Assert.assertNotNull( ns1.type() );
+    // FIXME Assert.assertTrue( ns1.type().equals(type) ); 
+    // FIXME Shouldn't this be null?
+    // Assert.assertNotNull( ns1.attribute("stem") );
+    // FIXME *shrug*
+    // Assert.assertTrue( ns1.attribute("stem").value().equals(stem1) );
+    // FIXME Assert.assertNotNull( ns1.attribute("descriptor") );
+    // FIXME Assert.assertTrue( ns1.attribute("descriptor").value().equals(desc1) );
+
+    // ns2
+    GrouperGroup ns2 = GrouperGroup.load(s, stem2, desc2);
+    Assert.assertNotNull(ns2);
+    Assert.assertTrue( klass.equals( ns2.getClass().getName() ) );
+    // FIXME Assert.assertTrue( ns2.exists() );
+    // FIXME Assert.assertNotNull( ns2.type() );
+    // FIXME Assert.assertTrue( ns2.type().equals(type) ); 
+    // FIXME Shouldn't this be null?
+    // Assert.assertNotNull( ns2.attribute("stem") );
+    // FIXME *shrug*
+    // Assert.assertTrue( ns2.attribute("stem").value().equals(stem2) );
+    // FIXME Assert.assertNotNull( ns2.attribute("descriptor") );
+    // FIXME Assert.assertTrue( ns2.attribute("descriptor").value().equals(desc2) );
+
+    // ns00
+    GrouperGroup ns00 = GrouperGroup.load(s, stem00, desc00);
+    Assert.assertNotNull(ns00);
+    Assert.assertTrue( klass.equals( ns00.getClass().getName() ) );
+    // FIXME Assert.assertTrue( ns00.exists() );
+    // FIXME Assert.assertNotNull( ns00.type() );
+    // FIXME Assert.assertTrue( ns00.type().equals(type) ); 
+    // FIXME Assert.assertNotNull( ns00.attribute("stem") );
+    // FIXME *shrug*
+    // Assert.assertTrue( ns00.attribute("stem").value().equals(stem00) );
+    // FIXME Assert.assertNotNull( ns00.attribute("descriptor") );
+    // FIXME Assert.assertTrue( ns00.attribute("descriptor").value().equals(desc00) );
+
+    // We're done
+    s.stop();
+  }
+
+  // TODO Assert ADMIN priv (create + fetch)
+  // TODO Delete group
+
+}
+
