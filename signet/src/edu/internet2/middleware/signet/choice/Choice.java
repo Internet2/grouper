21c4703c0f6c5336fb6426196ef632d125a4eb69
acohen <acohen@mycompany.com>
2005-01-21 23:48:22 +0000

    Updated Javadoc comments and re-generated Javadoc.
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@719 bc24b658-cf39-4249-b827-e3526eaaeb42

--- signet/src/edu/internet2/middleware/signet/choice/Choice.java
+++ signet/src/edu/internet2/middleware/signet/choice/Choice.java
@@ -1,6 +1,6 @@
 /*--
-$Id: Choice.java,v 1.2 2005-01-21 20:30:47 acohen Exp $
-$Date: 2005-01-21 20:30:47 $
+$Id: Choice.java,v 1.3 2005-01-21 23:48:22 acohen Exp $
+$Date: 2005-01-21 23:48:22 $
 
 Copyright 2004 Internet2 and Stanford University.  All Rights Reserved.
 Licensed under the Signet License, Version 1,
@@ -11,29 +11,29 @@ package edu.internet2.middleware.signet.choice;
 
 public interface Choice
 {
-	/*
+	/**
 	 * Returns the enclosing ChoiceSet of this Choice.
 	 */
 	public ChoiceSet getChoiceSet() throws ChoiceSetNotFoundException;
 	
-	/*
+	/**
 	 * Returns the value associated with this Choice.
 	 */
 	public String getValue();
 	
-	/*
+	/**
 	 * Returns a user-friendly representation of the value associated
 	 * with this Choice.
 	 */
 	public String getDisplayValue();
 	
-	/*
+	/**
 	 * Returns an int that is the relative display order of this
 	 * Choice among other Choices within a ChoiceSet.
 	 */
 	public int getDisplayOrder();
 
-	/*
+	/**
 	 * Returns an int that is used for comparing this Choice 
 	 * among other Choices within a ChoiceSet.
 	 */
