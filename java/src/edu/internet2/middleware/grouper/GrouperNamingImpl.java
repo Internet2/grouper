af23a62ede002b97b54b668dff8df53742d24559
Blair Christensen <blair@uchicago.edu>
2005-02-07 16:33:23 +0000

    remove some redundant validation checks
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk/grouper@754 bc24b658-cf39-4249-b827-e3526eaaeb42

--- java/src/edu/internet2/middleware/grouper/GrouperNamingImpl.java
+++ java/src/edu/internet2/middleware/grouper/GrouperNamingImpl.java
@@ -60,7 +60,7 @@ import  java.util.*;
  * <p />
  *
  * @author  blair christensen.
- * @version $Id: GrouperNamingImpl.java,v 1.38 2005-02-07 14:46:26 blair Exp $
+ * @version $Id: GrouperNamingImpl.java,v 1.39 2005-02-07 16:33:23 blair Exp $
  */
 public class GrouperNamingImpl implements GrouperNaming {
 
@@ -288,22 +288,13 @@ public class GrouperNamingImpl implements GrouperNaming {
   public boolean revoke(GrouperSession s, GrouperGroup g, String priv) {
     GrouperNamingImpl._init();
     boolean rv = false;
-    if (this.can(priv) == true) {
-      if (GrouperBackend.sessionValid(s)) {
-        /*
-         * FIXME I should be doing a GroupField lookup on `priv'
-         */
-        if (this.has(s, g, Grouper.PRIV_STEM)) {
-          Iterator iter = this.whoHas(s, g, priv).iterator();
-          while (iter.hasNext()) {
-            GrouperMember m = (GrouperMember) iter.next();
-            // TODO What if this fails for one or more members?
-            this.revoke(s, g, m, priv);
-          }
-          rv = true; // FIXME
-        }
-      }
+    Iterator iter = this.whoHas(s, g, priv).iterator();
+    while (iter.hasNext()) {
+      GrouperMember m = (GrouperMember) iter.next();
+      // TODO What if this fails for one or more members?
+      this.revoke(s, g, m, priv);
     }
+    rv = true; // FIXME
     // TODO Should this return a list of deleted members?
     // TODO I should probably throw an exception if invalid priv
     return rv;
