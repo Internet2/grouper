00d13fdbfabb4e4a43b36c849b06577c00a4d99c
Blair Christensen <blair@uchicago.edu>
2005-02-16 20:10:18 +0000

    Initial effort at creating HSQLDB & Oracle specific schema files
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@785 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper/sql/schema-oracle.sql
+++ grouper/sql/schema-oracle.sql
@@ -1,10 +1,10 @@
 
 -- 
 -- Grouper Database Schema
--- $Id: schema-hsqldb.sql,v 1.29 2005-02-16 20:10:18 blair Exp $
+-- $Id: schema-oracle.sql,v 1.1 2005-02-16 20:10:18 blair Exp $
 -- 
 
-DROP TABLE grouper_attribute IF EXISTS;
+DROP TABLE grouper_attribute;
 CREATE TABLE grouper_attribute (
   groupKey        VARCHAR(16) NOT NULL,
   groupField      VARCHAR(32) NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE grouper_attribute (
   CONSTRAINT      uniq_ga_gk_gf UNIQUE (groupKey, groupField)
 );
 
-DROP TABLE grouper_field IF EXISTS;
+DROP TABLE grouper_field;
 CREATE TABLE grouper_field (
   groupField  VARCHAR(32) NOT NULL PRIMARY KEY,
   readPriv    VARCHAR(32),
@@ -20,7 +20,7 @@ CREATE TABLE grouper_field (
   isList      VARCHAR(8)
 );
 
-DROP TABLE grouper_group IF EXISTS;
+DROP TABLE grouper_group;
 CREATE TABLE grouper_group (
   groupKey      VARCHAR(16) NOT NULL PRIMARY KEY,
   groupID       VARCHAR(16) NOT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE grouper_group (
   CONSTRAINT    uniq_gg_gid  UNIQUE (groupID)
 );
 
-DROP TABLE grouper_list IF EXISTS;
+DROP TABLE grouper_list;
 CREATE  TABLE grouper_list (
   groupKey      VARCHAR(16) NOT NULL,
   groupField    VARCHAR(32) NOT NULL,
@@ -46,7 +46,7 @@ CREATE  TABLE grouper_list (
 CREATE UNIQUE INDEX idx_gl_gk_gf_mk_pk ON grouper_list 
   (groupKey, groupField, memberKey, pathKey);
 
-DROP TABLE grouper_member IF EXISTS;
+DROP TABLE grouper_member;
 CREATE TABLE grouper_member (
   memberKey     VARCHAR(16) NOT NULL PRIMARY KEY,
   memberID      VARCHAR(16) NOT NULL,
@@ -56,28 +56,28 @@ CREATE TABLE grouper_member (
 CREATE UNIQUE INDEX idx_gm_sid_stid ON grouper_member
   (subjectID, subjectTypeID);
 
-DROP TABLE grouper_schema IF EXISTS;
+DROP TABLE grouper_schema;
 CREATE TABLE grouper_schema (
   groupKey    VARCHAR(16) NOT NULL,
   groupType   VARCHAR(32) NOT NULL,
   CONSTRAINT  uniq_gsch_gk_gt  UNIQUE (groupKey, groupType)
 );
 
-DROP TABLE grouper_session IF EXISTS;
+DROP TABLE grouper_session;
 CREATE TABLE grouper_session (
   sessionID  VARCHAR(16) NOT NULL PRIMARY KEY,
   memberID   VARCHAR(16) NOT NULL,
   startTime  VARCHAR(16)
 );
 
-DROP TABLE grouper_subject IF EXISTS;
+DROP TABLE grouper_subject;
 CREATE TABLE grouper_subject (
   subjectID     VARCHAR(64) NOT NULL PRIMARY KEY,
   subjectTypeID VARCHAR(32) NOT NULL,
   CONSTRAINT    uniq_gsub_sid_stid UNIQUE (subjectID, subjectTypeID)
 );
 
-DROP TABLE grouper_subjectAttribute IF EXISTS;
+DROP TABLE grouper_subjectAttribute;
 CREATE TABLE grouper_subjectAttribute (
   subjectID     VARCHAR(64) NOT NULL, 
   subjectTypeID VARCHAR(32) NOT NULL,
@@ -88,26 +88,26 @@ CREATE TABLE grouper_subjectAttribute (
   CONSTRAINT    uniq_gsubattr_sid_stid UNIQUE (subjectID, subjectTypeID)
 );
 
-DROP TABLE grouper_subjectType IF EXISTS;
+DROP TABLE grouper_subjectType;
 CREATE TABLE grouper_subjectType (
   subjectTypeID VARCHAR(32) NOT NULL PRIMARY KEY,
   name          VARCHAR(32) NOT NULL,
   adapterClass  VARCHAR(128) NOT NULL
 );
 
-DROP TABLE grouper_typeDef IF EXISTS;
+DROP TABLE grouper_typeDef;
 CREATE TABLE grouper_typeDef (
   groupType   VARCHAR(32) NOT NULL,
   groupField  VARCHAR(32) NOT NULL,
   CONSTRAINT  uniq_gtypdef_gt_gf  UNIQUE (groupType, groupField)
 );
 
-DROP TABLE grouper_type IF EXISTS;
+DROP TABLE grouper_type;
 CREATE TABLE grouper_type (
   groupType   VARCHAR(32) NOT NULL PRIMARY KEY
 );
 
-DROP TABLE grouper_viaElement IF EXISTS;
+DROP TABLE grouper_viaElement;
 CREATE  TABLE grouper_viaElement (
   pathKey       VARCHAR(16) NOT NULL,
   pathIdx       INTEGER NOT NULL,
@@ -118,7 +118,7 @@ CREATE  TABLE grouper_viaElement (
 CREATE  INDEX gve_pk ON grouper_viaElement (pathKey);
 CREATE  INDEX gve_gk ON grouper_viaElement (groupKey);
 
-DROP TABLE grouper_viaPath IF EXISTS;
+DROP TABLE grouper_viaPath;
 CREATE  TABLE grouper_viaPath (
   pathKey       VARCHAR(16) NOT NULL PRIMARY KEY
 );
