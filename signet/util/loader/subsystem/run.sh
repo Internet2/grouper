959e88323fa69a127756a4ec79d2d88bee459b81
mnguyen <mnguyen@mycompany.com>
2005-03-08 03:26:14 +0000

    Separate build.xml for web app and utilities.
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@863 bc24b658-cf39-4249-b827-e3526eaaeb42

---
+++ signet/util/loader/subsystem/run.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# -----------------------------------------------------------------------------
+# Script for command line util to load Subystem XML
+#
+# Environment Variable Prequisites
+#
+#   JAVA_HOME     Must point to your JDK.
+# -----------------------------------------------------------------------------
+
+JAVA_HOME=/usr/java
+
+BASEDIR=../
+CLASSPATH=$BASEDIR/classes
+
+for file in `ls $BASEDIR/lib/*.jar`;
+do
+if [ "$CLASSPATH" != "" ]; then
+   CLASSPATH=${CLASSPATH}:$file
+else
+   CLASSPATH=$file
+fi
+done
+
+echo "Using classpath: "$CLASSPATH
+
+$JAVA_HOME/bin/java -cp $CLASSPATH edu.internet2.signet.XMLLoad $1
