24d26038067195f02b2ce124215c88805f1c412b
Blair Christensen <blair@uchicago.edu>
2005-04-15 15:53:04 +0000

    * Rework schema generator * Make default Oracle schema a little less antagonistic to clean slates
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk/grouper@1124 bc24b658-cf39-4249-b827-e3526eaaeb42

--- sql/schema-oracle.sql
+++ sql/schema-oracle.sql
@@ -3,7 +3,7 @@
 -- Grouper Database Schema
 -- 
 
-DROP TABLE grouper_attribute;
+
 CREATE TABLE grouper_attribute (
   groupKey        VARCHAR2(64) NOT NULL,
   groupField      VARCHAR2(64) NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE grouper_attribute (
   CONSTRAINT      uniq_ga_gk_gf UNIQUE (groupKey, groupField)
 );
 
-DROP TABLE grouper_field;
+
 CREATE TABLE grouper_field (
   groupField  VARCHAR2(64) NOT NULL PRIMARY KEY,
   readPriv    VARCHAR2(64),
@@ -19,7 +19,7 @@ CREATE TABLE grouper_field (
   isList      VARCHAR2(8)
 );
 
-DROP TABLE grouper_group;
+
 CREATE TABLE grouper_group (
   groupKey      VARCHAR2(64) NOT NULL PRIMARY KEY,
   groupID       VARCHAR2(64) NOT NULL,
@@ -34,8 +34,8 @@ CREATE TABLE grouper_group (
   CONSTRAINT    uniq_gg_gid  UNIQUE (groupID)
 );
 
-DROP TABLE grouper_list;
-CREATE  TABLE grouper_list (
+
+CREATE TABLE grouper_list (
   listKey       VARCHAR2(64) NOT NULL PRIMARY KEY,
   groupKey      VARCHAR2(64) NOT NULL,
   groupField    VARCHAR2(64) NOT NULL,
@@ -46,7 +46,7 @@ CREATE  TABLE grouper_list (
 CREATE UNIQUE INDEX idx_gl_gk_gf_mk_ck ON grouper_list 
   (groupKey, groupField, memberKey, chainKey);
 
-DROP TABLE grouper_member;
+
 CREATE TABLE grouper_member (
   memberKey     VARCHAR2(64) NOT NULL PRIMARY KEY,
   memberID      VARCHAR2(64) NOT NULL,
@@ -56,8 +56,8 @@ CREATE TABLE grouper_member (
 CREATE UNIQUE INDEX idx_gm_sid_stid ON grouper_member
   (subjectID, subjectTypeID);
 
-DROP TABLE grouper_memberVia;
-CREATE  TABLE grouper_memberVia (
+
+CREATE TABLE grouper_memberVia (
   chainKey    VARCHAR2(64) NOT NULL,
   chainIdx    INTEGER NOT NULL,
   listKey     VARCHAR2(64) NOT NULL,
@@ -68,28 +68,28 @@ CREATE  TABLE grouper_memberVia (
 CREATE  INDEX idx_mv_ck ON grouper_memberVia (chainKey);
 CREATE  INDEX idx_mv_lk ON grouper_memberVia (listKey);
 
-DROP TABLE grouper_schema;
+
 CREATE TABLE grouper_schema (
   groupKey    VARCHAR2(64) NOT NULL,
   groupType   VARCHAR2(64) NOT NULL,
   CONSTRAINT  uniq_gsch_gk_gt  UNIQUE (groupKey, groupType)
 );
 
-DROP TABLE grouper_session;
+
 CREATE TABLE grouper_session (
   sessionID  VARCHAR2(64) NOT NULL PRIMARY KEY,
   memberID   VARCHAR2(64) NOT NULL,
   startTime  VARCHAR2(16)
 );
 
-DROP TABLE grouper_subject;
+
 CREATE TABLE grouper_subject (
   subjectID     VARCHAR2(64) NOT NULL PRIMARY KEY,
   subjectTypeID VARCHAR2(64) NOT NULL,
   CONSTRAINT    uniq_gsub_sid_stid UNIQUE (subjectID, subjectTypeID)
 );
 
-DROP TABLE grouper_subjectAttribute;
+
 CREATE TABLE grouper_subjectAttribute (
   subjectID     VARCHAR2(64) NOT NULL, 
   subjectTypeID VARCHAR2(64) NOT NULL,
@@ -100,21 +100,21 @@ CREATE TABLE grouper_subjectAttribute (
   CONSTRAINT    uniq_gsubattr_sid_stid UNIQUE (subjectID, subjectTypeID)
 );
 
-DROP TABLE grouper_subjectType;
+
 CREATE TABLE grouper_subjectType (
   subjectTypeID VARCHAR2(64) NOT NULL PRIMARY KEY,
   name          VARCHAR2(64) NOT NULL,
   adapterClass  VARCHAR2(128) NOT NULL
 );
 
-DROP TABLE grouper_typeDef;
+
 CREATE TABLE grouper_typeDef (
   groupType   VARCHAR2(64) NOT NULL,
   groupField  VARCHAR2(64) NOT NULL,
   CONSTRAINT  uniq_gtypdef_gt_gf  UNIQUE (groupType, groupField)
 );
 
-DROP TABLE grouper_type;
+
 CREATE TABLE grouper_type (
   groupType   VARCHAR2(64) NOT NULL PRIMARY KEY
 );
--- sql/test-data-hsqldb.sql
+++ sql/test-data-hsqldb.sql
@@ -3,11 +3,13 @@
 -- Grouper Test Data
 -- 
 
+DELETE FROM grouper_subject WHERE subjectID='member 0';
 INSERT INTO grouper_subject (subjectID, subjectTypeID)
   VALUES ('member 0', 'person');
+DELETE FROM grouper_subject WHERE subjectID='member 1';
 INSERT INTO grouper_subject (subjectID, subjectTypeID)
   VALUES ('member 1', 'person');
 
 COMMIT;
-SHUTDOWN COMPACT; 
+
 
