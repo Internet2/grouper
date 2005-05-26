f8e21cf8b247374b3aa7e9b56311154fa4e35e16
Blair Christensen <blair@uchicago.edu>
2005-05-26 14:02:57 +0000

    Add the minimal _GrouperSubject_ stub.  Now I just need to make it work.
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk/grouper@1218 bc24b658-cf39-4249-b827-e3526eaaeb42

---
+++ java/src/edu/internet2/middleware/grouper/GrouperSubject.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (C) 2004-2005 University Corporation for Advanced Internet Development, Inc.
+ * Copyright (C) 2004-2005 The University Of Chicago
+ * All Rights Reserved. 
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  * Neither the name of the University of Chicago nor the names
+ *    of its contributors nor the University Corporation for Advanced
+ *   Internet Development, Inc. may be used to endorse or promote
+ *   products derived from this software without explicit prior
+ *   written permission.
+ *
+ * You are under no obligation whatsoever to provide any enhancements
+ * to the University of Chicago, its contributors, or the University
+ * Corporation for Advanced Internet Development, Inc.  If you choose
+ * to provide your enhancements, or if you choose to otherwise publish
+ * or distribute your enhancements, in source code form without
+ * contemporaneously requiring end users to enter into a separate
+ * written license agreement for such enhancements, then you thereby
+ * grant the University of Chicago, its contributors, and the University
+ * Corporation for Advanced Internet Development, Inc. a non-exclusive,
+ * royalty-free, perpetual license to install, use, modify, prepare
+ * derivative works, incorporate into the software or other computer
+ * software, distribute, and sublicense your enhancements or derivative
+ * works thereof, in binary and source code form.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND WITH ALL FAULTS.  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT ARE DISCLAIMED AND the
+ * entire risk of satisfactory quality, performance, accuracy, and effort
+ * is with LICENSEE. IN NO EVENT SHALL THE COPYRIGHT OWNER, CONTRIBUTORS,
+ * OR THE UNIVERSITY CORPORATION FOR ADVANCED INTERNET DEVELOPMENT, INC.
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OR DISTRIBUTION OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package edu.internet2.middleware.grouper;
+
+
+import  edu.internet2.middleware.subject.*;
+import  edu.internet2.middleware.subject.provider.*;
+import  java.util.*;
+
+
+/** 
+ * TODO
+ * <p />
+ *
+ * @author  blair christensen.
+ * @version $Id: GrouperSubject.java,v 1.38 2005-05-26 14:02:57 blair Exp $
+ */
+public class GrouperSubject implements Subject {
+
+  /*
+   * PRIVATE INSTANCE METHODS
+   */
+  private SubjectType type = null;
+
+
+  /**
+   * {@inheritDoc}
+   */
+  public Map getAttributes() {
+    // TODO
+    return new HashMap();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public String getAttributeValue(String name) {
+    // TODO
+    return new String();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public Set getAttributeValues(String name) {
+    // TODO
+    return new HashSet();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public String getDescription() {
+    // TODO
+    return new String();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public String getId() {
+    // TODO
+    return new String();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public String getName() {
+    // TODO
+    return new String();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public SubjectType getType() {
+    return this.type;
+  }
+
+}
+
