4fb78a8aa9a714d9b184b2cd9c758e072ff94bd7
Blair Christensen <blair@uchicago.edu>
2005-10-20 02:32:28 +0000

    start fleshing out Stem
    git-svn-id: https://svn.internet2.edu/svn/i2mi/branches/GROUPER_RELENG_0_9@1628 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper/src/grouper/edu/internet2/middleware/grouper/Stem.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/Stem.java
@@ -18,322 +18,370 @@
 package edu.internet2.middleware.grouper;
 
 import java.io.Serializable;
-import java.util.Date;
-import java.util.Set;
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
-import org.apache.commons.lang.builder.ToStringBuilder;
+import java.util.*;
+import org.apache.commons.lang.builder.*;
 
 
 /** 
  * A namespace within the Groups Registry.
- * @author blair christensen.
+ * <p />
+ * @author  blair christensen.
+ * @version $Id: Stem.java,v 1.1.2.6 2005-10-20 02:32:28 blair Exp $
  *     
 */
 public class Stem implements Serializable {
 
-    /** identifier field */
-    private String id;
+  // Hibernate Properties
+  private String  id;
+  private Set     child_groups;
+  private Set     child_stems;
+  private String  create_source;
+  private Date    create_time;
+  private Member  creator_id;
+  private String  description;
+  private String  display_extension;
+  private String  display_name;
+  private String  extension;
+  private Member  modifier_id;
+  private String  modify_source;
+  private Date    modify_time;
+  private String  name;
+  private Stem    parent_stem;
+  private String  uuid;
+  private Integer version;
 
-    /** nullable persistent field */
-    private String create_source;
+  // Constructors
 
-    /** persistent field */
-    private Date create_time;
+  /**
+   * Default constructor for Hibernate.
+   */
+  public Stem() {
+    // Nothing
+  }
 
-    /** nullable persistent field */
-    private String description;
+  // Public Instance Methods
 
-    /** nullable persistent field */
-    private String display_extension;
+  /**
+   * Add a new group to the registry.
+   * <pre class="eg">
+   * // Add a group with the extension "edu" beneath this stem.
+   * try {
+   *   Group edu = ns.addChildGroup("edu", "edu domain");
+   * }
+   * catch (GroupAddException e) {
+   *   // Group not added
+   * }
+   * </pre>
+   * @param   extension         Group's extension
+   * @param   displayExtension  Groups' displayExtension
+   * @return  The added {@link Group}
+   * @throws  GroupAddException 
+   */
+  public Group addChildGroup(String extension, String displayExtension) 
+    throws GroupAddException 
+  {
+    throw new RuntimeException("Not implemented");
+  }
 
-    /** nullable persistent field */
-    private String display_name;
+  /**
+   * Add a new stem to the registry.
+   * <pre class="eg">
+   * // Add a stem with the extension "edu" beneath this stem.
+   * try {
+   *   Stem edu = ns.addChildStem("edu", "edu domain");
+   * }
+   * catch (StemAddException e) {
+   *   // Stem not added
+   * }
+   * </pre>
+   * @param   extension         Stem's extension
+   * @param   displayExtension  Stem' displayExtension
+   * @return  The added {@link Stem}
+   * @throws  StemAddException 
+   */
+  public Stem addChildStem(String extension, String displayExtension) 
+    throws StemAddException 
+  {
+    throw new RuntimeException("Not implemented");
+  }
 
-    /** nullable persistent field */
-    private String extension;
+  /**
+   * Get child groups of this stem.
+   * <pre class="eg">
+   * // Get child groups 
+   * Set childGroups = ns.getChildGroups();
+   * </pre>
+   * @return  Set of {@link Group} objects
+   */
+  public Set getChildGroups() {
+    throw new RuntimeException("Not implemented");
+  }
 
-    /** nullable persistent field */
-    private String modify_source;
+  /**
+   * Get child stems of this stem.
+   * <pre class="eg">
+   * // Get child stems 
+   * Set childStems = ns.getChildStems();
+   * </pre>
+   * @return  Set of {@link Stem} objects
+   */
+  public Set getChildStems() {
+    throw new RuntimeException("Not implemented");
+  }
 
-    /** nullable persistent field */
-    private Date modify_time;
+  /**
+   * Get (optional and questionable) create source for this stem.
+   * <pre class="eg">
+   * // Get create source
+`  * String source = ns.getCreateSource();
+   * </pre>
+   * @return  Create source for this stem.
+   */
+  public String getCreateSource() {
+    throw new RuntimeException("Not implemented");
+  }
+  
+  /**
+   * Get subject that created this stem.
+   * <pre class="eg">
+   * // Get creator of this stem.
+   * try {
+   *   Subject creator = ns.getCreateSubject();
+   * }
+   * catch (SubjectNotFoundException e) {
+   *   // Couldn't find subject
+   * }
+   * </pre>
+   * @return  {@link Subject} that created this stem.
+   * @throws  SubjectNotFoundException
+   */
+  public Subject getCreateSubject() 
+    throws SubjectNotFoundException
+  {
+    throw new RuntimeException("Not implemented");
+  }
+  
+  /**
+   * Get creation time for this stem.
+   * <pre class="eg">
+   * // Get create time.
+   * Date created = ns.getCreateTime();
+   * </pre>
+   * @return  {@link Date} that this stem was created.
+   */
+  public Date getCreateTime() {
+    throw new RuntimeException("Not implemented");
+  }
+  
+  /**
+   * Get (optional and questionable) modify source for this stem.
+   * <pre class="eg">
+   * // Get modify source
+`  * String source = ns.getModifySource();
+   * </pre>
+   * @return  Modify source for this stem.
+   */
+  public String getModifySource() {
+    throw new RuntimeException("Not implemented");
+  }
+  
+  /**
+   * Get subject that last modified this stem.
+   * <pre class="eg">
+   * // Get last modifier of this stem.
+   * try {
+   *   Subject modifier = ns.getModifySubject();
+   * }
+   * catch (SubjectNotFoundException e) {
+   *   // Couldn't find subject
+   * }
+   * </pre>
+   * @return  {@link Subject} that last modified this stem.
+   * @throws  SubjectNotFoundException
+   */
+  public Subject getModifySubject() 
+    throws SubjectNotFoundException
+  {
+    throw new RuntimeException("Not implemented");
+  }
+  
+  /**
+   * Get last modified time for this stem.
+   * <pre class="eg">
+   * // Get last modified time.
+   * Date modified = ns.getModifyTime();
+   * </pre>
+   * @return  {@link Date} that this stem was last modified.
+   */
+  public Date getModifyTime() {
+    throw new RuntimeException("Not implemented");
+  }
+  
+  public String toString() {
+    return new ToStringBuilder(this)
+           .append("display_name", getDisplay_name())
+           .append("name", getName())
+           .append("uuid", getUuid())
+           .append("creator_id", getCreator_id())
+           .append("modifier_id", getModifier_id())
+           .toString();
+  }
 
-    /** nullable persistent field */
-    private String name;
+  public boolean equals(Object other) {
+    if ( (this == other ) ) return true;
+    if ( !(other instanceof Stem) ) return false;
+    Stem castOther = (Stem) other;
+    return new EqualsBuilder()
+           .append(this.getUuid(), castOther.getUuid())
+           .append(this.getCreator_id(), castOther.getCreator_id())
+           .append(this.getModifier_id(), castOther.getModifier_id())
+           .isEquals();
+  }
 
-    /** persistent field */
-    private String uuid;
+  public int hashCode() {
+    return new HashCodeBuilder()
+           .append(getUuid())
+           .append(getCreator_id())
+           .append(getModifier_id())
+           .toHashCode();
+  }
 
-    /** nullable persistent field */
-    private Integer version;
+  // Hibernate Accessors
+  private String getId() {
+    return this.id;
+  }
 
-    /** nullable persistent field */
-    private edu.internet2.middleware.grouper.Member creator_id;
+  private void setId(String id) {
+    this.id = id;
+  }
 
-    /** nullable persistent field */
-    private edu.internet2.middleware.grouper.Member modifier_id;
+  private String getCreate_source() {
+    return this.create_source;
+  }
 
-    /** nullable persistent field */
-    private edu.internet2.middleware.grouper.Stem parent_stem;
+  private void setCreate_source(String create_source) {
+    this.create_source = create_source;
+  }
 
-    /** persistent field */
-    private Set child_groups;
+  private Date getCreate_time() {
+    return this.create_time;
+  }
 
-    /** persistent field */
-    private Set child_stems;
+  private void setCreate_time(Date create_time) {
+    this.create_time = create_time;
+  }
 
-    /** full constructor */
-    public Stem(String create_source, Date create_time, String description, String display_extension, String display_name, String extension, String modify_source, Date modify_time, String name, String uuid, Integer version, edu.internet2.middleware.grouper.Member creator_id, edu.internet2.middleware.grouper.Member modifier_id, edu.internet2.middleware.grouper.Stem parent_stem, Set child_groups, Set child_stems) {
-        this.create_source = create_source;
-        this.create_time = create_time;
-        this.description = description;
-        this.display_extension = display_extension;
-        this.display_name = display_name;
-        this.extension = extension;
-        this.modify_source = modify_source;
-        this.modify_time = modify_time;
-        this.name = name;
-        this.uuid = uuid;
-        this.version = version;
-        this.creator_id = creator_id;
-        this.modifier_id = modifier_id;
-        this.parent_stem = parent_stem;
-        this.child_groups = child_groups;
-        this.child_stems = child_stems;
-    }
+  private String getDescription() {
+    return this.description;
+  }
 
-    /** default constructor */
-    public Stem() {
-    }
+  private void setDescription(String description) {
+    this.description = description;
+  }
 
-    /** minimal constructor */
-    public Stem(Date create_time, String uuid, Set child_groups, Set child_stems) {
-        this.create_time = create_time;
-        this.uuid = uuid;
-        this.child_groups = child_groups;
-        this.child_stems = child_stems;
-    }
+  private String getDisplay_extension() {
+    return this.display_extension;
+  }
 
-    private String getId() {
-        return this.id;
-    }
+  private void setDisplay_extension(String display_extension) {
+    this.display_extension = display_extension;
+  }
 
-    private void setId(String id) {
-        this.id = id;
-    }
+  private String getDisplay_name() {
+    return this.display_name;
+  }
 
-    /** 
-     * Get create source.
-     *       
-     */
-    private String getCreate_source() {
-        return this.create_source;
-    }
+  private void setDisplay_name(String display_name) {
+    this.display_name = display_name;
+  }
 
-    private void setCreate_source(String create_source) {
-        this.create_source = create_source;
-    }
+  private String getExtension() {
+    return this.extension;
+  }
 
-    /** 
-     * Get create time.
-     *       
-     */
-    private Date getCreate_time() {
-        return this.create_time;
-    }
+  private void setExtension(String extension) {
+    this.extension = extension;
+  }
 
-    private void setCreate_time(Date create_time) {
-        this.create_time = create_time;
-    }
+  private String getModify_source() {
+    return this.modify_source;
+  }
 
-    /** 
-     * Get description.
-     *       
-     */
-    private String getDescription() {
-        return this.description;
-    }
+  private void setModify_source(String modify_source) {
+    this.modify_source = modify_source;
+  }
 
-    private void setDescription(String description) {
-        this.description = description;
-    }
+  private Date getModify_time() {
+    return this.modify_time;
+  }
 
-    /** 
-     * Get displayExtension.
-     *       
-     */
-    private String getDisplay_extension() {
-        return this.display_extension;
-    }
+  private void setModify_time(Date modify_time) {
+    this.modify_time = modify_time;
+  }
 
-    private void setDisplay_extension(String display_extension) {
-        this.display_extension = display_extension;
-    }
+  private String getName() {
+    return this.name;
+  }
 
-    /** 
-     * Get displayName.
-     *       
-     */
-    private String getDisplay_name() {
-        return this.display_name;
-    }
+  private void setName(String name) {
+    this.name = name;
+  }
 
-    private void setDisplay_name(String display_name) {
-        this.display_name = display_name;
-    }
+  private String getUuid() {
+    return this.uuid;
+  }
 
-    /** 
-     * Get extension.
-     *       
-     */
-    private String getExtension() {
-        return this.extension;
-    }
+  private void setUuid(String uuid) {
+    this.uuid = uuid;
+  }
 
-    private void setExtension(String extension) {
-        this.extension = extension;
-    }
+  private Integer getVersion() {
+    return this.version;
+  }
 
-    /** 
-     * Get modify source.
-     *       
-     */
-    private String getModify_source() {
-        return this.modify_source;
-    }
+  private void setVersion(Integer version) {
+    this.version = version;
+  }
 
-    private void setModify_source(String modify_source) {
-        this.modify_source = modify_source;
-    }
+  private Member getCreator_id() {
+    return this.creator_id;
+  }
 
-    private Date getModify_time() {
-        return this.modify_time;
-    }
+  private void setCreator_id(Member creator_id) {
+    this.creator_id = creator_id;
+  }
 
-    private void setModify_time(Date modify_time) {
-        this.modify_time = modify_time;
-    }
+  private Member getModifier_id() {
+    return this.modifier_id;
+  }
 
-    /** 
-     * Get name.
-     *       
-     */
-    private String getName() {
-        return this.name;
-    }
+  private void setModifier_id(Member modifier_id) {
+      this.modifier_id = modifier_id;
+  }
 
-    private void setName(String name) {
-        this.name = name;
-    }
+  private Stem getParent_stem() {
+    return this.parent_stem;
+  }
 
-    /** 
-     * Get UUID.
-     *       
-     */
-    private String getUuid() {
-        return this.uuid;
-    }
+  private void setParent_stem(Stem parent_stem) {
+    this.parent_stem = parent_stem;
+  }
 
-    private void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
+  private Set getChild_groups() {
+    return this.child_groups;
+  }
 
-    private Integer getVersion() {
-        return this.version;
-    }
+  private void setChild_groups(Set child_groups) {
+    this.child_groups = child_groups;
+  }
 
-    private void setVersion(Integer version) {
-        this.version = version;
-    }
+  private Set getChild_stems() {
+    return this.child_stems;
+  }
 
-    /** 
-     * Get creator.
-     *       
-     */
-    private edu.internet2.middleware.grouper.Member getCreator_id() {
-        return this.creator_id;
-    }
-
-    private void setCreator_id(edu.internet2.middleware.grouper.Member creator_id) {
-        this.creator_id = creator_id;
-    }
-
-    /** 
-     * Get modifier.
-     *       
-     */
-    private edu.internet2.middleware.grouper.Member getModifier_id() {
-        return this.modifier_id;
-    }
-
-    private void setModifier_id(edu.internet2.middleware.grouper.Member modifier_id) {
-        this.modifier_id = modifier_id;
-    }
-
-    /** 
-     * Get parent stem.
-     *       
-     */
-    private edu.internet2.middleware.grouper.Stem getParent_stem() {
-        return this.parent_stem;
-    }
-
-    private void setParent_stem(edu.internet2.middleware.grouper.Stem parent_stem) {
-        this.parent_stem = parent_stem;
-    }
-
-    /** 
-     * Get child groups.
-     *       
-     */
-    private Set getChild_groups() {
-        return this.child_groups;
-    }
-
-    private void setChild_groups(Set child_groups) {
-        this.child_groups = child_groups;
-    }
-
-    /** 
-     * Get child stems.
-     *       
-     */
-    private Set getChild_stems() {
-        return this.child_stems;
-    }
-
-    private void setChild_stems(Set child_stems) {
-        this.child_stems = child_stems;
-    }
-
-    public String toString() {
-        return new ToStringBuilder(this)
-            .append("display_name", getDisplay_name())
-            .append("name", getName())
-            .append("uuid", getUuid())
-            .append("creator_id", getCreator_id())
-            .append("modifier_id", getModifier_id())
-            .toString();
-    }
-
-    public boolean equals(Object other) {
-        if ( (this == other ) ) return true;
-        if ( !(other instanceof Stem) ) return false;
-        Stem castOther = (Stem) other;
-        return new EqualsBuilder()
-            .append(this.getUuid(), castOther.getUuid())
-            .append(this.getCreator_id(), castOther.getCreator_id())
-            .append(this.getModifier_id(), castOther.getModifier_id())
-            .isEquals();
-    }
-
-    public int hashCode() {
-        return new HashCodeBuilder()
-            .append(getUuid())
-            .append(getCreator_id())
-            .append(getModifier_id())
-            .toHashCode();
-    }
+  private void setChild_stems(Set child_stems) {
+    this.child_stems = child_stems;
+  }
 
 }
+
