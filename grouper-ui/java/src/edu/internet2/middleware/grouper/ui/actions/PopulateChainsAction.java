3077806445ad228ebd76fdb0a798a9358e926743
isgwb <isgwb@mycompany.com>
2005-11-04 15:18:03 +0000

    New Action to show all ways in which a subject is a member of a group
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@1725 bc24b658-cf39-4249-b827-e3526eaaeb42

---
+++ grouper-ui/java/src/edu/internet2/middleware/grouper/ui/actions/PopulateChainsAction.java
@@ -0,0 +1,334 @@
+/*
+ * Copyright (C) 2004-2005 University Corporation for Advanced Internet Development, Inc.
+ * Copyright (C) 2004-2005 The University Of Bristol
+ * All Rights Reserved. 
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  * Neither the name of the University of Bristol nor the names
+ *    of its contributors nor the University Corporation for Advanced
+ *   Internet Development, Inc. may be used to endorse or promote
+ *   products derived from this software without explicit prior
+ *   written permission.
+ *
+ * You are under no obligation whatsoever to provide any enhancements
+ * to the University of Bristol, its contributors, or the University
+ * Corporation for Advanced Internet Development, Inc.  If you choose
+ * to provide your enhancements, or if you choose to otherwise publish
+ * or distribute your enhancements, in source code form without
+ * contemporaneously requiring end users to enter into a separate
+ * written license agreement for such enhancements, then you thereby
+ * grant the University of Bristol, its contributors, and the University
+ * Corporation for Advanced Internet Development, Inc. a non-exclusive,
+ * royalty-free, perpetual license to install, use, modify, prepare
+ * derivative works, incorporate into the software or other computer
+ * software, distribute, and sublicense your enhancements or derivative
+ * works thereof, in binary and source code form.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND WITH ALL FAULTS.  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT ARE DISCLAIMED AND the
+ * entire risk of satisfactory quality, performance, accuracy, and effort
+ * is with LICENSEE. IN NO EVENT SHALL THE COPYRIGHT OWNER, CONTRIBUTORS,
+ * OR THE UNIVERSITY CORPORATION FOR ADVANCED INTERNET DEVELOPMENT, INC.
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OR DISTRIBUTION OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package edu.internet2.middleware.grouper.ui.actions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.DynaActionForm;
+
+import edu.internet2.middleware.grouper.GrouperGroup;
+import edu.internet2.middleware.grouper.GrouperHelper;
+import edu.internet2.middleware.grouper.GrouperSession;
+import edu.internet2.middleware.grouper.SubjectFactory;
+import edu.internet2.middleware.subject.Subject;
+
+
+/**
+ * Top level Struts Action which shows all the ways in which a member is 
+ * a member of a group - used so users can follow effective memberships  
+ * <p/>
+ * <table width="75%" border="1">
+  <tr bgcolor="#CCCCCC"> 
+    <td width="51%"><strong><font face="Arial, Helvetica, sans-serif">Request 
+      Parameter</font></strong></td>
+    <td width="12%"><strong><font face="Arial, Helvetica, sans-serif">Direction</font></strong></td>
+    <td width="37%"><strong><font face="Arial, Helvetica, sans-serif">Description</font></strong></td>
+  </tr>
+  <tr> 
+    <td><p><font face="Arial, Helvetica, sans-serif">asMemberOf</font></p></td>
+    <td><font face="Arial, Helvetica, sans-serif">IN</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">Identifies group which is focus 
+      of action</font></td>
+  </tr>
+  <tr> 
+    <td><p><font face="Arial, Helvetica, sans-serif">subjectId</font></p></td>
+    <td><font face="Arial, Helvetica, sans-serif">IN</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">Identifies the Subject we are 
+      viewing chains for</font></td>
+  </tr>
+  <tr> 
+    <td><p><font face="Arial, Helvetica, sans-serif">subjectType</font></p></td>
+    <td><font face="Arial, Helvetica, sans-serif">IN</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">Identifies the type of the Subject 
+      we are viewing chains for</font></td>
+  </tr>
+  <tr> 
+    <td><p><font face="Arial, Helvetica, sans-serif">groupId</font></p></td>
+    <td><font face="Arial, Helvetica, sans-serif">IN</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">Identifiesgroup which is focus 
+      of action</font></td>
+  </tr>
+  <tr bgcolor="#CCCCCC"> 
+    <td><strong><font face="Arial, Helvetica, sans-serif">Request Attribute</font></strong></td>
+    <td><strong><font face="Arial, Helvetica, sans-serif">Direction</font></strong></td>
+    <td><strong><font face="Arial, Helvetica, sans-serif">Description</font></strong></td>
+  </tr>
+  <tr bgcolor="#FFFFFF"> 
+    <td><font face="Arial, Helvetica, sans-serif">subtitle</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">OUT</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">subtitle</font><font face="Arial, Helvetica, sans-serif">: 
+      text derived <br>
+      from groups.action.show-members key in nav ResourceBundle</font></td>
+  </tr>
+  <tr bgcolor="#FFFFFF"> 
+    <td><font face="Arial, Helvetica, sans-serif">chainPaths</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">OUT</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">List of objects representing 
+      the chains</font></td>
+  </tr>
+  <tr bgcolor="#FFFFFF"> 
+    <td><font face="Arial, Helvetica, sans-serif">browsePath</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">OUT</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">List of stems to the current 
+      group </font></td>
+  </tr>
+  <tr bgcolor="#FFFFFF"> 
+    <td><font face="Arial, Helvetica, sans-serif">browseParent</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">OUT</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">The current group we are showing 
+      chains for</font></td>
+  </tr>
+  <tr bgcolor="#FFFFFF"> 
+    <td><font face="Arial, Helvetica, sans-serif">subject</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">OUT</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">The subject we are showing chains 
+      for </font></td>
+  </tr>
+  <tr bgcolor="#FFFFFF"> 
+    <td><font face="Arial, Helvetica, sans-serif">requestParams</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">OUT</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">request.getParameterMap</font></td>
+  </tr>
+  <tr bgcolor="#CCCCCC"> 
+    <td><strong><font face="Arial, Helvetica, sans-serif">Session Attribute</font></strong></td>
+    <td><strong><font face="Arial, Helvetica, sans-serif">Direction</font></strong></td>
+    <td><strong><font face="Arial, Helvetica, sans-serif">Description</font></strong></td>
+  </tr>
+  <tr bgcolor="#FFFFFF"> 
+    <td><font face="Arial, Helvetica, sans-serif">findForNode</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">IN</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">Identifies group which is focus 
+      of action</font></td>
+  </tr>
+  <tr bgcolor="#CCCCCC"> 
+    <td><strong><font face="Arial, Helvetica, sans-serif">Strut's Action Parameter</font></strong></td>
+    <td><strong><font face="Arial, Helvetica, sans-serif">Direction</font></strong></td>
+    <td><strong><font face="Arial, Helvetica, sans-serif">Description</font></strong></td>
+  </tr>
+  <tr> 
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+
+<p>&nbsp;</p><table width="75%" border="1">
+  <tr bgcolor="#CCCCCC"> 
+    <td width="51%"><strong><font face="Arial, Helvetica, sans-serif">Request 
+      Parameter</font></strong></td>
+    <td width="12%"><strong><font face="Arial, Helvetica, sans-serif">Direction</font></strong></td>
+    <td width="37%"><strong><font face="Arial, Helvetica, sans-serif">Description</font></strong></td>
+  </tr>
+  <tr> 
+    <td><font face="Arial, Helvetica, sans-serif">stemId</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">IN</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">Identifies the stem (if any) 
+      to which Naming privileges will be assigned</font></td>
+  </tr>
+  <tr> 
+    <td><font face="Arial, Helvetica, sans-serif">groupId</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">IN</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">Identifies the group (if any) 
+      to which membership / Access privileges will be assigned</font></td>
+  </tr>
+  <tr> 
+    <td><font face="Arial, Helvetica, sans-serif">stems</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">IN</font></td>
+    <td><font face="Arial, Helvetica, sans-serif"><em>true</em> indicates that 
+      a stemId should be expected</font></td>
+  </tr>
+  <tr> 
+    <td><font face="Arial, Helvetica, sans-serif">members</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">IN</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">Array of Subject Ids</font></td>
+  </tr>
+  <tr bgcolor="#FFFFFF"> 
+    <td><font face="Arial, Helvetica, sans-serif">subjectType:&lt;subjectId&gt;</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">IN</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">Subject type Subject ids derived 
+      from 'members'</font></td>
+  </tr>
+  <tr bgcolor="#FFFFFF"> 
+    <td><font face="Arial, Helvetica, sans-serif">privileges</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">IN</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">Array of privilege names to 
+      be assigned</font></td>
+  </tr>
+  <tr bgcolor="#FFFFFF"> 
+    <td><font face="Arial, Helvetica, sans-serif">callerPageId</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">IN</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">pageId of saved page which should 
+      be returned to</font></td>
+  </tr>
+  <tr bgcolor="#CCCCCC"> 
+    <td><strong><font face="Arial, Helvetica, sans-serif">Request Attribute</font></strong></td>
+    <td><strong><font face="Arial, Helvetica, sans-serif">Direction</font></strong></td>
+    <td><strong><font face="Arial, Helvetica, sans-serif">Description</font></strong></td>
+  </tr>
+  <tr> 
+    <td><font face="Arial, Helvetica, sans-serif">&nbsp;thisPageId</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">&nbsp;OUT</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">&nbsp;Allows callerPageId to 
+      be added to links/forms so this page can be returned to</font></td>
+  </tr>
+  <tr> 
+    <td><font face="Arial, Helvetica, sans-serif">message</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">OUT</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">Message instance: text derived 
+      from priv.message.error.no-priv / priv.message.assigned key in nav ResourceBundle</font></td>
+  </tr>
+  <tr bgcolor="#CCCCCC"> 
+    <td><strong><font face="Arial, Helvetica, sans-serif">Session Attribute</font></strong></td>
+    <td><strong><font face="Arial, Helvetica, sans-serif">Direction</font></strong></td>
+    <td><strong><font face="Arial, Helvetica, sans-serif">Description</font></strong></td>
+  </tr>
+  <tr bgcolor="#FFFFFF"> 
+    <td><font face="Arial, Helvetica, sans-serif">findForNode</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">IN</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">If stemId and groupId are not 
+      set default to this</font></td>
+  </tr>
+  <tr bgcolor="#FFFFFF"> 
+    <td><font face="Arial, Helvetica, sans-serif">findForPriv</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">IN</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">If present display list of privilegees, 
+      otherwise display list of members</font></td>
+  </tr>
+  <tr bgcolor="#CCCCCC"> 
+    <td><strong><font face="Arial, Helvetica, sans-serif">Strut's Action Parameter</font></strong></td>
+    <td><strong><font face="Arial, Helvetica, sans-serif">Direction</font></strong></td>
+    <td><strong><font face="Arial, Helvetica, sans-serif">Description</font></strong></td>
+  </tr>
+  <tr> 
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+ * 
+ * 
+ * @author Gary Brown.
+ * @version $Id: PopulateChainsAction.java,v 1.1 2005-11-04 15:18:03 isgwb Exp $
+ */
+public class PopulateChainsAction extends GrouperCapableAction {
+
+	//------------------------------------------------------------ Local
+	// Forwards
+	static final private String FORWARD_Chain = "Chain";
+
+	
+
+	//------------------------------------------------------------ Action
+	// Methods
+
+	public ActionForward grouperExecute(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response,
+			HttpSession session, GrouperSession grouperSession)
+			throws Exception {
+		
+		session.setAttribute("subtitle","groups.action.show-members");
+		String subjectId = request.getParameter("subjectId");
+		String subjectType = request.getParameter("subjectType");
+		
+		Subject subject = SubjectFactory.getSubject(subjectId,subjectType);
+		DynaActionForm groupForm = (DynaActionForm) form;
+		saveAsCallerPage(request,groupForm,"findForNode");
+		
+		//Identify the group whose membership we are showing
+		String groupId = (String)groupForm.get("groupId");
+		//TODO: check following - shouldn't I always pass parameter
+		if (groupId == null || groupId.length() == 0)
+			groupId = (String) session.getAttribute("findForNode");
+		if (groupId == null)
+			groupId = request.getParameter("asMemberOf");
+		GrouperGroup group = GrouperGroup.loadByID(grouperSession,groupId);
+		
+		List ways = GrouperHelper.getAllWaysInWhichSubjectIsMemberOFGroup(grouperSession,subject,group);
+		List chains = GrouperHelper.groupList2SubjectsMaps(grouperSession,ways);
+		GrouperGroup chainGroup = null;
+		List groupChain = new ArrayList();
+		Map chain;
+		List chainPath;
+		for(int j=0;j<chains.size();j++) {
+			chainPath = new ArrayList();
+			chain = (Map) chains.get(j);
+			String[] chainGroupIds = (String[]) chain.get("chainGroupIds");
+			for(int i=0;i<chainGroupIds.length;i++) {
+				chainGroup = GrouperGroup.loadByID(grouperSession,chainGroupIds[i]);
+				chainPath.add(GrouperHelper.group2Map(grouperSession,chainGroup));
+			}
+			chain.put("chainPath",chainPath);
+		}
+		List path = GrouperHelper.parentStemsAsMaps(grouperSession, group);
+		request.setAttribute("chainPaths", chains);
+		request.setAttribute("browsePath", path);
+		request.setAttribute("browseParent", GrouperHelper.group2Map(
+				grouperSession, group));
+		request.setAttribute("requestParams",request.getParameterMap());
+		request.setAttribute("subject",GrouperHelper.subject2Map(subject));
+		
+		
+		
+		return mapping.findForward(FORWARD_Chain);
+
+	}
+
+}
