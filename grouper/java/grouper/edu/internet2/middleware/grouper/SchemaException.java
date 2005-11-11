d3d2dbc5d52bacdc286f293a05021e30edb69f06
Blair Christensen <blair@uchicago.edu>
2005-11-11 17:01:41 +0000

    new exception type
    git-svn-id: https://svn.internet2.edu/svn/i2mi/branches/GROUPER_RELENG_0_9@1813 bc24b658-cf39-4249-b827-e3526eaaeb42

---
+++ grouper/java/grouper/edu/internet2/middleware/grouper/SchemaException.java
@@ -0,0 +1,40 @@
+/*
+  Copyright 2004-2005 University Corporation for Advanced Internet Development, Inc.
+  Copyright 2004-2005 The University Of Chicago
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+package edu.internet2.middleware.grouper;
+
+/**
+ * General Groups Registry schema exception.
+ * <p />
+ * @author  blair christensen.
+ * @version $Id: SchemaException.java,v 1.1.2.1 2005-11-11 17:01:41 blair Exp $
+ */
+public class SchemaException extends Exception {
+  public SchemaException() { 
+    super(); 
+  }
+  public SchemaException(String msg) { 
+    super(msg); 
+  }
+  public SchemaException(String msg, Throwable cause) { 
+    super(msg, cause); 
+  }
+  public SchemaException(Throwable cause) { 
+    super(cause); 
+  }
+}
+
