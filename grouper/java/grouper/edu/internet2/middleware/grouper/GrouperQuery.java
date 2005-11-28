2d08a5501aa768ba87ce864d2d9caf7554c6fafd
Blair Christensen <blair@uchicago.edu>
2005-11-28 20:13:44 +0000

    add group and stem name filters
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@1901 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper/java/grouper/edu/internet2/middleware/grouper/GrouperQuery.java
+++ grouper/java/grouper/edu/internet2/middleware/grouper/GrouperQuery.java
@@ -25,7 +25,7 @@ import  java.util.*;
  * Perform arbitrary queries against the Groups Registry.
  * <p />
  * @author  blair christensen.
- * @version $Id: GrouperQuery.java,v 1.3 2005-11-28 18:13:18 blair Exp $
+ * @version $Id: GrouperQuery.java,v 1.4 2005-11-28 20:13:44 blair Exp $
  */
 public class GrouperQuery implements Serializable {
 
@@ -62,10 +62,12 @@ public class GrouperQuery implements Serializable {
    * <li>{@link ComplementFilter}</li>
    * <li>{@link GroupCreatedAfterFilter}</li>
    * <li>{@link GroupCreatedBeforeFilter}</li>
+   * <li>{@link GroupNameFilter}</li>
    * <li>{@link IntersectionFilter}</li>
    * <li>{@link NullFilter}</li>
    * <li>{@link StemCreatedAfterFilter}</li>
    * <li>{@link StemCreatedBeforeFilter}</li>
+   * <li>{@link StemNameFilter}</li>
    * <li>{@link UnionFilter}</li>
    * </ul>
    * @param   s       Query within this session context.
