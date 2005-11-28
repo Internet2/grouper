f225a3d8dcbc2341bc7694b6c32e7a9387078446
Blair Christensen <blair@uchicago.edu>
2005-11-28 21:53:05 +0000

    prep work for attaching attributes (and i reckon memberships at some point as well) to group subjects
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@1904 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper/src/grouper/edu/internet2/middleware/grouper/GrouperSubject.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/GrouperSubject.java
@@ -26,13 +26,13 @@ import  java.util.*;
  * {@link Subject} returned by the {@link GrouperSourceAdapter}.
  * <p />
  * @author  blair christensen.
- * @version $Id: GrouperSubject.java,v 1.2 2005-11-11 18:32:07 blair Exp $
+ * @version $Id: GrouperSubject.java,v 1.3 2005-11-28 21:53:05 blair Exp $
  */
 public class GrouperSubject implements Subject {
 
   // Private Instance Methods
   private GrouperSourceAdapter  adapter = null;
-  private Map                   attrs   = null; 
+  private Map                   attrs   = new HashMap(); 
   private String                id      = null;
   private String                name    = null;
   private SubjectType           type    = SubjectTypeEnum.valueOf("group");
@@ -43,6 +43,8 @@ public class GrouperSubject implements Subject {
     this.id       = g.getUuid();
     this.name     = g.getName();
     this.adapter  = sa;
+    // Attach attributes
+    this._addAttrs(g);
   } // protected GrouperSubject(g, sa)
 
 
@@ -59,8 +61,8 @@ public class GrouperSubject implements Subject {
     throw new RuntimeException("Not implemented");
   } // public Set getAttributeValues(name)
 
+  // TODO Is this even part of the latest spec?
   public String getDescription() {
-    // TODO Is this still part of the API?
     throw new RuntimeException("Not implemented");
   } // public String getDescription()
 
@@ -80,5 +82,68 @@ public class GrouperSubject implements Subject {
     return this.type;
   } // public SubjectType getType()
 
+
+  // Private Instance Methods
+  private void _addAttrs(Group g) {
+/* TODO 
+    try {
+      // Don't bother with any of the create* attrs unless we can find
+      // the creating subject
+      Subject creator = g.getCreateSubject();
+      this.attrs.put(
+        "createSubjectId"   , creator.getId()
+      );
+      this.attrs.put(
+        "createSubjectType" , creator.getType().getName()
+      );
+      this.attrs.put(
+        "createTime"        , g.getCreateTime().toString()
+      );
+    }
+    catch (SubjectNotFoundException eSNF0) {
+      // No creator?
+    }
+*/
+/* TODO
+    this.attrs.put(
+      "description"       , g.getDescription()
+    );
+*/
+/* TODO
+    this.attrs.put(
+      "displayExtension"  , g.getDisplayExtension()
+    );
+*/
+    this.attrs.put(
+      "displayName"       , g.getDisplayName()
+    );
+    this.attrs.put(
+      "extension"         , g.getDisplayName()
+    );
+/* TODO
+    try {
+      // Don't bother with any of the modify* attrs unless we can find
+      // the modifying subject
+      Subject modifier = g.getModifySubject();
+      this.attrs.put(
+        "modifySubjectId"   , modifier.getId()
+      );
+      this.attrs.put(
+        "modifySubjectType" , modifier.getType().getName()
+      );
+      this.attrs.put(
+        "modifyTime"        , g.getModifyTime().toString()
+      );
+    }
+    catch (SubjectNotFoundException eSNF1) {
+      // No modifier?
+    }
+*/
+    this.attrs.put(
+      "name"              , g.getName()
+    );
+    // TODO Attach custom attributes
+    // TODO Attach lists
+  } // private void _addAttrs(g)
 }
 
