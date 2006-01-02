1e09fb41f0acb330cdd72e43fe8d4b5132af018a
acohen <acohen@mycompany.com>
2006-01-02 19:14:20 +0000

    Simplified the shutdown.sh script, and fixed some line-break problems in startup.sh.
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@2181 bc24b658-cf39-4249-b827-e3526eaaeb42

--- signet_demo/scripts/startup.sh
+++ signet_demo/scripts/startup.sh
@@ -5,8 +5,7 @@
 unset CATALINA_HOME
 
 if [ ! -x "$JAVA_HOME"/bin/java ]; then
-  echo This script requires that the JAVA_HOME environment variable be
-properly
+  echo This script requires that the JAVA_HOME environment variable be properly
   echo set. That means that it must name a directory which contains
   echo "bin/java".
   exit 1
@@ -15,20 +14,16 @@ fi
 TOMCAT_DIR=jakarta-tomcat-5.0.28
 
 if [ ! -x "$TOMCAT_DIR" ]; then
-  echo This script must be run from the signet_demo home directory. That\'s
-the
-  echo "directory that contains the $TOMCAT_DIR and $HSQLDB_DIR
-directories."
+  echo This script must be run from the signet_demo home directory. That\'s the
+  echo "directory that contains the $TOMCAT_DIR and $HSQLDB_DIR directories."
   exit 1
 fi
 
 HSQLDB_DIR=hsqldb
 
 if [ ! -x "$HSQLDB_DIR" ]; then
-  echo This script must be run from the signet_demo home directory. That\'s
-the
-  echo "directory that contains the $TOMCAT_DIR and $HSQLDB_DIR
-directories."
+  echo This script must be run from the signet_demo home directory. That\'s the
+  echo "directory that contains the $TOMCAT_DIR and $HSQLDB_DIR directories."
   exit 1
 fi
 
@@ -55,6 +50,4 @@ pushd $TOMCAT_EXECUTABLE_DIR
 popd
 
 cd $HSQLDB_EXECUTABLE_DIR
-"$JAVA_HOME"/bin/java -classpath hsqldb.jar org.hsqldb.Server -database.0
-mydb -dbname.0 xdb >! hsqldb.out &
-
+"$JAVA_HOME"/bin/java -classpath hsqldb.jar org.hsqldb.Server -database.0 mydb -dbname.0 xdb >! hsqldb.out &
