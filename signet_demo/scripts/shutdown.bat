80540dd077f1a78283b23d188d0f192e80640006
acohen <acohen@mycompany.com>
2006-01-04 20:32:34 +0000

    Added quotes to Windows scripts to accommodate pathnames that include spaces.
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@2192 bc24b658-cf39-4249-b827-e3526eaaeb42

--- signet_demo/scripts/shutdown.bat
+++ signet_demo/scripts/shutdown.bat
@@ -3,7 +3,7 @@ if "%OS%" == "Windows_NT" setlocal
 rem ---------------------------------------------------------------------------
 rem Start script for the Signet demo system
 rem
-rem $Id: shutdown.bat,v 1.3 2005-12-21 23:38:25 acohen Exp $
+rem $Id: shutdown.bat,v 1.4 2006-01-04 20:32:34 acohen Exp $
 rem ---------------------------------------------------------------------------
 
 if exist "%JAVA_HOME%\bin\java.exe" goto okJavaHome
@@ -28,32 +28,31 @@ echo directory that contains the "%TOMCAT_DIR%" and "%HSQLDB_DIR%" directories.
 goto end
 
 :okHome
-set TOMCAT_EXECUTABLE_DIR=%TOMCAT_DIR%\bin
+set TOMCAT_EXECUTABLE_DIR="%TOMCAT_DIR%\bin"
 set TOMCAT_EXECUTABLE=shutdown.bat
-set HSQLDB_EXECUTABLE_DIR=%HSQLDB_DIR%\lib
+set HSQLDB_EXECUTABLE_DIR="%HSQLDB_DIR%\lib"
 set HSQLDB_EXECUTABLE=hsqldb.jar
 
 rem Check that target executables exist
 
 if exist "%TOMCAT_EXECUTABLE_DIR%\%TOMCAT_EXECUTABLE%" goto okTomcatExec
-echo Cannot find %TOMCAT_EXECUTABLE_DIR%\%TOMCAT_EXECUTABLE%
+echo Cannot find "%TOMCAT_EXECUTABLE_DIR%\%TOMCAT_EXECUTABLE%"
 echo This file is needed to run this program
 goto end
 
 :okTomcatExec
 if exist "%HSQLDB_EXECUTABLE_DIR%\%HSQLDB_EXECUTABLE%" goto okExec
-echo Cannot find %HSQLDB_EXECUTABLE_DIR%\%HSQLDB_EXECUTABLE%
+echo Cannot find "%HSQLDB_EXECUTABLE_DIR%\%HSQLDB_EXECUTABLE%"
 echo This file is needed to run this program
 goto end
 
 :okExec
 
-pushd %TOMCAT_EXECUTABLE_DIR%
+pushd "%TOMCAT_EXECUTABLE_DIR%"
 call "%TOMCAT_EXECUTABLE%"
 popd
 
-echo DEBUG: HSQLDB_EXECUTABLE_DIR=%HSQLDB_EXECUTABLE_DIR%
-cd %HSQLDB_EXECUTABLE_DIR%
-start "Shutting down HSQLDB database server for Signet demo" %JAVA_HOME%\bin\java.exe -jar hsqldb.jar --rcfile ..\..\config\sqltool.rc --sql "SHUTDOWN" localhost-sa
+cd "%HSQLDB_EXECUTABLE_DIR%"
+start "Shutting down HSQLDB database server for Signet demo" "%JAVA_HOME%"\bin\java.exe -jar hsqldb.jar --rcfile ..\..\config\sqltool.rc --sql "SHUTDOWN" localhost-sa
 
 :end
--- signet_demo/scripts/startup.bat
+++ signet_demo/scripts/startup.bat
@@ -3,7 +3,7 @@ if "%OS%" == "Windows_NT" setlocal
 rem ---------------------------------------------------------------------------
 rem Start script for the Signet demo system
 rem
-rem $Id: startup.bat,v 1.4 2005-12-22 13:16:56 acohen Exp $
+rem $Id: startup.bat,v 1.5 2006-01-04 20:32:34 acohen Exp $
 rem ---------------------------------------------------------------------------
 
 if exist "%JAVA_HOME%\bin\java.exe" goto okJavaHome
@@ -28,21 +28,21 @@ echo directory that contains the "%TOMCAT_DIR%" and "%HSQLDB_DIR" directories.
 goto end
 
 :okHome
-set TOMCAT_EXECUTABLE_DIR=%TOMCAT_DIR%\bin
+set TOMCAT_EXECUTABLE_DIR="%TOMCAT_DIR%\bin"
 set TOMCAT_EXECUTABLE=startup.bat
-set HSQLDB_EXECUTABLE_DIR=%HSQLDB_DIR%\lib
+set HSQLDB_EXECUTABLE_DIR="%HSQLDB_DIR%\lib"
 set HSQLDB_EXECUTABLE=hsqldb.jar
 
 rem Check that target executables exist
 
 if exist "%TOMCAT_EXECUTABLE_DIR%\%TOMCAT_EXECUTABLE%" goto okTomcatExec
-echo Cannot find %TOMCAT_EXECUTABLE_DIR%\%TOMCAT_EXECUTABLE%
+echo Cannot find "%TOMCAT_EXECUTABLE_DIR%\%TOMCAT_EXECUTABLE%"
 echo This file is needed to run this program
 goto end
 
 :okTomcatExec
 if exist "%HSQLDB_EXECUTABLE_DIR%\%HSQLDB_EXECUTABLE%" goto okExec
-echo Cannot find %HSQLDB_EXECUTABLE_DIR%\%HSQLDB_EXECUTABLE%
+echo Cannot find "%HSQLDB_EXECUTABLE_DIR%\%HSQLDB_EXECUTABLE%"
 echo This file is needed to run this program
 goto end
 
@@ -52,7 +52,7 @@ pushd %TOMCAT_EXECUTABLE_DIR%
 call "%TOMCAT_EXECUTABLE%"
 popd
 
-cd %HSQLDB_EXECUTABLE_DIR%
-start "HSQLDB database server for Signet demo" %JAVA_HOME%\bin\java.exe -classpath hsqldb.jar org.hsqldb.Server -database.0 mydb -dbname.0 xdb
+cd "%HSQLDB_EXECUTABLE_DIR%"
+start "HSQLDB database server for Signet demo" "%JAVA_HOME%"\bin\java.exe -classpath hsqldb.jar org.hsqldb.Server -database.0 mydb -dbname.0 xdb
 
 :end
