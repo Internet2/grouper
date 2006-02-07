206eed18a740ff8981a12543bfa40371a53378bd
acohen <acohen@mycompany.com>
2006-02-07 03:58:47 +0000

    Added a Windows batch-file to invoke the utility that defines the Signet system administrator.
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@2286 bc24b658-cf39-4249-b827-e3526eaaeb42

--- signet/util/admin/SignetProxy/run.sh
+++ signet/util/admin/SignetProxy/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # -----------------------------------------------------------------------------
-# Script for command line utilility to assign System Administrator proxy
+# Script for command line utility to assign System Administrator proxy
 #
 # Takes two arguments
 #    1 - Action to take -- grant | revoke | list
