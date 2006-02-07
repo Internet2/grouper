83a3fa6dc3f18d4997b19ee52e4315fc06f38493
lmcrae <lmcrae@mycompany.com>
2006-02-07 20:20:24 +0000

    New Util layout
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@2305 bc24b658-cf39-4249-b827-e3526eaaeb42

---
+++ signet/util/SubjectFileLoader/run.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# -----------------------------------------------------------------------------
+# Script for command line util to load Subject definitions
+#
+# Environment Variable Prequisites
+#
+#   JAVA_HOME     Must point to your JDK.
+# -----------------------------------------------------------------------------
+
+BASEDIR=../..
+CLASSPATH=$BASEDIR/config
+
+for file in `ls $BASEDIR/lib/*.jar`;
+do
+if [ "$CLASSPATH" != "" ]; then
+   CLASSPATH=${CLASSPATH}:$file
+else
+   CLASSPATH=$file
+fi
+done
+
+echo "Using classpath: "$CLASSPATH
+
+$JAVA_HOME/bin/java -cp $CLASSPATH edu.internet2.middleware.signet.util.SubjectFileLoader $1
