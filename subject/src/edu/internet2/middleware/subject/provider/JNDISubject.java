7876c7fc1827686bc3b9cb07607dfa3cf9dd9602
Tom Barton <tbarton@uchicago.edu>
2006-03-12 23:10:50 +0000

    Add JNDISourceAdapter, JNDISubject, and extensions BaseSourceAdapter, SourceManager, and Search.
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@2515 bc24b658-cf39-4249-b827-e3526eaaeb42

---
+++ subject/src/edu/internet2/middleware/subject/provider/JNDISubject.java
@@ -0,0 +1,132 @@
+/*--
+
+Copyright 2005 Internet2.  All Rights Reserved.
+See doc/license.txt in this distribution.
+*/
+/*
+ * JNDISubject.java
+ *
+ * Created on March 6, 2006
+ *
+ * Author Ellen Sluss
+ */
+package edu.internet2.middleware.subject.provider;
+
+import java.util.Set;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import edu.internet2.middleware.subject.Source;
+import edu.internet2.middleware.subject.Subject;
+import edu.internet2.middleware.subject.SubjectType;
+
+
+
+/**
+ * JNDI Subject implementation.
+ */
+public class JNDISubject 
+	implements Subject {
+	
+
+	private static Log log = LogFactory.getLog(JNDISubject.class);
+	
+	private JNDISourceAdapter adapter;
+	
+	private String id;
+	private String name;
+        private String description = null;
+	private SubjectType type = null;
+	private Map attributes = null;
+	
+	/*
+	 * Constructor called by SourceManager.
+	 */
+	protected JNDISubject(String id, String name, String description,
+			SubjectType type, JNDISourceAdapter adapter) {
+		log.debug("Name = "  + name);
+		this.id = id;
+		this.name = name;
+		this.type = type;
+                this.description = description;
+		this.adapter = adapter;
+	}
+
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public String getId() {
+		return this.id;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public SubjectType getType() {
+		return this.type;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public String getName() {
+		return this.name;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public String getDescription() {
+		return this.description;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public String getAttributeValue(String name) {
+		if (attributes == null) {
+			this.adapter.loadAttributes(this);
+		}
+		Set values = (Set)this.attributes.get(name);
+		if (values != null) {
+			return ((String[])values.toArray(new String[0]))[0];
+		}
+		else {
+			return null;
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public Set getAttributeValues(String name) {
+		if (attributes == null) {
+			this.adapter.loadAttributes(this);
+		}
+		return (Set)this.attributes.get(name);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public Map getAttributes() {
+		if (attributes == null) {
+			this.adapter.loadAttributes(this);
+		}
+		return attributes;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public Source getSource() {
+		return this.adapter;
+	}
+	
+	protected void setAttributes(Map attributes) {
+		this.attributes = attributes;
+	}
+}
