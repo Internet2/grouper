e2b9bb69de1e7403c2cb58b7d5c8194e017454a1
Blair Christensen <blair@uchicago.edu>
2006-04-28 17:12:34 +0000

    Modify `testGetSourcesByType()` to both **actually** test things as well as to reflect that the default `conf/sources.xml` now has **no** pre-defined sources.
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@2693 bc24b658-cf39-4249-b827-e3526eaaeb42

--- subject/tests/edu/internet2/middleware/subject/provider/SourceManagerTests.java
+++ subject/tests/edu/internet2/middleware/subject/provider/SourceManagerTests.java
@@ -6,7 +6,7 @@ import java.util.Iterator;
 import edu.internet2.middleware.subject.provider.BaseSourceAdapter;
 import edu.internet2.middleware.subject.provider.SourceManager;
 
-import junit.framework.TestCase;
+import junit.framework.*;
 
 /**
  * Unit tests for SourceManager.
@@ -48,18 +48,26 @@ public class SourceManagerTests extends TestCase {
 		}
 	}
 
-	/**
-	 * Test case for getting sources by SubjectType
-	 */
-	public void testGetSourcesByType() {
-		Collection sources = mgr.getSources(SubjectTypeEnum.valueOf("person"));
-		System.out.println("Person sources = " + sources);
+  /**
+   * Test case for getting sources by SubjectType
+   */
+  public void testGetSourcesByType() {
+    // Given that each of the current source adapters all have a fair
+    // amount of overhead, external dependencies and configuration
+    // requirements that might vary **widely** depending upon the
+    // circumstances it seems easier to test with **no** configured
+    // adapters. 
+    Collection people = mgr.getSources(SubjectTypeEnum.valueOf("person"));
+    Assert.assertNotNull("people !null", people);
+    Assert.assertEquals("Person sources == 0", 0, people.size());
 		
-		sources = mgr.getSources(SubjectTypeEnum.valueOf("group"));
-		System.out.println("Group sources = " + sources);
-		
-		sources = mgr.getSources(SubjectTypeEnum.valueOf("application"));
-		System.out.println("Application sources = " + sources);
-	}
+    Collection groups = mgr.getSources(SubjectTypeEnum.valueOf("group"));
+    Assert.assertNotNull("groups !null", groups);
+    Assert.assertEquals("Group sources == 0", 0, groups.size());
+
+    Collection apps   = mgr.getSources(SubjectTypeEnum.valueOf("application"));
+    Assert.assertNotNull("apps !null", apps);
+    Assert.assertEquals("Application sources == 0", 0, apps.size());
+  } // public void testSourcesByType()
 
 }
