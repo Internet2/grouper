ea21a8b026c608972b77a30edcdd17f7d872590c
Blair Christensen <blair@uchicago.edu>
2006-05-11 17:50:08 +0000

    a couple more tests
    git-svn-id: https://svn.internet2.edu/svn/i2mi/branches/GROUPER_1_0_TAKE2@2710 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper/src/test/edu/internet2/middleware/grouper/SuiteCompositesU.java
+++ grouper/src/test/edu/internet2/middleware/grouper/SuiteCompositesU.java
@@ -22,7 +22,7 @@ import  org.apache.commons.logging.*;
 
 /**
  * @author  blair christensen.
- * @version $Id: SuiteCompositesU.java,v 1.1.2.11 2006-05-11 17:29:40 blair Exp $
+ * @version $Id: SuiteCompositesU.java,v 1.1.2.12 2006-05-11 17:50:08 blair Exp $
  */
 public class SuiteCompositesU extends TestCase {
 
@@ -56,9 +56,9 @@ public class SuiteCompositesU extends TestCase {
     suite.addTestSuite( TestCompositeU18.class  );  // del union: two comp children, no parents
     suite.addTestSuite( TestCompositeU19.class  );  // add union: no children, parent
     suite.addTestSuite( TestCompositeU20.class  );  // del union: no children, parent
-/*  TODO
     suite.addTestSuite( TestCompositeU21.class  );  // add union: no children, comp parent
     suite.addTestSuite( TestCompositeU22.class  );  // del union: no children, comp parent
+/*  TODO
     suite.addTestSuite( TestCompositeU23.class  );  // add union: one child, parent
     suite.addTestSuite( TestCompositeU24.class  );  // del union: one child, parent
     suite.addTestSuite( TestCompositeU25.class  );  // add union: one child, comp parent
