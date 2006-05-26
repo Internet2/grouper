48a4ecc97206c05c55f3af53e72e72c574160ca7
Blair Christensen <blair@uchicago.edu>
2006-05-26 17:53:21 +0000

    * start breaking apart _GrouperShell_ so that i can then start adding   some real functionality to it * add embedded "exit" and "quit" commands
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@2736 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper/contrib/cdg/src/cdg/com/devclue/grouper/shell/GrouperShell.java
+++ grouper/contrib/cdg/src/cdg/com/devclue/grouper/shell/GrouperShell.java
@@ -15,28 +15,80 @@ import  java.util.*;
  * Grouper Management Shell.
  * <p />
  * @author  blair christensen.
- * @version $Id: GrouperShell.java,v 1.1 2006-05-25 15:20:19 blair Exp $
+ * @version $Id: GrouperShell.java,v 1.2 2006-05-26 17:53:21 blair Exp $
  */
 public class GrouperShell {
 
-  // Main //
+  // PRIVATE INSTANCE VARIABLES //
+  Interpreter i;
+
+
+  // MAIN //
+
+  /**
+   * @since 0.3
+   */
   public static void main(String args[]) {
     try {
-      Interpreter     i   = new Interpreter();
-      i.eval("importCommands(\"com.devclue.grouper.stem\")");
-      String          cmd = new String();
-      BufferedReader  in  = new BufferedReader(new InputStreamReader(System.in));
-      while ( (cmd = in.readLine() ) != null ) {
-        System.err.println("GOT: <<" + cmd + ">>");
-        Object obj = i.eval(cmd);
-        System.err.println("OBJ: <<" + obj + ">>");
-      }
+      GrouperShell shell = new GrouperShell();
+      shell._run();
     }
     catch (Exception e) {
       System.err.println(e.getMessage());
+      System.exit(1);
     }
     System.exit(0);
   } // public static void main(args)
+
+
+  // CONSTRUCTORS //
+  private GrouperShell() 
+    throws  bsh.EvalError
+  {
+    this.i = new Interpreter();
+    this._importCommands();
+  } // private GrouperShell()
+
+
+  // PRIVATE INSTANCE METHODS //
+  //  @since  0.3
+  private void _importCommand(String name) 
+    throws  bsh.EvalError
+  {
+    String cmd = "importCommands(\"com.devclue.grouper." + name + "\")";
+    this.i.eval(cmd);
+  } // private void _importCommand(name)
+
+  //  @since  0.3
+  private void _importCommands() 
+    throws  bsh.EvalError
+  {
+    this._importCommand(  "group"   );
+    this._importCommand(  "member"  );
+    this._importCommand(  "stem"    );
+    this._importCommand(  "subject" );
+  } // private void _importCommands()
+
+  //  @since  0.3
+  private void _run() 
+    throws  IOException
+  {
+    String          cmd = new String();
+    BufferedReader  in  = new BufferedReader(new InputStreamReader(System.in));
+    while ( (cmd = in.readLine() ) != null ) {
+      System.err.println("GOT: <<" + cmd + ">>");
+      if ( cmd.equals("exit") || cmd.equals("quit") ) {
+        break;
+      }
+      try {
+        Object obj = i.eval(cmd);
+        System.err.println("OBJ: <<" + obj + ">>");
+      }
+      catch (bsh.EvalError eBEE) {
+        System.err.println("ERROR: " + eBEE.getMessage());
+      }
+    }
+  } // private void _run()
 
 }
 
