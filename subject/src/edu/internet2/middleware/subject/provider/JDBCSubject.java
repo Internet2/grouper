97dc1d9a1d8876dd80ee67cb33b82a250e922842
Ellen Sluss <esluss@uchicago.edu>
2006-06-07 19:03:54 +0000

    no message
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@2765 bc24b658-cf39-4249-b827-e3526eaaeb42

--- subject/src/edu/internet2/middleware/subject/provider/JDBCSubject.java
+++ subject/src/edu/internet2/middleware/subject/provider/JDBCSubject.java
@@ -1,14 +1,15 @@
 /*--
-$Id: JDBCSubject.java,v 1.2 2005-06-20 14:49:52 mnguyen Exp $
-$Date: 2005-06-20 14:49:52 $
-
+$Id: JDBCSubject.java,v 1.3 2006-06-07 19:03:54 esluss Exp $
+$Date: 2006-06-07 19:03:54 $
+ 
 Copyright 2005 Internet2 and Stanford University.  All Rights Reserved.
 See doc/license.txt in this distribution.
-*/
+ */
 package edu.internet2.middleware.subject.provider;
 
 import java.util.Set;
 import java.util.Map;
+import java.lang.String;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -21,107 +22,107 @@ import edu.internet2.middleware.subject.SubjectType;
 /**
  * JDBC Subject implementation.
  */
-public class JDBCSubject 
-	implements Subject {
-	
-	private static final String NAME_ATTRIBUTE = "name";
-	private static final String TYPE_ATTRIBUTE = "type";
-	private static final String DESC_ATTRIBUTE = "description";
-
-	private static Log log = LogFactory.getLog(JDBCSubject.class);
-	
-	private JDBCSourceAdapter adapter;
-	
-	private String id;
-	private String name;
-	private SubjectType type = null;
-	private Map attributes = null;
-	
-	/*
-	 * Constructor called by SourceManager.
-	 */
-	protected JDBCSubject(String id, String name, 
-			SubjectType type, JDBCSourceAdapter adapter) {
-		log.debug("Name = "  + name);
-		this.id = id;
-		this.name = name;
-		this.type = type;
-		this.adapter = adapter;
-	}
-
-	
-	/**
-	 * {@inheritDoc}
-	 */
-	public String getId() {
-		return this.id;
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 */
-	public SubjectType getType() {
-		return this.type;
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 */
-	public String getName() {
-		return this.name;
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 */
-	public String getDescription() {
-		return getAttributeValue(DESC_ATTRIBUTE);
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public String getAttributeValue(String name) {
-		if (attributes == null) {
-			this.adapter.loadAttributes(this);
-		}
-		Set values = (Set)this.attributes.get(name);
-		if (values != null) {
-			return ((String[])values.toArray(new String[0]))[0];
-		}
-		else {
-			return null;
-		}
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public Set getAttributeValues(String name) {
-		if (attributes == null) {
-			this.adapter.loadAttributes(this);
-		}
-		return (Set)this.attributes.get(name);
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public Map getAttributes() {
-		if (attributes == null) {
-			this.adapter.loadAttributes(this);
-		}
-		return attributes;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public Source getSource() {
-		return this.adapter;
-	}
-	
-	protected void setAttributes(Map attributes) {
-		this.attributes = attributes;
-	}
+public class JDBCSubject
+        implements Subject {
+    
+    
+    private static Log log = LogFactory.getLog(JDBCSubject.class);
+    
+    private JDBCSourceAdapter adapter;
+    
+    private String id;
+    private String name;
+    private String description = null;
+    private String descriptionName;
+    private SubjectType type = null;
+    private Map attributes = null;
+    
+        /*
+         * Constructor called by SourceManager.
+         */
+    protected JDBCSubject(String id, String name, String description,
+            SubjectType type, JDBCSourceAdapter adapter) {
+        log.debug("Name = "  + name);
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.description =description;
+        this.adapter = adapter;
+    }
+    
+    
+    /**
+     * {@inheritDoc}
+     */
+    public String getId() {
+        return this.id;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public SubjectType getType() {
+        return this.type;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public String getName() {
+        return this.name;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    
+    public String getDescription() {
+        return this.description;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public String getAttributeValue(String name) {
+        if (attributes == null) {
+            log.error("No attributes.");
+        }
+        Set values = (Set)this.attributes.get(name);
+        if (values != null) {
+            return ((String[])values.toArray(new String[0]))[0];
+        } else {
+            return null;
+        }
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public Set getAttributeValues(String name) {
+        if (attributes == null) {
+            log.error("No attributes.");
+        }
+        return (Set)this.attributes.get(name);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public Map getAttributes() {
+        if (attributes == null) {
+            //this.adapter.loadAttributes(this);
+        }
+        return attributes;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public Source getSource() {
+        return this.adapter;
+    }
+    
+    protected void setAttributes(Map attributes) {
+        this.attributes = attributes;
+    }
 }
