caa1c6d91c0d7f77260a9687ee7adfd143849dd5
isgwb <isgwb@mycompany.com>
2006-07-06 09:29:53 +0000

    Deal with Composites + also provide link so that privileges can be directly assigned to the subject
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@2885 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper-ui/java/src/edu/internet2/middleware/grouper/ui/actions/PopulateChainsAction.java
+++ grouper-ui/java/src/edu/internet2/middleware/grouper/ui/actions/PopulateChainsAction.java
@@ -18,6 +18,7 @@ limitations under the License.
 package edu.internet2.middleware.grouper.ui.actions;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -30,14 +31,18 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.DynaActionForm;
 
+import edu.internet2.middleware.grouper.Composite;
+import edu.internet2.middleware.grouper.CompositeFinder;
 import edu.internet2.middleware.grouper.Field;
 import edu.internet2.middleware.grouper.FieldFinder;
 import edu.internet2.middleware.grouper.Group;
 import edu.internet2.middleware.grouper.GroupFinder;
 import edu.internet2.middleware.grouper.GrouperHelper;
 import edu.internet2.middleware.grouper.GrouperSession;
+import edu.internet2.middleware.grouper.MemberFinder;
 import edu.internet2.middleware.grouper.Membership;
 import edu.internet2.middleware.grouper.SubjectFinder;
+import edu.internet2.middleware.grouper.ui.GroupOrStem;
 import edu.internet2.middleware.subject.Subject;
 
 
@@ -77,8 +82,7 @@ import edu.internet2.middleware.subject.Subject;
       of action</font></td>
   </tr>
   <tr> 
-    <td><p><font face="Arial, Helvetica, sans-serif">listField</font></p>
-      </td>
+    <td><p><font face="Arial, Helvetica, sans-serif">listField</font></p></td>
     <td><font face="Arial, Helvetica, sans-serif">IN</font></td>
     <td><font face="Arial, Helvetica, sans-serif">Identifies list field we are 
       interested in</font></td>
@@ -108,6 +112,12 @@ import edu.internet2.middleware.subject.Subject;
       chains for</font></td>
   </tr>
   <tr bgcolor="#FFFFFF"> 
+    <td><font face="Arial, Helvetica, sans-serif">composite</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">OUT</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">If the group has a composite 
+      member, make the Composite available as a Map</font></td>
+  </tr>
+  <tr bgcolor="#FFFFFF"> 
     <td><font face="Arial, Helvetica, sans-serif">subject</font></td>
     <td><font face="Arial, Helvetica, sans-serif">OUT</font></td>
     <td><font face="Arial, Helvetica, sans-serif">The subject we are showing chains 
@@ -117,6 +127,29 @@ import edu.internet2.middleware.subject.Subject;
     <td><font face="Arial, Helvetica, sans-serif">requestParams</font></td>
     <td><font face="Arial, Helvetica, sans-serif">OUT</font></td>
     <td><font face="Arial, Helvetica, sans-serif">request.getParameterMap</font></td>
+  </tr>
+  <tr bgcolor="#FFFFFF"> 
+    <td><font face="Arial, Helvetica, sans-serif">linkParams</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">OUT</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">Empty Map for use in templates 
+      to build parameters for a link</font></td>
+  </tr>
+  <tr bgcolor="#FFFFFF"> 
+    <td><font face="Arial, Helvetica, sans-serif">groupMemberParams</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">OUT</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">Map with parameters for link 
+      to populateGroupMember. Allows direct assignment of privileges to the subject</font></td>
+  </tr>
+  <tr bgcolor="#FFFFFF"> 
+    <td><font face="Arial, Helvetica, sans-serif">privs</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">OUT</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">Set of directly assigned privileges 
+      for the subject on this group</font></td>
+  </tr>
+  <tr bgcolor="#FFFFFF"> 
+    <td><font face="Arial, Helvetica, sans-serif">privsSize</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">OUT</font></td>
+    <td><font face="Arial, Helvetica, sans-serif">Number of items in privs</font></td>
   </tr>
   <tr bgcolor="#CCCCCC"> 
     <td><strong><font face="Arial, Helvetica, sans-serif">Session Attribute</font></strong></td>
@@ -143,7 +176,7 @@ import edu.internet2.middleware.subject.Subject;
  * 
  * 
  * @author Gary Brown.
- * @version $Id: PopulateChainsAction.java,v 1.7 2006-02-22 15:32:50 isgwb Exp $
+ * @version $Id: PopulateChainsAction.java,v 1.8 2006-07-06 09:29:53 isgwb Exp $
  */
 public class PopulateChainsAction extends GrouperCapableAction {
 
@@ -183,32 +216,45 @@ public class PopulateChainsAction extends GrouperCapableAction {
 		if (groupId == null)
 			groupId = request.getParameter("asMemberOf");
 		Group group = GroupFinder.findByUuid(grouperSession,groupId); 
-		List ways = GrouperHelper.getAllWaysInWhichSubjectIsMemberOFGroup(grouperSession,subject,group,mField);
-		Group g = null;
-		Membership m = null;
 		Map gMap = null;
-		List chains=new ArrayList();
-		List chain;
-		for(int i=0;i<ways.size();i++) {
-			m = (Membership)ways.get(i);
-			g=m.getGroup();
-			gMap = GrouperHelper.group2Map(grouperSession,g);
-			chain=GrouperHelper.getChain(grouperSession,m);
-			gMap.put("chainPath",chain);
-			gMap.put("chainPathSize",new Integer(chain.size()));
+		if(group.hasComposite()) {
+			Composite comp = CompositeFinder.findAsOwner(group);
+			gMap = GrouperHelper.getCompositeMap(grouperSession,comp,subject);
+			request.setAttribute("composite",gMap);
+			request.setAttribute("linkParams",new HashMap());
+		}else{
+			List ways = GrouperHelper.getAllWaysInWhichSubjectIsMemberOFGroup(grouperSession,subject,group,mField);
+			Group g = null;
+			Membership m = null;
 			
-			chains.add(gMap);
-		}
-		
-		
-		request.setAttribute("chainPaths", chains);
+			List chains=new ArrayList();
+			List chain;
+			for(int i=0;i<ways.size();i++) {
+				m = (Membership)ways.get(i);
+				g=m.getGroup();
 	
+				gMap = GrouperHelper.group2Map(grouperSession,g);
+				chain=GrouperHelper.getChain(grouperSession,m);
+				gMap.put("chainPath",chain);
+				gMap.put("chainPathSize",new Integer(chain.size()));
+				
+				chains.add(gMap);
+			}
+			
+			
+			request.setAttribute("chainPaths", chains);
+		}
 		request.setAttribute("browseParent", GrouperHelper.group2Map(
 				grouperSession, group));
 		request.setAttribute("requestParams",request.getParameterMap());
+		Map groupMemberParams = new HashMap(request.getParameterMap());
+		groupMemberParams.put("callerPageId",request.getAttribute("thisPageId"));
+		request.setAttribute("groupMemberParams",groupMemberParams);
 		request.setAttribute("subject",GrouperHelper.subject2Map(subject));
-		
-		
+		Map privs = GrouperHelper.hasAsMap(grouperSession,GroupOrStem.findByGroup(grouperSession,group),MemberFinder.findBySubject(grouperSession,subject),FieldFinder.find("members"));
+		privs.remove("MEMBER");
+		request.setAttribute("privs",privs.keySet());
+		request.setAttribute("privsSize",new Integer(privs.size()));
 		
 		return mapping.findForward(FORWARD_Chain);
 
