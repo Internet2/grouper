26bae8215879e14a18c7a71b74e1e6de37ba09b4
Blair Christensen <blair@uchicago.edu>
2006-08-04 16:19:43 +0000

    `gsh.bat` script from Gary Brown.
    I think I'll need to update a couple of variables so that they are autopopulated
    but I'm awaiting confirmation of that before making that change.
    
    
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk/grouper@3088 bc24b658-cf39-4249-b827-e3526eaaeb42

---
+++ contrib/gsh/src/bin/gsh.bat
@@ -0,0 +1,38 @@
+rem Author: Gary Brown
+rem $Id: gsh.bat,v 1.1 2006-08-04 16:19:43 blair Exp $
+
+@echo off
+rem if "%OS%" == "Windows_NT" setlocal
+if not "%1" == "acp" goto execute
+
+set GSH_CP=%GSH_CP%;%2
+
+goto end
+
+:execute
+rem Populated at build
+set GROUPER_HOME=C:\projects\GrouperI2MI_1-0\grouper
+set GSH_HOME=C:\projects\GrouperI2MI_1-0\grouper\contrib\gsh
+
+rem The user's CLASSPATH
+set GSH_CP=%CLASSPATH%
+rem Append Grouper .jar
+
+rem Append Grouper's build classes
+set GSH_CP=%GSH_CP%;%GROUPER_HOME%/build/grouper
+rem Append Grouper's configuration
+set GSH_CP=%GSH_CP%;%GROUPER_HOME%/conf
+
+
+rem Append Grouper's 3rd party libs
+for %%f in (%GROUPER_HOME%\lib\*.jar) do call %GSH_HOME%\bin\gsh acp %%f
+rem Append gsh .jar
+
+rem Append gsh's build classes
+set GSH_CP=%GSH_CP%;%GSH_HOME%/build/gsh
+rem Append gsh's 3rd party libs
+for %%f in (%GSH_HOME%\lib\*.jar) do call %GSH_HOME%\bin\gsh acp %%f
+
+java -classpath %GSH_CP% %GSH_JVMARGS% com.devclue.grouper.shell.GrouperShell %*
+set GSH_CP=
+:end
