399d5f40f7fb82b77ae7ee337750288c380ec918
Blair Christensen <blair@uchicago.edu>
2006-08-14 15:31:19 +0000

    fix version number
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@3134 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper/src/grouper/edu/internet2/middleware/grouper/eg/FindGrouperSystem.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/eg/FindGrouperSystem.java
@@ -24,8 +24,8 @@ import  org.apache.commons.logging.*;       // For logging
  * Example: Find the Groups Registry's <i>root</i> {@link Subject}:
  * <i>GrouperSystem</i>.
  * @author  blair christensen.
- * @version $Id: FindGrouperSystem.java,v 1.2 2006-08-11 18:50:49 blair Exp $
- * @since   1.0.1
+ * @version $Id: FindGrouperSystem.java,v 1.3 2006-08-14 15:31:19 blair Exp $
+ * @since   1.1.0
  */
 public class FindGrouperSystem {
 
