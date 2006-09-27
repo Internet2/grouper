6ab121cd8172928eef29f149a6863e426edff201
Blair Christensen <blair@uchicago.edu>
2006-09-27 13:10:39 +0000

    BUGFIX: remember to disable the wheel group after explicitly enabling it
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk/grouper@3308 bc24b658-cf39-4249-b827-e3526eaaeb42

--- src/test/edu/internet2/middleware/grouper/TestWheelGroup3.java
+++ src/test/edu/internet2/middleware/grouper/TestWheelGroup3.java
@@ -22,7 +22,7 @@ import  org.apache.commons.logging.*;
 
 /**
  * @author  blair christensen.
- * @version $Id: TestWheelGroup3.java,v 1.2 2006-09-13 14:41:11 blair Exp $
+ * @version $Id: TestWheelGroup3.java,v 1.3 2006-09-27 13:10:39 blair Exp $
  * @since   1.1.0
  */
 public class TestWheelGroup3 extends TestCase {
@@ -69,6 +69,9 @@ public class TestWheelGroup3 extends TestCase {
     }
     catch (Exception e) {
       T.e(e);
+    }
+    finally {
+      GrouperConfig.setProperty(GrouperConfig.GWU, "false" ); // turn wheel back off
     }
   } // public void testNotWheelThenMakeWheel()
 
