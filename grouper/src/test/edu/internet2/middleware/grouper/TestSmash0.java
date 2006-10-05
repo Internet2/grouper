0f16f1195fbfca4a603ed2014f64be42fa86c2ed
Blair Christensen <blair@uchicago.edu>
2006-10-05 14:27:40 +0000

    add some basic smash code
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@3377 bc24b658-cf39-4249-b827-e3526eaaeb42

---
+++ grouper/src/test/edu/internet2/middleware/grouper/TestSmash0.java
@@ -0,0 +1,70 @@
+/*
+  Copyright 2004-2006 University Corporation for Advanced Internet Development, Inc.
+  Copyright 2004-2006 The University Of Chicago
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+package edu.internet2.middleware.grouper;
+import  edu.internet2.middleware.subject.*;
+import  junit.framework.*;
+import  org.apache.commons.logging.*;
+
+/**
+ * @author  blair christensen.
+ * @version $Id: TestSmash0.java,v 1.1 2006-10-05 14:27:40 blair Exp $
+ * @since   1.1.0
+ */
+public class TestSmash0 extends GrouperTest {
+
+  private static final Log LOG = LogFactory.getLog(TestSmash0.class);
+
+  public TestSmash0(String name) {
+    super(name);
+  }
+
+  protected void setUp () {
+    LOG.debug("setUp");
+    RegistryReset.reset();
+  }
+
+  protected void tearDown () {
+    LOG.debug("tearDown");
+  }
+
+  public void testAdd100Stems100Stems100Stems() {
+    LOG.info("testAdd100Stems100Stems100Stems");
+    try {
+      GrouperSession  s     = GrouperSession.start( SubjectFinder.findRootSubject() );
+      Stem            root  = StemFinder.findRootStem(s);
+      int             cnt   = 0;
+      Stem            top, child;
+      for (int i = 0; i < 100; i++) {
+        top = root.addChildStem("top " + i + " [" + cnt++ + "]", "top " + i);
+        for (int j = 0; j < 100; j ++) {
+          child = top.addChildStem("child " + j + " [" + cnt++ + "]", "child " + j);
+          for (int k = 0; k < 100; k ++) {
+            child.addChildStem("grandchild " + k + " [" + cnt++ + "]", "grandchild " + k);
+          }
+        }
+      }
+      s.stop();
+      Assert.assertTrue(true);
+    }
+    catch (Exception e) {
+      T.e(e);
+    }
+  } // public void testAdd100Stems100Stems100Stems()
+
+} // public class TestSmash0
+
