0c171c7ef546ae99565408fd9ad1382a1a394d8c
Blair Christensen <blair@uchicago.edu>
2006-12-19 19:34:08 +0000

    Remove all direct Hibernate references from `HibernateSubjectFinder` and remove class.
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk/grouper@3549 bc24b658-cf39-4249-b827-e3526eaaeb42

---
+++ src/grouper/edu/internet2/middleware/grouper/RegistrySubjectDAO.java
@@ -0,0 +1,64 @@
+/*
+  Copyright 2004-2006 University Corporation for Advanced Internet Development, Inc.
+  Copyright 2004-2006 The University Of Chicago
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+package edu.internet2.middleware.grouper;
+import  edu.internet2.middleware.subject.*;
+import  net.sf.hibernate.*;
+
+/**
+ * Stub Hibernate {@link HibernateSubject} DAO.
+ * <p/>
+ * @author  blair christensen.
+ * @version $Id: RegistrySubjectDAO.java,v 1.1 2006-12-19 19:34:08 blair Exp $
+ * @since   1.2.0
+ */
+class RegistrySubjectDAO {
+
+  // PRIVATE CLASS CONSTANTS //
+  private static final String KLASS = RegistrySubjectDAO.class.getName();
+
+
+  // PROTECTED CLASS METHODS //
+
+  // @since   1.2.0
+  protected static HibernateSubject find(String id, String type) 
+    throws  SubjectNotFoundException
+  {
+    try {
+      Session hs  = HibernateHelper.getSession();
+      Query   qry = hs.createQuery(
+        "from HibernateSubject as hs where " 
+        + "     hs.subjectId      = :id    "
+        + " and hs.subjectTypeId  = :type  "
+      );
+      qry.setCacheable(true); // This was set to false but I'm not sure why
+      qry.setString( "id",   id   );
+      qry.setString( "type", type );
+      HibernateSubject subj = (HibernateSubject) qry.uniqueResult();
+      hs.close();
+      if (subj == null) {
+        throw new SubjectNotFoundException("subject not found"); 
+      }
+      return subj;
+    }
+    catch (HibernateException eH) {
+      throw new SubjectNotFoundException( eH.getMessage(), eH );
+    }
+  } // protected static HibernateSubject find(id, type)
+
+} // class RegistrySubjectDAO
+
