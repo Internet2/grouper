3603daf6ef55dc77a610f876b17b5821590eb3aa
Blair Christensen <blair@uchicago.edu>
2006-12-20 19:14:11 +0000

    Extract Hibernate methods from `GrouperSession` to `HibernateGrouperSessionDAO`
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@3559 bc24b658-cf39-4249-b827-e3526eaaeb42

---
+++ grouper/src/grouper/edu/internet2/middleware/grouper/HibernateGrouperSessionDAO.java
@@ -0,0 +1,75 @@
+/*
+  Copyright 2004-2006 University Corporation for Advanced Internet Development, Inc.
+  Copyright 2004-2006 The University Of Chicago
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+package edu.internet2.middleware.grouper;
+import  net.sf.hibernate.*;
+
+/**
+ * Stub Hibernate {@link GrouperSession} DAO.
+ * <p/>
+ * @author  blair christensen.
+ * @version $Id: HibernateGrouperSessionDAO.java,v 1.1 2006-12-20 19:14:11 blair Exp $
+ * @since   1.2.0
+ */
+class HibernateGrouperSessionDAO {
+
+  // PRIVATE CLASS CONSTANTS //
+  private static final String KLASS = HibernateGrouperSessionDAO.class.getName();
+
+
+  // PROTECTED CLASS METHODS //
+
+  // @since   1.2.0
+  protected static GrouperSession create(GrouperSession s)
+    throws  SessionException // TODO 20061220 proper exception?
+  {
+    try {
+      Session     hs  = HibernateHelper.getSession();
+      Transaction tx  = hs.beginTransaction();
+      hs.saveOrUpdate( s.getMember_id() );
+      hs.save(s);
+      tx.commit();
+      hs.close();
+      return s;
+    }
+    catch (HibernateException eH)         {
+      String msg = E.S_START + eH.getMessage();
+      ErrorLog.fatal(HibernateGrouperSessionDAO.class, msg);
+      throw new SessionException(msg, eH);
+    } 
+  } // protected static GrouperSession create(s)
+
+  // @since   1.2.0 
+  protected static void delete(GrouperSession s) 
+    throws  SessionException // TODO 20061220 proper exception
+  {
+    try {
+      Session     hs  = HibernateHelper.getSession();
+      Transaction tx  = hs.beginTransaction();
+      hs.delete(s);
+      tx.commit();
+      hs.close();
+    }
+    catch (HibernateException eH) {
+      String msg = E.S_STOP + eH.getMessage();
+      ErrorLog.error(HibernateGrouperSessionDAO.class, msg);
+      throw new SessionException(msg, eH);
+    }
+  } // protected static void delete(s)
+
+} // class HibernateGrouperSessionDAO
+
