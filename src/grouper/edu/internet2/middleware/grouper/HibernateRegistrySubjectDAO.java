99f19cae0032837e150d43574e1bc60f0d8bf09c
Blair Christensen <blair@uchicago.edu>
2006-12-21 16:24:18 +0000

    Extract Hibernate method from `Membership` to `HibernateMembershipDAO`
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk/grouper@3562 bc24b658-cf39-4249-b827-e3526eaaeb42

--- src/grouper/edu/internet2/middleware/grouper/HibernateRegistrySubjectDAO.java
+++ src/grouper/edu/internet2/middleware/grouper/HibernateRegistrySubjectDAO.java
@@ -23,7 +23,7 @@ import  net.sf.hibernate.*;
  * Stub Hibernate {@link HibernateSubject} DAO.
  * <p/>
  * @author  blair christensen.
- * @version $Id: HibernateRegistrySubjectDAO.java,v 1.2 2006-12-20 18:31:49 blair Exp $
+ * @version $Id: HibernateRegistrySubjectDAO.java,v 1.3 2006-12-21 16:24:18 blair Exp $
  * @since   1.2.0
  */
 class HibernateRegistrySubjectDAO {
@@ -41,13 +41,21 @@ class HibernateRegistrySubjectDAO {
     try {
       Session     hs  = HibernateHelper.getSession();
       Transaction tx  = hs.beginTransaction();
-      hs.save(subj);
-      tx.commit();
-      hs.close();
+      try {
+        hs.save(subj);
+        tx.commit();
+      }
+      catch (HibernateException eH) {
+        tx.rollback();
+        throw new GrouperException( eH.getMessage(), eH );
+      }
+      finally {
+        hs.close();
+      }
       return subj;
     }
     catch (HibernateException eH) {
-      throw new GrouperException(eH.getMessage(), eH);
+      throw new GrouperException( eH.getMessage(), eH );
     }
   } // protected static HibernateSubject add(id, type, name)
 
