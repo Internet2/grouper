f002161937fc1f784ec876a8d232219c42e8450c
Blair Christensen <blair@uchicago.edu>
2006-12-21 16:02:12 +0000

    eliminate Hibernate method from `GrouperSubject`
    this also causes `GrouperSubject` objects to no longer retrieve the group's
    attributes with every method invocation.  that might also make for a slight
    performance improvement.
    
    
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@3561 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper/src/grouper/edu/internet2/middleware/grouper/GrouperSubject.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/GrouperSubject.java
@@ -18,7 +18,11 @@
 package edu.internet2.middleware.grouper;
 import  edu.internet2.middleware.subject.*;
 import  edu.internet2.middleware.subject.provider.*;
-import  java.util.*;
+import  java.util.Iterator;
+import  java.util.HashMap;
+import  java.util.LinkedHashSet;
+import  java.util.Map;
+import  java.util.Set;
 import  net.sf.hibernate.*;
 import  org.apache.commons.lang.builder.*;
 
@@ -26,7 +30,7 @@ import  org.apache.commons.lang.builder.*;
  * {@link Subject} returned by the {@link GrouperSourceAdapter}.
  * <p/>
  * @author  blair christensen.
- * @version $Id: GrouperSubject.java,v 1.26 2006-10-11 14:35:10 blair Exp $
+ * @version $Id: GrouperSubject.java,v 1.27 2006-12-21 16:02:12 blair Exp $
  */
 public class GrouperSubject implements Subject {
 
@@ -36,7 +40,7 @@ public class GrouperSubject implements Subject {
 
   // PRIVATE INSTANCE VARIABLES //
   private GrouperSourceAdapter  adapter = null;
-  private Map                   attrs   = new HashMap(); 
+  private Map                   attrs   = new HashMap();
   private Group                 g       = null;
   private String                id      = null;
   private String                name    = null;
@@ -76,13 +80,11 @@ public class GrouperSubject implements Subject {
   } // public boolean equals(other)
 
   public Map getAttributes() {
-    this._addAttrs();
-    return this.attrs;
+    return this._getAttributes();
   } // public Map getAttributes()
 
   public String getAttributeValue(String name) {
-    this._addAttrs();
-    if (this.attrs.containsKey(name)) {
+    if ( this._getAttributes().containsKey(name) ) {
       return (String) this.attrs.get(name);
     }
     return GrouperConfig.EMPTY_STRING;
@@ -126,24 +128,30 @@ public class GrouperSubject implements Subject {
 
 
   // PRIVATE INSTANCE METHODS //
-  
-  private void _addAttrs() {
-    try {
-      Session hs = HibernateHelper.getSession();
-      hs.refresh(this.g);
-      hs.close();
+ 
+  // @since   1.2.0 
+  private Map _getAttributes() {
+    if ( this.attrs.size() == 0 ) {
+      this.g = HibernateGroupDAO.findByUuid( this.getId() );
+      if (this.g == null) {
+        ErrorLog.error(GrouperSubject.class, "unable to retrieve group attributes");
+      }
+      else {
+        this._populateAttributes(); // populates `this.attrs`
+      }
     }
-    catch (HibernateException eH) {
-      String msg = "unable to refresh group subject (" + this.name + "): " + eH.getMessage();
-      ErrorLog.error(GrouperSubject.class, msg);
-    }
+    return this.attrs;
+  } // private Map _getAttributes()
+
+  // @since   1.2.0
+  private void _populateAttributes() {
     try {
       // Don't bother with any of the create* attrs unless we can find
       // the creating subject
       Subject creator = this.g.getCreateSubject();
-      this.attrs.put("createSubjectId"    , creator.getId()                   );
-      this.attrs.put("createSubjectType"  , creator.getType().getName()       );
-      this.attrs.put("createTime"         , this.g.getCreateTime().toString() ); 
+      this.attrs.put( "createSubjectId",   creator.getId()                   );
+      this.attrs.put( "createSubjectType", creator.getType().getName()       );
+      this.attrs.put( "createTime",        this.g.getCreateTime().toString() ); 
     }
     catch (SubjectNotFoundException eSNF0) {
       ErrorLog.error(GrouperSubject.class, E.GSUBJ_NOCREATOR + eSNF0.getMessage());
@@ -152,24 +160,21 @@ public class GrouperSubject implements Subject {
       // Don't bother with any of the modify* attrs unless we can find
       // the modifying subject
       Subject modifier = this.g.getModifySubject();
-      this.attrs.put("modifySubjectId"    , modifier.getId()                  );
-      this.attrs.put("modifySubjectType"  , modifier.getType().getName()      );
-      this.attrs.put("modifyTime"         , this.g.getModifyTime().toString() ); 
+      this.attrs.put( "modifySubjectId",   modifier.getId()                  );
+      this.attrs.put( "modifySubjectType", modifier.getType().getName()      );
+      this.attrs.put( "modifyTime",        this.g.getModifyTime().toString() ); 
     }
     catch (SubjectNotFoundException eSNF1) {
       // No modifier
     }
-    Map       attrs = this.g.getAttributes();
     Map.Entry e;
-    Iterator  iter  = attrs.entrySet().iterator();
-    while (iter.hasNext()) {
-      e = (Map.Entry) iter.next();
-      this.attrs.put(  (String) e.getKey(), (String) e.getValue() );
+    Iterator  it  = this.g.internal_getAttributes().entrySet().iterator();
+    while (it.hasNext()) {
+      e = (Map.Entry) it.next();
+      this.attrs.put( (String) e.getKey(), ( (Attribute) e.getValue() ).getValue() );
     }
-    DebugLog.info(
-      GrouperSubject.class, 
-      "[" + this.name + "] attached attributes: " + this.attrs.size()
-    );
-  } // private void _addAttrs(g)
-}
+    DebugLog.info( GrouperSubject.class, "[" + this.name + "] attached attributes: " + this.attrs.size() );
+  } // private void _populateAttributes()
+
+} // public class GrouperSubject implements Subject
 
