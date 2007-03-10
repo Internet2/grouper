3e5b5aac32960fbd7adafd19a943bd6752a3e885
khuxtable <khuxtable@mycompany.com>
2007-03-10 00:41:06 +0000

    Added code to escape LDAP filter characters, thus preventing LDAP injection.
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@3703 bc24b658-cf39-4249-b827-e3526eaaeb42

--- subject/src/edu/internet2/middleware/subject/provider/JNDISourceAdapter.java
+++ subject/src/edu/internet2/middleware/subject/provider/JNDISourceAdapter.java
@@ -1,6 +1,6 @@
 /*--
-$Id: JNDISourceAdapter.java,v 1.5 2006-06-27 22:33:51 blair Exp $
-$Date: 2006-06-27 22:33:51 $
+$Id: JNDISourceAdapter.java,v 1.6 2007-03-10 00:41:06 khuxtable Exp $
+$Date: 2007-03-10 00:41:06 $
 
 Copyright 2005 Internet2 and Stanford University.  All Rights Reserved.
 See doc/license.txt in this distribution.
@@ -188,188 +188,206 @@ public class JNDISourceAdapter
         }
         return new JNDISubject(subjectID,  name, description, this.getSubjectType(), this);
     }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public void init() throws SourceUnavailableException {
+        try {
+            Properties props = getInitParams();
+            setupEnvironment(props);
+        } catch (Exception ex) {
+            throw new SourceUnavailableException(
+                    "Unable to init JNDI source", ex);
+        }
+    }
+    
+    
+    /**
+     * Setup environment.
+     * @throws SourceUnavailableException
+     */
+    protected void setupEnvironment(Properties props)
+    throws SourceUnavailableException {
         
-        /**
-         * {@inheritDoc}
-         */
-        public void init() throws SourceUnavailableException {
-            try {
-                Properties props = getInitParams();
-                setupEnvironment(props);
-            } catch (Exception ex) {
-                throw new SourceUnavailableException(
-                        "Unable to init JNDI source", ex);
+        environment.put(Context.INITIAL_CONTEXT_FACTORY,  props.getProperty("INITIAL_CONTEXT_FACTORY") );
+        environment.put(Context.PROVIDER_URL, props.getProperty("PROVIDER_URL") );
+        environment.put(Context.SECURITY_AUTHENTICATION, props.getProperty("SECURITY_AUTHENTICATION"));
+        environment.put(Context.SECURITY_PRINCIPAL, props.getProperty("SECURITY_PRINCIPAL") );
+        environment.put(Context.SECURITY_CREDENTIALS,props.getProperty("SECURITY_CREDENTIALS"));
+        if (props.getProperty("SECURITY_PROTOCOL")!= null) {
+            environment.put(Context.SECURITY_PROTOCOL, "ssl");
+        }
+        Context context = null;
+        try {
+            log.debug("Creating Directory Context");
+            context = new InitialDirContext(environment);
+        } catch (AuthenticationException ex) {
+            log.error("Error with Authentication " + ex.getMessage(), ex);
+            throw new SourceUnavailableException("Error with Authentication ", ex);
+        } catch (NamingException ex) {
+            log.error("Naming Error " + ex.getMessage(), ex);
+            throw new SourceUnavailableException("Naming Error", ex);
+        } finally {
+            if (context != null) {
+                try {
+                    context.close();
+                } catch (NamingException ne) {
+                    // squelch, since it is already closed
+                }
             }
         }
+        log.info("Success in connecting to LDAP");
         
-        
-        /**
-         * Setup environment.
-         * @throws SourceUnavailableException
-         */
-        protected void setupEnvironment(Properties props)
-        throws SourceUnavailableException {
-            
-            environment.put(Context.INITIAL_CONTEXT_FACTORY,  props.getProperty("INITIAL_CONTEXT_FACTORY") );
-            environment.put(Context.PROVIDER_URL, props.getProperty("PROVIDER_URL") );
-            environment.put(Context.SECURITY_AUTHENTICATION, props.getProperty("SECURITY_AUTHENTICATION"));
-            environment.put(Context.SECURITY_PRINCIPAL, props.getProperty("SECURITY_PRINCIPAL") );
-            environment.put(Context.SECURITY_CREDENTIALS,props.getProperty("SECURITY_CREDENTIALS"));
-            if (props.getProperty("SECURITY_PROTOCOL")!= null) {
-                environment.put(Context.SECURITY_PROTOCOL, "ssl");
-            }
-            Context context = null;
-            try {
-                log.debug("Creating Directory Context");
-                context = new InitialDirContext(environment);
-            } catch (AuthenticationException ex) {
-                log.error("Error with Authentication " + ex.getMessage(), ex);
-                throw new SourceUnavailableException("Error with Authentication ", ex);
-            } catch (NamingException ex) {
-                log.error("Naming Error " + ex.getMessage(), ex);
-                throw new SourceUnavailableException("Naming Error", ex);
-            } finally {
-                if (context != null) {
-                    try {
-                        context.close();
-                    } catch (NamingException ne) {
-                        // squelch, since it is already closed
-                    }
-                }
-            }
-            log.info("Success in connecting to LDAP");
-            
-            nameAttributeName = props.getProperty("Name_AttributeType");
-            if (nameAttributeName==null) {
-                log.error("Name_AttributeType not defined");
-            }
-            subjectIDAttributeName = props.getProperty("SubjectID_AttributeType");
-            if (subjectIDAttributeName==null) {
-                log.error("SubjectID_AttributeType not defined");
-            }
-            descriptionAttributeName = props.getProperty("Description_AttributeType");
-            if (descriptionAttributeName==null) {
-                log.error("Description_AttributeType not defined");
-            }
-            
+        nameAttributeName = props.getProperty("Name_AttributeType");
+        if (nameAttributeName==null) {
+            log.error("Name_AttributeType not defined");
         }
-        
-        
-        /**
-         * Loads attributes for the argument subject.
-         */
-        protected Map loadAttributes(JNDISubject subject) {
-            Map attributes = new HashMap();
-            Search search = getSearch("searchSubject");
-            if (search == null) {
-                log.error("searchType: \"search\" not defined.");
-                return attributes;
-            }
-            //setting attributeNames to null will cause all attributes for a subject to be returned
-            String[] attributeNames = null;
-            Set attributeNameSet = this.getAttributes();
-            if (attributeNameSet.size()==0) {
-                attributeNames = null;
-            } else {
-                attributeNames = new String[attributeNameSet.size()];
-                int i = 0;
-                for (Iterator it = attributeNameSet.iterator(); it.hasNext(); attributeNames[i++]= (String) it.next());
-            }
-            try {
-                Attributes ldapAttributes = getLdapUnique(search,subject.getId(), attributeNames);
-                for (NamingEnumeration e = ldapAttributes.getAll(); e.hasMore();) {
-                    Attribute attr = (Attribute) e.next();
-                    String name = attr.getID();
-                    Set values = new HashSet();
-                    for (NamingEnumeration en = attr.getAll(); en.hasMore(); ) {
-                        String value = (String) en.next();
-                        values.add(value);
-                    }
-                    attributes.put(name, values);
-                }
-                subject.setAttributes(attributes);
-            } catch (SubjectNotFoundException ex ) {
-                log.error("SubjectNotFound: "+ subject.getId() +" " + ex.getMessage(), ex);
-            } catch (SubjectNotUniqueException ex ) {
-                log.error("SubjectNotUnique: "+ subject.getId() +" " + ex.getMessage(), ex);
-            } catch (NamingException ex ) {
-                log.error("LDAP Naming Except: " + ex.getMessage(), ex);
-            }
-            return attributes;
+        subjectIDAttributeName = props.getProperty("SubjectID_AttributeType");
+        if (subjectIDAttributeName==null) {
+            log.error("SubjectID_AttributeType not defined");
         }
-        
-        protected NamingEnumeration getLdapResults(Search search, String searchValue, String[] attributeNames) {
-            DirContext context = null;
-            NamingEnumeration results = null;
-            String filter = search.getParam("filter");
-            if (filter==null) {
-                log.error("Search filter not found for search type:  " + search.getSearchType());
-                return results;
-            }
-            filter = filter.replaceAll("%TERM%", searchValue);
-            String base = search.getParam("base");
-            if (base==null){
-                base = "";
-                log.error("Search base not found for:  " + search.getSearchType()+  ". Using base \"\" ");
-                
-            }
-            int scopeNum = -1;
-            String scope = search.getParam("scope");
-            if (scope!=null) {
-                scopeNum = getScope(scope);
-            }
-            if (scopeNum == -1) {
-                scopeNum = SearchControls.SUBTREE_SCOPE;
-                log.error("Search scope not found for: " + search.getSearchType() + ". Using scope SUBTREE_SCOPE." );
-            }
-            log.debug("searchType: " + search.getSearchType() + " filter: " + filter + " base: " + base + " scope: "+ scope);
-            try  {
-                context = new InitialDirContext(environment);
-                SearchControls constraints = new SearchControls( );
-                constraints.setSearchScope( scopeNum );
-                constraints.setReturningAttributes(attributeNames);
-                results = context.search( base, filter, constraints );
-            } catch ( AuthenticationException ex ) {
-                log.error("Ldap Authentication Exception: " + ex.getMessage(), ex);
-            } catch (NamingException ex) {
-                log.error("Ldap NamingException: " + ex.getMessage(), ex);
-                
-            } finally {
-                if (context != null) {
-                    try {
-                        context.close();
-                    } catch (NamingException ne) {
-                        // squelch, since it is already closed
-                    }
-                }
-            }
-            return results;
-            
-        }
-        
-        
-        protected Attributes getLdapUnique( Search search, String searchValue, String[] attributeNames) throws SubjectNotFoundException,SubjectNotUniqueException  {
-            Attributes attributes = null;
-            NamingEnumeration results = getLdapResults(search, searchValue, attributeNames);
-            
-            try  {
-                if (results == null || !results.hasMore()) {
-                    String errMsg = "No results: " + search.getSearchType() + " filter:" + search.getParam("filter")
-                    + " searchValue: " + searchValue;
-                    throw new SubjectNotFoundException( errMsg);
-                }
-                
-                SearchResult si = ( SearchResult )results.next( );
-                attributes = si.getAttributes();
-                if ( results.hasMore()) {
-                    si = (SearchResult) results.next();
-                    String errMsg ="Search is not unique:" + si.getName() + "\n";
-                    throw new SubjectNotUniqueException( errMsg );
-                }
-            } catch (NamingException ex) {
-                log.error("Ldap NamingException: " + ex.getMessage(), ex);
-            }
-            return attributes;
+        descriptionAttributeName = props.getProperty("Description_AttributeType");
+        if (descriptionAttributeName==null) {
+            log.error("Description_AttributeType not defined");
         }
         
     }
+    
+    
+    /**
+     * Loads attributes for the argument subject.
+     */
+    protected Map loadAttributes(JNDISubject subject) {
+        Map attributes = new HashMap();
+        Search search = getSearch("searchSubject");
+        if (search == null) {
+            log.error("searchType: \"search\" not defined.");
+            return attributes;
+        }
+        //setting attributeNames to null will cause all attributes for a subject to be returned
+        String[] attributeNames = null;
+        Set attributeNameSet = this.getAttributes();
+        if (attributeNameSet.size()==0) {
+            attributeNames = null;
+        } else {
+            attributeNames = new String[attributeNameSet.size()];
+            int i = 0;
+            for (Iterator it = attributeNameSet.iterator(); it.hasNext(); attributeNames[i++]= (String) it.next());
+        }
+        try {
+            Attributes ldapAttributes = getLdapUnique(search,subject.getId(), attributeNames);
+            for (NamingEnumeration e = ldapAttributes.getAll(); e.hasMore();) {
+                Attribute attr = (Attribute) e.next();
+                String name = attr.getID();
+                Set values = new HashSet();
+                for (NamingEnumeration en = attr.getAll(); en.hasMore(); ) {
+                    String value = (String) en.next();
+                    values.add(value);
+                }
+                attributes.put(name, values);
+            }
+            subject.setAttributes(attributes);
+        } catch (SubjectNotFoundException ex ) {
+            log.error("SubjectNotFound: "+ subject.getId() +" " + ex.getMessage(), ex);
+        } catch (SubjectNotUniqueException ex ) {
+            log.error("SubjectNotUnique: "+ subject.getId() +" " + ex.getMessage(), ex);
+        } catch (NamingException ex ) {
+            log.error("LDAP Naming Except: " + ex.getMessage(), ex);
+        }
+        return attributes;
+    }
+    
+    protected NamingEnumeration getLdapResults(Search search, String searchValue, String[] attributeNames) {
+        DirContext context = null;
+        NamingEnumeration results = null;
+        String filter = search.getParam("filter");
+        if (filter==null) {
+            log.error("Search filter not found for search type:  " + search.getSearchType());
+            return results;
+        }
+        filter = filter.replaceAll("%TERM%", escapeSearchFilter(searchValue));
+        String base = search.getParam("base");
+        if (base==null){
+            base = "";
+            log.error("Search base not found for:  " + search.getSearchType()+  ". Using base \"\" ");
+            
+        }
+        int scopeNum = -1;
+        String scope = search.getParam("scope");
+        if (scope!=null) {
+            scopeNum = getScope(scope);
+        }
+        if (scopeNum == -1) {
+            scopeNum = SearchControls.SUBTREE_SCOPE;
+            log.error("Search scope not found for: " + search.getSearchType() + ". Using scope SUBTREE_SCOPE." );
+        }
+        log.debug("searchType: " + search.getSearchType() + " filter: " + filter + " base: " + base + " scope: "+ scope);
+        try  {
+            context = new InitialDirContext(environment);
+            SearchControls constraints = new SearchControls( );
+            constraints.setSearchScope( scopeNum );
+            constraints.setReturningAttributes(attributeNames);
+            results = context.search( base, filter, constraints );
+        } catch ( AuthenticationException ex ) {
+            log.error("Ldap Authentication Exception: " + ex.getMessage(), ex);
+        } catch (NamingException ex) {
+            log.error("Ldap NamingException: " + ex.getMessage(), ex);
+            
+        } finally {
+            if (context != null) {
+                try {
+                    context.close();
+                } catch (NamingException ne) {
+                    // squelch, since it is already closed
+                }
+            }
+        }
+        return results;
+        
+    }
+    
+    
+    protected Attributes getLdapUnique( Search search, String searchValue, String[] attributeNames) throws SubjectNotFoundException,SubjectNotUniqueException  {
+        Attributes attributes = null;
+        NamingEnumeration results = getLdapResults(search, searchValue, attributeNames);
+        
+        try  {
+            if (results == null || !results.hasMore()) {
+                String errMsg = "No results: " + search.getSearchType() + " filter:" + search.getParam("filter")
+                + " searchValue: " + searchValue;
+                throw new SubjectNotFoundException( errMsg);
+            }
+            
+            SearchResult si = ( SearchResult )results.next( );
+            attributes = si.getAttributes();
+            if ( results.hasMore()) {
+                si = (SearchResult) results.next();
+                String errMsg ="Search is not unique:" + si.getName() + "\n";
+                throw new SubjectNotUniqueException( errMsg );
+            }
+        } catch (NamingException ex) {
+            log.error("Ldap NamingException: " + ex.getMessage(), ex);
+        }
+        return attributes;
+    }
+    
+    /**
+     * Escape a search filter to prevent LDAP injection.
+     * From http://www.owasp.org/index.php/Preventing_LDAP_Injection_in_Java
+     * 
+     * @param filter
+     * @return escaped filter
+     */
+    protected String escapeSearchFilter(String filter) {
+        //From RFC 2254
+        String escapedStr = new String(filter);
+        escapedStr = escapedStr.replaceAll("\\\\","\\\\5c");
+        // We want people to be able to use wildcards.
+        // escapedStr = escapedStr.replaceAll("\\*","\\\\2a");
+        escapedStr = escapedStr.replaceAll("\\(","\\\\28");
+        escapedStr = escapedStr.replaceAll("\\)","\\\\29");
+        escapedStr = escapedStr.replaceAll("\\"+Character.toString('\u0000'), "\\\\00");
+        return escapedStr;
+    }
+}
