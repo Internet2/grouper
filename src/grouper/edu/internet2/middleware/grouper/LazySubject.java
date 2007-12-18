0056df127353d531ca7adb22bff8a93ef6b9f1f5
isgwb <isgwb@mycompany.com>
2007-12-18 10:33:09 +0000

    See https://bugs.internet2.edu/jira/browse/GRP-78. Lazily instantiate subjects so that we only incur the minimum cost in the UI which pages the results
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk/grouper@4109 bc24b658-cf39-4249-b827-e3526eaaeb42

---
+++ src/grouper/edu/internet2/middleware/grouper/LazySubject.java
@@ -0,0 +1,165 @@
+/*
+  Copyright (C) 2004-2007 University Corporation for Advanced Internet Development, Inc.
+  Copyright (C) 2004-2007 The University Of Chicago
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+package edu.internet2.middleware.grouper;
+import  edu.internet2.middleware.subject.*;
+import  java.util.Map;
+import  java.util.Set;
+
+
+/** 
+ * {@link Subject} from a {@link Membership} - getMember().getSubject()
+ * only called if necessary i.e. the UI pages results and so it is often not
+ * necessary to instantiate all the Subjects (and Members) 
+ * <p/>
+ * @author  Gary Brown.
+ * @version $Id: LazySubject.java,v 1.1 2007-12-18 10:33:09 isgwb Exp $
+ */
+
+public class LazySubject implements Subject {
+
+ private Membership membership;
+ private Subject subject;
+
+
+  // CONSTRUCTORS //
+  protected LazySubject(Membership ms) 
+  {
+    this.membership = ms;
+  } // protected LazySubject(ms)
+
+  
+  
+  /* (non-Javadoc)
+ * @see edu.internet2.middleware.subject.Subject#getAttributes()
+ */
+public Map getAttributes() {
+	try {
+		return getSubject().getAttributes();
+	}catch(Exception e) {
+		throw new GrouperRuntimeException(e);
+	}
+}
+
+
+	
+	/* (non-Javadoc)
+	 * @see edu.internet2.middleware.subject.Subject#getAttributeValue(java.lang.String)
+	 */
+	public String getAttributeValue(String name) {
+		try {
+			return getSubject().getAttributeValue(name);
+		}catch(Exception e) {
+			throw new GrouperRuntimeException(e);
+		}
+	}
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see edu.internet2.middleware.subject.Subject#getAttributeValues(java.lang.String)
+	 */
+	public Set getAttributeValues(String name) {
+		try {
+			return getSubject().getAttributeValues(name);
+		}catch(Exception e) {
+			throw new GrouperRuntimeException(e);
+		}
+	}
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see edu.internet2.middleware.subject.Subject#getDescription()
+	 */
+	public String getDescription() {
+		try {
+			return getSubject().getDescription();
+		}catch(Exception e) {
+			throw new GrouperRuntimeException(e);
+		}
+	}
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see edu.internet2.middleware.subject.Subject#getId()
+	 */
+	public String getId() {
+		try {
+			return membership.getMember().getSubjectId();
+		}catch(Exception e) {
+			throw new GrouperRuntimeException(e);
+		}
+	}
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see edu.internet2.middleware.subject.Subject#getName()
+	 */
+	public String getName() {
+		try {
+			return getSubject().getName();
+		}catch(Exception e) {
+			throw new GrouperRuntimeException(e);
+		}
+	}
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see edu.internet2.middleware.subject.Subject#getSource()
+	 */
+	public Source getSource() {
+		try {
+			return membership.getMember().getSubjectSource();
+		}catch(Exception e) {
+			throw new GrouperRuntimeException(e);
+		}
+	}
+	
+	/* (non-Javadoc)
+	 * @see edu.internet2.middleware.subject.Subject#getType()
+	 */
+	public SubjectType getType() {
+		try {
+			return membership.getMember().getSubjectType();
+		}catch(Exception e) {
+			throw new GrouperRuntimeException(e);
+		}
+	}
+	
+	private Subject getSubject() throws SubjectNotFoundException,MemberNotFoundException{
+		  if(subject==null) subject=membership.getMember().getSubject();
+		  return subject;
+	 }
+	
+	public boolean equals(Object other) {
+		if(other instanceof LazySubject) {
+			return membership.getMemberUuid().equals(((LazySubject)other).getMembership().getMemberUuid());
+		}
+	    return SubjectHelper.eq(this, other);
+	  } // public boolean equals(other)
+	
+	protected Membership getMembership() {
+		return membership;
+	}
+  
+
+} // public class LazySubject implements Subject
+
