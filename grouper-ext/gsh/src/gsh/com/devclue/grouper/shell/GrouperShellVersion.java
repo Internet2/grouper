0f73328148213b1125519309feeaf7f526463d08
Tom Zeller <tzeller@dragonacea.biz>
2008-04-27 14:57:17 +0000

    'ant compile.class.version' will make eclipse compile happily
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@4510 bc24b658-cf39-4249-b827-e3526eaaeb42

---
+++ grouper-ext/gsh/src/gsh/com/devclue/grouper/shell/GrouperShellVersion.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2007 blair christensen.
+ * All Rights Reserved.
+ *
+ * You may use and distribute under the same terms as Grouper itself.
+ */
+
+package com.devclue.grouper.shell;
+
+/**
+ * gsh version.
+ * <p>
+ * <i>GrouperShellVersion.java</i> is generated dynamically from <i>GrouperShellVersion.java.in</i>.
+ * </p>
+ * @author  blair christensen.
+ * @version $Id: GrouperShellVersion.java,v 1.1 2008-04-27 14:57:17 tzeller Exp $
+ * @since   0.1.1
+ */
+class GrouperShellVersion {
+
+  // PROTECTED CLASS CONSTANTS //
+  protected static final String VERSION = "0.1.1";
+
+} // class GrouperShellVersion
+
