a36dc553692527d3c6e7096b6556c3305a30af28
isgwb <isgwb@mycompany.com>
2008-04-29 12:33:16 +0000

    Use GROUPER_EXT_BIN as a filter token + use it and GROUPER_EXT_LIB as paths rather than GSH_HOME
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@4515 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper-ext/gsh/src/bin/gsh.bat
+++ grouper-ext/gsh/src/bin/gsh.bat
@@ -1,41 +1,1 @@
-rem Author: Gary Brown
-rem $Id: gsh.bat,v 1.1.1.1 2008-04-27 14:52:17 tzeller Exp $
-
-@echo off
-rem if "%OS%" == "Windows_NT" setlocal
-if not "%1" == "acp" goto execute
-
-set GSH_CP=%GSH_CP%;%2
-
-goto end
-
-:execute
-rem POPULATED AT BUILD
-set GROUPER_HOME=@GROUPER_HOME@
-set GSH_HOME=@GSH_HOME@
-set MEM_START=@GSH_MEM_START@
-set MEM_MAX=@GSH_MEM_MAX@
-rem POPULATED AT BUILD
-
-rem The user's CLASSPATH
-set GSH_CP=%CLASSPATH%
-rem Append Grouper .jar
-
-rem Append Grouper's build classes
-set GSH_CP=%GSH_CP%;%GROUPER_HOME%/build/grouper
-rem Append Grouper's configuration
-set GSH_CP=%GSH_CP%;%GROUPER_HOME%/conf
-
-
-rem Append Grouper's 3rd party libs
-for %%f in (%GROUPER_HOME%\lib\*.jar) do call %GSH_HOME%\bin\gsh acp %%f
-rem Append gsh .jar
-
-rem Append gsh's build classes
-set GSH_CP=%GSH_CP%;%GSH_HOME%/build/gsh
-rem Append gsh's 3rd party libs
-for %%f in (%GSH_HOME%\lib\*.jar) do call %GSH_HOME%\bin\gsh acp %%f
-
-java -classpath %GSH_CP% -Xms%MEM_START% -Xmx%MEM_MAX% %GSH_JVMARGS% com.devclue.grouper.shell.GrouperShell %*
-set GSH_CP=
-:end
+rem Author: Gary Brownrem $Id: gsh.bat,v 1.2 2008-04-29 12:33:16 isgwb Exp $@echo offrem if "%OS%" == "Windows_NT" setlocalif not "%1" == "acp" goto executeset GSH_CP=%GSH_CP%;%2goto end:executerem POPULATED AT BUILDset GROUPER_HOME=@GROUPER_HOME@set GSH_HOME=@GSH_HOME@set GROUPER_EXT_LIB=@GROUPER_EXT_LIB@set GROUPER_EXT_BIN=@GROUPER_EXT_BIN@set MEM_START=@GSH_MEM_START@set MEM_MAX=@GSH_MEM_MAX@rem POPULATED AT BUILDrem The user's CLASSPATHset GSH_CP=%CLASSPATH%rem Append Grouper .jarrem Append Grouper's build classesset GSH_CP=%GSH_CP%;%GROUPER_HOME%/build/grouperrem Append Grouper's configurationset GSH_CP=%GSH_CP%;%GROUPER_HOME%/confrem Append Grouper's 3rd party libsfor %%f in (%GROUPER_HOME%\lib\*.jar) do call %GROUPER_EXT_BIN%\gsh acp %%frem Append gsh .jarrem Append gsh's build classesset GSH_CP=%GSH_CP%;%GSH_HOME%/build/gshrem Append gsh's 3rd party libsfor %%f in (%GROUPER_EXT_LIB%\*.jar) do call %GROUPER_EXT_BIN%\gsh acp %%fjava -classpath %GSH_CP% -Xms%MEM_START% -Xmx%MEM_MAX% %GSH_JVMARGS% com.devclue.grouper.shell.GrouperShell %*set GSH_CP=:end
