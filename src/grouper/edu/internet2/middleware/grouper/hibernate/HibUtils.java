bb03772dc5400706e985d00e149999782df91916
Chris Hyzer <mchyzer@isc.upenn.edu>
2008-05-06 18:37:45 +0000

    fix foreign key issue, add dummy source adapter
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk/grouper@4546 bc24b658-cf39-4249-b827-e3526eaaeb42

--- src/grouper/edu/internet2/middleware/grouper/hibernate/HibUtils.java
+++ src/grouper/edu/internet2/middleware/grouper/hibernate/HibUtils.java
@@ -4,15 +4,26 @@
 package edu.internet2.middleware.grouper.hibernate;
 
 import java.lang.reflect.Array;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
+import org.hibernate.Hibernate;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.criterion.Criterion;
 import org.hibernate.criterion.Junction;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.type.Type;
 
+import edu.internet2.middleware.grouper.internal.dao.GrouperDAOException;
 import edu.internet2.middleware.grouper.util.GrouperUtil;
 
 /**
@@ -208,4 +219,267 @@ public class HibUtils {
     
   }
   
+  /**
+   * execute some sql
+   * @param sql can be insert, update, delete, or ddl
+   * @param params prepared statement params
+   * @return the number of rows affected or 0 for ddl
+   */
+  @SuppressWarnings("deprecation")
+  public static int executeSql(final String sql, final List<Object> params) {
+
+    int result = (Integer)HibernateSession.callbackHibernateSession(GrouperTransactionType.READ_WRITE_OR_USE_EXISTING, new HibernateHandler() {
+
+      public Object callback(HibernateSession hibernateSession)
+          throws GrouperDAOException {
+        
+        //we dont close this connection or anything since could be pooled
+        Connection connection = hibernateSession.getSession().connection();
+        PreparedStatement preparedStatement = null;
+
+        try {
+          preparedStatement = connection.prepareStatement(sql);
+  
+          attachParams(preparedStatement, params);
+          
+          int result = preparedStatement.executeUpdate();
+          
+          hibernateSession.commit(GrouperCommitType.COMMIT_IF_NEW_TRANSACTION);
+          return result;
+        } catch (Exception e) {
+          throw new  RuntimeException ("Problem with sql: " + sql, e);
+        } finally {
+          closeQuietly(preparedStatement);
+        }
+      }
+      
+    });
+    return result;
+     
+  }
+
+  /**
+   * close a connection null safe and dont throw exception
+   * @param connection
+   */
+  public static void closeQuietly(Connection connection) {
+    if (connection != null) {
+      try {
+        connection.close();
+      } catch (Exception e) {
+        //ignore
+      }
+    }
+  }
+
+  /**
+   * close a resultSet null safe and dont throw exception
+   * @param resultSet
+   */
+  public static void closeQuietly(ResultSet resultSet) {
+    if (resultSet != null) {
+      try {
+        resultSet.close();
+      } catch (Exception e) {
+        //ignore
+      }
+    }
+  }
+
+  /**
+   * close a session null safe and dont throw exception
+   * @param session
+   */
+  public static void closeQuietly(Session session) {
+    if (session != null) {
+      try {
+        session.close();
+      } catch (Exception e) {
+        //ignore
+      }
+    }
+  }
+
+  /**
+   * close a statement null safe and dont throw exception
+   * @param statement
+   */
+  public static void closeQuietly(Statement statement) {
+    if (statement != null) {
+      try {
+        statement.close();
+      } catch (Exception e) {
+        //ignore
+      }
+    }
+  }
+
+  /**
+   * rollback a connection quietly
+   * @param connection
+   */
+  public static void rollbackQuietly(Connection connection) {
+    if (connection != null) {
+      try {
+        connection.rollback();
+      } catch (Exception e) {
+        //ignore
+      }
+    }
+  }
+
+  /**
+   * rollback a transaction quietly
+   * @param transaction
+   */
+  public static void rollbackQuietly(Transaction transaction) {
+    if (transaction != null && transaction.isActive()) {
+      try {
+        transaction.rollback();
+      } catch (Exception e) {
+        //ignore
+      }
+    }
+  }
+
+  /**
+   * Attach params for a prepared statement.  The type of the params and types must be the
+   * same (e.g. either both array or list, but not one is Array, and the other list
+   * @param statement
+   * @param params either null, Object, Object[], or List of Objects
+   * @throws HibernateException
+   * @throws SQLException
+   */
+  @SuppressWarnings("unchecked")
+  public static void attachParams(PreparedStatement statement, Object params)
+      throws HibernateException, SQLException {
+    if (GrouperUtil.length(params) == 0) {
+      return;
+    }
+    List<Object> paramList = listObject(params);
+    List<Type> typeList = hibernateTypes(paramList);
+    attachParams(statement, paramList, typeList);
+  }
+
+  /**
+   * Attach params for a prepared statement.  The type of the params and types must be the
+   * same (e.g. either both array or list, but not one is Array, and the other list
+   * @param statement
+   * @param params either null, Object, Object[], or List of Objects
+   * @param types either null, Type, Type[], or List of Objects
+   * @throws HibernateException
+   * @throws SQLException
+   */
+  static void attachParams(PreparedStatement statement, Object params, Object types)
+      throws HibernateException, SQLException {
+    int paramLength = GrouperUtil.length(params);
+    int typeLength = GrouperUtil.length(types);
+    
+    //nothing to do if nothing to do
+    if (paramLength == 0 && typeLength == 0) {
+      return;
+    }
+  
+      if (paramLength != typeLength) {
+      throw new RuntimeException("The params length must equal the types length and params " +
+      "and types must either both or neither be null");
+      }
+    
+      List paramList = listObject(params);
+      List typeList = listObject(types);
+  
+      //loop through, set the params
+      Type currentType = null;
+      for (int i = 0; i < paramLength; i++) {
+        //not sure why the session implementer is null, if this ever fails for a type, 
+        //might want to not use hibernate and brute force it
+        currentType = (Type) typeList.get(i);
+        currentType.nullSafeSet(statement, paramList.get(i), i + 1, null);
+      }
+  
+  }
+
+  /**
+   * convert an object to a list of objects
+   * @param object
+   * @return the list of objects
+   */
+  @SuppressWarnings("unchecked")
+  public static List<Object> listObject(Object object) {
+    
+    //if its already a list then we are all good
+    if (object instanceof List) {
+      return (List<Object>)object; 
+    }
+    
+    return GrouperUtil.toList(object);
+  
+  }
+
+  /**
+   * Returns a Hibernate Type for the given java type. Handles both primitives and Objects.
+   * Will throw an exception if the given object is null or if a type cannot be found for it.
+   * @param o is the object to find the Type for.
+   * @return the Type.
+   */
+  public static Type hibernateType(Object o) {
+    if (o == null) {
+      //its possible to bind null (e.g. for an update), so just use object to do this
+      return Hibernate.OBJECT;
+    }
+    Class clazz = o.getClass();
+  
+    if (clazz == int.class || o instanceof Integer) {
+      return Hibernate.INTEGER;
+    } else if (clazz == double.class || clazz == Double.class) {
+      return Hibernate.DOUBLE;
+    } else if (clazz == long.class || clazz == Long.class) {
+      return Hibernate.LONG;
+    } else if (clazz == float.class || clazz == Float.class) {
+      return Hibernate.FLOAT;
+    } else if (clazz == byte.class || clazz == Byte.class) {
+      return Hibernate.BYTE;
+    } else if (clazz == boolean.class || clazz == Boolean.class) {
+      return Hibernate.TRUE_FALSE;
+    } else if (clazz == char.class || clazz == Character.class) {
+      return Hibernate.CHARACTER;
+    } else if (clazz == short.class || clazz == Short.class) {
+      return Hibernate.SHORT;
+    } else if (clazz == java.util.Date.class || clazz == java.sql.Date.class) {
+      //return Hibernate.TIMESTAMP;
+      return Hibernate.DATE;
+    } else if (clazz == Timestamp.class) {
+      return Hibernate.TIMESTAMP;
+    } else if (clazz == String.class) {
+      return Hibernate.STRING;
+    }
+    throw new RuntimeException(
+        "Cannot find a hibernate type to associate with java type " + clazz);
+  }
+
+  /**
+   * Returns a list of Hibernate types corresponding to the given params.
+   * @param params are the objects to get the types for. Can be list, Object, or array.
+   * @return the corresponding types.
+   */
+  public static List<Type> hibernateTypes(List<Object> params) {
+  
+    int length = GrouperUtil.length(params);
+  
+    //if null, make sure the same (or exception later)
+    if (length == 0) {
+      return null;
+    }
+  
+    // Get the types.
+    // Create a list of hibernate types.
+    List<Type> types = new ArrayList<Type>();
+  
+    for (int i = 0; i < length; i++) {
+      Object o = params.get(i);
+      types.add(hibernateType(o));
+    }
+    return types;
+  }
+
 }
