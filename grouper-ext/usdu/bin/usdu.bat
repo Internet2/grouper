57e920920cb0a354a1fc39d5425af395dd3d5bf7
Tom Zeller <tzeller@dragonacea.biz>
2008-05-09 03:04:13 +0000

    use invoker.jar instead of commons-launcher
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@4562 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper-ext/usdu/bin/usdu.bat
+++ grouper-ext/usdu/bin/usdu.bat
@@ -1,5 +1,36 @@
+%rem Author: Gary Brown
+
+rem $Id: usdu.bat,v 1.2 2008-05-09 03:00:21 tzeller Exp $
+
+
 @echo off
 
-# Run Grouper's Unresolvable Subject Deletion Utility
+rem if "%OS%" == "Windows_NT" setlocal
 
-java -cp @GROUPER_EXT_BIN@ LauncherBootstrap -executablename usdu -launchfile usdu.launcher.xml usdu "$@"
+if not "%1" == "acp" goto execute
+
+goto end
+
+:execute
+
+
+rem POPULATED AT BUILD
+
+set GROUPER_HOME=@GROUPER_HOME@
+
+set GSH_HOME=@GSH_HOME@
+
+set GROUPER_EXT_LIB=@GROUPER_EXT_LIB@
+
+set GROUPER_EXT_BIN=@GROUPER_EXT_BIN@
+
+set MEM_START=@GSH_MEM_START@
+
+set MEM_MAX=@GSH_MEM_MAX@
+
+rem POPULATED AT BUILD
+
+
+java -Xms%MEM_START% -Xmx%MEM_MAX% -jar %GROUPER_HOME%/lib/invoker.jar -cpdir %GROUPER_CONF% -cpalljars %GROUPER_HOME%/lib -cpalljars %GROUPER_EXT_LIB% -cpjar %GROUPER_HOME%/dist/lib/grouper.jar %GSH_JVMARGS% com.devclue.grouper.shell.GrouperShell %*
+
+:end
