d435c0331d08a9a72280d0fa8b670b6a50e71bbf
khuxtable <khuxtable@mycompany.com>
2008-07-17 12:26:54 +0000

    Updated "person" to "local_person" in commented-out code. Reformatted source.
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@4876 bc24b658-cf39-4249-b827-e3526eaaeb42

--- ldappc/src/test/java/edu/internet2/middleware/ldappcTest/dbBuilder/SignetProvisionerPermissionsTest.java
+++ ldappc/src/test/java/edu/internet2/middleware/ldappcTest/dbBuilder/SignetProvisionerPermissionsTest.java
@@ -14,7 +14,7 @@
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
-*/
+ */
 
 package edu.internet2.middleware.ldappcTest.dbBuilder;
 
@@ -41,11 +41,10 @@ import edu.internet2.middleware.subject.Subject;
 
 /**
  * Class for testing provisioning permissions.
- *
- * @author Gil Singer 
+ * 
+ * @author Gil Singer
  */
-public class SignetProvisionerPermissionsTest extends BaseTestCase
-{
+public class SignetProvisionerPermissionsTest extends BaseTestCase {
     /**
      * the LdappcSignetProvisioner instance
      */
@@ -54,31 +53,30 @@ public class SignetProvisionerPermissionsTest extends BaseTestCase
     /**
      * a subject
      */
-    private Subject subject;
+    private Subject                 subject;
 
     /**
      * the input options
      */
-    private InputOptions options;
+    private InputOptions            options;
+
     /**
      * Constructor
      */
-    public SignetProvisionerPermissionsTest(String name) 
-    {
+    public SignetProvisionerPermissionsTest(String name) {
         super(name);
     }
-    
+
     /**
      * Setup the fixture.
      */
-    protected void setUp() 
-    {
-        DisplayTest.showRunClass(getClass().getName()); 
+    protected void setUp() {
+        DisplayTest.showRunClass(getClass().getName());
 
         SubjectCache subjectCache = new SubjectCache();
         // TODO Must populate subject cache with hash tables for sources.
 
-        String[] args = {"-subject", "GrouperSystem", "-permissions"};
+        String[] args = { "-subject", "GrouperSystem", "-permissions" };
         options = new InputOptions(args);
         ldappcSignetProvisioner = new LdappcSignetProvisioner(options, subjectCache);
     }
@@ -86,112 +84,93 @@ public class SignetProvisionerPermissionsTest extends BaseTestCase
     /**
      * Tear down the fixture.
      */
-    protected void tearDown() 
-    {
+    protected void tearDown() {
     }
 
     /**
      * The main method for running the test.
      */
-    public static void main(String args[]) 
-    {
+    public static void main(String args[]) {
         BaseTestCase.runTestRunner(SignetProvisionerPermissionsTest.class);
     }
 
     /**
      * A test of adding a member to a group.
      */
-    public void testProvisionPermissions() 
-    {
+    public void testProvisionPermissions() {
         DisplayTest.showRunTitle("Test provisioning permissions");
-        
-        
+
         boolean success = doProvisioning(options);
 
-        if (!success)
-        {
+        if (!success) {
             fail("Provisioning of permissions failed to complete.");
         }
 
-        /* TODO: replace below with appropriate changes.
-        try
-        {
-        }
-        catch (???NotFoundException gnfe) 
-        {
-            ErrorLog.error(this.getClass(), "Permission not found: " + gnfe.getMessage());
-            System.out.println("DEBUG Group not found: " + gnfe.getMessage());
-        }
-        */
-     }
+        /*
+         * TODO: replace below with appropriate changes. try { } catch
+         * (???NotFoundException gnfe) { ErrorLog.error(this.getClass(),
+         * "Permission not found: " + gnfe.getMessage());
+         * System.out.println("DEBUG Group not found: " + gnfe.getMessage()); }
+         */
+    }
 
-     /**
-      * Do the provisioning
-      * @return false if fails 
-      */
-    private boolean doProvisioning(InputOptions options)
-     {
+    /**
+     * Do the provisioning
+     * 
+     * @return false if fails
+     */
+    private boolean doProvisioning(InputOptions options) {
         boolean success = false;
 
-        if (options.isFatal())
-        {
+        if (options.isFatal()) {
             String msg = "A fatal error occurred in SignetProvisionerPermissionsTest -- see the error log file";
             DebugLog.info(this.getClass(), msg);
             ErrorLog.warn(this.getClass(), "A fatal error occurred in SignetProvisionerPermissionsTest.");
-        }     
-        else
-        {
+        } else {
             ldappcSignetProvisioner.provisionPermissions();
             success = true;
         }
 
         return success;
     }
-    
+
     /**
-     * A test of finding a PrivilegedSubject's permission, changing it
-     * in Signet, doing the provisioning, verifying that the LDAP directory
-     * reflects the change, changing it back and verifying the the
-     * LDAP directory values is back to the original.  (TODO)
+     * A test of finding a PrivilegedSubject's permission, changing it in
+     * Signet, doing the provisioning, verifying that the LDAP directory
+     * reflects the change, changing it back and verifying the the LDAP
+     * directory values is back to the original. (TODO)
      */
-    public void testChangePermission() 
-    {
+    public void testChangePermission() {
         DisplayTest.showRunTitle("Test changing a permission");
-        
+
         //
         // Find a PrivilegedSubject's permission and change it
-        // in the Signet database. 
+        // in the Signet database.
         //
-        
+
         Signet signet = null;
-        try
-        {
+        try {
             signet = new Signet();
-        }
-        catch (SignetRuntimeException sre)
-        {
-            LdappcConfigurationException ace = new LdappcConfigurationException(
-                    "Failed to create Signet instance: Signet database may not be running.",
-                    sre);
-            throw(ace);
+        } catch (SignetRuntimeException sre) {
+            LdappcConfigurationException ace = new LdappcConfigurationException("Failed to create Signet instance: Signet database may not be running.", sre);
+            throw (ace);
         }
 
         // Uncomment this to display the data accessed below.
-        //SignetUtil.displaySubjectPrivileges("person", "SD00009");
-        
+        // SignetUtil.displaySubjectPrivileges("local_person", "SD00009");
+
         //
         // Get the PrivilegedSubject, which should be uid=lsaito, Saito, Lee
         //
         SignetSubject privSubject = null;
         privSubject = signet.getSubject("local_person", "SD00009");
         if (privSubject == null) {
-             fail("Could not find PrivilegedSubject.");
-        } 
+            fail("Could not find PrivilegedSubject.");
+        }
         Set privileges = privSubject.getPrivileges();
         Privilege privilege = null;
         Iterator it = privileges.iterator();
-        while (it.hasNext())
-        {
+        while (it.hasNext()) {
             privilege = (Privilege) it.next();
             Permission permission = privilege.getPermission();
             TreeNode scope = privilege.getScope();
@@ -199,45 +178,43 @@ public class SignetProvisionerPermissionsTest extends BaseTestCase
             LimitValue limitValue = null;
             Limit limit = null;
             Iterator it2 = limitValues.iterator();
-            while (it2.hasNext())
-            {
-                limitValue = (LimitValue)it2.next();
-                limit = limitValue.getLimit(); 
-                if ("Spending limit".equals(limit.getName()))
-                {
-                    assertEquals( "Value of LimitValue is incorrect.", "10000", limitValue.getValue() );
-                    assertEquals( "Id of Limit is incorrect", "spending-limit", limit.getId() );
-                    assertEquals( "Id of subsystem is incorrect", "projectx", limit.getSubsystem().getId() );
+            while (it2.hasNext()) {
+                limitValue = (LimitValue) it2.next();
+                limit = limitValue.getLimit();
+                if ("Spending limit".equals(limit.getName())) {
+                    assertEquals("Value of LimitValue is incorrect.", "10000", limitValue.getValue());
+                    assertEquals("Id of Limit is incorrect", "spending-limit", limit.getId());
+                    assertEquals("Id of subsystem is incorrect", "projectx", limit.getSubsystem().getId());
                 }
             }
         }
 
         //
-        // I can not find methods for changing or deleting values in the Signet database; TODO: finish
-        // the rest of this when I figure out how.  
+        // I can not find methods for changing or deleting values in the Signet
+        // database;
+        // TODO: finish the rest of this when I figure out how.
         //
 
         //
         // Provision the Signet change to the LDAP directory
         //
-        
+
         boolean success = doProvisioning(options);
 
-        if (!success)
-        {
+        if (!success) {
             fail("Provisioning of permissions failed to complete.");
         }
-        
+
         // TODO the following tests:
 
         //
         // Verify that the LDAP directory has the changed value.
         //
-          
+
         //
         // Change the value back to the original value in the Signet database.
         //
-          
+
         //
         // Do the provisioning again
         //
@@ -245,6 +222,6 @@ public class SignetProvisionerPermissionsTest extends BaseTestCase
         //
         // Verify that the LDAP directory has the original value.
         //
-     }
+    }
 
 }
