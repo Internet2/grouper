387951830b10142f90a068c1e80296b3205f3c12
khuxtable <khuxtable@mycompany.com>
2008-08-12 20:26:24 +0000

    Check for eduPermission MACE OID rather than the one in the proposal.
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@5033 bc24b658-cf39-4249-b827-e3526eaaeb42

--- apacheds-ldappc-schema/src/test/java/edu/internet2/middleware/ldappc/apachedsSchema/SchemaTest.java
+++ apacheds-ldappc-schema/src/test/java/edu/internet2/middleware/ldappc/apachedsSchema/SchemaTest.java
@@ -104,7 +104,7 @@ public class SchemaTest extends AbstractServerTest {
                 isEduMemberFound = true;
             }
 
-            if (id.indexOf("1.3.6.1.4.1.5923.1.1.123456789") != -1) {
+            if (id.indexOf("1.3.6.1.4.1.5923.1.8") != -1) {
                 isEduPermissionFound = true;
             }
 
