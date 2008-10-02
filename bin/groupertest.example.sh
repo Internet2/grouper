c1758e97770031a9d7d96e34623c087eed09271b
Chris Hyzer <mchyzer@isc.upenn.edu>
2008-10-02 21:12:36 +0000

    fix issues
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk/grouper@5176 bc24b658-cf39-4249-b827-e3526eaaeb42

--- bin/groupertest.example.sh
+++ bin/groupertest.example.sh
@@ -18,6 +18,6 @@ MEM_START=64m
 
 MEM_MAX=256m
 
-${JAVA} -Xms${MEM_START} -Xmx${MEM_MAX} -jar ${GROUPER_HOME}/lib/grouper/invoker.jar -cpdir ${GROUPER_CONF} -cpalljars ${GROUPER_HOME}/lib -cpjar ${GROUPER_HOME}/dist/lib/grouper.jar -cpjar %GROUPER_HOME%/dist/binary/zip/lib/test/grouper-test.jar ${GROUPERTEST_JVMARGS} ${GROUPERTEST} "$@"
+${JAVA} -Xms${MEM_START} -Xmx${MEM_MAX} -jar ${GROUPER_HOME}/lib/grouper/invoker.jar -cpdir ${GROUPER_CONF} -cpalljars ${GROUPER_HOME}/lib -cpjar ${GROUPER_HOME}/dist/lib/grouper.jar -cpjar ${GROUPER_HOME}/dist/binary/zip/lib/test/grouper-test.jar ${GROUPERTEST_JVMARGS} ${GROUPERTEST} "$@"
 
 exit $?
