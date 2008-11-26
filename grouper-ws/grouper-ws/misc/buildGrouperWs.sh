542ea7dbbf97a6a3438b4cd39633c5e1826a19ec
Chris Hyzer <mchyzer@isc.upenn.edu>
2008-11-26 13:25:54 +0000

    build script for internet2
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@5322 bc24b658-cf39-4249-b827-e3526eaaeb42

---
+++ grouper-ws/grouper-ws/misc/buildGrouperWs.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [ $# -ne "1" ]
+then
+  echo
+  echo "Give the version to build as the command line argument!"
+  echo "e.g. HEAD, GROUPER_1_3_1, etc"
+  echo "e.g. buildGrouperWs.sh HEAD"
+  echo
+  exit 1
+fi  
+
+cd /tmp
+if [ ! -d /tmp/grouperWs ]; then
+  /bin/mkdir /tmp/grouperWs
+  /bin/chmod g+w /tmp/grouperWs
+fi
+
+cd /tmp/grouperWs
+
+export buildDir=/tmp/grouperWs/build_$USER
+
+if [ -d $buildDir ]; then
+  /bin/rm -rf $buildDir
+fi
+
+if [ ! -d $buildDir ]; then
+  /bin/mkdir $buildDir
+fi
+
+cd $buildDir
+
+export CVSROOT=/home/cvs/i2mi
+
+/usr/bin/cvs export -r $1 grouper-ws
+
+cd $buildDir/grouper-ws
+
+/home/mchyzer/ant/bin/ant distPackage
+
+echo
+echo "result is in $buildDir/grouper/dist/binary/" 
+echo
