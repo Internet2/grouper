3ef13ce3548fbcec8813807e7f7de637ee14799a
Tom Zeller <tzeller@dragonacea.biz>
2009-01-16 15:51:35 +0000

    Although the reason is unclear, now ldappc.xml can be found in the classpath.
    git-svn-id: https://svn.internet2.edu/svn/i2mi/branches/GROUPER_1_4_LDAPPC_BRANCH@5419 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper/src/grouper/edu/internet2/middleware/ldappc/ConfigManager.java
+++ grouper/src/grouper/edu/internet2/middleware/ldappc/ConfigManager.java
@@ -44,6 +44,7 @@ import edu.internet2.middleware.ldappc.logging.DebugLog;
 import edu.internet2.middleware.ldappc.logging.ErrorLog;
 import edu.internet2.middleware.ldappc.util.LdapSearchFilter;
 import edu.internet2.middleware.ldappc.util.ResourceBundleUtil;
+import edu.internet2.middleware.subject.SubjectUtils;
 
 /**
  * Class for accessing values from the Auth2Ldap configuration file.
@@ -735,7 +736,9 @@ public class ConfigManager implements SignetProvisionerConfiguration, GrouperPro
      */
     public static URL getSystemResourceURL(String resource, boolean isRequired) throws LdappcConfigurationException
     {
-        URL url = ClassLoader.getSystemResource(resource);
+        // 20090116 tz doesn't work, strange
+        // URL url = ClassLoader.getSystemResource(resource);
+        URL url = SubjectUtils.computeUrl(resource, false);
         if (isRequired && url == null)
         {
             //
