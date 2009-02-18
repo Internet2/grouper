62e151f16cf0208dde02fe51b8bf20e2b092ca47
Chris Hyzer <mchyzer@isc.upenn.edu>
2009-02-18 20:07:28 +0000

    update version
    git-svn-id: https://svn.internet2.edu/svn/i2mi/branches/GROUPER_1_4_BRANCH@5502 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper/src/grouper/edu/internet2/middleware/grouper/misc/GrouperVersion.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/misc/GrouperVersion.java
@@ -1,6 +1,6 @@
 /*
  * @author mchyzer
- * $Id: GrouperVersion.java,v 1.5.2.1 2009-01-26 05:29:34 mchyzer Exp $
+ * $Id: GrouperVersion.java,v 1.5.2.2 2009-02-18 20:07:28 mchyzer Exp $
  */
 package edu.internet2.middleware.grouper.misc;
 
@@ -22,7 +22,7 @@ public class GrouperVersion {
    * update this before each
    * non-release-candidate release (e.g. in preparation for it)
    */
-  public static final String GROUPER_VERSION = "1.4.1";
+  public static final String GROUPER_VERSION = "1.4.2";
   
   /** major number */
   private int major;
