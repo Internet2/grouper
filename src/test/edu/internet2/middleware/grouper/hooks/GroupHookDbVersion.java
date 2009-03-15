3c10eb93a2e0c144c0a6a34ee7afd044f7c6a9c2
Chris Hyzer <mchyzer@isc.upenn.edu>
2009-03-15 08:24:48 +0000

    merge with 1.4
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk/grouper@5540 bc24b658-cf39-4249-b827-e3526eaaeb42

---
+++ src/test/edu/internet2/middleware/grouper/hooks/GroupHookDbVersion.java
@@ -0,0 +1,141 @@
+/*
+ * @author mchyzer
+ * $Id: GroupHookDbVersion.java,v 1.2 2009-03-15 08:24:48 mchyzer Exp $
+ */
+package edu.internet2.middleware.grouper.hooks;
+
+import org.apache.commons.logging.Log;
+
+import edu.internet2.middleware.grouper.Group;
+import edu.internet2.middleware.grouper.GroupFinder;
+import edu.internet2.middleware.grouper.GrouperSession;
+import edu.internet2.middleware.grouper.exception.GroupNotFoundException;
+import edu.internet2.middleware.grouper.hibernate.GrouperTransaction;
+import edu.internet2.middleware.grouper.hibernate.GrouperTransactionHandler;
+import edu.internet2.middleware.grouper.hibernate.GrouperTransactionType;
+import edu.internet2.middleware.grouper.hooks.beans.HooksContext;
+import edu.internet2.middleware.grouper.hooks.beans.HooksGroupBean;
+import edu.internet2.middleware.grouper.internal.dao.GrouperDAOException;
+import edu.internet2.middleware.grouper.util.GrouperUtil;
+
+/**
+ *
+ */
+public class GroupHookDbVersion extends GroupHooks {
+  /** Extension field in Grouper. */
+  private static final String EXTENSION        = "extension";
+
+  /** Serial version UID. */
+  private static final long   serialVersionUID = 8623167847413079614L;
+
+  /** Logger. */
+  private static final Log LOGGER  = GrouperUtil.getLog(GroupHookDbVersion.class);
+
+  /**
+   * Builds an instance of ESCOGroupHooks.
+   */
+  public GroupHookDbVersion() {
+
+    if (LOGGER.isInfoEnabled()) {
+      final StringBuilder sb = new StringBuilder(
+          "Creation of an hooks of class: ");
+      sb.append(getClass().getSimpleName());
+      sb.append(".");
+      LOGGER.info(sb.toString());
+    }
+  }
+
+  /**
+   * Tests if the extension of a group is modified.
+   * @param group the group to test.
+   * @return True if the extension of the group is modified.
+   */
+  protected boolean isExtensionUpdate(final Group group) {
+
+    if (!group.dbVersionDifferentFields().contains(EXTENSION)) {
+      return false;
+    }
+
+    System.out.println(group.dbVersion().getAttributeOrNull(
+        EXTENSION));
+    
+    final String currentExtension = group.getAttributeOrNull(EXTENSION);
+    
+    int dbDifferentFieldsSize = group.dbVersionDifferentFields().size();
+    
+    LOGGER
+        .debug("isExtensionUpdate (1) => " + group.dbVersionDifferentFields() + ", currentExtension: " + currentExtension);
+    final String previousExtension = group.dbVersion().getAttributeOrNull(
+        EXTENSION);
+    LOGGER
+        .debug("isExtensionUpdate (2) => " + group.dbVersionDifferentFields() + ", previousExtension: " + previousExtension );
+
+    if (dbDifferentFieldsSize != group.dbVersionDifferentFields().size()) {
+      throw new RuntimeException("Why is dbVersionDifferentFieldsSize different??? " + dbDifferentFieldsSize 
+          + " != " + group.dbVersionDifferentFields().size());
+    }
+    
+    if (!currentExtension.equals(previousExtension)) {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+  * @param hooksContext
+  * @param postUpdateBean
+  * @see edu.internet2.middleware.grouper.hooks.GroupHooks#groupPostUpdate(HooksContext, HooksGroupBean)
+  */
+  @Override
+  public void groupPostUpdate(final HooksContext hooksContext,
+      final HooksGroupBean postUpdateBean) {
+
+    final Group group = postUpdateBean.getGroup();
+    LOGGER.debug("Current extension: " + group.getAttributeOrNull(EXTENSION));
+    LOGGER.debug("Previous extension: "
+        + group.dbVersion().getAttributeOrNull(EXTENSION));
+    
+    Group dbGroup = (Group)GrouperTransaction.callbackGrouperTransaction(GrouperTransactionType.READONLY_NEW, 
+        new GrouperTransactionHandler() {
+
+      public Object callback(GrouperTransaction grouperTransaction)
+          throws GrouperDAOException {
+        
+        GrouperSession grouperSession = null;
+        try {
+          grouperSession = GrouperSession.startRootSession(false);
+          Group dbGroup = GroupFinder.findByUuid(grouperSession, group.getUuid());
+          //load from db
+          dbGroup.getExtension();
+          return dbGroup;
+        } catch (GroupNotFoundException gnfe) {
+          return null;
+        } catch (Exception e) {
+          throw new RuntimeException(e);
+        } finally {
+          GrouperSession.stopQuietly(grouperSession);
+        }
+      }
+    
+    });
+    if (dbGroup != null) {
+      LOGGER.debug("From db group extension: " + dbGroup.getExtension());
+    }
+  }
+
+  /**
+   * @param hooksContext
+   * @param preUpdateBean
+   * @see edu.internet2.middleware.grouper.hooks.GroupHooks#groupPreUpdate(HooksContext, HooksGroupBean)
+   */
+  @Override
+  public void groupPreUpdate(final HooksContext hooksContext,
+      final HooksGroupBean preUpdateBean) {
+    final Group group = preUpdateBean.getGroup();
+    LOGGER.debug("groupPreUpdate call isExtensionUpdate");
+    if (isExtensionUpdate(group)) {
+      LOGGER.debug("isExtensionUpdate");
+    }
+  }
+
+}
---
+++ src/test/edu/internet2/middleware/grouper/hooks/GroupHooksDbVersionTest.java
@@ -0,0 +1,107 @@
+/*
+ * @author mchyzer
+ * $Id: GroupHooksDbVersionTest.java,v 1.2 2009-03-15 08:24:48 mchyzer Exp $
+ */
+package edu.internet2.middleware.grouper.hooks;
+
+import junit.textui.TestRunner;
+import edu.internet2.middleware.grouper.Group;
+import edu.internet2.middleware.grouper.GroupFinder;
+import edu.internet2.middleware.grouper.GroupType;
+import edu.internet2.middleware.grouper.GrouperSession;
+import edu.internet2.middleware.grouper.GrouperTest;
+import edu.internet2.middleware.grouper.SessionHelper;
+import edu.internet2.middleware.grouper.Stem;
+import edu.internet2.middleware.grouper.StemHelper;
+import edu.internet2.middleware.grouper.hooks.logic.GrouperHookType;
+import edu.internet2.middleware.grouper.privs.AccessPrivilege;
+import edu.internet2.middleware.grouper.registry.RegistryReset;
+import edu.internet2.middleware.grouper.util.GrouperUtil;
+
+
+/**
+ *
+ */
+public class GroupHooksDbVersionTest extends GrouperTest {
+
+  /**
+   * 
+   * @param args
+   */
+  public static void main(String[] args) {
+    TestRunner.run(new GroupHooksDbVersionTest("testGroupHooksDbVersion"));
+    //TestRunner.run(GroupHooksDbVersionTest.class);
+  }
+  
+  /**
+   * @param name
+   */
+  public GroupHooksDbVersionTest(String name) {
+    super(name);
+  }
+
+  /** edu stem */
+  private Stem edu;
+  
+  /** root stem */
+  private Stem root;
+  
+  /** grouper sesion */
+  private GrouperSession grouperSession; 
+
+  /**
+   * @see edu.internet2.middleware.grouper.GrouperTest#tearDown()
+   */
+  @Override
+  protected void tearDown() {
+    super.tearDown();
+    overrideHooksRemove();
+  }
+
+  /**
+   * 
+   */
+  private void overrideHooksRemove() {
+    //dont have the test hook imple
+    GrouperHookType.addHookOverride(GrouperHookType.GROUP.getPropertyFileKey(), (Class<?>)null);
+  }
+
+  /**
+   * 
+   * @see edu.internet2.middleware.grouper.GrouperTest#setUp()
+   */
+  protected void setUp () {
+    super.setUp();
+    overrideHooksAdd();
+    RegistryReset.internal_resetRegistryAndAddTestSubjects();
+    grouperSession     = SessionHelper.getRootSession();
+    root  = StemHelper.findRootStem(grouperSession);
+    edu   = StemHelper.addChildStem(root, "edu", "education");
+  }
+
+  /**
+   * 
+   */
+  private void overrideHooksAdd() {
+    //this is the test hook imple
+    GrouperHookType.addHookOverride(GrouperHookType.GROUP.getPropertyFileKey(), 
+        GrouperUtil.toListClasses(GroupHookDbVersion.class));
+  }
+
+  /**
+   * @throws Exception 
+   * 
+   */
+  public void testGroupHooksDbVersion() throws Exception {
+    
+    //try an insert
+    Group group = edu.addChildGroup("myGroup", "myGroup");
+    
+    group = GroupFinder.findByName(this.grouperSession, "edu:myGroup");
+    
+    group.setExtension("anotherExt");
+    group.store();
+    
+  }
+  
+}
