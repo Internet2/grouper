78ab8373d72710615903ba505e61c86e5118bcfd
Tom Zeller <tzeller@dragonacea.biz>
2009-04-26 23:36:59 +0000

    Hack to handle String RDNs containing 'special' characters which JNDI returns quoted, but for which NameParser.parse() will throw an InvalidNameException.
    git-svn-id: https://svn.internet2.edu/svn/i2mi/branches/GROUPER_1_4_BRANCH@5641 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper/src/grouper/edu/internet2/middleware/ldappc/synchronize/GroupEntrySynchronizer.java
+++ grouper/src/grouper/edu/internet2/middleware/ldappc/synchronize/GroupEntrySynchronizer.java
@@ -1200,6 +1200,13 @@ public class GroupEntrySynchronizer extends GroupSynchronizer
                     continue;
                 }
 
+                // 20090426 tz there should be a better way to handle
+                // names that begin and end with quotes as returned by JNDI
+                if (entryName.startsWith("\"") && entryName.endsWith("\"")) {
+                    entryName = entryName.replaceFirst("^\"", "");
+                    entryName = entryName.replaceFirst("\"$", "");
+                }
+                
                 //
                 // Build the entry's DN
                 //
