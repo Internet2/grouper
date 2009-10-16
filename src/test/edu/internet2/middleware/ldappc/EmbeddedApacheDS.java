52807f58c20f5f456169b429359b10d3f82f4adb
Tom Zeller <tzeller@dragonacea.biz>
2009-10-16 16:11:03 +0000

    Properties.store(Writer, String) is not available in java 1.5, instead use store(OutputStream, String)
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk/grouper@5944 bc24b658-cf39-4249-b827-e3526eaaeb42

--- src/test/edu/internet2/middleware/ldappc/EmbeddedApacheDS.java
+++ src/test/edu/internet2/middleware/ldappc/EmbeddedApacheDS.java
@@ -1,7 +1,7 @@
 package edu.internet2.middleware.ldappc;
 
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Hashtable;
@@ -216,7 +216,7 @@ public class EmbeddedApacheDS {
   public File getNewPropertiesFile() throws IOException {
     File tmpPropertiesFile = File.createTempFile("EmbeddedApacheDSProps", null);
     tmpPropertiesFile.deleteOnExit();
-    getProperties().store(new FileWriter(tmpPropertiesFile), null);
+    getProperties().store(new FileOutputStream(tmpPropertiesFile), null);
     return tmpPropertiesFile;
   }
 }
