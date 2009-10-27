29091fbec1afef5d55e37bff5a919f25b4199558
Chris Hyzer <mchyzer@isc.upenn.edu>
2009-10-27 14:46:25 +0000

    make this IE8 compatible
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@5991 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper-ui/webapp/grouperUi/public/assets/js/jquery.history.js
+++ grouper-ui/webapp/grouperUi/public/assets/js/jquery.history.js
@@ -1,5 +1,5 @@
 /*
- * downloaded 7/6/9 from: http://www.mikage.to/jquery/jquery_history.html
+ * downloaded 10/27/2009 from: http://www.mikage.to/jquery/jquery_history.html
  * jQuery history plugin
  * 
  * sample page: http://www.mikage.to/jquery/jquery_history.html
@@ -17,6 +17,7 @@ jQuery.extend({
   historyCurrentHash: undefined,
   historyCallback: undefined,
   historyIframeSrc: undefined,
+  historyNeedIframe: jQuery.browser.msie && (jQuery.browser.version < 8 || document.documentMode < 8),
   
   historyInit: function(callback, src){
     jQuery.historyCallback = callback;
@@ -24,17 +25,15 @@ jQuery.extend({
     var current_hash = location.hash.replace(/\?.*$/, '');
     
     jQuery.historyCurrentHash = current_hash;
-    // if ((jQuery.browser.msie) && (jQuery.browser.version < 8)) {
-    if (jQuery.browser.msie) {
+    if (jQuery.historyNeedIframe) {
       // To stop the callback firing twice during initilization if no hash present
       if (jQuery.historyCurrentHash == '') {
-      jQuery.historyCurrentHash = '#';
-    }
+        jQuery.historyCurrentHash = '#';
+      }
     
       // add hidden iframe for IE
       jQuery("body").prepend('<iframe id="jQuery_history" style="display: none;"'+
-        (jQuery.historyIframeSrc ? ' src="'+jQuery.historyIframeSrc+'"' : '')
-        +'></iframe>'
+        ' src="javascript:false;"></iframe>'
       );
       var ihistory = jQuery("#jQuery_history")[0];
       var iframe = ihistory.contentWindow.document;
@@ -65,8 +64,7 @@ jQuery.extend({
   },
   
   historyCheck: function(){
-    // if ((jQuery.browser.msie) && (jQuery.browser.version < 8)) {
-    if (jQuery.browser.msie) {
+    if (jQuery.historyNeedIframe) {
       // On IE, check for location.hash of iframe
       var ihistory = jQuery("#jQuery_history")[0];
       var iframe = ihistory.contentDocument || ihistory.contentWindow.document;
@@ -135,8 +133,7 @@ jQuery.extend({
     }
     jQuery.historyCurrentHash = newhash;
     
-    // if ((jQuery.browser.msie) && (jQuery.browser.version < 8)) {
-    if (jQuery.browser.msie) {
+    if (jQuery.historyNeedIframe) {
       var ihistory = jQuery("#jQuery_history")[0];
       var iframe = ihistory.contentWindow.document;
       iframe.open();
