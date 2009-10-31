ba3af01b79f40daef798a93e97b80096a12051c7
Chris Hyzer <mchyzer@isc.upenn.edu>
2009-10-31 16:27:12 +0000

    extract lazy source, and update final subject jar
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@6017 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper/src/grouper/edu/internet2/middleware/grouper/subj/UnresolvableSubject.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/subj/UnresolvableSubject.java
@@ -15,15 +15,9 @@
 
 package edu.internet2.middleware.grouper.subj;
 
-import java.util.Set;
-
-import edu.internet2.middleware.grouper.SubjectFinder;
-import edu.internet2.middleware.grouper.exception.GrouperException;
+import edu.internet2.middleware.subject.LazySource;
 import edu.internet2.middleware.subject.Source;
-import edu.internet2.middleware.subject.SourceUnavailableException;
 import edu.internet2.middleware.subject.Subject;
-import edu.internet2.middleware.subject.SubjectNotFoundException;
-import edu.internet2.middleware.subject.SubjectNotUniqueException;
 import edu.internet2.middleware.subject.SubjectType;
 import edu.internet2.middleware.subject.provider.SubjectImpl;
 
@@ -32,7 +26,7 @@ import edu.internet2.middleware.subject.provider.SubjectImpl;
  * Allows the UI to continue working when, otherwise, a SubjectNotFoundException would cause an error.
  * <p/>
  * @author  Gary Brown.
- * @version $Id: UnresolvableSubject.java,v 1.5 2009-09-02 05:57:26 mchyzer Exp $
+ * @version $Id: UnresolvableSubject.java,v 1.6 2009-10-31 16:27:12 mchyzer Exp $
  */
 @SuppressWarnings("serial")
 public class UnresolvableSubject extends SubjectImpl {
@@ -41,7 +35,7 @@ public class UnresolvableSubject extends SubjectImpl {
   private SubjectType subjectType = new LazySubjectType();
 
   /** */
-  private Source subjectSource = new LazySource();
+  private Source subjectSource = null;
 
   /**
    * 
@@ -58,7 +52,19 @@ public class UnresolvableSubject extends SubjectImpl {
    * @see edu.internet2.middleware.subject.Subject#getSource()
    */
   public Source getSource() {
+    if (this.subjectSource == null) {
+      this.subjectSource = new LazySource(this.getSourceId());
+    }
     return this.subjectSource;
+  }
+
+  /**
+   * @see edu.internet2.middleware.subject.provider.SubjectImpl#setSourceId(java.lang.String)
+   */
+  @Override
+  public void setSourceId(String sourceId1) {
+    super.setSourceId(sourceId1);
+    this.subjectSource = null;
   }
 
   /**
@@ -66,144 +72,6 @@ public class UnresolvableSubject extends SubjectImpl {
    */
   public SubjectType getType() {
     return this.subjectType;
-  }
-
-  /**
-   * Circumvent the need to instantiate a Subject to get a source id
-   * @since 1.3.1
-   *
-   */
-  class LazySource implements Source {
-
-    /** */
-    private Source source;
-
-    /**
-     * 
-     * @return source
-     */
-    private Source getSource() {
-      if (source != null)
-        return source;
-      try {
-        source = SubjectFinder.getSource(getId());
-      } catch (SourceUnavailableException e) {
-        throw new GrouperException(e);
-      }
-      return source;
-    }
-
-    /**
-     * 
-     * @see edu.internet2.middleware.subject.Source#getId()
-     */
-    public String getId() {
-      return UnresolvableSubject.this.getSourceId();
-    }
-
-    /**
-     * 
-     * @see edu.internet2.middleware.subject.Source#getName()
-     */
-    public String getName() {
-      return getSource().getName();
-    }
-
-    /**
-     * 
-     * @see edu.internet2.middleware.subject.Source#getSubject(java.lang.String)
-     */
-    @Deprecated
-    public Subject getSubject(String id) throws SubjectNotFoundException,
-        SubjectNotUniqueException {
-      return getSource().getSubject(id, true);
-    }
-
-    /**
-     * 
-     * @see edu.internet2.middleware.subject.Source#getSubjectByIdentifier(java.lang.String)
-     */
-    @Deprecated
-    public Subject getSubjectByIdentifier(String id) throws SubjectNotFoundException,
-        SubjectNotUniqueException {
-      return getSource().getSubjectByIdentifier(id, true);
-    }
-
-    /**
-     * 
-     * @see edu.internet2.middleware.subject.Source#getSubject(java.lang.String, boolean)
-     */
-    public Subject getSubject(String id, boolean exceptionIfNull)
-        throws SubjectNotFoundException, SubjectNotUniqueException {
-      return getSource().getSubject(id, exceptionIfNull);
-    }
-
-    /**
-     * 
-     * @see edu.internet2.middleware.subject.Source#getSubjectByIdentifier(java.lang.String, boolean)
-     */
-    public Subject getSubjectByIdentifier(String id, boolean exceptionIfNull)
-        throws SubjectNotFoundException, SubjectNotUniqueException {
-      return getSource().getSubjectByIdentifier(id, exceptionIfNull);
-    }
-
-    /**
-     * 
-     * @see edu.internet2.middleware.subject.Source#getSubjectTypes()
-     */
-    public Set getSubjectTypes() {
-      return getSource().getSubjectTypes();
-    }
-
-    /**
-     * 
-     * @see edu.internet2.middleware.subject.Source#init()
-     */
-    public void init() throws SourceUnavailableException {
-      getSource().init();
-
-    }
-
-    /**
-     * 
-     * @see edu.internet2.middleware.subject.Source#search(java.lang.String)
-     */
-    public Set search(String query) {
-      return getSource().search(query);
-    }
-
-    /**
-     * 
-     * @see edu.internet2.middleware.subject.Source#setId(java.lang.String)
-     */
-    public void setId(String id) {
-      getSource().setId(id);
-
-    }
-
-    /**
-     * 
-     * @see edu.internet2.middleware.subject.Source#setName(java.lang.String)
-     */
-    public void setName(String name) {
-      getSource().setName(name);
-
-    }
-
-    /**
-     * @see edu.internet2.middleware.subject.Source#checkConfig()
-     */
-    public void checkConfig() {
-    }
-
-    /**
-     * @see edu.internet2.middleware.subject.Source#printConfig()
-     */
-    public String printConfig() {
-      String message = "sources.xml lazy source id:   " + this.getId();
-      return message;
-    }
-
   }
 
   /**
