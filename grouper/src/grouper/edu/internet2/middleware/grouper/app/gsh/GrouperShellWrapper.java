ec41e61672965c0dbdb3974d8f156ad3090dbdb2
Tom Zeller <tzeller@dragonacea.biz>
2009-11-30 17:57:38 +0000

    reverted, removed java.version checking
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@6137 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper/src/grouper/edu/internet2/middleware/grouper/app/gsh/GrouperShellWrapper.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/app/gsh/GrouperShellWrapper.java
@@ -1,31 +1,21 @@
 /**
  * @author mchyzer
- * $Id: GrouperShellWrapper.java,v 1.2 2009-11-30 17:28:43 tzeller Exp $
+ * $Id: GrouperShellWrapper.java,v 1.3 2009-11-30 17:57:38 tzeller Exp $
  */
 package edu.internet2.middleware.grouper.app.gsh;
 
 
 
-/**
- * Run GrouperShell only with the required Java version or higher.
- */
 public class GrouperShellWrapper {
-
-  public static final String REQUIRED_VERSION = "1.6";
 
   /**
    * @param args
    */
   public static void main(String[] args) {
     try {
-      String javaVersion = System.getProperty("java.version");
-      if (!javaVersion.startsWith(REQUIRED_VERSION)) {
-        System.err.println("GSH requires Java " + REQUIRED_VERSION + "+. This is version " + javaVersion + ".");
-      } else {
         GrouperShell.main(args);
-      }
     } catch (NoClassDefFoundError cnfe) {
-      System.err.println("There was a NoClassDefFoundError.  This could be because you are not using Java 1.6+ which is required for GSH.");
+      System.err.println("There was a NoClassDefFoundError.  This could be because you are not using Java 1.6+ which is required for GSH.  This is version " + System.getProperty("java.version") + ".");
       throw cnfe;
     }
 
