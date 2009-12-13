701854944b62a75d39b59376896f991f8a0458a3
Chris Hyzer <mchyzer@isc.upenn.edu>
2009-12-13 22:33:04 +0000

    addGroup
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@6151 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper-misc/grouperKimConnector/src/edu/internet2/middleware/grouperKimConnector/groupUpdate/GrouperKimGroupUpdateServiceImpl.java
+++ grouper-misc/grouperKimConnector/src/edu/internet2/middleware/grouperKimConnector/groupUpdate/GrouperKimGroupUpdateServiceImpl.java
@@ -1,6 +1,6 @@
 /**
  * @author mchyzer
- * $Id: GrouperKimGroupUpdateServiceImpl.java,v 1.1 2009-12-13 06:57:50 mchyzer Exp $
+ * $Id: GrouperKimGroupUpdateServiceImpl.java,v 1.2 2009-12-13 22:33:04 mchyzer Exp $
  */
 package edu.internet2.middleware.grouperKimConnector.groupUpdate;
 
@@ -8,15 +8,23 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.kuali.rice.kim.bo.group.dto.GroupInfo;
+import org.kuali.rice.kim.bo.types.dto.AttributeSet;
 import org.kuali.rice.kim.service.GroupUpdateService;
 
 import edu.internet2.middleware.grouperClient.api.GcAddMember;
 import edu.internet2.middleware.grouperClient.api.GcDeleteMember;
+import edu.internet2.middleware.grouperClient.api.GcGroupSave;
 import edu.internet2.middleware.grouperClient.util.GrouperClientUtils;
 import edu.internet2.middleware.grouperClient.ws.beans.WsAddMemberResult;
 import edu.internet2.middleware.grouperClient.ws.beans.WsAddMemberResults;
 import edu.internet2.middleware.grouperClient.ws.beans.WsDeleteMemberResult;
 import edu.internet2.middleware.grouperClient.ws.beans.WsDeleteMemberResults;
+import edu.internet2.middleware.grouperClient.ws.beans.WsGroup;
+import edu.internet2.middleware.grouperClient.ws.beans.WsGroupDetail;
+import edu.internet2.middleware.grouperClient.ws.beans.WsGroupLookup;
+import edu.internet2.middleware.grouperClient.ws.beans.WsGroupSaveResult;
+import edu.internet2.middleware.grouperClient.ws.beans.WsGroupSaveResults;
+import edu.internet2.middleware.grouperClient.ws.beans.WsGroupToSave;
 import edu.internet2.middleware.grouperClient.ws.beans.WsSubjectLookup;
 import edu.internet2.middleware.grouperClientExt.org.apache.commons.logging.Log;
 import edu.internet2.middleware.grouperKimConnector.util.GrouperKimUtils;
@@ -113,8 +121,7 @@ public class GrouperKimGroupUpdateServiceImpl implements GroupUpdateService {
     try {
       
       //lets see if there is a source to use
-      String sourceId = GrouperClientUtils.propertiesValue("grouper.kim.plugin.subjectSourceId", false);
-      sourceId = GrouperClientUtils.isBlank(sourceId) ? null : sourceId;
+      String sourceId = GrouperKimUtils.subjectSourceId();
       
       WsAddMemberResults wsAddMemberResults = new GcAddMember().assignGroupUuid(groupId)
         .addSubjectLookup(new WsSubjectLookup(principalId, sourceId, null)).execute();
@@ -157,10 +164,135 @@ public class GrouperKimGroupUpdateServiceImpl implements GroupUpdateService {
   }
 
   /**
+   * GroupInfo createGroup(GroupInfo groupInfo)
+   *                   throws java.lang.UnsupportedOperationException
+   *
+   * Creates a new group using the given GroupInfo. 
+   * 
    * @see org.kuali.rice.kim.service.GroupUpdateService#createGroup(org.kuali.rice.kim.bo.group.dto.GroupInfo)
    */
-  public GroupInfo createGroup(GroupInfo arg0) throws UnsupportedOperationException {
-    return null;
+  public GroupInfo createGroup(GroupInfo groupInfo) throws UnsupportedOperationException {
+    
+    
+
+    Map<String, Object> debugMap = new LinkedHashMap<String, Object>();
+    boolean hadException = false;
+    
+    try {
+      
+      debugMap.put("operation", "createGroup");
+      debugMap.put("groupInfo.isActive", groupInfo.isActive());
+      debugMap.put("groupInfo.getGroupDescription", groupInfo.getGroupDescription());
+      debugMap.put("groupInfo.getGroupIdOrig", groupInfo.getGroupId());
+      debugMap.put("groupInfo.getGroupName", groupInfo.getGroupName());
+      debugMap.put("groupInfo.getKimTypeId", groupInfo.getKimTypeId());
+      debugMap.put("groupInfo.getNamespaceCode", groupInfo.getNamespaceCode());
+      AttributeSet attributeSet = groupInfo.getAttributes();
+
+      GcGroupSave gcGroupSave = new GcGroupSave();
+
+      WsGroupLookup wsGroupLookup = new WsGroupLookup();
+      wsGroupLookup.setGroupName(groupInfo.getGroupName());
+      
+      
+      WsGroupToSave wsGroupToSave = new WsGroupToSave();
+      wsGroupToSave.setWsGroupLookup(wsGroupLookup);
+      gcGroupSave.addGroupToSave(wsGroupToSave);
+      
+      WsGroup wsGroup = new WsGroup();
+      wsGroupToSave.setWsGroup(wsGroup);
+      
+      //assign group types and attributes
+      String[] groupTypesOfKimGroups = GrouperKimUtils.grouperTypesOfKimGroups();
+      boolean hasGroupTypes = GrouperClientUtils.length(groupTypesOfKimGroups) > 0;
+      
+      for (String key : attributeSet.keySet()) {
+        String value = attributeSet.get(key);
+        //store in debug map for debugging or errors
+        debugMap.put("groupInfo.attribute." + key, value);
+      }
+      
+      if (!groupInfo.isActive()) {
+        throw new UnsupportedOperationException("Cannot save a group which is not active to grouper");
+      }
+      
+      wsGroup.setDescription(GrouperClientUtils.abbreviate(groupInfo.getGroupDescription(), 1024));
+      
+      //not sure why it would add a group and specify the group id... but we can try to support that
+      if (!GrouperClientUtils.isBlank(groupInfo.getGroupId())) {
+        wsGroup.setUuid(groupInfo.getGroupId());
+        wsGroupLookup.setUuid(groupInfo.getGroupId());
+      }
+        
+      String stemName = GrouperKimUtils.kimStem();
+      if (!GrouperClientUtils.isBlank(groupInfo.getNamespaceCode())) {
+        stemName += ":" + groupInfo.getNamespaceCode();
+      }
+      String grouperGroupName = stemName + ":" + groupInfo.getGroupName();
+      debugMap.put("grouperGroupName", grouperGroupName);
+      
+      wsGroup.setName(grouperGroupName);
+
+      //use the group name as display extension too
+      wsGroup.setDisplayExtension(groupInfo.getGroupName());
+            
+      if (hasGroupTypes) {
+        for (int i=0;i<groupTypesOfKimGroups.length;i++) {
+          debugMap.put("groupTypesOfKimGroups." + i, groupTypesOfKimGroups[i]);
+        }
+        gcGroupSave.assignIncludeGroupDetail(true);
+        WsGroupDetail wsGroupDetail = new WsGroupDetail();
+        wsGroupDetail.setTypeNames(groupTypesOfKimGroups);
+        wsGroup.setDetail(wsGroupDetail);
+        
+        //assign attributes
+        int attributeSize = GrouperClientUtils.length(attributeSet);
+        if (attributeSize > 0) {
+          String[] attributeNames = new String[attributeSize];
+          wsGroupDetail.setAttributeNames(attributeNames);
+          String[] attributeValues = new String[attributeSize];
+          wsGroupDetail.setAttributeValues(attributeValues);
+          
+          int index = 0;
+          for (String key : attributeSet.keySet()) {
+            String value = attributeSet.get(key);
+            attributeNames[index] = key;
+            attributeValues[index] = value;
+            index++;
+          }
+        }
+      } else {
+        debugMap.put("groupTypesOfKimGroups", null);
+        //if no types, then cannot have attributes, so dont assign them
+      }
+      
+      
+      WsGroupSaveResults wsGroupSaveResults = gcGroupSave.execute();
+      
+      //we did one assignment, we have one result
+      WsGroupSaveResult wsGroupSaveResult = wsGroupSaveResults.getResults()[0];
+      
+      String resultCode = wsGroupSaveResult.getResultMetadata().getResultCode();
+
+      debugMap.put("resultCode", resultCode);
+
+      String uuid = wsGroupSaveResult.getWsGroup().getUuid();
+      debugMap.put("newGroupId", uuid);
+      groupInfo.setGroupId(uuid);
+      
+      return groupInfo;
+      
+    } catch (RuntimeException re) {
+      String errorPrefix = GrouperKimUtils.mapForLog(debugMap) + ", ";
+      LOG.error(errorPrefix, re);
+      GrouperClientUtils.injectInException(re, errorPrefix);
+      throw re;
+    } finally {
+      if (LOG.isDebugEnabled() && !hadException) {
+        LOG.debug(GrouperKimUtils.mapForLog(debugMap));
+      }
+    }
+    
   }
 
   /**
@@ -280,8 +412,7 @@ public class GrouperKimGroupUpdateServiceImpl implements GroupUpdateService {
     try {
       
       //lets see if there is a source to use
-      String sourceId = GrouperClientUtils.propertiesValue("grouper.kim.plugin.subjectSourceId", false);
-      sourceId = GrouperClientUtils.isBlank(sourceId) ? null : sourceId;
+      String sourceId = GrouperKimUtils.subjectSourceId();
       
       WsDeleteMemberResults wsDeleteMemberResults = new GcDeleteMember()
         .addSubjectLookup(new WsSubjectLookup(principalId, sourceId, null)).assignGroupUuid(groupId).execute();
--- grouper-misc/grouperKimConnector/src/edu/internet2/middleware/grouperKimConnector/util/GrouperKimUtils.java
+++ grouper-misc/grouperKimConnector/src/edu/internet2/middleware/grouperKimConnector/util/GrouperKimUtils.java
@@ -1,6 +1,6 @@
 /**
  * @author mchyzer
- * $Id: GrouperKimUtils.java,v 1.1 2009-12-13 06:57:50 mchyzer Exp $
+ * $Id: GrouperKimUtils.java,v 1.2 2009-12-13 22:33:03 mchyzer Exp $
  */
 package edu.internet2.middleware.grouperKimConnector.util;
 
@@ -10,10 +10,21 @@ import edu.internet2.middleware.grouperClient.util.GrouperClientUtils;
 
 
 /**
- *
+ * utility methods for grouper kim integration
  */
 public class GrouperKimUtils {
 
+  /**
+   * source id to use for all subjects, or null if none specified (dont bind to one source)
+   * @return the source id
+   */
+  public static String subjectSourceId() {
+    //lets see if there is a source to use
+    String sourceId = GrouperClientUtils.propertiesValue("grouper.kim.plugin.subjectSourceId", false);
+    sourceId = GrouperClientUtils.isBlank(sourceId) ? null : sourceId;
+    return sourceId;
+  }
+  
   /**
    * 
    * @param debugMap
@@ -32,6 +43,30 @@ public class GrouperKimUtils {
     //take off the last two chars
     result.delete(result.length()-2, result.length());
     return result.toString();
+  }
+  
+  /**
+   * stem where KIM groups are.  The KIM namespace is underneath, then the group.
+   * @return the rice stem in the registry, without trailing colon
+   */
+  public static String kimStem() {
+    String kimStem = GrouperClientUtils.propertiesValue("kim.stem", true);
+    if (kimStem.endsWith(":")) {
+      kimStem = kimStem.substring(0,kimStem.length()-1);
+    }
+    return kimStem;
+  }
+   
+  /**
+   * return the grouper type of kim groups or null if none
+   * @return the type
+   */
+  public static String[] grouperTypesOfKimGroups() {
+    String typeString = GrouperClientUtils.propertiesValue("grouper.types.of.kim.groups", false);
+    if (GrouperClientUtils.isBlank(typeString)) {
+      return null;
+    }
+    return GrouperClientUtils.splitTrim(typeString, ",");
   }
   
 }
