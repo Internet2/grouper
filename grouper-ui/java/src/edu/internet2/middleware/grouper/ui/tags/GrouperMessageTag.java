463a40ebcc1d08db4e87f67c35acb3724f84dbe2
Chris Hyzer <mchyzer@isc.upenn.edu>
2010-03-26 17:57:21 +0000

    add readonly mode to grouper
    git-svn-id: https://svn.internet2.edu/svn/i2mi/branches/GROUPER_UI_1_4_BRANCH@6439 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper-ui/java/src/edu/internet2/middleware/grouper/ui/tags/GrouperMessageTag.java
+++ grouper-ui/java/src/edu/internet2/middleware/grouper/ui/tags/GrouperMessageTag.java
@@ -45,8 +45,6 @@ import org.apache.taglibs.standard.tag.common.fmt.BundleSupport;
 import org.apache.taglibs.standard.tag.common.fmt.SetLocaleSupport;
 import org.apache.taglibs.standard.tag.el.fmt.MessageTag;
 
-import sun.util.logging.resources.logging;
-
 import edu.internet2.middleware.grouper.cfg.GrouperConfig;
 import edu.internet2.middleware.grouper.ui.actions.LowLevelGrouperCapableAction;
 import edu.internet2.middleware.grouper.ui.util.MapBundleWrapper;
