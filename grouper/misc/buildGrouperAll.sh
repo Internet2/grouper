ef8fdbde26c81b17bb29f1dfcf4271b03bc95399
Chris Hyzer <mchyzer@isc.upenn.edu>
2010-03-31 17:57:55 +0000

    copy all packages to one dir
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@6457 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper/misc/buildGrouperAll.sh
+++ grouper/misc/buildGrouperAll.sh
@@ -1,5 +1,4 @@
-#!/bin/bash                                                                                                         \
-                                                                                                                     
+#!/bin/bash                                                                                                          
 
 if [ $# -ne "1" ]
 then
@@ -11,15 +10,40 @@ then
   exit 1
 fi
 
-/home/mchyzer/bin/buildGrouper.sh $1
-/home/mchyzer/bin/buildGrouperWs.sh $1
-/home/mchyzer/bin/buildGrouperUi.sh $1
-/home/mchyzer/bin/buildGrouperQs.sh $1
-/home/mchyzer/bin/buildGrouperClient.sh $1
+#/home/mchyzer/bin/buildGrouper.sh $1
+#/home/mchyzer/bin/buildGrouperWs.sh $1
+#/home/mchyzer/bin/buildGrouperUi.sh $1
+#/home/mchyzer/bin/buildGrouperQs.sh $1
+#/home/mchyzer/bin/buildGrouperClient.sh $1
+
+if [ ! -d /home/mchyzer/tmp/grouperAll ]; then
+  /bin/mkdir /home/mchyzer/tmp/grouperAll
+  /bin/chmod g+w /home/mchyzer/tmp/grouperAll
+fi
+
+cd /home/mchyzer/tmp/grouperAll
+
+export buildDir=/home/mchyzer/tmp/grouperAll/build_$USER
+
+if [ -d $buildDir ]; then
+  /bin/rm -rf $buildDir
+fi
+
+if [ ! -d $buildDir ]; then
+  /bin/mkdir $buildDir
+fi
+
+#lets move instead of copy so we dont waste space
+
+mv -v /home/mchyzer/tmp/grouper/build_$USER/grouper*.tar.gz $buildDir
+mv -v /home/mchyzer/tmp/grouperClient/build_$USER/grouper*.tar.gz $buildDir
+mv -v /home/mchyzer/tmp/grouper-qs/build_$USER/grouper*.tar.gz $buildDir
+mv -v /home/mchyzer/tmp/grouperUi/build_$USER/grouper*.tar.gz $buildDir
+mv -v /home/mchyzer/tmp/grouperWs/build_$USER/grouper*.tar.gz $buildDir
+
+
+#allow someone from group to delete later on
+/bin/chmod -R g+w $buildDir
 
 echo
-echo "Overall done!"
-echo
-
-
-
+echo "Overall done! All packages have been moved to: $buildDir"
