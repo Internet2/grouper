d4fd620219e543e33eeaf76f943b162ef60f12c4
Chris Hyzer <mchyzer@isc.upenn.edu>
2010-05-20 05:28:18 +0000

    provision to entered groups
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@6688 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper-misc/grouperKimConnector/src/edu/internet2/middleware/grouperKimConnector/identity/GrouperKimSaveMembershipProperties.java
+++ grouper-misc/grouperKimConnector/src/edu/internet2/middleware/grouperKimConnector/identity/GrouperKimSaveMembershipProperties.java
@@ -4,6 +4,13 @@
  */
 package edu.internet2.middleware.grouperKimConnector.identity;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import edu.internet2.middleware.grouperClient.util.ExpirableCache;
 import edu.internet2.middleware.grouperClient.util.GrouperClientUtils;
 
@@ -34,8 +41,77 @@ public class GrouperKimSaveMembershipProperties {
   
   /** email addresses for sending a message to admins */
   private String emailAdmins;
+
+  /** this will be prefixed to the entered group name so the whole stem doesnt have to be put on screen (also helps sandbox out the security) */
+  private String enteredGroupNamePrefix;
   
   
+  /**
+   * this will be prefixed to the entered group name so the whole stem doesnt have to be put on screen (also helps sandbox out the security)
+   * @return the enteredGroupNamePrefix
+   */
+  public String getEnteredGroupNamePrefix() {
+    return this.enteredGroupNamePrefix;
+  }
+
+  /**
+   * this will be prefixed to the entered group name so the whole 
+   * stem doesnt have to be put on screen (also helps sandbox out the security)
+   * @param enteredGroupNamePrefix1 the enteredGroupNamePrefix to set
+   */
+  public void setEnteredGroupNamePrefix(String enteredGroupNamePrefix1) {
+    this.enteredGroupNamePrefix = enteredGroupNamePrefix1;
+  }
+
+
+
+
+  /** 
+   * if checkboxes or textfields or whatever, put the prefix of the edoclite field here.
+   * so if the field prefix is "groups", then it will look for groups0, groups1, etc to groups200...
+   * the value of the field is the group to add to
+   */
+  private String edocliteFieldPrefix;
+  
+  
+  /**
+   * if checkboxes or textfields or whatever, put the prefix of the edoclite field here.
+   * so if the field prefix is "groups", then it will look for groups0, groups1, etc to groups200...
+   * the value of the field is the group to add to
+   * @return the edocliteFieldPrefix
+   */
+  public String getEdocliteFieldPrefix() {
+    return this.edocliteFieldPrefix;
+  }
+
+
+
+  
+  /**
+   * if checkboxes or textfields or whatever, put the prefix of the edoclite field here.
+   * so if the field prefix is "groups", then it will look for groups0, groups1, etc to groups200...
+   * the value of the field is the group to add to
+   * @param edocliteFieldPrefix1 the edocliteFieldPrefix to set
+   */
+  public void setEdocliteFieldPrefix(String edocliteFieldPrefix1) {
+    this.edocliteFieldPrefix = edocliteFieldPrefix1;
+  }
+
+
+
+  /** set of strings of groups allowed to be used (if empty, then allow all) */
+  private Set<String> allowedGroups = new HashSet<String>();
+  
+  /**
+   * set of strings of groups allowed to be used (if empty, then allow all)
+   * @return the groups
+   */
+  public Set<String> getAllowedGroups() {
+    return this.allowedGroups;
+  }
+
+
+
   /**
    * email addresses for sending a message to admins
    * @return the emailAdmins
@@ -44,7 +120,28 @@ public class GrouperKimSaveMembershipProperties {
     return this.emailAdmins;
   }
 
-
+  /**
+   * if allowed to access this group by name
+   * @param groupName
+   * @return true if allowed to access group by name
+   */
+  public boolean allowedToAccessGroup(String groupName) {
+    
+    //see if fails the regex (if there is a regex)
+    if (!GrouperClientUtils.isBlank(this.groupRegex)) {
+      if (!groupName.matches(this.groupRegex)) {
+        return false;
+      }
+    }
+    
+    //see if not in the list, if there is a list
+    if (this.getAllowedGroups().size() > 0) {
+      if (!this.getAllowedGroups().contains(groupName)) {
+        return false;
+      }
+    }
+    return true;
+  }
   
   /**
    * email addresses for sending a message to admins
@@ -141,10 +238,38 @@ public class GrouperKimSaveMembershipProperties {
    */
   public static GrouperKimSaveMembershipProperties grouperKimSaveMembershipProperties(String docTypeName) {
     
-    //kuali.edoclite.saveMembership.docTypeName.0 = sampleProvisionGroup.doctype
-    //kuali.edoclite.saveMembership.groupRegex.0 = ^temp:[^:]+rovisionGroup$
-    //kuali.edoclite.saveMembership.addMembershipToGroups.0 = temp:provisionGroup
-    //kuali.edoclite.saveMembership.removeMembershipFromGroups.0 = temp:anotherProvisionGroup
+    //###############################
+    //# configure postprocessor actions on document types.  The string "sampleProvisioning" ties the configs
+    //# together, change that label for multiple
+    //
+    //# doctype name that this applies to
+    //kuali.edoclite.saveMembership.sampleProvisioning.docTypeName = sampleProvisioning.doctype
+    //
+    //# regex of group allowed to assign to, extra layer of security, optional
+    //kuali.edoclite.saveMembership.sampleProvisioning.groupRegex = ^temp:[^:]+rovisionGroup$
+    //
+    //# list of allowed to assign to (comma separate), extra layer of security, optional, 
+    //#generally mutually exclusive with the groupRegex
+    //kuali.edoclite.saveMembership.sampleProvisioning.allowedGroups = a:b:c, d:e:f:G
+    //
+    //# edocliteFieldPrefix if checkboxes or textfields or whatever, put the prefix of the edoclite field here.
+    //#so if the field prefix is "groups", then it will look for groups0, groups1, etc to groups200...
+    //#the value of the field is the group to add to
+    //kuali.edoclite.saveMembership.sampleProvisioning.edocliteFieldPrefix = provisionGroup
+    //
+    //#this will be prefixed to the entered group name so the whole stem doesnt 
+    //#have to be put on screen (also helps sandbox out the security)
+    //kuali.edoclite.saveMembership.sampleProvisioning.enteredGroupNamePrefix = school:some:prefix:
+    //
+    //# groups (comma separated) id or name which the initiator will be assigned to when the document is final
+    //kuali.edoclite.saveMembership.sampleProvisioning.addMembershipToGroups = temp:provisionGroup
+    //
+    //# groups (comma separated) id or name which the initiator will be unassigned from when the document is final
+    //kuali.edoclite.saveMembership.sampleProvisioning.removeMembershipFromGroups = temp:anotherProvisionGroup
+    //
+    //# email addresses (comma separated) that should get an admin email that this was done (or errors)
+    //kuali.edoclite.saveMembership.sampleProvisioning.emailAdmins = mchyzer@isc.upenn.edu
+    
     
     GrouperKimSaveMembershipProperties grouperKimSaveMembershipProperties = 
       grouperKimSaveMembershipPropertiesCache.get(docTypeName);
@@ -154,38 +279,68 @@ public class GrouperKimSaveMembershipProperties {
       
       //loop through and find this config
       
-      for (int i=0;i<100;i++) {
-        
-        String currentDocTypeName = GrouperClientUtils.propertiesValue("kuali.edoclite.saveMembership.docTypeName." + i, false);
-        if (GrouperClientUtils.isBlank(currentDocTypeName)) {
-          break;
-        }
-        if (GrouperClientUtils.equals(docTypeName, currentDocTypeName)) {
+      Pattern pattern = Pattern.compile("^kuali\\.edoclite\\.saveMembership\\.(.+)\\.docTypeName$");
+      
+      Properties properties = GrouperClientUtils.grouperClientProperties();
+      for (Object keyObject : properties.keySet()) {
+        String key = (String)keyObject;
+        Matcher matcher = pattern.matcher(key);
+        if (matcher.matches()) {
+          String configName = matcher.group(1);
           
-          //we found it
-          {
-            String groupRegex = GrouperClientUtils.propertiesValue("kuali.edoclite.saveMembership.groupRegex." + i, false);
-            grouperKimSaveMembershipProperties.setGroupRegex(groupRegex);
+          String currentDocTypeName = GrouperClientUtils.propertiesValue(key, true);
+  
+          
+          if (GrouperClientUtils.equals(docTypeName, currentDocTypeName)) {
+            
+            //we found it
+            {
+              String groupRegex = GrouperClientUtils.propertiesValue(
+                  "kuali.edoclite.saveMembership." + configName + ".groupRegex", false);
+              grouperKimSaveMembershipProperties.setGroupRegex(groupRegex);
+            }
+            
+            {
+              String groupsString = GrouperClientUtils.propertiesValue(
+                  "kuali.edoclite.saveMembership." + configName + ".allowedGroups", false);
+              if (!GrouperClientUtils.isBlank(groupsString)) {
+                List<String> groupsList = GrouperClientUtils.splitTrimToList(groupsString, ",");
+                grouperKimSaveMembershipProperties.getAllowedGroups().addAll(groupsList);
+              }
+            }
+            
+            {
+              String edocliteFieldPrefix = GrouperClientUtils.propertiesValue(
+                  "kuali.edoclite.saveMembership." + configName + ".edocliteFieldPrefix", false);
+              grouperKimSaveMembershipProperties.setEdocliteFieldPrefix(edocliteFieldPrefix);
+            }
+            {
+              String enteredGroupNamePrefix = GrouperClientUtils.propertiesValue(
+                  "kuali.edoclite.saveMembership." + configName + ".enteredGroupNamePrefix", false);
+              grouperKimSaveMembershipProperties.setEnteredGroupNamePrefix(enteredGroupNamePrefix);
+            }
+                        
+            {
+              String addMembershipToGroups = GrouperClientUtils.propertiesValue(
+                  "kuali.edoclite.saveMembership." + configName + ".addMembershipToGroups", false);
+              grouperKimSaveMembershipProperties.setAddMembershipToGroups(addMembershipToGroups);
+            }
+            
+            {
+              String removeMembershipFromGroups = GrouperClientUtils.propertiesValue(
+                  "kuali.edoclite.saveMembership." + configName + ".removeMembershipFromGroups", false);
+              grouperKimSaveMembershipProperties.setRemoveMembershipFromGroups(removeMembershipFromGroups);
+            }
+            
+            {
+              String emailAdmins = GrouperClientUtils.propertiesValue(
+                  "kuali.edoclite.saveMembership." + configName + ".emailAdmins", false);
+              grouperKimSaveMembershipProperties.setEmailAdmins(emailAdmins);
+            }
+            
+            break;
           }
-          
-          {
-            String addMembershipToGroups = GrouperClientUtils.propertiesValue("kuali.edoclite.saveMembership.addMembershipToGroups." + i, false);
-            grouperKimSaveMembershipProperties.setAddMembershipToGroups(addMembershipToGroups);
-          }
-          
-          {
-            String removeMembershipFromGroups = GrouperClientUtils.propertiesValue("kuali.edoclite.saveMembership.removeMembershipFromGroups." + i, false);
-            grouperKimSaveMembershipProperties.setRemoveMembershipFromGroups(removeMembershipFromGroups);
-          }
-          
-          {
-            String emailAdmins = GrouperClientUtils.propertiesValue("kuali.edoclite.saveMembership.emailAdmins." + i, false);
-            grouperKimSaveMembershipProperties.setEmailAdmins(emailAdmins);
-          }
-          
-          break;
-        }
-        
+        }        
       }
       grouperKimSaveMembershipPropertiesCache.put(docTypeName, grouperKimSaveMembershipProperties);
     }
--- grouper-misc/grouperKimConnector/src/edu/internet2/middleware/grouperKimConnector/postProcessor/GrouperEdoclitePostProcessor.java
+++ grouper-misc/grouperKimConnector/src/edu/internet2/middleware/grouperKimConnector/postProcessor/GrouperEdoclitePostProcessor.java
@@ -4,6 +4,10 @@
  */
 package edu.internet2.middleware.grouperKimConnector.postProcessor;
 
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathException;
+import javax.xml.xpath.XPathFactory;
+
 import org.kuali.rice.kew.edl.EDocLitePostProcessor;
 import org.kuali.rice.kew.postprocessor.DocumentRouteStatusChange;
 import org.kuali.rice.kew.postprocessor.ProcessDocReport;
@@ -12,6 +16,8 @@ import org.kuali.rice.kew.service.KEWServiceLocator;
 import org.kuali.rice.kew.util.KEWConstants;
 import org.kuali.rice.kim.bo.Person;
 import org.kuali.rice.kim.service.KIMServiceLocator;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import edu.internet2.middleware.grouperClient.api.GcAddMember;
 import edu.internet2.middleware.grouperClient.api.GcDeleteMember;
@@ -21,6 +27,7 @@ import edu.internet2.middleware.grouperClient.ws.beans.WsDeleteMemberResults;
 import edu.internet2.middleware.grouperClient.ws.beans.WsSubject;
 import edu.internet2.middleware.grouperClient.ws.beans.WsSubjectLookup;
 import edu.internet2.middleware.grouperClientExt.edu.internet2.middleware.grouperClientMail.GrouperClientEmail;
+import edu.internet2.middleware.grouperClientExt.org.apache.commons.logging.Log;
 import edu.internet2.middleware.grouperKimConnector.identity.GrouperKimSaveMembershipProperties;
 import edu.internet2.middleware.grouperKimConnector.util.GrouperKimServiceUtils;
 import edu.internet2.middleware.grouperKimConnector.util.GrouperKimSubject;
@@ -31,6 +38,11 @@ import edu.internet2.middleware.grouperKimConnector.util.GrouperKimUtils;
  * provision groups
  */
 public class GrouperEdoclitePostProcessor extends EDocLitePostProcessor {
+
+  /**
+   * logger
+   */
+  private static final Log LOG = GrouperClientUtils.retrieveLog(GrouperEdoclitePostProcessor.class);
 
   /**
    * when the document goes to final, provision the group
@@ -57,89 +69,194 @@ public class GrouperEdoclitePostProcessor extends EDocLitePostProcessor {
       
       //if going to final state
       if (GrouperClientUtils.equals(KEWConstants.ROUTE_HEADER_FINAL_CD, event.getNewRouteStatus())) {
-        
-        DocumentRouteHeaderValue documentRouteHeaderValue = KEWServiceLocator
-          .getRouteHeaderService().getRouteHeader(event.getRouteHeaderId());
-        String docTypeName = documentRouteHeaderValue.getDocumentType().getName();
-        
-        GrouperKimSaveMembershipProperties grouperKimSaveMembershipProperties = 
-          GrouperKimSaveMembershipProperties.grouperKimSaveMembershipProperties(docTypeName);
-        
-        if (grouperKimSaveMembershipProperties != null) {
+        boolean sendingEmail = false;
+        StringBuilder report = new StringBuilder();
+        GrouperKimSaveMembershipProperties grouperKimSaveMembershipProperties = null;
+        String docTypeName = null;
+        try {
           
-          Person person = KIMServiceLocator.getPersonService().getPerson(documentRouteHeaderValue.getInitiatorWorkflowId());
-          String entityId = person.getEntityId();
-          boolean sendingEmail = !GrouperClientUtils.isBlank(grouperKimSaveMembershipProperties.getEmailAdmins());
+          DocumentRouteHeaderValue documentRouteHeaderValue = KEWServiceLocator
+            .getRouteHeaderService().getRouteHeader(event.getRouteHeaderId());
+          docTypeName = documentRouteHeaderValue.getDocumentType().getName();
           
-          GrouperKimSubject grouperKimSubject = GrouperKimServiceUtils.convertEntityIdToSubject(entityId);
+          grouperKimSaveMembershipProperties = 
+            GrouperKimSaveMembershipProperties.grouperKimSaveMembershipProperties(docTypeName);
           
-          WsSubject wsSubject = grouperKimSubject.getWsSubject();
-          String[] attributeNames = grouperKimSubject.getSubjectAttributeNames();
-          StringBuilder report = new StringBuilder();
-          
-          //add subject info to email report
-          if (sendingEmail) {
-            report.append(GrouperKimUtils.convertWsSubjectToStringForLog(wsSubject, attributeNames));
-            report.append("\n\n");
-          }
-          
+          if (grouperKimSaveMembershipProperties != null) {
+            
+            Person person = KIMServiceLocator.getPersonService().getPerson(documentRouteHeaderValue.getInitiatorWorkflowId());
+            String entityId = person.getEntityId();
+            sendingEmail = !GrouperClientUtils.isBlank(grouperKimSaveMembershipProperties.getEmailAdmins());
+            
+            GrouperKimSubject grouperKimSubject = GrouperKimServiceUtils.convertEntityIdToSubject(entityId);
+            
+            WsSubject wsSubject = grouperKimSubject.getWsSubject();
+            String[] attributeNames = grouperKimSubject.getSubjectAttributeNames();
+            
+            DocumentRouteHeaderValue val = KEWServiceLocator.getRouteHeaderService().getRouteHeader(event.getRouteHeaderId());
+            Document document = null;
+            
+            try {
+              document = getEDLContent(val);
+            } catch (Exception e) {
+              throw new RuntimeException("Cant find document from route header id: " + event.getRouteHeaderId(), e);
+            }
+            
+            Element root = document.getDocumentElement();
+            XPath xpath = XPathFactory.newInstance().newXPath();
 
-          if (!GrouperClientUtils.isBlank(grouperKimSaveMembershipProperties.getAddMembershipToGroups())) {
+            //Here is the doc: routeContext.getDocumentContent().getDocContent()
+            //<documentContent>
+            //  <applicationContent>
+            //    <data edlName="sampleRouteSplitLogicJoin.doctype">
+            //      <version current="false" date="Mon Feb 15 14:54:20 EST 2010" version="0" />
+            //      <version current="false" date="Mon Feb 15 14:54:24 EST 2010" version="1">
+            //        <field name="rightBranchCheckbox">
+            //          <value>true</value>
+            //        </field>
+            //      </version>
+            //     <version current="true" date="Mon Feb 15 14:56:25 EST 2010" version="5">
+            //        <field name="rightBranchCheckbox">
+            //          <value>true</value>
+            //        </field>
+            //      </version>
+            //    </data>
+            //  </applicationContent>
+            //</documentContent>
 
-            String[] groupsToAdd = GrouperClientUtils.splitTrim(grouperKimSaveMembershipProperties.getAddMembershipToGroups(), ",");
-            for (String groupToAdd : groupsToAdd) {
-              String groupName = groupToAdd.contains(":") ? groupToAdd : null;
-              String groupId = groupToAdd.contains(":") ? null : groupToAdd;
-              GcAddMember gcAddMember = new GcAddMember().addSubjectLookup(
-                  new WsSubjectLookup(wsSubject.getId(), wsSubject.getSourceId(), null));
-              if (!GrouperClientUtils.isBlank(groupName)) {
+            //add subject info to email report
+            if (sendingEmail) {
+              report.append(GrouperKimUtils.convertWsSubjectToStringForLog(wsSubject, attributeNames));
+              report.append("\n\n");
+            }
+
+            if (!GrouperClientUtils.isBlank(grouperKimSaveMembershipProperties.getEdocliteFieldPrefix())) {
+              
+              String eDocliteFieldPrefix = grouperKimSaveMembershipProperties.getEdocliteFieldPrefix();
+              
+              OUTER200: for (int i=0;i<200;i++) {
+                
+                String groupName = null;
+                try {
+                  //this xpath will give the value element for the field with name rightBranchCheckbox
+                  //in the version tag where current is true
+                  groupName = xpath.evaluate(
+                      "/documentContent/applicationContent/data/version[@current = \"true\"]" +
+                      "/field[@name = \"" + eDocliteFieldPrefix + i + "\"]/value", root);
+
+                } catch (NullPointerException npe) {
+                  //ignore this
+                } catch (XPathException xpe) {
+                  //this means we are probably done here
+                  break OUTER200;
+                }
+
+                if (GrouperClientUtils.isBlank(groupName)) {
+                  continue;
+                }
+                
+                groupName = GrouperClientUtils.trim(groupName);
+                
+                if (!GrouperClientUtils.isBlank(grouperKimSaveMembershipProperties.getEnteredGroupNamePrefix())) {
+                  
+                  groupName = grouperKimSaveMembershipProperties.getEnteredGroupNamePrefix() + groupName;
+                  
+                }
+                
+                //make sure its ok
+                if (!grouperKimSaveMembershipProperties.allowedToAccessGroup(groupName)) {
+                  throw new RuntimeException("Not allowed to access groupName: '" + groupName + "'");
+                }
+                
+                GcAddMember gcAddMember = new GcAddMember().addSubjectLookup(
+                    new WsSubjectLookup(wsSubject.getId(), wsSubject.getSourceId(), null));
                 gcAddMember.assignGroupName(groupName);
-              }
-              if (!GrouperClientUtils.isBlank(groupId)) {
-                gcAddMember.assignGroupUuid(groupId);
-              }
-              WsAddMemberResults wsAddMemberResults = gcAddMember.execute();
-              
-              // based on result, add report
-              if (sendingEmail) {
-                report.append("Group addMember: ").append(wsAddMemberResults.getWsGroupAssigned().getName());
-                report.append(" - ").append(wsAddMemberResults.getResults()[0].getResultMetadata().getResultCode()).append("\n\n");
-              }
-              
-            }
-          }
-          
-          if (!GrouperClientUtils.isBlank(grouperKimSaveMembershipProperties.getRemoveMembershipFromGroups())) {
+                WsAddMemberResults wsAddMemberResults = gcAddMember.execute();
+                
+                // based on result, add report
+                if (sendingEmail) {
+                  report.append("Group addMember: ").append(wsAddMemberResults.getWsGroupAssigned().getName());
+                  report.append(" - ").append(wsAddMemberResults.getResults()[0].getResultMetadata().getResultCode()).append("\n\n");
+                }
 
-            String[] groupsToRemove = GrouperClientUtils.splitTrim(grouperKimSaveMembershipProperties.getRemoveMembershipFromGroups(), ",");
-            for (String groupToRemove : groupsToRemove) {
-              String groupName = groupToRemove.contains(":") ? groupToRemove : null;
-              String groupId = groupToRemove.contains(":") ? null : groupToRemove;
-              GcDeleteMember gcDeleteMember = new GcDeleteMember().addSubjectLookup(
-                  new WsSubjectLookup(wsSubject.getId(), wsSubject.getSourceId(), null));
-              if (!GrouperClientUtils.isBlank(groupName)) {
-                gcDeleteMember.assignGroupName(groupName);
+                
               }
-              if (!GrouperClientUtils.isBlank(groupId)) {
-                gcDeleteMember.assignGroupUuid(groupId);
-              }
-              WsDeleteMemberResults wsDeleteMemberResults = gcDeleteMember.execute();
               
-              // based on result, add report
-              if (sendingEmail) {
-                report.append("Group removeMember: ").append(wsDeleteMemberResults.getWsGroup().getName());
-                report.append(" - ").append(wsDeleteMemberResults.getResults()[0].getResultMetadata().getResultCode()).append("\n\n");
+              
+            }
+  
+            if (!GrouperClientUtils.isBlank(grouperKimSaveMembershipProperties.getAddMembershipToGroups())) {
+  
+              String[] groupsToAdd = GrouperClientUtils.splitTrim(grouperKimSaveMembershipProperties.getAddMembershipToGroups(), ",");
+              for (String groupToAdd : groupsToAdd) {
+                String groupName = groupToAdd.contains(":") ? groupToAdd : null;
+                String groupId = groupToAdd.contains(":") ? null : groupToAdd;
+                GcAddMember gcAddMember = new GcAddMember().addSubjectLookup(
+                    new WsSubjectLookup(wsSubject.getId(), wsSubject.getSourceId(), null));
+                if (!GrouperClientUtils.isBlank(groupName)) {
+                  gcAddMember.assignGroupName(groupName);
+                }
+                if (!GrouperClientUtils.isBlank(groupId)) {
+                  gcAddMember.assignGroupUuid(groupId);
+                }
+                WsAddMemberResults wsAddMemberResults = gcAddMember.execute();
+                
+                // based on result, add report
+                if (sendingEmail) {
+                  report.append("Group addMember: ").append(wsAddMemberResults.getWsGroupAssigned().getName());
+                  report.append(" - ").append(wsAddMemberResults.getResults()[0].getResultMetadata().getResultCode()).append("\n\n");
+                }
+                
               }
             }
+            
+            if (!GrouperClientUtils.isBlank(grouperKimSaveMembershipProperties.getRemoveMembershipFromGroups())) {
+  
+              String[] groupsToRemove = GrouperClientUtils.splitTrim(grouperKimSaveMembershipProperties.getRemoveMembershipFromGroups(), ",");
+              for (String groupToRemove : groupsToRemove) {
+                String groupName = groupToRemove.contains(":") ? groupToRemove : null;
+                String groupId = groupToRemove.contains(":") ? null : groupToRemove;
+                GcDeleteMember gcDeleteMember = new GcDeleteMember().addSubjectLookup(
+                    new WsSubjectLookup(wsSubject.getId(), wsSubject.getSourceId(), null));
+                if (!GrouperClientUtils.isBlank(groupName)) {
+                  gcDeleteMember.assignGroupName(groupName);
+                }
+                if (!GrouperClientUtils.isBlank(groupId)) {
+                  gcDeleteMember.assignGroupUuid(groupId);
+                }
+                WsDeleteMemberResults wsDeleteMemberResults = gcDeleteMember.execute();
+                
+                // based on result, add report
+                if (sendingEmail) {
+                  report.append("Group removeMember: ").append(wsDeleteMemberResults.getWsGroup().getName());
+                  report.append(" - ").append(wsDeleteMemberResults.getResults()[0].getResultMetadata().getResultCode()).append("\n\n");
+                }
+              }
+            }
+            
+            if (sendingEmail) {
+              new GrouperClientEmail().setBody(report.toString()).setSubject("Grouper Rice auto-provision for document: " + docTypeName)
+                .setTo(grouperKimSaveMembershipProperties.getEmailAdmins()).send();
+            }
+            
           }
+        } catch (RuntimeException e) {
           
-          if (sendingEmail) {
-            new GrouperClientEmail().setBody(report.toString()).setSubject("Grouper Rice auto-provision for document: " + docTypeName)
-              .setTo(grouperKimSaveMembershipProperties.getEmailAdmins()).send();
+          //lets still send an email so people know what is going on...
+          if (sendingEmail && grouperKimSaveMembershipProperties != null) {
+            try {
+              report.append("\n\n").append(GrouperClientUtils.getFullStackTrace(e));
+              new GrouperClientEmail().setBody(report.toString()).setSubject("Grouper Rice auto-provision ERROR for document: " + docTypeName)
+                .setTo(grouperKimSaveMembershipProperties.getEmailAdmins()).send();
+              
+            } catch (Exception e2) {
+              LOG.error("error though not preempting other exception", e2);
+              //dont rethrow in case preempting existing exception
+            }
+            
           }
-          
+          throw e;
         }
-        
       }
     }
   }
