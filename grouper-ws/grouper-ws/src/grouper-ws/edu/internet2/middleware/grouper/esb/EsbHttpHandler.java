7feda423d4815144666f25450370943777e8fb5c
hebronr <hebronr@mycompany.com>
2011-04-02 17:37:21 +0000

    Refactored EsdHttpHandler for code style
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@7279 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper-ws/grouper-ws/src/grouper-ws/edu/internet2/middleware/grouper/esb/EsbHttpHandler.java
+++ grouper-ws/grouper-ws/src/grouper-ws/edu/internet2/middleware/grouper/esb/EsbHttpHandler.java
@@ -1,7 +1,3 @@
-/*
- * @author Rob Hebron
- */
-
 package edu.internet2.middleware.grouper.esb;
 
 import java.io.DataInputStream;
@@ -19,56 +15,68 @@ import edu.internet2.middleware.grouper.util.GrouperUtil;
 
 /**
  * 
- * Class to processes data received on servlet interface, extracts the payload and passes it to {@link EsbListener} 
- * for processing, returning an http result code and human readable result string to calling client
- *
+ * Class to processes data received on servlet interface, extracts the payload
+ * and passes it to {@link EsbListener} for processing, returning an http result
+ * code and human readable result string to calling client
+ * 
+ * @author rob
+ * @version $Revision: 1.0 $
  */
+@SuppressWarnings("serial")
 public class EsbHttpHandler extends HttpServlet {
 
-  private GrouperSession grouperSession;
+    /**
+     * Field LOG.
+     */
+    private static final Log LOG = GrouperUtil.getLog(EsbHttpHandler.class);
 
-  private EsbListener esbListener;
+    /**
+     * Method doPost.
+     * 
+     * @param request
+     *            HttpServletRequest
+     * @param response
+     *            HttpServletResponse
+     * @throws IOException
+     */
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        try {
+            if (request.getContentLength() < 0) {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Invalid content received, ignoring");
+                }
+                response.setContentType("text/html;charset=utf-8");
+                response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+                response.flushBuffer();
+            } else {
+                final byte[] data = new byte[request.getContentLength()];
+                final DataInputStream in = new DataInputStream(
+                        request.getInputStream());
+                in.readFully(data);
+                in.close();
+                final String jsonString = new String(data);
+                final GrouperSession grouperSession = GrouperSession
+                        .startRootSession();
+                final EsbListener esbListener = new EsbListener();
+                final String result = esbListener.processEvent(jsonString,
+                        grouperSession);
+                response.setContentType("text/html;charset=utf-8");
+                response.setStatus(HttpServletResponse.SC_OK);
+                response.getWriter().print(result);
+                response.flushBuffer();
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Result " + result);
+                }
+                grouperSession.stop();
 
-  private static final Log LOG = GrouperUtil.getLog(EsbHttpHandler.class);
-  
-  public void doPost(HttpServletRequest request,
-      HttpServletResponse response) {
-	  //Enumeration<String> paramEnum = request.getParameterNames();
-	  //paramEnum.hasMoreElements();
-	  //String postContent = (String) paramEnum.nextElement();
-	  try {
-	  if(request.getContentLength()<0) {
-		  if (LOG.isDebugEnabled())
-		        LOG.debug("Invalid content received, ignoring");
-		      response.setContentType("text/html;charset=utf-8");
-		      response.setStatus(HttpServletResponse.SC_NO_CONTENT);
-		      response.flushBuffer();
-	  } else {
-		  byte[] data = new byte[request.getContentLength()];
-	      DataInputStream in = new DataInputStream(request.getInputStream());
-	      in.readFully(data);
-	      in.close();
-	      String jsonString = new String(data);
-	      this.grouperSession = GrouperSession.startRootSession();
-	      if (this.esbListener == null)
-	        this.esbListener = new EsbListener();
-	      String result = esbListener.processEvent(jsonString, grouperSession);
-	      response.setContentType("text/html;charset=utf-8");
-	      response.setStatus(HttpServletResponse.SC_OK);
-	      response.getWriter().print(result);
-	      response.flushBuffer();
-	      if (LOG.isDebugEnabled()) {
-	        LOG.debug("Result " + result);
-	      }
-	      this.grouperSession.stop();
-		  
-	  }
-	  }catch (IOException e) {
-		  e.printStackTrace();
-	  }
-	  
-  }
-  
-  
+            }
+        } catch (IOException e) {
+            LOG.error(e.getMessage());
+            throw e;
+
+        }
+
+    }
 
 }
