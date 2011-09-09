a1a0f44909bfa9c6f4af795fb44845866f9dccf0
Chris Hyzer <mchyzer@isc.upenn.edu>
2011-09-09 17:47:35 +0000

    initial import
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@7545 bc24b658-cf39-4249-b827-e3526eaaeb42

---
+++ grouper-misc/poc_secureUserData/src/edu/internet2/middleware/poc_secureUserData/SudFullSync.java
@@ -0,0 +1,20 @@
+package edu.internet2.middleware.poc_secureUserData;
+
+/**
+ * @author mchyzer
+ * $Id$
+ */
+
+/**
+ * main class that syncs the secure user data
+ */
+public class SudFullSync {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    
+  }
+
+}
