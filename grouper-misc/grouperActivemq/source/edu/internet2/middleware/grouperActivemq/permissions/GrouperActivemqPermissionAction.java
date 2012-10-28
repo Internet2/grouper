e93c60c5e8d729b1b12c56db397d0174f73e35ea
Chris Hyzer <mchyzer@isc.upenn.edu>
2012-10-28 18:46:46 +0000

    finish activemq connector
    git-svn-id: https://svn.internet2.edu/svn/i2mi/branches/GROUPER_2_1_BRANCH@8487 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper-misc/grouperActivemq/source/edu/internet2/middleware/grouperActivemq/permissions/GrouperActivemqPermissionAction.java
+++ grouper-misc/grouperActivemq/source/edu/internet2/middleware/grouperActivemq/permissions/GrouperActivemqPermissionAction.java
@@ -4,28 +4,110 @@
  */
 package edu.internet2.middleware.grouperActivemq.permissions;
 
+import edu.internet2.middleware.grouperClient.util.GrouperClientUtils;
+
 
 /**
- *
+ * action for a permission assignment
  */
 public enum GrouperActivemqPermissionAction {
 
   /** send a message */
-  sendMessage,
+  sendMessage {
+
+    @Override
+    public boolean actionAllowedIfArgumentAllowed(
+        GrouperActivemqPermissionAction grouperActivemqPermissionAction) {
+      return grouperActivemqPermissionAction == sendMessageInherit;
+    }
+  },
   
   /** send a message on this destination or sub destinations */
-  sendMessageInherit,
+  sendMessageInherit {
+
+    @Override
+    public boolean actionAllowedIfArgumentAllowed(
+        GrouperActivemqPermissionAction grouperActivemqPermissionAction) {
+      return false;
+    }
+  },
   
   /** receive a message */
-  receiveMessage,
+  receiveMessage {
+
+    @Override
+    public boolean actionAllowedIfArgumentAllowed(
+        GrouperActivemqPermissionAction grouperActivemqPermissionAction) {
+      return grouperActivemqPermissionAction == receiveMessageInherit;
+    }
+  },
   
   /** receive a message on this destination or sub destinations */
-  receiveMessageInherit,
+  receiveMessageInherit {
+
+    @Override
+    public boolean actionAllowedIfArgumentAllowed(
+        GrouperActivemqPermissionAction grouperActivemqPermissionAction) {
+      return false;
+    }
+  },
   
   /** create a queue/topic in this destination or somewhere under this destination */
-  createDestination,
+  createDestination {
+
+    @Override
+    public boolean actionAllowedIfArgumentAllowed(
+        GrouperActivemqPermissionAction grouperActivemqPermissionAction) {
+      return grouperActivemqPermissionAction == createDestinationInherit;
+    }
+  },
   
   /** delete a queue/topic in this destinate or somewhere under this destination */
-  deleteDestination;
+  deleteDestination {
+
+    @Override
+    public boolean actionAllowedIfArgumentAllowed(
+        GrouperActivemqPermissionAction grouperActivemqPermissionAction) {
+      return grouperActivemqPermissionAction == deleteDestinationInherit;
+    }
+  }, 
+  
+  /** create a queue/topic in this destination or somewhere under this destination */
+  createDestinationInherit {
+  
+    @Override
+    public boolean actionAllowedIfArgumentAllowed(
+        GrouperActivemqPermissionAction grouperActivemqPermissionAction) {
+      return false;
+    }
+  }, 
+  
+  /** delete a queue/topic in this destinate or somewhere under this destination */
+  deleteDestinationInherit {
+  
+    @Override
+    public boolean actionAllowedIfArgumentAllowed(
+        GrouperActivemqPermissionAction grouperActivemqPermissionAction) {
+      return false;
+    }
+  };
+ 
+  /**
+   * do a case-insensitive matching
+   * 
+   * @param string
+   * @param exceptionOnNotFound
+   * @return the enum or null or exception if not found
+   */
+  public static GrouperActivemqPermissionAction valueOfIgnoreCase(String string, boolean exceptionOnNotFound) {
+    return GrouperClientUtils.enumValueOfIgnoreCase(GrouperActivemqPermissionAction.class,string, exceptionOnNotFound );
+  }
+
+  /**
+   * 
+   * @param grouperActivemqPermissionAction
+   * @return true if this action is allowed since argument is allowed
+   */
+  public abstract boolean actionAllowedIfArgumentAllowed(GrouperActivemqPermissionAction grouperActivemqPermissionAction);
   
 }
