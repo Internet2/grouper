e49f10295ea07f963d53ac9a3a579ba161999132
Chris Hyzer <mchyzer@isc.upenn.edu>
2013-08-08 20:53:28 +0000

    GRP-925: LDAP_GROUPS_FROM_ATTRIBUTES where membership carried in two attributes
    git-svn-id: https://svn.internet2.edu/svn/i2mi/branches/GROUPER_2_1_BRANCH@8693 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper/src/grouper/edu/internet2/middleware/grouper/app/loader/db/GrouperLoaderResultset.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/app/loader/db/GrouperLoaderResultset.java
@@ -709,7 +709,14 @@ public class GrouperLoaderResultset {
                 attributesList.add(subjectAttributeName);
               }
               //there must be a group attribute points to group
-              attributesList.add(groupAttributeName);
+              // and multiple attributes may be present and separated
+              // by a comma
+              String[] groupAttributeNameArray = null;
+              groupAttributeNameArray = GrouperUtil.splitTrim(groupAttributeName, ",");
+              for (String attribute: groupAttributeNameArray) {
+                attributesList.add(attribute);
+              }
+              
               String[] extraAttributeArray = null;
 
               if (!StringUtils.isBlank(extraAttributes)) {
@@ -814,120 +821,124 @@ public class GrouperLoaderResultset {
                 if (StringUtils.isBlank(groupAttributeName)) {
                   throw new RuntimeException("LDAP_GROUPS_FROM_ATTRIBUTES loader type requires group attribute name");
                 }
+
+                // loop over attribute names that indicate group membership
+                for (String attribute: groupAttributeNameArray) {
                 
-                Attribute groupAttribute = searchResult.getAttributes().get(
-                    groupAttributeName);
+                  Attribute groupAttribute = searchResult.getAttributes().get(attribute);
 
-                if (groupAttribute != null) {
-                  for (int i = 0; i < groupAttribute.size(); i++) {
-
-                    Object attributeValue = groupAttribute.get(i);
-                    attributeValue = GrouperUtil.typeCast(attributeValue, String.class);
-                    if (attributeValue != null) {
-                      subObjectCount++;
-                      
-                      //lets see if we know the groupName
-                      String groupName = attributeNameToGroupNameMap.get(attributeValue);
-                      if (StringUtils.isBlank(groupName)) {
-
-                        //lets see if valid attribute, see if a filter expression is set
-                        if (!StringUtils.isBlank(ldapAttributeFilterExpression)) {
-                          //see if we have already calculated it
-                          if (!validAttributes.containsKey(attributeValue)) {
-                            
-                            Map<String, Object> variableMap = new HashMap<String, Object>();
-                            variableMap.put("attributeValue", attributeValue);
-                            
-                            //lets run the filter on the attribute name
-                            String attributeResultBooleanString = GrouperUtil.substituteExpressionLanguage(
-                                ldapAttributeFilterExpression, variableMap, true, false, false);
-                            
-                            boolean attributeResultBoolean = false;
-                            try {
-                              attributeResultBoolean = GrouperUtil.booleanValue(attributeResultBooleanString);
-                            } catch (RuntimeException re) {
-                              throw new RuntimeException("Error parsing boolean: '" + attributeResultBooleanString 
-                                  + "', expecting true or false, from expression: " + ldapAttributeFilterExpression );
+                  if (groupAttribute != null) {
+                    for (int i = 0; i < groupAttribute.size(); i++) {
+  
+                      Object attributeValue = groupAttribute.get(i);
+                      attributeValue = GrouperUtil.typeCast(attributeValue, String.class);
+                      if (attributeValue != null) {
+                        subObjectCount++;
+                        
+                        //lets see if we know the groupName
+                        String groupName = attributeNameToGroupNameMap.get(attributeValue);
+                        if (StringUtils.isBlank(groupName)) {
+  
+                          //lets see if valid attribute, see if a filter expression is set
+                          if (!StringUtils.isBlank(ldapAttributeFilterExpression)) {
+                            //see if we have already calculated it
+                            if (!validAttributes.containsKey(attributeValue)) {
+                              
+                              Map<String, Object> variableMap = new HashMap<String, Object>();
+                              variableMap.put("attributeValue", attributeValue);
+                              
+                              //lets run the filter on the attribute name
+                              String attributeResultBooleanString = GrouperUtil.substituteExpressionLanguage(
+                                  ldapAttributeFilterExpression, variableMap, true, false, false);
+                              
+                              boolean attributeResultBoolean = false;
+                              try {
+                                attributeResultBoolean = GrouperUtil.booleanValue(attributeResultBooleanString);
+                              } catch (RuntimeException re) {
+                                throw new RuntimeException("Error parsing boolean: '" + attributeResultBooleanString 
+                                    + "', expecting true or false, from expression: " + ldapAttributeFilterExpression );
+                              }
+                              
+                              if (LOG.isDebugEnabled()) {
+                                LOG.debug("Attribute '" + attributeValue + "' is allowed to be used based on expression? " 
+                                    + attributeResultBoolean + ", '" + ldapAttributeFilterExpression + "', note the attributeValue is" +
+                                    		" in a variable called attributeValue");
+                              }
+                              
+                              validAttributes.put((String)attributeValue, attributeResultBoolean);
+                              
                             }
                             
-                            if (LOG.isDebugEnabled()) {
-                              LOG.debug("Attribute '" + attributeValue + "' is allowed to be used based on expression? " 
-                                  + attributeResultBoolean + ", '" + ldapAttributeFilterExpression + "', note the attributeValue is" +
-                                  		" in a variable called attributeValue");
+                            //lets see if filtering
+                            if (!validAttributes.get(attributeValue)) {
+                              continue;
                             }
+                          }
+                          
+                          subObjectValidCount++;
+                          
+                          String defaultFolder = defaultLdapFolder();
+                          
+                          groupName = defaultFolder + attributeValue;
+                          
+                          
+                          String loaderGroupDisplayName = null;
+                          String loaderGroupDescription = null;
+                          
+                          if (!StringUtils.isBlank(groupNameExpression)
+                              || !StringUtils.isBlank(groupDisplayNameExpression)
+                              || !StringUtils.isBlank(groupDescriptionExpression)) {
                             
-                            validAttributes.put((String)attributeValue, attributeResultBoolean);
+                            //calculate it
+  
+                            Map<String, Object> envVars = new HashMap<String, Object>();
+  
+                            envVars.put("groupAttribute", attributeValue);
                             
+                            if (!StringUtils.isBlank(groupNameExpression)) {
+                              groupName = LoaderLdapUtils.substituteEl(groupNameExpression,
+                                  envVars);
+                            }
+                            if (!StringUtils.isBlank(groupDisplayNameExpression)) {
+                              String elGroupDisplayName = LoaderLdapUtils.substituteEl(groupDisplayNameExpression,
+                                  envVars);
+                              loaderGroupDisplayName = groupParentFolderName + elGroupDisplayName;
+                            }
+                            if (!StringUtils.isBlank(groupDescriptionExpression)) {
+                              String elGroupDescription = LoaderLdapUtils.substituteEl(groupDescriptionExpression,
+                                  envVars);
+                              loaderGroupDescription = elGroupDescription;
+                            }
                           }
                           
-                          //lets see if filtering
-                          if (!validAttributes.get(attributeValue)) {
-                            continue;
-                          }
-                        }
-                        
-                        subObjectValidCount++;
-                        
-                        String defaultFolder = defaultLdapFolder();
-                        
-                        groupName = defaultFolder + attributeValue;
-                        
-                        
-                        String loaderGroupDisplayName = null;
-                        String loaderGroupDescription = null;
-                        
-                        if (!StringUtils.isBlank(groupNameExpression)
-                            || !StringUtils.isBlank(groupDisplayNameExpression)
-                            || !StringUtils.isBlank(groupDescriptionExpression)) {
+                          groupName = groupParentFolderName + groupName;
                           
-                          //calculate it
-
-                          Map<String, Object> envVars = new HashMap<String, Object>();
-
-                          envVars.put("groupAttribute", attributeValue);
+                          if (!StringUtils.isBlank(loaderGroupDisplayName)) {
+                            groupNameToDisplayName.put(groupName, loaderGroupDisplayName);
+                          }
                           
-                          if (!StringUtils.isBlank(groupNameExpression)) {
-                            groupName = LoaderLdapUtils.substituteEl(groupNameExpression,
-                                envVars);
+                          if (!StringUtils.isBlank(loaderGroupDescription)) {
+                            groupNameToDescription.put(groupName, loaderGroupDescription);
                           }
-                          if (!StringUtils.isBlank(groupDisplayNameExpression)) {
-                            String elGroupDisplayName = LoaderLdapUtils.substituteEl(groupDisplayNameExpression,
-                                envVars);
-                            loaderGroupDisplayName = groupParentFolderName + elGroupDisplayName;
-                          }
-                          if (!StringUtils.isBlank(groupDescriptionExpression)) {
-                            String elGroupDescription = LoaderLdapUtils.substituteEl(groupDescriptionExpression,
-                                envVars);
-                            loaderGroupDescription = elGroupDescription;
-                          }
-                        }
-                        
-                        groupName = groupParentFolderName + groupName;
-                        
-                        if (!StringUtils.isBlank(loaderGroupDisplayName)) {
-                          groupNameToDisplayName.put(groupName, loaderGroupDisplayName);
-                        }
-                        
-                        if (!StringUtils.isBlank(loaderGroupDescription)) {
-                          groupNameToDescription.put(groupName, loaderGroupDescription);
-                        }
-                        
-                        //cache this
-                        attributeNameToGroupNameMap.put((String)attributeValue, groupName);
-                        
-                        //init the subject list
+                          
+                          //cache this
+                          attributeNameToGroupNameMap.put((String)attributeValue, groupName);
+                          
+                          //init the subject list
                         if (!result.containsKey(groupName)) {
                           result.put(groupName, new ArrayList<String>());
                         }
                       }
-                      //get the "row" for the group
-                      List<String> valueResults = result.get(groupName);
-                      //add the subject
-                      valueResults.add((String) subjectId);
+                        //get the "row" for the group
+                        List<String> valueResults = result.get(groupName);
+                        //add the subject
+                        valueResults.add((String) subjectId);
+                      }
                     }
                   }
-                }
-              }
+                
+                } // end of looping over attributes indicating group membership
+              } // end of looping over search results
 
               
               if (LOG.isDebugEnabled()) {
