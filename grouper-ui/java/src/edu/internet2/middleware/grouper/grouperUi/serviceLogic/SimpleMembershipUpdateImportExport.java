cfab8f2baa39f5e3579561b04b989c3994a94108
Chris Hyzer <mchyzer@isc.upenn.edu>
2014-03-22 21:25:38 +0000

    group import
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@8912 bc24b658-cf39-4249-b827-e3526eaaeb42

--- grouper-ui/java/src/edu/internet2/middleware/grouper/grouperUi/serviceLogic/SimpleMembershipUpdateImportExport.java
+++ grouper-ui/java/src/edu/internet2/middleware/grouper/grouperUi/serviceLogic/SimpleMembershipUpdateImportExport.java
@@ -652,17 +652,6 @@ public class SimpleMembershipUpdateImportExport {
     /**
      * @param message
      * @param cause
-     * @param enableSuppression
-     * @param writableStackTrace
-     */
-    public GrouperImportException(String message, Throwable cause,
-        boolean enableSuppression, boolean writableStackTrace) {
-      super(message, cause, enableSuppression, writableStackTrace);
-    }
-
-    /**
-     * @param message
-     * @param cause
      */
     public GrouperImportException(String message, Throwable cause) {
       super(message, cause);
