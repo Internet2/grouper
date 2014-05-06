e9c1e02849899ff01e7a32f0bfd4465580fc9828
Chris Hyzer <mchyzer@isc.upenn.edu>
2014-05-06 03:17:55 +0000

    updates to dojo to make things work better
    git-svn-id: https://svn.internet2.edu/svn/i2mi/trunk@8969 bc24b658-cf39-4249-b827-e3526eaaeb42

---
+++ grouper-misc/grouper-ui-dojo/dojo/grouper/nls/grouper_ja-jp.js
@@ -0,0 +1,9 @@
+define('grouper/nls/grouper_ja-jp',{
+'dijit/form/nls/ComboBox':{"previousMessage":"以前の選択項目","nextMessage":"追加の選択項目"}
+,
+'dijit/nls/loading':{"loadingState":"ロード中...","errorState":"エラーが発生しました。"}
+,
+'dijit/form/nls/validate':{"rangeMessage":"この値は範囲外です。","invalidMessage":"入力した値は無効です。","missingMessage":"この値は必須です。"}
+,
+'dijit/nls/common':{"buttonOk":"OK","buttonCancel":"キャンセル","buttonSave":"保存","itemClose":"閉じる"}
+});
---
+++ grouper-misc/grouper-ui-dojo/dojo/grouper/nls/grouper_pt-pt.js
@@ -0,0 +1,9 @@
+define('grouper/nls/grouper_pt-pt',{
+'dijit/form/nls/ComboBox':{"previousMessage":"Opções anteriores","nextMessage":"Mais opções"}
+,
+'dijit/nls/loading':{"loadingState":"A carregar...","errorState":"Lamentamos, mas ocorreu um erro"}
+,
+'dijit/form/nls/validate':{"rangeMessage":"Este valor encontra-se fora do intervalo.","invalidMessage":"O valor introduzido não é válido.","missingMessage":"Este valor é requerido."}
+,
+'dijit/nls/common':{"buttonOk":"OK","buttonCancel":"Cancelar","buttonSave":"Guardar","itemClose":"Fechar"}
+});
