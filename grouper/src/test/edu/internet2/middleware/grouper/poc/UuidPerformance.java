f05eb13aa7c8a57f5272181042b1aad88de58bc2
Misagh Moayyed <mmoayyed@unicon.net>
2014-08-06 11:31:29 -0700

    fixed build issues

--- grouper/src/test/edu/internet2/middleware/grouper/poc/UuidPerformance.java
+++ grouper/src/test/edu/internet2/middleware/grouper/poc/UuidPerformance.java
@@ -1,5 +1,5 @@
-/*******************************************************************************
- * Copyright 2012 Internet2
+/**
+ * Copyright 2014 Internet2
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ******************************************************************************/
+ */
+
 /*
  * @author mchyzer
  * $Id: UuidPerformance.java,v 1.2 2009-08-11 20:18:08 mchyzer Exp $
