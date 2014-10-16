c7cfd640a83859f3f26a27484409e507a9dcbe42
mchyzer <mchyzer@yahoo.com>
2014-10-15 20:29:42 -0400

    GRP-1062: add symmetric encryption method to grouper client library

---
+++ grouper-misc/grouperClient/src/java/edu/internet2/middleware/grouperClient/encryption/GcSymmetricEncryptAesCbcPkcs5Padding.java
@@ -0,0 +1,143 @@
+/**
+ * @author mchyzer $Id$
+ */
+package edu.internet2.middleware.grouperClient.encryption;
+
+import java.util.Random;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import edu.internet2.middleware.grouperClientExt.org.apache.commons.codec.binary.Base64;
+
+/**
+ * AES/ECB/PKCS5Padding
+ */
+public class GcSymmetricEncryptAesCbcPkcs5Padding implements GcEncryptionInterface {
+
+  /**
+   * 
+   * @param args
+   */
+  public static void main(String[] args) {
+
+    String secret = GcGenerateKey.generateKeyAes128base64();
+    
+    System.out.println("Key is: " + secret);
+    
+    String encrypted = new GcSymmetricEncryptAesCbcPkcs5Padding().encrypt(secret, "xyz");
+    
+    System.out.println("xyz encrypted: " + encrypted);
+    
+    String clear = new GcSymmetricEncryptAesCbcPkcs5Padding().decrypt(secret, encrypted);
+    
+    System.out.println("Clear (should be xyz): " + clear);
+    
+    encrypted = new GcSymmetricEncryptAesCbcPkcs5Padding().encrypt(secret, "xyz");
+    
+    System.out.println("xyz encrypted (should be different with different initialization vector): " + encrypted);
+    
+    clear = new GcSymmetricEncryptAesCbcPkcs5Padding().decrypt(secret, encrypted);
+    
+    System.out.println("Clear (should be xyz): " + clear);
+    
+  }
+  
+  /**
+   * @param key is base64 128 bit key, generate from GcGenerateKey
+   * @see GcEncryptionInterface#encrypt(String, String)
+   * @return the base64 encrypted result with initialization vector
+   */
+  public String encrypt(String key, String data) {
+
+    try {
+
+      byte[] keyBytes = null;
+      
+      try {
+        keyBytes = new Base64().decode(key.getBytes());
+      } catch (Exception e) {
+        throw new RuntimeException("Key must be 128 bit and encoded with base64", e);
+      }
+
+      if (keyBytes.length != 128/8) {
+        throw new RuntimeException("Key must be 128 bit and encoded with base64");
+      }
+      
+      SecretKeySpec skeySpec = new SecretKeySpec(keyBytes, "AES");
+
+      byte[] iv = new byte[16]; //Means 128 bit
+      new Random().nextBytes(iv);
+      
+      IvParameterSpec ivspec = new IvParameterSpec(iv);
+
+      // initialize the cipher for encrypt mode
+      Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+      cipher.init(Cipher.ENCRYPT_MODE, skeySpec, ivspec);
+
+      // encrypt the message
+      byte[] encrypted = cipher.doFinal(data.getBytes());
+
+      byte[] result = new byte[iv.length + encrypted.length];
+      
+      //copy initialization vector to result
+      System.arraycopy(iv, 0, result, 0, iv.length);
+      System.arraycopy(encrypted, 0, result, iv.length, encrypted.length);
+      
+      return new String(new Base64().encode(result), "UTF-8");
+
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
+  }
+
+  /**
+   * @see GcEncryptionInterface#decrypt(String, String)
+   */
+  public String decrypt(String key, String encryptedData) {
+
+    try {
+
+      byte[] keyBytes = null;
+      
+      try {
+        keyBytes = new Base64().decode(key.getBytes());
+      } catch (Exception e) {
+        throw new RuntimeException("Key must be 128 bit and encoded with base64", e);
+      }
+
+      if (keyBytes.length != 128/8) {
+        throw new RuntimeException("Key must be 128 bit and encoded with base64");
+      }
+
+      SecretKeySpec skeySpec = new SecretKeySpec(keyBytes, "AES");
+
+      // build the initialization vector.  This example is all zeros, but it 
+      // could be any value or generated using a random number generator.
+      byte[] iv = new byte[16];
+      
+      byte[] totalBytes = new Base64().decode(encryptedData.getBytes());
+
+      byte[] encryptedDataBytes = new byte[totalBytes.length-iv.length];
+
+      System.arraycopy(totalBytes, 0, iv, 0, iv.length);
+      System.arraycopy(totalBytes, iv.length, encryptedDataBytes, 0, encryptedDataBytes.length);
+
+      IvParameterSpec ivspec = new IvParameterSpec(iv);
+
+      // reinitialize the cipher for decryption
+      Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+      cipher.init(Cipher.DECRYPT_MODE, skeySpec, ivspec);
+
+      // decrypt the message
+      byte[] decrypted = cipher.doFinal(encryptedDataBytes);
+
+      return new String(decrypted, "UTF-8");
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+}
