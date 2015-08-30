b0531fb076000844b8c12cc7f035bae582ebe9b6
Chris Hyzer <mchyzer@yahoo.com>
2015-08-30 17:27:14 -0400

    GRP-1188: print an error if Grouper is used with invalid version of Java

--- grouper-misc/grouper-installer/src/java/edu/internet2/middleware/grouperInstaller/GrouperInstaller.java
+++ grouper-misc/grouper-installer/src/java/edu/internet2/middleware/grouperInstaller/GrouperInstaller.java
@@ -8082,6 +8082,16 @@ public class GrouperInstaller {
    * @return true if install, or false if upgrade
    */
   private GrouperInstallerMainFunction grouperInstallerMainFunction() {
+
+    if (GrouperInstallerUtils.propertiesValueBoolean("grouperInstaller.checkJavaVersion", true, false)) {
+      String javaVersion = System.getProperty("java.version");
+      if (javaVersion != null && !javaVersion.startsWith("1.6") && !javaVersion.startsWith("1.7")) {
+        System.out.println("Error: Java should be version 6 or 7 (1.6 or 1.7), but is detected as: " + javaVersion);        
+        System.out.println("Press <enter> to continue...");
+        readFromStdIn("grouperInstaller.autorun.wrongJavaContinue");
+      }
+    }
+
     String input = null;
     String defaultAction = GrouperInstallerUtils.propertiesValue("grouperInstaller.default.installOrUpgrade", false);
     defaultAction = GrouperInstallerUtils.defaultIfBlank(defaultAction, "install");
