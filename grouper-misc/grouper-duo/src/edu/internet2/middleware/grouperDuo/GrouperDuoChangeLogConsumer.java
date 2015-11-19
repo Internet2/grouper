bf72cb508b448e42756bb68aedaf4d77a20854b2
Chris Hyzer <mchyzer@yahoo.com>
2015-11-19 13:14:57 -0500

    register cron
    Conflicts:
    	grouper-misc/grouper-duo/src/edu/internet2/middleware/grouperDuo/GrouperDuoChangeLogConsumer.java

--- grouper-misc/grouper-duo/src/edu/internet2/middleware/grouperDuo/GrouperDuoChangeLogConsumer.java
+++ grouper-misc/grouper-duo/src/edu/internet2/middleware/grouperDuo/GrouperDuoChangeLogConsumer.java
@@ -28,6 +28,14 @@ import edu.internet2.middleware.subject.Subject;
 public class GrouperDuoChangeLogConsumer extends ChangeLogConsumerBase {
 
   /**
+   * 
+   */
+  public GrouperDuoChangeLogConsumer() {
+    //make sure this is registered
+    GrouperDuoDaemon.scheduleJobsOnce();
+  }
+
+  /**
    * @see edu.internet2.middleware.grouper.changeLog.ChangeLogConsumerBase#processChangeLogEntries(java.util.List, edu.internet2.middleware.grouper.changeLog.ChangeLogProcessorMetadata)
    */
   @Override
