dc524b26e70cdf7d177727291969ce5a89c6f77a
Misagh Moayyed <mmoayyed@unicon.net>
2016-01-12 14:22:23 -0700

    build docs

--- travis/javadocs-ghpages.sh
+++ travis/javadocs-ghpages.sh
@@ -17,7 +17,7 @@ fi
 if [ "$invokeJavadoc" == true ]; then
   echo -e "Start to publish lastest Javadoc to gh-pages...\n"
   
-  echo -e "Invoking Maven to generate the site documentation...\n"
+  echo -e "Invoking Gradle to generate the site documentation...\n"
   echo "Current working directory is $PWD"
   ./gradlew javadoc -q
   
