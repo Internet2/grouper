1e4698623710696fb326bbcd3b3b7e1824de3b50
Chris Hyzer <mchyzer@yahoo.com>
2016-02-17 16:27:14 -0500

    GRP-1237: export to GSH commands commit 9

--- grouper/src/grouper/edu/internet2/middleware/grouper/app/gsh/GrouperShell.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/app/gsh/GrouperShell.java
@@ -407,6 +407,7 @@ private static boolean handleSpecialCase(String[] args) {
       this.interpreter.eval(  "import edu.internet2.middleware.grouper.hibernate.*;");
       this.interpreter.eval(  "import edu.internet2.middleware.grouper.permissions.*;");
       this.interpreter.eval(  "import edu.internet2.middleware.grouper.util.*;");
+      this.interpreter.eval(  "import edu.internet2.middleware.grouper.xml.export.*;");
       this.interpreter.eval(  "import edu.internet2.middleware.subject.*;");
       this.interpreter.eval(  "import edu.internet2.middleware.subject.provider.*;");
       this.interpreter.eval(  "import edu.internet2.middleware.grouper.userData.*;");
