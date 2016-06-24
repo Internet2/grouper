de3c6c61b171edcada33a1904586c4b3d9f7169a
mchyzer <mchyzer@isc.upenn.edu>
2016-06-24 15:01:58 -0400

    GRP-1328: threadlocal clearing issue with Crypto

---
+++ grouper/src/test/edu/internet2/middleware/grouper/util/GrouperThreadLocalStateTest.java
@@ -0,0 +1,66 @@
+/**
+ * @author mchyzer
+ * $Id$
+ */
+package edu.internet2.middleware.grouper.util;
+
+import junit.textui.TestRunner;
+import edu.internet2.middleware.grouper.helper.GrouperTest;
+import edu.internet2.middleware.morphString.Crypto;
+
+
+/**
+ *
+ */
+public class GrouperThreadLocalStateTest extends GrouperTest {
+
+  /**
+   * 
+   * @param args
+   */
+  public static void main(String[] args) {
+    TestRunner.run(new GrouperThreadLocalStateTest("testCryptoThreadlocal"));
+  }
+  
+  /**
+   * @param name
+   */
+  public GrouperThreadLocalStateTest(String name) {
+    super(name);
+  }
+
+  /**
+   */
+  public GrouperThreadLocalStateTest() {
+    super();
+  }
+
+  /**
+   * 
+   */
+  public void testCryptoThreadlocal() {
+    ThreadLocal threadLocalCrypto = (ThreadLocal)GrouperUtil.fieldValue(Crypto.class, null, "threadLocalCrypto", false, true, false);
+    threadLocalCrypto.remove();
+    if (threadLocalCrypto.get() != null) {
+      fail("not null");
+    }
+    
+    Crypto.getThreadLocalCrypto();
+    
+    threadLocalCrypto = (ThreadLocal)GrouperUtil.fieldValue(Crypto.class, null, "threadLocalCrypto", false, true, false);
+
+    if (threadLocalCrypto.get() == null) {
+      fail("null");
+    }
+    
+    GrouperThreadLocalState.removeCurrentThreadLocals();
+    
+    threadLocalCrypto = (ThreadLocal)GrouperUtil.fieldValue(Crypto.class, null, "threadLocalCrypto", false, true, false);
+
+    if (threadLocalCrypto.get() != null) {
+      fail("not null");
+    }
+
+  }
+  
+}
