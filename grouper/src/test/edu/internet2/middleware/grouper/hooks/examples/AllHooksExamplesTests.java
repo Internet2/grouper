2286dd3fd9bda86c8d61fb02f31ecbb1e4f24216
mchyzer <mchyzer@isc.upenn.edu>
2016-06-29 11:19:04 -0400

    GRP-1332: unique case insensitive group extensions

--- grouper/src/test/edu/internet2/middleware/grouper/hooks/examples/AllHooksExamplesTests.java
+++ grouper/src/test/edu/internet2/middleware/grouper/hooks/examples/AllHooksExamplesTests.java
@@ -1,27 +1,23 @@
 /**
  * Copyright 2014 Internet2
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
  */
 /*
- * @author mchyzer
- * $Id: AllHooksExamplesTests.java,v 1.1 2009-03-21 19:48:50 mchyzer Exp $
+ * @author mchyzer $Id: AllHooksExamplesTests.java,v 1.1 2009-03-21 19:48:50 mchyzer Exp $
  */
 package edu.internet2.middleware.grouper.hooks.examples;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
-
 
 /**
  *
@@ -38,7 +34,10 @@ public class AllHooksExamplesTests {
     //$JUnit-BEGIN$
     suite.addTestSuite(AttributeSecurityFromTypeHookTest.class);
     suite.addTestSuite(GroupAttributeNameValidationHookTest.class);
+    suite.addTestSuite(GroupUniqueNameCaseInsensitiveHookTest.class);
+    suite.addTestSuite(LDAPProvisioningHookTest.class);
     suite.addTestSuite(MembershipOneInFolderMaxHookTest.class);
+    suite.addTestSuite(StemUniqueNameCaseInsensitiveHookTest.class);
     suite.addTestSuite(UniqueObjectHookTest.class);
     //$JUnit-END$
     return suite;
