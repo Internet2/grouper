f3b5bfb5e924f97317730b296630be5b150fea91
Vivek Sachdeva <vsachdeva@cj.com>
2016-07-01 11:53:14 -0700

    Add capabillities to update, delete and set privileges while creating/updating a group

--- grouper-ws/grouper-ws-scim/src/main/java/edu/internet2/middleware/grouper/ws/scim/group/GrouperGroupService.java
+++ grouper-ws/grouper-ws-scim/src/main/java/edu/internet2/middleware/grouper/ws/scim/group/GrouperGroupService.java
@@ -16,16 +16,26 @@
 package edu.internet2.middleware.grouper.ws.scim.group;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
 import javax.ws.rs.core.Response.Status;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import edu.internet2.middleware.grouper.Group;
 import edu.internet2.middleware.grouper.GroupFinder;
 import edu.internet2.middleware.grouper.GroupSave;
 import edu.internet2.middleware.grouper.GrouperSession;
+import edu.internet2.middleware.grouper.SubjectFinder;
+import edu.internet2.middleware.grouper.exception.GroupAddAlreadyExistsException;
+import edu.internet2.middleware.grouper.group.TypeOfGroup;
+import edu.internet2.middleware.grouper.misc.SaveMode;
+import edu.internet2.middleware.grouper.privs.AccessPrivilege;
 import edu.psu.swe.scim.server.exception.UnableToCreateResourceException;
 import edu.psu.swe.scim.server.exception.UnableToDeleteResourceException;
 import edu.psu.swe.scim.server.exception.UnableToRetrieveExtensionsException;
@@ -43,6 +53,8 @@ import edu.psu.swe.scim.spec.resources.ScimGroup;
 @Named
 @ApplicationScoped
 public class GrouperGroupService implements Provider<ScimGroup> {
+  
+  private static final Log LOG = LogFactory.getLog(GrouperGroupService.class);
 
   @Override
   public ScimGroup create(ScimGroup scimGroup) throws UnableToCreateResourceException {
@@ -55,7 +67,15 @@ public class GrouperGroupService implements Provider<ScimGroup> {
       Group group = groupSave.assignName(scimGroup.getId())
           .assignDisplayName(scimGroup.getDisplayName())
           .assignCreateParentStemsIfNotExist(true)
+          .assignSaveMode(SaveMode.INSERT)
+          .assignTypeOfGroup(scimGroup.getExtension(GroupExtension.class).getTypeOfGroup() == null ? null: 
+            TypeOfGroup.valueOf(scimGroup.getExtension(GroupExtension.class).getTypeOfGroup()))
           .assignDescription(scimGroup.getExtension(GroupExtension.class).getDescription())
+          .assignPrivAllAttrRead(scimGroup.getExtension(GroupExtension.class).getAssignAttributeReadPrivToAll())
+          .assignPrivAllOptin(scimGroup.getExtension(GroupExtension.class).getAssignOptInPrivToAll())
+          .assignPrivAllOptout(scimGroup.getExtension(GroupExtension.class).getAssignOptOutPrivToAll())
+          .assignPrivAllRead(scimGroup.getExtension(GroupExtension.class).getAssignReadPrivToAll())
+          .assignPrivAllView(scimGroup.getExtension(GroupExtension.class).getAssignViewPrivToAll())
           .save();
             
       Group savedGroup = GroupFinder.findByName(grouperSession, group.getName(), true);
@@ -65,9 +85,29 @@ public class GrouperGroupService implements Provider<ScimGroup> {
       scimGroupOutput.setId(savedGroup.getName());
       scimGroupOutput.setDisplayName(savedGroup.getDisplayName());
       scimGroupOutput.setExternalId(savedGroup.getUuid());
+      GroupExtension groupExtension = new GroupExtension();
+      groupExtension.setDescription(savedGroup.getDescription());
+      groupExtension.setTypeOfGroup(savedGroup.getTypeOfGroup().getName());
+      Set<AccessPrivilege> privs = savedGroup.getPrivs(SubjectFinder.findAllSubject());
+      Set<String> privNames = new HashSet<String>();
+      for (AccessPrivilege accessPriv: privs) {
+        privNames.add(accessPriv.getName());
+      }
+      
+      groupExtension.setAssignAttributeReadPrivToAll(privNames.contains(AccessPrivilege.GROUP_ATTR_READ.getName()));
+      groupExtension.setAssignOptInPrivToAll(privNames.contains(AccessPrivilege.OPTIN.getName()));
+      groupExtension.setAssignOptOutPrivToAll(privNames.contains(AccessPrivilege.OPTOUT.getName()));
+      groupExtension.setAssignReadPrivToAll(privNames.contains(AccessPrivilege.READ.getName()));
+      groupExtension.setAssignViewPrivToAll(privNames.contains(AccessPrivilege.VIEW.getName()));
+      scimGroupOutput.addExtension(groupExtension);
+      
+    } catch(IllegalArgumentException e) {
+      throw new UnableToCreateResourceException(Status.BAD_REQUEST, "Please check the request payload and try again.");
+    } catch(GroupAddAlreadyExistsException e) {
+      throw new UnableToCreateResourceException(Status.BAD_REQUEST, "Group with name "+scimGroup.getId()+" already exists.");
     } catch(InvalidExtensionException ie) {
-      //throw new UnableToCreateResourceException(Status., what);
-      ie.printStackTrace();
+      LOG.error("Unable to create group with id "+scimGroup.getId(), ie);
+      throw new UnableToCreateResourceException(Status.INTERNAL_SERVER_ERROR, "Something went wrong. Please try again later.");
     } finally {
       GrouperSession.stopQuietly(grouperSession);
     }
@@ -75,8 +115,7 @@ public class GrouperGroupService implements Provider<ScimGroup> {
   }
 
   @Override
-  public ScimGroup update(String name, ScimGroup scimGroup)
-      throws UnableToUpdateResourceException {
+  public ScimGroup update(String name, ScimGroup scimGroup) throws UnableToUpdateResourceException {
     
     GrouperSession grouperSession = null;
     ScimGroup scimGroupOutput = null;
@@ -85,21 +124,58 @@ public class GrouperGroupService implements Provider<ScimGroup> {
       Group grp = GroupFinder.findByName(grouperSession, name, false);
       
       if (grp == null) {
-        throw new UnableToUpdateResourceException(Status.NOT_FOUND, "Resource with id " + scimGroup.getId() + " not found");
+        throw new UnableToUpdateResourceException(Status.NOT_FOUND, "group with name " + scimGroup.getId() + " not found.");
       }
       
-      grp.setDisplayName(scimGroup.getDisplayName());
-      grp.setDescription(scimGroup.getExtension(GroupExtension.class).getDescription());
+//      GroupSave groupSave1 = new GroupSave(GROUPER_SESSION).assignUuid(group.getId())
+//          .assignSaveMode(SaveMode.UPDATE)
+//          .assignName(group.getParentStemName() + ":" + extension)
+//          .assignDisplayExtension(displayExtension).assignDescription(description).assignTypeOfGroup(typeOfGroup)
+//          .assignPrivAllAdmin(adminChecked).assignPrivAllAttrRead(attrReadChecked)
+//          .assignPrivAllAttrUpdate(attrUpdateChecked).assignPrivAllOptin(optinChecked)
+//          .assignPrivAllOptout(optoutChecked).assignPrivAllRead(readChecked)
+//          .assignPrivAllUpdate(updateChecked).assignPrivAllView(viewChecked);
       
-      Group savedGroup = GroupFinder.findByName(grouperSession, grp.getName(), true);
+      GroupSave groupSave = new GroupSave(grouperSession).assignName(scimGroup.getId())
+          .assignUuid(grp.getUuid())
+          .assignSaveMode(SaveMode.UPDATE)
+          .assignDescription(scimGroup.getExtension(GroupExtension.class).getDescription())
+          .assignPrivAllAttrRead(scimGroup.getExtension(GroupExtension.class).getAssignAttributeReadPrivToAll())
+          .assignPrivAllOptin(scimGroup.getExtension(GroupExtension.class).getAssignOptInPrivToAll())
+          .assignPrivAllOptout(scimGroup.getExtension(GroupExtension.class).getAssignOptOutPrivToAll())
+          .assignPrivAllRead(scimGroup.getExtension(GroupExtension.class).getAssignReadPrivToAll())
+          .assignPrivAllView(scimGroup.getExtension(GroupExtension.class).getAssignViewPrivToAll());
+      
+      if (scimGroup.getExtension(GroupExtension.class).getTypeOfGroup() != null) {
+        groupSave.assignTypeOfGroup(TypeOfGroup.valueOf(scimGroup.getExtension(GroupExtension.class).getTypeOfGroup()));
+      }
+      Group savedGroup = groupSave.save();
       
       scimGroupOutput = new ScimGroup();
       scimGroupOutput.setId(savedGroup.getName());
       scimGroupOutput.setDisplayName(savedGroup.getDisplayName());
       scimGroupOutput.setExternalId(savedGroup.getUuid());
+      GroupExtension groupExtension = new GroupExtension();
+      groupExtension.setDescription(savedGroup.getDescription());
+      groupExtension.setTypeOfGroup(savedGroup.getTypeOfGroup().getName());
+      Set<AccessPrivilege> privs = savedGroup.getPrivs(SubjectFinder.findAllSubject());
+      Set<String> privNames = new HashSet<String>();
+      for (AccessPrivilege accessPriv: privs) {
+        privNames.add(accessPriv.getName());
+      }
       
+      groupExtension.setAssignAttributeReadPrivToAll(privNames.contains(AccessPrivilege.GROUP_ATTR_READ.getName()));
+      groupExtension.setAssignOptInPrivToAll(privNames.contains(AccessPrivilege.OPTIN.getName()));
+      groupExtension.setAssignOptOutPrivToAll(privNames.contains(AccessPrivilege.OPTOUT.getName()));
+      groupExtension.setAssignReadPrivToAll(privNames.contains(AccessPrivilege.READ.getName()));
+      groupExtension.setAssignViewPrivToAll(privNames.contains(AccessPrivilege.VIEW.getName()));
+      scimGroupOutput.addExtension(groupExtension);
+      
+    } catch(IllegalArgumentException e) {
+      throw new UnableToUpdateResourceException(Status.BAD_REQUEST, "Please check the request payload and try again.");
     } catch(InvalidExtensionException ie) {
-      ie.printStackTrace();
+      LOG.error("Unable to create group with id "+scimGroup.getId(), ie);
+      throw new UnableToUpdateResourceException(Status.INTERNAL_SERVER_ERROR, "Something went wrong. Please try again later.");
     } 
     finally {
       GrouperSession.stopQuietly(grouperSession);
@@ -125,14 +201,23 @@ public class GrouperGroupService implements Provider<ScimGroup> {
       
       GroupExtension groupExtension = new GroupExtension();
       groupExtension.setDescription(grp.getDescription());
-      
-      try {
-        group.addExtension(groupExtension);
-      } catch (InvalidExtensionException e) {
-        e.printStackTrace();
+      groupExtension.setTypeOfGroup(grp.getTypeOfGroup().getName());
+      Set<AccessPrivilege> privs = grp.getPrivs(SubjectFinder.findAllSubject());
+      Set<String> privNames = new HashSet<String>();
+      for (AccessPrivilege accessPriv: privs) {
+        privNames.add(accessPriv.getName());
       }
       
+      groupExtension.setAssignAttributeReadPrivToAll(privNames.contains(AccessPrivilege.GROUP_ATTR_READ.getName()));
+      groupExtension.setAssignOptInPrivToAll(privNames.contains(AccessPrivilege.OPTIN.getName()));
+      groupExtension.setAssignOptOutPrivToAll(privNames.contains(AccessPrivilege.OPTOUT.getName()));
+      groupExtension.setAssignReadPrivToAll(privNames.contains(AccessPrivilege.READ.getName()));
+      groupExtension.setAssignViewPrivToAll(privNames.contains(AccessPrivilege.VIEW.getName()));
+      group.addExtension(groupExtension);      
       return group;
+    } catch(InvalidExtensionException ie) {
+      LOG.error("Unable to get a group "+ name, ie);
+      throw new UnableToRetrieveResourceException(Status.INTERNAL_SERVER_ERROR, "Something went wrong. Please try again later.");
     } finally {
       GrouperSession.stopQuietly(grouperSession);
     }
@@ -143,7 +228,6 @@ public class GrouperGroupService implements Provider<ScimGroup> {
   public FilterResponse<ScimGroup> find(Filter filter, PageRequest pageRequest,
       SortRequest sortRequest) throws UnableToRetrieveResourceException {
     
-    // TODO Auto-generated method stub
     return null;
   }
 
@@ -168,5 +252,5 @@ public class GrouperGroupService implements Provider<ScimGroup> {
     List list =  Arrays.asList(GroupExtension.class);
     return list;
   }
-
+  
 }
