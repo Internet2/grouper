5da5fb1c4230d4a28d3be6f7d16e6c9aaf0acd69
mchyzer <mchyzer@isc.upenn.edu>
2016-12-05 18:14:07 -0500

    externalize config in POC

--- grouper-ws/grouper-ws/src/test/edu/internet2/middleware/grouperClient/poc/WsParamsRestPoc.java
+++ grouper-ws/grouper-ws/src/test/edu/internet2/middleware/grouperClient/poc/WsParamsRestPoc.java
@@ -19,6 +19,7 @@
  */
 package edu.internet2.middleware.grouperClient.poc;
 
+import edu.internet2.middleware.grouper.ws.util.RestClientSettings;
 import edu.internet2.middleware.grouperClientExt.org.apache.commons.httpclient.Credentials;
 import edu.internet2.middleware.grouperClientExt.org.apache.commons.httpclient.HttpClient;
 import edu.internet2.middleware.grouperClientExt.org.apache.commons.httpclient.HttpMethod;
@@ -39,12 +40,12 @@ public class WsParamsRestPoc {
   public static void main(String[] args) throws Exception {
 
     HttpClient httpClient = new HttpClient();
-    HttpMethod httpMethod = new GetMethod("http://localhost:8091/grouperWs/servicesRest/v1_4_000/groups/test%3Atest1/members?wsLiteObjectType=WsRestGetMembersLiteRequest&subjectAttributeNames=PENNNAME");
-    Credentials defaultcreds = new UsernamePasswordCredentials("mchyzer", 
-    "");
+    HttpMethod httpMethod = new GetMethod(RestClientSettings.URL + "/json/" + RestClientSettings.VERSION + "/groups/test%3Atest1/members?wsLiteObjectType=WsRestGetMembersLiteRequest&subjectAttributeNames=PENNNAME");
+    Credentials defaultcreds = new UsernamePasswordCredentials(RestClientSettings.USER, 
+        RestClientSettings.PASS);
 
     httpClient.getState()
-      .setCredentials(new AuthScope("localhost", 8091), defaultcreds);
+      .setCredentials(new AuthScope(RestClientSettings.HOST, RestClientSettings.PORT), defaultcreds);
     httpClient.getParams().setAuthenticationPreemptive(true);
     httpClient.executeMethod(httpMethod);
     String result = httpMethod.getResponseBodyAsString();
