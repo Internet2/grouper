261488c93bd258a66bce543e529c8ae1bdb74b29
Vivek Sachdeva <erviveksachdeva@gmail.com>
2017-05-20 22:41:44 -0700

    GRP-1544: Change the error message for accessibility

--- grouper-ui/webapp/grouperExternal/public/assets/dojo/grouper/nls/grouperDojo_en-us.js
+++ grouper-ui/webapp/grouperExternal/public/assets/dojo/grouper/nls/grouperDojo_en-us.js
@@ -3,7 +3,7 @@ define('grouper/nls/grouperDojo_en-us',{
 ,
 'dijit/nls/loading':{"loadingState":"Loading...","errorState":"Sorry, an error occurred"}
 ,
-'dijit/form/nls/validate':{"rangeMessage":"This value is out of range.","invalidMessage":"The value entered is not valid.","missingMessage":"This value is required."}
+'dijit/form/nls/validate':{"rangeMessage":"This value is out of range.","invalidMessage":"No matches found.","missingMessage":"This value is required."}
 ,
 'dijit/nls/common':{"buttonOk":"OK","buttonCancel":"Cancel","buttonSave":"Save","itemClose":"Close"}
 });
