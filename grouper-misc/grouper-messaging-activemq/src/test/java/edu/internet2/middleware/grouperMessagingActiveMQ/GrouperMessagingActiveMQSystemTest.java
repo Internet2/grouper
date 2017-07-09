1b9c88dd042bfbf77d4ce15c0d69cce71c469595
Vivek Sachdeva <erviveksachdeva@gmail.com>
2017-07-09 16:10:56 -0700

    GRP-1574: (Commit 1): Add grouper activemq amqp messaging integration

---
+++ grouper-misc/grouper-messaging-activemq/src/test/java/edu/internet2/middleware/grouperMessagingActiveMQ/GrouperMessagingActiveMQSystemTest.java
@@ -0,0 +1,64 @@
+package edu.internet2.middleware.grouperMessagingActiveMQ;
+
+import static edu.internet2.middleware.grouperClient.messaging.GrouperMessageQueueType.queue;
+
+import java.util.Collection;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+import edu.internet2.middleware.grouperClient.messaging.GrouperMessage;
+import edu.internet2.middleware.grouperClient.messaging.GrouperMessageQueueParam;
+import edu.internet2.middleware.grouperClient.messaging.GrouperMessageQueueType;
+import edu.internet2.middleware.grouperClient.messaging.GrouperMessageReceiveParam;
+import edu.internet2.middleware.grouperClient.messaging.GrouperMessageSendParam;
+import junit.framework.TestCase;
+
+public class GrouperMessagingActiveMQSystemTest extends TestCase {
+  
+  /**
+   * @param name
+   */
+  public GrouperMessagingActiveMQSystemTest(String name) {
+    super(name);
+  }
+  
+  private static final String TEST_QUEUE = "test_queue";
+  
+  public void testSendReceiveDeleteQueue() throws Exception {
+    sendReceiveDelete(TEST_QUEUE, queue);
+  }
+  
+  private void sendReceiveDelete(String queueName, GrouperMessageQueueType queueType) throws InterruptedException {
+    
+    final String messageSystemName = "activemq";
+    final String messageId = RandomStringUtils.randomAlphanumeric(50);
+    final String testMessageBody = "test message - "+messageId;
+    
+    GrouperMessagingActiveMQSystem system = new GrouperMessagingActiveMQSystem();
+    
+    GrouperMessageSendParam sendParam = new GrouperMessageSendParam();
+    GrouperMessageQueueParam queueParam = new GrouperMessageQueueParam();
+    queueParam.assignQueueType(queueType);
+    queueParam.assignQueueOrTopicName(queueName);
+    sendParam.assignGrouperMessageQueueParam(queueParam);
+    sendParam.assignGrouperMessageSystemName(messageSystemName);
+    
+    sendParam.addMessageBody(testMessageBody);
+         
+    system.send(sendParam);
+    
+    GrouperMessageReceiveParam receiveParam = new GrouperMessageReceiveParam();
+    receiveParam.assignGrouperMessageSystemName(messageSystemName);
+    receiveParam.assignGrouperMessageQueueParam(queueParam);
+    receiveParam.assignMaxMessagesToReceiveAtOnce(10);
+    receiveParam.assignLongPollMillis(1000);
+    
+    Collection<GrouperMessage> grouperMessages = system.receive(receiveParam).getGrouperMessages();
+    
+    GrouperMessage grouperMessage = grouperMessages.iterator().next();
+    
+    assertTrue(grouperMessage.getMessageBody().equals(testMessageBody));
+    
+  }
+
+}
