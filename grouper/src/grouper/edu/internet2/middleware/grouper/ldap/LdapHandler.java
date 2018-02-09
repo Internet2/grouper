a0a41a22dc109341cdffba69f989e1b436bf3d17
Shilen Patel <sipatel2@gmail.com>
2018-02-09 12:59:50 -0500

    GRP-1684: vt-ldap to ldaptive migration for grouper loader (commit 1)
    Conflicts:
    	grouper/conf/grouper-loader.base.properties
    	grouper/src/grouper/edu/internet2/middleware/grouper/app/loader/db/GrouperLoaderResultset.java
    	grouper/src/grouper/edu/internet2/middleware/grouper/ldap/LdapSession.java

--- grouper/src/grouper/edu/internet2/middleware/grouper/ldap/LdapHandler.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/ldap/LdapHandler.java
@@ -19,16 +19,15 @@
  */
 package edu.internet2.middleware.grouper.ldap;
 
-import javax.naming.NamingException;
-
 /**
  * Implement this (usually in an anonymous inner class) to get a 
  * reference to the ldap session object
  * @version $Id: HibernateHandler.java,v 1.3 2009-02-06 16:33:18 mchyzer Exp $
  * @author mchyzer
+ * @param <T> 
  */
 
-public interface LdapHandler {
+public interface LdapHandler<T> {
 
   /**
    * This method will be called with the hibernate session object to do 
@@ -36,7 +35,7 @@ public interface LdapHandler {
    * thrown by this method... others should be handled somehow..
    * @param ldapHandlerBean holds the ldap object to do operations on
    * @return the return value to be passed to return value of callback method
-   * @throws NamingException if there is a problem
+   * @throws Exception 
    */
-  public Object callback(LdapHandlerBean ldapHandlerBean) throws NamingException;
+  public Object callback(LdapHandlerBean<T> ldapHandlerBean) throws Exception;
 }
