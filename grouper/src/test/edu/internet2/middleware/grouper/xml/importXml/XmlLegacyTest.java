e044943fbd60d5ab6817e1350677daca29806336
mchyzer <mchyzer@isc.upenn.edu>
2018-03-21 15:38:13 -0400

    fix tests 20180321c

--- grouper/src/test/edu/internet2/middleware/grouper/xml/importXml/XmlLegacyTest.java
+++ grouper/src/test/edu/internet2/middleware/grouper/xml/importXml/XmlLegacyTest.java
@@ -71,7 +71,7 @@ public class XmlLegacyTest extends GrouperTest {
    * @param args
    */
   public static void main(String[] args) {
-    TestRunner.run(new XmlLegacyTest("testStemUpdateDescription"));
+    TestRunner.run(new XmlLegacyTest("testFullExportFullImportCustomTypes"));
   }
   
   /** */
