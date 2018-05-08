6d2e09109dbcd6ffb8212e65c8319c2e5c65c9cd
mchyzer <mchyzer@163.253.48.84>
2018-05-08 18:44:56 -0400

    GRP-1623: (commit 6) grouper deprovisioning

--- grouper/src/test/edu/internet2/middleware/grouper/app/deprovisioning/GrouperDeprovisioningOverallConfigurationTest.java
+++ grouper/src/test/edu/internet2/middleware/grouper/app/deprovisioning/GrouperDeprovisioningOverallConfigurationTest.java
@@ -2,12 +2,26 @@ package edu.internet2.middleware.grouper.app.deprovisioning;
 
 import edu.internet2.middleware.grouper.helper.GrouperTest;
 
-
+/**
+ * test get and store configuration to the database
+ * @author mchyzer
+ *
+ */
 public class GrouperDeprovisioningOverallConfigurationTest extends GrouperTest {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
 
+  }
+
+  public GrouperDeprovisioningOverallConfigurationTest() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
+
+  public GrouperDeprovisioningOverallConfigurationTest(String name) {
+    super(name);
+    // TODO Auto-generated constructor stub
   }
 
 }
