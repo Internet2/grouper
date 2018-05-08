6f70838eaad11d4367365917659a033f7d96a01e
mchyzer <mchyzer@163.253.48.84>
2018-05-08 17:07:15 -0400

    GRP-1623: (commit 4) grouper deprovisioning

---
+++ grouper/src/grouper/edu/internet2/middleware/grouper/app/deprovisioning/GrouperDeprovisioningRealm.java
@@ -0,0 +1,12 @@
+package edu.internet2.middleware.grouper.app.deprovisioning;
+
+
+public class GrouperDeprovisioningRealm {
+
+  private String label;
+  
+  private String groupNameMeansInRealm;
+  
+  
+  
+}
