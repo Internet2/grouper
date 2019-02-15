f30b16f49b2c806b0d2cc8836999ec885d365aee
Vivek Sachdeva <erviveksachdeva@gmail.com>
2019-02-14 18:20:41 -0800

    GRP-2023: (Commit-2): Add UsduServiceTest

--- grouper/src/grouper/edu/internet2/middleware/grouper/app/usdu/SubjectResolutionAttributeValue.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/app/usdu/SubjectResolutionAttributeValue.java
@@ -2,55 +2,91 @@ package edu.internet2.middleware.grouper.app.usdu;
 
 import edu.internet2.middleware.grouperClientExt.org.apache.commons.lang3.BooleanUtils;
 
+/**
+ * bean that represents metadata attributes on member 
+ */
 public class SubjectResolutionAttributeValue {
   
-  
+  /**
+   * false if the subject is not resolvable
+   */
   private String subjectResolutionResolvableString;
   
-  
+  /**
+   * date when the subject was last resolvable
+   */
   private String subjectResolutionDateLastResolvedString;
   
-  
+  /**
+   * number of days subject has been unresolved
+   */
   private String subjectResolutionDaysUnresolvedString;
   
-  
-  private String subjectResolutionLastCheckedString;
+  /**
+   * date when the subject was last checked for resolvable/unresolvable
+   */
+  private String subjectResolutionDateLastCheckedString;
 
-  
+  /**
+   * 
+   * @return date when the subject was last resolvable
+   */
   public String getSubjectResolutionDateLastResolvedString() {
     return subjectResolutionDateLastResolvedString;
   }
 
-  
+  /**
+   * date when the subject was last resolvable
+   * @param subjectResolutionDateLastResolvedString
+   */
   public void setSubjectResolutionDateLastResolvedString(String subjectResolutionDateLastResolvedString) {
     this.subjectResolutionDateLastResolvedString = subjectResolutionDateLastResolvedString;
   }
 
-
-  public String getSubjectResolutionLastCheckedString() {
-    return subjectResolutionLastCheckedString;
+  /**
+   * 
+   * @return date when the subject was last checked for resolvable/unresolvable
+   */
+  public String getSubjectResolutionDateLastCheckedString() {
+    return subjectResolutionDateLastCheckedString;
   }
 
-  
-  public void setSubjectResolutionLastCheckedString(String subjectResolutionLastCheckedString) {
-    this.subjectResolutionLastCheckedString = subjectResolutionLastCheckedString;
+  /**
+   * date when the subject was last checked for resolvable/unresolvable
+   * @param subjectResolutionLastCheckedString
+   */
+  public void setSubjectResolutionDateLastCheckedString(String subjectResolutionDateLastCheckedString) {
+    this.subjectResolutionDateLastCheckedString = subjectResolutionDateLastCheckedString;
   }
 
-  
+  /**
+   * 
+   * @param subjectResolutionResolvableString false if the subject is not resolvable
+   */
   public void setSubjectResolutionResolvableString(String subjectResolutionResolvableString) {
     this.subjectResolutionResolvableString = subjectResolutionResolvableString;
   }
 
-
+  /**
+   * false if the subject is not resolvable
+   * @param subjectResolutionDaysUnresolvedString
+   */
   public void setSubjectResolutionDaysUnresolvedString(String subjectResolutionDaysUnresolvedString) {
     this.subjectResolutionDaysUnresolvedString = subjectResolutionDaysUnresolvedString;
   }
 
-
+  /**
+   * 
+   * @return false if the subject is not resolvable
+   */
   public boolean isSubjectResolutionResolvable() {
     return BooleanUtils.toBoolean(subjectResolutionResolvableString);
   }
   
+  /**
+   * 
+   * @return number of days subject has been unresolved
+   */
   public Long getSubjectResolutionDaysUnresolved() {
     try {
       return Long.valueOf(subjectResolutionDaysUnresolvedString);
--- grouper/src/grouper/edu/internet2/middleware/grouper/app/usdu/UsduSource.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/app/usdu/UsduSource.java
@@ -1,72 +1,122 @@
 package edu.internet2.middleware.grouper.app.usdu;
 
-
+/**
+ * 
+ * bean to hold the usdu configuration defined in properties files
+ */
 public class UsduSource {
 
+  /**
+   * source id of the subjects
+   */
   private String sourceId;
   
+  /**
+   * source label of the subjects
+   */
   private String sourceLabel;
   
+  /**
+   * maximum number of subjects that can be deleted in one job run
+   */
   private int maxUnresolvableSubjects;
   
+  /**
+   * should the subjects up to maxUnresolvableSubjects be deleted
+   * if more than that found while running the job 
+   */
   private boolean removeUpToFailsafe;
   
+  /**
+   * after how many days subject should be deleted after being unresolved
+   */
   private int deleteAfterDays;
 
   
+  /**
+   * 
+   * @return source id of the subjects
+   */
   public String getSourceId() {
     return sourceId;
   }
 
-  
+  /**
+   * source id of the subjects
+   * @param sourceId
+   */
   public void setSourceId(String sourceId) {
     this.sourceId = sourceId;
   }
 
-  
+  /**
+   * 
+   * @return source label of the subjects
+   */
   public String getSourceLabel() {
     return sourceLabel;
   }
 
-  
+  /**
+   * source label of the subjects
+   * @param sourceLabel
+   */
   public void setSourceLabel(String sourceLabel) {
     this.sourceLabel = sourceLabel;
   }
 
-  
+  /**
+   * should the subjects up to maxUnresolvableSubjects be deleted
+   * if more than that found while running the job 
+   * @return
+   */
   public boolean isRemoveUpToFailsafe() {
     return removeUpToFailsafe;
   }
 
-  
+  /**
+   * should the subjects up to maxUnresolvableSubjects be deleted
+   * if more than that found while running the job 
+   * @param removeUpToFailsafe
+   */
   public void setRemoveUpToFailsafe(boolean removeUpToFailsafe) {
     this.removeUpToFailsafe = removeUpToFailsafe;
   }
 
 
-  
+  /**
+   * 
+   * @return maximum number of subjects that can be deleted in one job run
+   */
   public int getMaxUnresolvableSubjects() {
     return maxUnresolvableSubjects;
   }
 
 
-  
+  /**
+   * maximum number of subjects that can be deleted in one job run
+   * @param maxUnresolvableSubjects
+   */
   public void setMaxUnresolvableSubjects(int maxUnresolvableSubjects) {
     this.maxUnresolvableSubjects = maxUnresolvableSubjects;
   }
 
 
-  
+  /**
+   * after how many days subject should be deleted after being unresolved
+   * @return
+   */
   public int getDeleteAfterDays() {
     return deleteAfterDays;
   }
 
 
-  
+  /**
+   * after how many days subject should be deleted after being unresolved
+   * @param deleteAfterDays
+   */
   public void setDeleteAfterDays(int deleteAfterDays) {
     this.deleteAfterDays = deleteAfterDays;
   }
-
-  
   
 }
