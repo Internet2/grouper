cb48d975bc3b9142d4f416dcca2799ef767e1327
Chad Redman <chad_redman@unc.edu>
2020-01-28 23:55:01 -0500

    Travis: (commit 2) run grouper tests; add build stages

--- travis/deploy-to-sonatype.sh
+++ travis/deploy-to-sonatype.sh
@@ -1,18 +1,40 @@
 #!/bin/bash
 
 # Only invoke the deployment to Sonatype when it's not a PR and only for master
-if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-  case "${TRAVIS_JOB_NUMBER}" in
-    *\.1) 
-      echo -e "deploying SNAPSHOT artifacts to Sonatype for Travis job ${TRAVIS_JOB_NUMBER}"
-      cp travis/mvn.settings.xml $HOME/.m2/settings.xml
-      mvn -f ./grouper-parent -DskipTests=true -Dlicense.skip=true deploy
-      echo -e "Successfully deployed SNAPSHOT artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}";;
-    *)
-      echo -e "Skipping Sonatype deployment because job ${TRAVIS_JOB_NUMBER} does not match *.1"
-  esac
-else
-  echo -e "Skipping Sonatype deployment because this is either a pull request or not the master branch"
+if [ "$TRAVIS_PULL_REQUEST" == "true" ]; then
+  echo -e "Skipping Sonatype deployment because this is a pull request"
+  exit 0
 fi
+
+# TODO temp allow travis branch to deploy
+#[ "$TRAVIS_BRANCH" != "master" ]
+#  echo -e "Skipping Sonatype deployment because this is not the master branch"
+#  exit 0
+#fi
+
+GROUPER_VERSION=$(perl -e '$_ = $ENV{q~TRAVIS_TAG~}; print $1 if /^GROUPER_RELEASE_((\d+\.)+\d+)$/')
+if [ $? -ne 0 ]; then
+  echo -e "Failed to parse travis tag '$TRAVIS_TAG' against /^GROUPER_RELEASE_((\d+\.)+\d+)$/"
+  exit 129
+fi
+if [ -z "$GROUPER_VERSION" ]; then
+  echo -e "Travis tag '$TRAVIS_TAG' did not match pattern /^GROUPER_RELEASE_((\d+\.)+\d+)$/"
+  exit 130
+fi
+
+echo -e "Extracted grouper version '$GROUPER_VERSION' from tag '$TRAVIS_TAG'"
+
+case "${TRAVIS_JOB_NUMBER}" in
+  *\.1) 
+    echo -e "Setting Grouper version to $GROUPER_VERSION in Maven pom files"
+    cp travis/mvn.settings.xml $HOME/.m2/settings.xml
+    mvn -f ./grouper-parent versions:set -DnewVersion=$GROUPER_VERSION
+
+    echo -e "building and deploying release artifacts to Sonatype for Travis job ${TRAVIS_JOB_NUMBER}"
+    mvn -f ./grouper-parent clean compile package deploy
+    echo -e "Successfully deployed release artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}";;
+  *)
+    echo -e "Skipping Sonatype deployment because job ${TRAVIS_JOB_NUMBER} does not match *.1"
+esac
 
 
