d6ecbf3e1ff3ba0adb897f85408f06d797e0058a
mchyzer <mchyzer@192.168.1.240>
2020-02-02 15:27:49 -0500

    make tests suites with unique names and link to parent suite

--- grouper/src/test/edu/internet2/middleware/grouper/app/AllAppTests.java
+++ grouper/src/test/edu/internet2/middleware/grouper/app/AllAppTests.java
@@ -19,17 +19,20 @@
  */
 package edu.internet2.middleware.grouper.app;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
 import edu.internet2.middleware.grouper.app.attestation.AllAttestationTests;
 import edu.internet2.middleware.grouper.app.deprovisioning.AllDeprovisioningTests;
 import edu.internet2.middleware.grouper.app.gsh.AllGshTests;
 import edu.internet2.middleware.grouper.app.loader.AllLoaderTests;
+import edu.internet2.middleware.grouper.app.messaging.AllAppMessagingTests;
 import edu.internet2.middleware.grouper.app.messaging.MessageConsumerDaemonTest;
+import edu.internet2.middleware.grouper.app.provisioning.AllProvisioningTests;
+import edu.internet2.middleware.grouper.app.reports.AllReportsTests;
 import edu.internet2.middleware.grouper.app.tableSync.AllTableSyncTests;
 import edu.internet2.middleware.grouper.app.upgradeTasks.AllUpgradeTasksTests;
 import edu.internet2.middleware.grouper.app.usdu.AllUsduTests;
 import edu.internet2.middleware.grouper.cfg.GrouperConfig;
+import junit.framework.Test;
+import junit.framework.TestSuite;
 
 
 /**
@@ -56,6 +59,9 @@ public class AllAppTests {
     
     suite.addTest(AllAttestationTests.suite());
     suite.addTest(AllDeprovisioningTests.suite());
+    suite.addTest(AllAppMessagingTests.suite());
+    suite.addTest(AllProvisioningTests.suite());
+    suite.addTest(AllReportsTests.suite());
     suite.addTest(AllUsduTests.suite());
     suite.addTest(AllUpgradeTasksTests.suite());
     return suite;
