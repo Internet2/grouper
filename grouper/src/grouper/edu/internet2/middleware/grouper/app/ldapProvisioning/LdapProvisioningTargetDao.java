2884c5b367bb164b763303df1349e8885fe68e9a
Shilen Patel <sipatel2@gmail.com>
2020-09-24 11:08:19 -0400

    minor progress

--- grouper/src/grouper/edu/internet2/middleware/grouper/app/ldapProvisioning/LdapProvisioningTargetDao.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/app/ldapProvisioning/LdapProvisioningTargetDao.java
@@ -20,6 +20,7 @@ import edu.internet2.middleware.grouper.app.provisioning.targetDao.TargetDaoInse
 import edu.internet2.middleware.grouper.app.provisioning.targetDao.TargetDaoInsertGroupResponse;
 import edu.internet2.middleware.grouper.app.provisioning.targetDao.TargetDaoRetrieveAllGroupsRequest;
 import edu.internet2.middleware.grouper.app.provisioning.targetDao.TargetDaoRetrieveAllGroupsResponse;
+import edu.internet2.middleware.grouper.app.provisioning.targetDao.TargetDaoTimingInfo;
 import edu.internet2.middleware.grouper.app.provisioning.targetDao.TargetDaoUpdateGroupRequest;
 import edu.internet2.middleware.grouper.app.provisioning.targetDao.TargetDaoUpdateGroupResponse;
 import edu.internet2.middleware.grouper.ldap.LdapAttribute;
@@ -30,177 +31,206 @@ import edu.internet2.middleware.grouper.ldap.LdapSearchScope;
 import edu.internet2.middleware.grouperClientExt.org.apache.commons.lang3.StringUtils;
 
 
+// TODO exception handling per wiki
+
 public class LdapProvisioningTargetDao extends GrouperProvisionerTargetDaoBase {
   
   @Override
   public TargetDaoRetrieveAllGroupsResponse retrieveAllGroups(TargetDaoRetrieveAllGroupsRequest targetDaoRetrieveAllGroupsRequest) {
     
-    boolean includeAllMembershipsIfApplicable = targetDaoRetrieveAllGroupsRequest == null ? false : targetDaoRetrieveAllGroupsRequest.isIncludeAllMembershipsIfApplicable();
-    
-    List<ProvisioningGroup> results = new ArrayList<ProvisioningGroup>();
-    
-    LdapSyncConfiguration ldapSyncConfiguration = (LdapSyncConfiguration) this.getGrouperProvisioner().retrieveGrouperProvisioningConfiguration();
-    String ldapConfigId = ldapSyncConfiguration.getLdapExternalSystemConfigId();
-    String groupSearchAllFilter = ldapSyncConfiguration.getGroupSearchAllFilter();
-    
-    if (StringUtils.isEmpty(groupSearchAllFilter)) {
-      throw new RuntimeException("Why is groupSearchAllFilter empty?");
-    }
+    long startNanos = System.nanoTime();
 
-    String groupSearchBaseDn = ldapSyncConfiguration.getGroupSearchBaseDn();
-
-    Set<String> groupSearchAttributeNames = new TreeSet<String>(String.CASE_INSENSITIVE_ORDER);
-    groupSearchAttributeNames.addAll(ldapSyncConfiguration.getGroupSearchAttributes());
+    try {
+      boolean includeAllMembershipsIfApplicable = targetDaoRetrieveAllGroupsRequest == null ? false : targetDaoRetrieveAllGroupsRequest.isIncludeAllMembershipsIfApplicable();
       
-    Set<String> groupAttributesMultivalued = new TreeSet<String>(String.CASE_INSENSITIVE_ORDER);
-    if (ldapSyncConfiguration.getGroupAttributesMultivalued() != null) {
-      groupAttributesMultivalued.addAll(ldapSyncConfiguration.getGroupAttributesMultivalued());
-    }
-    
-    groupSearchAttributeNames.add("objectClass");
-    groupAttributesMultivalued.add("objectClass");
-    
-    if (includeAllMembershipsIfApplicable) {
-      String groupAttributeNameForMemberships = ldapSyncConfiguration.getGroupAttributeNameForMemberships();
-      groupSearchAttributeNames.add(groupAttributeNameForMemberships);
-      groupAttributesMultivalued.add(groupAttributeNameForMemberships);
-    }
-    
-    List<LdapEntry> ldapEntries = new LdapSyncDaoForLdap().search(ldapConfigId, groupSearchBaseDn, groupSearchAllFilter, LdapSearchScope.SUBTREE_SCOPE, new ArrayList<String>(groupSearchAttributeNames));
-    
-    for (LdapEntry ldapEntry : ldapEntries) {
-      ProvisioningGroup targetGroup = new ProvisioningGroup();
-      targetGroup.setName(ldapEntry.getDn());
+      List<ProvisioningGroup> results = new ArrayList<ProvisioningGroup>();
       
-      for (LdapAttribute ldapAttribute : ldapEntry.getAttributes()) {
-        targetGroup.assignAttributeValue(ldapAttribute.getName(), ldapAttribute.getValues());
-        Object value = null;
-        if (groupAttributesMultivalued.contains(ldapAttribute.getName())) {
-          value = new HashSet<Object>(ldapAttribute.getValues());
-        } else if (ldapAttribute.getValues().size() == 1) {
-          value = ldapAttribute.getValues().iterator().next();
-        }
+      LdapSyncConfiguration ldapSyncConfiguration = (LdapSyncConfiguration) this.getGrouperProvisioner().retrieveGrouperProvisioningConfiguration();
+      String ldapConfigId = ldapSyncConfiguration.getLdapExternalSystemConfigId();
+      String groupSearchAllFilter = ldapSyncConfiguration.getGroupSearchAllFilter();
+      
+      if (StringUtils.isEmpty(groupSearchAllFilter)) {
+        throw new RuntimeException("Why is groupSearchAllFilter empty?");
+      }
+  
+      String groupSearchBaseDn = ldapSyncConfiguration.getGroupSearchBaseDn();
+  
+      Set<String> groupSearchAttributeNames = new TreeSet<String>(String.CASE_INSENSITIVE_ORDER);
+      groupSearchAttributeNames.addAll(ldapSyncConfiguration.getGroupSearchAttributes());
         
-        targetGroup.assignAttributeValue(ldapAttribute.getName(), value);
+      Set<String> groupAttributesMultivalued = new TreeSet<String>(String.CASE_INSENSITIVE_ORDER);
+      if (ldapSyncConfiguration.getGroupAttributesMultivalued() != null) {
+        groupAttributesMultivalued.addAll(ldapSyncConfiguration.getGroupAttributesMultivalued());
       }
       
-      results.add(targetGroup);
+      groupSearchAttributeNames.add("objectClass");
+      groupAttributesMultivalued.add("objectClass");
+      
+      if (includeAllMembershipsIfApplicable) {
+        String groupAttributeNameForMemberships = ldapSyncConfiguration.getGroupAttributeNameForMemberships();
+        groupSearchAttributeNames.add(groupAttributeNameForMemberships);
+        groupAttributesMultivalued.add(groupAttributeNameForMemberships);
+      }
+      
+      List<LdapEntry> ldapEntries = new LdapSyncDaoForLdap().search(ldapConfigId, groupSearchBaseDn, groupSearchAllFilter, LdapSearchScope.SUBTREE_SCOPE, new ArrayList<String>(groupSearchAttributeNames));
+      
+      for (LdapEntry ldapEntry : ldapEntries) {
+        ProvisioningGroup targetGroup = new ProvisioningGroup();
+        targetGroup.setName(ldapEntry.getDn());
+        
+        for (LdapAttribute ldapAttribute : ldapEntry.getAttributes()) {
+          targetGroup.assignAttributeValue(ldapAttribute.getName(), ldapAttribute.getValues());
+          Object value = null;
+          if (groupAttributesMultivalued.contains(ldapAttribute.getName())) {
+            value = new HashSet<Object>(ldapAttribute.getValues());
+          } else if (ldapAttribute.getValues().size() == 1) {
+            value = ldapAttribute.getValues().iterator().next();
+          }
+          
+          targetGroup.assignAttributeValue(ldapAttribute.getName(), value);
+        }
+        
+        results.add(targetGroup);
+      }
+  
+      return new TargetDaoRetrieveAllGroupsResponse(results);
+    } finally {
+      this.addTargetDaoTimingInfo(new TargetDaoTimingInfo("retrieveAllGroups", startNanos));
     }
-
-    return new TargetDaoRetrieveAllGroupsResponse(results);
   }
   
   @Override
   public TargetDaoInsertGroupResponse insertGroup(TargetDaoInsertGroupRequest targetDaoInsertGroupRequest) {
-    ProvisioningGroup targetGroup = targetDaoInsertGroupRequest.getTargetGroup();
-    LdapSyncConfiguration ldapSyncConfiguration = (LdapSyncConfiguration) this.getGrouperProvisioner().retrieveGrouperProvisioningConfiguration();
-    String ldapConfigId = ldapSyncConfiguration.getLdapExternalSystemConfigId();
-    
-    if (StringUtils.isBlank(targetGroup.getName())) {
-      throw new RuntimeException("Why is targetGroup.getName() blank?");
-    }
-    
-    LdapEntry ldapEntry = new LdapEntry(targetGroup.getName());
-    for (String attributeName : targetGroup.getAttributes().keySet()) {
-      ProvisioningAttribute targetAttribute = targetGroup.getAttributes().get(attributeName);
-      Object value = targetAttribute.getValue();
-      
-      LdapAttribute ldapAttribute = new LdapAttribute(targetAttribute.getName());
+    long startNanos = System.nanoTime();
 
-      if (value instanceof String && !StringUtils.isEmpty((String)value)) {
-        ldapAttribute.addValue((String)value);
-      } else if (value instanceof Collection) {
-      @SuppressWarnings("unchecked")
-      Collection<Object> values = (Collection<Object>)targetAttribute.getValue();
-      if (values.size() > 0) {
-        ldapAttribute.addValues(values);
-        } 
+    try {
+      ProvisioningGroup targetGroup = targetDaoInsertGroupRequest.getTargetGroup();
+      LdapSyncConfiguration ldapSyncConfiguration = (LdapSyncConfiguration) this.getGrouperProvisioner().retrieveGrouperProvisioningConfiguration();
+      String ldapConfigId = ldapSyncConfiguration.getLdapExternalSystemConfigId();
+      
+      if (StringUtils.isBlank(targetGroup.getName())) {
+        throw new RuntimeException("Why is targetGroup.getName() blank?");
       }
       
-      if (ldapAttribute.getValues().size() > 0) {
-        ldapEntry.addAttribute(ldapAttribute);
+      LdapEntry ldapEntry = new LdapEntry(targetGroup.getName());
+      for (String attributeName : targetGroup.getAttributes().keySet()) {
+        ProvisioningAttribute targetAttribute = targetGroup.getAttributes().get(attributeName);
+        Object value = targetAttribute.getValue();
+        
+        LdapAttribute ldapAttribute = new LdapAttribute(targetAttribute.getName());
+  
+        if (value instanceof String && !StringUtils.isEmpty((String)value)) {
+          ldapAttribute.addValue((String)value);
+        } else if (value instanceof Collection) {
+        @SuppressWarnings("unchecked")
+        Collection<Object> values = (Collection<Object>)targetAttribute.getValue();
+        if (values.size() > 0) {
+          ldapAttribute.addValues(values);
+          } 
+        }
+        
+        if (ldapAttribute.getValues().size() > 0) {
+          ldapEntry.addAttribute(ldapAttribute);
+        }
       }
+      
+      new LdapSyncDaoForLdap().create(ldapConfigId, ldapEntry);
+      targetGroup.setProvisioned(true);
+      return new TargetDaoInsertGroupResponse();
+    } finally {
+      this.addTargetDaoTimingInfo(new TargetDaoTimingInfo("insertGroup", startNanos));
     }
-    
-    new LdapSyncDaoForLdap().create(ldapConfigId, ldapEntry);
-    return null;
   }
   
   @Override
   public TargetDaoDeleteGroupResponse deleteGroup(TargetDaoDeleteGroupRequest targetDaoDeleteGroupRequest) {
     
-    ProvisioningGroup targetGroup = targetDaoDeleteGroupRequest == null ? null : targetDaoDeleteGroupRequest.getTargetGroup();
-    LdapSyncConfiguration ldapSyncConfiguration = (LdapSyncConfiguration) this.getGrouperProvisioner().retrieveGrouperProvisioningConfiguration();
-    String ldapConfigId = ldapSyncConfiguration.getLdapExternalSystemConfigId();
-    
-    if (StringUtils.isBlank(targetGroup.getName())) {
-      throw new RuntimeException("Why is targetGroup.getName() blank?");
+    long startNanos = System.nanoTime();
+
+    try {
+      ProvisioningGroup targetGroup = targetDaoDeleteGroupRequest == null ? null : targetDaoDeleteGroupRequest.getTargetGroup();
+      LdapSyncConfiguration ldapSyncConfiguration = (LdapSyncConfiguration) this.getGrouperProvisioner().retrieveGrouperProvisioningConfiguration();
+      String ldapConfigId = ldapSyncConfiguration.getLdapExternalSystemConfigId();
+      
+      if (StringUtils.isBlank(targetGroup.getName())) {
+        throw new RuntimeException("Why is targetGroup.getName() blank?");
+      }
+      new LdapSyncDaoForLdap().delete(ldapConfigId, targetGroup.getName());
+      targetGroup.setProvisioned(true);
+      return new TargetDaoDeleteGroupResponse();
+    } finally {
+      this.addTargetDaoTimingInfo(new TargetDaoTimingInfo("deleteGroup", startNanos));
     }
-    new LdapSyncDaoForLdap().delete(ldapConfigId, targetGroup.getName());
-    return null;
   }
 
   public TargetDaoUpdateGroupResponse updateGroup(TargetDaoUpdateGroupRequest targetDaoUpdateGroupRequest) {
 
-    ProvisioningGroup targetGroup = targetDaoUpdateGroupRequest.getTargetGroup();
-    Set<ProvisioningObjectChange> provisionObjectChanges = targetGroup.getInternal_objectChanges();
+    long startNanos = System.nanoTime();
 
-    LdapSyncConfiguration ldapSyncConfiguration = (LdapSyncConfiguration) this.getGrouperProvisioner().retrieveGrouperProvisioningConfiguration();
-    String ldapConfigId = ldapSyncConfiguration.getLdapExternalSystemConfigId();
-    
-    List<LdapModificationItem> ldapModificationItems = new ArrayList<LdapModificationItem>();
-        
-    for (ProvisioningObjectChange provisionObjectChange : provisionObjectChanges) {
+    try {
+      ProvisioningGroup targetGroup = targetDaoUpdateGroupRequest.getTargetGroup();
+      Set<ProvisioningObjectChange> provisionObjectChanges = targetGroup.getInternal_objectChanges();
+  
+      LdapSyncConfiguration ldapSyncConfiguration = (LdapSyncConfiguration) this.getGrouperProvisioner().retrieveGrouperProvisioningConfiguration();
+      String ldapConfigId = ldapSyncConfiguration.getLdapExternalSystemConfigId();
       
-      String attributeName = provisionObjectChange.getAttributeName();
-      ProvisioningObjectChangeAction action = provisionObjectChange.getProvisioningObjectChangeAction();
-      Object newValue = provisionObjectChange.getNewValue();
-      Object oldValue = provisionObjectChange.getOldValue();
-      
-      if (action == ProvisioningObjectChangeAction.delete) {
-        if (newValue != null) {
-          throw new RuntimeException("Deleting value but there's a new value=" + newValue + ", attributeName=" + attributeName);
-        }
-                
-        if (oldValue == null) {
-          // delete the whole attribute
-          LdapModificationItem item = new LdapModificationItem(LdapModificationType.REMOVE_ATTRIBUTE, new LdapAttribute(attributeName));
-          ldapModificationItems.add(item);
-        } else {
-          LdapModificationItem item = new LdapModificationItem(LdapModificationType.REMOVE_ATTRIBUTE, new LdapAttribute(attributeName, oldValue));
-          ldapModificationItems.add(item);
-        }
-      } else if (action == ProvisioningObjectChangeAction.update) {
-        if (oldValue != null) {
-          LdapModificationItem item = new LdapModificationItem(LdapModificationType.REMOVE_ATTRIBUTE, new LdapAttribute(attributeName, oldValue));
-          ldapModificationItems.add(item);
-        }
+      List<LdapModificationItem> ldapModificationItems = new ArrayList<LdapModificationItem>();
+          
+      for (ProvisioningObjectChange provisionObjectChange : provisionObjectChanges) {
         
-        if (newValue != null) {
+        String attributeName = provisionObjectChange.getAttributeName();
+        ProvisioningObjectChangeAction action = provisionObjectChange.getProvisioningObjectChangeAction();
+        Object newValue = provisionObjectChange.getNewValue();
+        Object oldValue = provisionObjectChange.getOldValue();
+        
+        if (action == ProvisioningObjectChangeAction.delete) {
+          if (newValue != null) {
+            throw new RuntimeException("Deleting value but there's a new value=" + newValue + ", attributeName=" + attributeName);
+          }
+                  
+          if (oldValue == null) {
+            // delete the whole attribute
+            LdapModificationItem item = new LdapModificationItem(LdapModificationType.REMOVE_ATTRIBUTE, new LdapAttribute(attributeName));
+            ldapModificationItems.add(item);
+          } else {
+            LdapModificationItem item = new LdapModificationItem(LdapModificationType.REMOVE_ATTRIBUTE, new LdapAttribute(attributeName, oldValue));
+            ldapModificationItems.add(item);
+          }
+        } else if (action == ProvisioningObjectChangeAction.update) {
+          if (oldValue != null) {
+            LdapModificationItem item = new LdapModificationItem(LdapModificationType.REMOVE_ATTRIBUTE, new LdapAttribute(attributeName, oldValue));
+            ldapModificationItems.add(item);
+          }
+          
+          if (newValue != null) {
+            LdapModificationItem item = new LdapModificationItem(LdapModificationType.ADD_ATTRIBUTE, new LdapAttribute(attributeName, newValue));
+            ldapModificationItems.add(item);
+          }
+        } else if (action == ProvisioningObjectChangeAction.insert) {
+          if (oldValue != null) {
+            throw new RuntimeException("Inserting value but there's an old value=" + oldValue + ", attributeName=" + attributeName);
+          }
+          
+          if (newValue == null) {
+            throw new RuntimeException("Inserting value but there's no new value for attributeName=" + attributeName);
+          }
+          
           LdapModificationItem item = new LdapModificationItem(LdapModificationType.ADD_ATTRIBUTE, new LdapAttribute(attributeName, newValue));
           ldapModificationItems.add(item);
+        } else {
+          throw new RuntimeException("Unexpected provisioningObjectChangeAction: " + action);
         }
-      } else if (action == ProvisioningObjectChangeAction.insert) {
-        if (oldValue != null) {
-          throw new RuntimeException("Inserting value but there's an old value=" + oldValue + ", attributeName=" + attributeName);
-        }
-        
-        if (newValue == null) {
-          throw new RuntimeException("Inserting value but there's no new value for attributeName=" + attributeName);
-        }
-        
-        LdapModificationItem item = new LdapModificationItem(LdapModificationType.ADD_ATTRIBUTE, new LdapAttribute(attributeName, newValue));
-        ldapModificationItems.add(item);
-      } else {
-        throw new RuntimeException("Unexpected provisioningObjectChangeAction: " + action);
       }
+  
+      if (ldapModificationItems.size() > 0) {
+        new LdapSyncDaoForLdap().modify(ldapConfigId, targetGroup.getName(), ldapModificationItems); // TODO partial errors?
+      }
+      
+      targetGroup.setProvisioned(true);
+      return new TargetDaoUpdateGroupResponse();
+    } finally {
+      this.addTargetDaoTimingInfo(new TargetDaoTimingInfo("updateGroup", startNanos));
     }
-
-    if (ldapModificationItems.size() > 0) {
-      new LdapSyncDaoForLdap().modify(ldapConfigId, targetGroup.getName(), ldapModificationItems);
-    }
-    
-    return null;
   }
   
   @Override
