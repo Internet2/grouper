7c12732cdc0ed69e353b76fe89e6da80c4a55aff
Chris Hyzer <mchyzer@isc.upenn.edu>
2020-12-07 00:59:59 -0500

    add docs in script

--- grouper-misc/openldap-dinkel-grouper/downloadAndRunLdap.sh
+++ grouper-misc/openldap-dinkel-grouper/downloadAndRunLdap.sh
@@ -17,3 +17,10 @@ wget https://raw.githubusercontent.com/Internet2/grouper/GROUPER_2_5_BRANCH/grou
 docker build -t openldap-dinkel-grouper .
 docker run -d -p 389:389 --name openldap-dinkel-grouper --mount type=bind,source="$PWD/ldap-seed-data",target=/etc/ldap/prepopulate \
    -e SLAPD_PASSWORD=secret -e SLAPD_CONFIG_PASSWORD=secret -e SLAPD_DOMAIN=example.edu openldap-dinkel-grouper
+
+# wget https://raw.githubusercontent.com/Internet2/grouper/GROUPER_2_5_BRANCH/grouper-misc/openldap-dinkel-grouper/downloadAndRunLdap.sh
+# chmod +x downloadAndRunLdap.sh
+# ./downloadAndRunLdap.sh
+# cleanup: 
+#   docker rm -f openldap-dinkel-grouper
+#   docker rmi openldap-dinkel-grouper:latest
