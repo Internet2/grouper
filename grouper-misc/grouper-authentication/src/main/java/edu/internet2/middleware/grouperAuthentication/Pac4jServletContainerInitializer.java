ebef63ba143cea7e9d433931aa866ce8ffd41fe2
Jj! <jj@unicon.net>
2021-01-29 08:39:43 -0600

    WIP new SCIM implementation

--- grouper-misc/grouper-authentication/src/main/java/edu/internet2/middleware/grouperAuthentication/Pac4jServletContainerInitializer.java
+++ grouper-misc/grouper-authentication/src/main/java/edu/internet2/middleware/grouperAuthentication/Pac4jServletContainerInitializer.java
@@ -1,8 +1,8 @@
 package edu.internet2.middleware.grouperAuthentication;
 
+import edu.internet2.middleware.grouper.cfg.GrouperHibernateConfig;
 import edu.internet2.middleware.grouper.ui.util.GrouperUiConfig;
 import org.pac4j.core.authorization.authorizer.DefaultAuthorizers;
-import org.pac4j.core.matching.matcher.DefaultMatchers;
 import org.pac4j.core.util.Pac4jConstants;
 import org.pac4j.jee.filter.CallbackFilter;
 import org.pac4j.jee.filter.SecurityFilter;
@@ -20,7 +20,10 @@ public class Pac4jServletContainerInitializer implements ServletContainerInitial
 
     @Override
     public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
-        if (GrouperUiConfig.retrieveConfig().propertyValueBoolean("external.authentication.enabled", false)) {
+        boolean runGrouperUi = GrouperHibernateConfig.retrieveConfig().propertyValueBoolean("grouper.is.ui", false);
+        boolean runGrouperWs = GrouperHibernateConfig.retrieveConfig().propertyValueBoolean("grouper.is.ws", false);
+
+        if (runGrouperUi && GrouperUiConfig.retrieveConfig().propertyValueBoolean("external.authentication.enabled", false)) {
             LOGGER.info("initializing pac4j");
 
             FilterRegistration.Dynamic callbackFilter = ctx.addFilter("callbackFilter", CallbackFilter.class);
@@ -29,10 +32,12 @@ public class Pac4jServletContainerInitializer implements ServletContainerInitial
             callbackFilter.addMappingForUrlPatterns(null, false, GrouperUiConfig.retrieveConfig().propertyValueString("external.authentication.callbackUrl", "/callback"));
 
             FilterRegistration.Dynamic securityFilter = ctx.addFilter("securityFilter", SecurityFilter.class);
+
             securityFilter.setInitParameter("configFactory", Pac4jConfigFactory.class.getCanonicalName());
             securityFilter.setInitParameter("clients", "client");
             securityFilter.setInitParameter(Pac4jConstants.MATCHERS, String.join(Pac4jConstants.ELEMENT_SEPARATOR, "excludePathServicesRest", "excludePathServices"));
             securityFilter.setInitParameter(Pac4jConstants.AUTHORIZERS, DefaultAuthorizers.NONE);
+
             securityFilter.addMappingForUrlPatterns(null, false, "/*");
 
             LOGGER.info("finished initializing pac4j");
