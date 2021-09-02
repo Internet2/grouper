07cfa8089a17cb0e0ca805359e254bf5aa6b8afd
Chris Hyzer <mchyzer@isc.upenn.edu>
2021-09-02 09:57:42 -0400

    take out sql provisioning tests

--- grouper/src/test/edu/internet2/middleware/grouper/app/sqlProvisioning/AllSqlProvisioningTests.java
+++ grouper/src/test/edu/internet2/middleware/grouper/app/sqlProvisioning/AllSqlProvisioningTests.java
@@ -1,5 +1,6 @@
 package edu.internet2.middleware.grouper.app.sqlProvisioning;
 
+import edu.internet2.middleware.grouper.cfg.GrouperConfig;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -9,9 +10,11 @@ public class AllSqlProvisioningTests extends TestCase {
 
   public static Test suite() {
     TestSuite suite = new TestSuite(AllSqlProvisioningTests.class.getName());
+    if (GrouperConfig.retrieveConfig().propertyValueBoolean("junit.test.sqlProvisioning", false)) {
     //$JUnit-BEGIN$
     suite.addTestSuite(SqlProvisionerTest.class);
     //$JUnit-END$
+    }
     return suite;
   }
 
