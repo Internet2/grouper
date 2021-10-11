cec48d1c74bc46e0a7855cfbdeba7e33e8b1d99e
Jj! <jj@unicon.net>
2021-10-11 10:43:36 -0500

    updated query for something more efficient

--- grouper-misc/grouper-custom-ws/src/main/java/edu/internet2/middleware/grouper/ws/rest/provider/MembershipCountRestProvider.java
+++ grouper-misc/grouper-custom-ws/src/main/java/edu/internet2/middleware/grouper/ws/rest/provider/MembershipCountRestProvider.java
@@ -12,11 +12,22 @@ public class MembershipCountRestProvider implements CustomGrouperRestProvider {
 
     @Override
     public Object provide(CustomGrouperRestRequest o) {
-        String effectiveQuery = "select count(*) from grouper_memberships_v where group_name = ? and list_type = 'list' and list_name = 'members'";
+        // TODO: allow filters
+        String effectiveQuery = "select count(distinct gm.SUBJECT_ID)" +
+                "from grouper_memberships_all_v gms," +
+                "     grouper_members gm," +
+                "     grouper_groups gg," +
+                "     grouper_fields gfl" +
+                " where gg.name = ?" +
+                "  and gms.OWNER_GROUP_ID = gg.id" +
+                "  and gms.FIELD_ID = gfl.ID" +
+                "  and gms.MEMBER_ID = gm.ID" +
+                "  and gms.IMMEDIATE_MSHIP_ENABLED = 'T'" +
+                "  and gfl.TYPE = 'list' and gfl.NAME='members' and gm.SUBJECT_SOURCE <> 'g:gsa'";
         int effectiveCount = new GcDbAccess().sql(effectiveQuery).addBindVar(o.getUrlStrings().get(2)).selectList(Integer.class).get(0);
 
 
-        String immediateQuery = "select count(*) from grouper_memberships_v where group_name = ? and list_type = 'list' and list_name = 'members' and membership_type = 'immediate'";
+        String immediateQuery = effectiveQuery + " and gms.mship_type = 'immediate'";
         int immediateCount = new GcDbAccess().sql(immediateQuery).addBindVar(o.getUrlStrings().get(2)).selectList(Integer.class).get(0);
 
         return new MembershipCountRestProviderResponse(immediateCount, effectiveCount);
