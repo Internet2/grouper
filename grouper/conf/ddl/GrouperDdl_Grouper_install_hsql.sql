4ee32077c80d86b83d4bb4fd3affa2dd64cd1e87
Chris Hyzer <mchyzer@isc.upenn.edu>
2021-10-20 11:18:59 -0400

    prepare for 2.6.1 hsql ddl

--- grouper/conf/ddl/GrouperDdl_Grouper_install_hsql.sql
+++ grouper/conf/ddl/GrouperDdl_Grouper_install_hsql.sql
@@ -1894,6 +1894,33 @@ CREATE TABLE grouper_file
 
 CREATE UNIQUE INDEX grpfile_unique_idx ON grouper_file (file_path);
 
+CREATE TABLE grouper_prod_zoom_user
+(
+    config_id VARCHAR(50) NOT NULL,
+    member_id VARCHAR(40),
+    id VARCHAR(40) NOT NULL,
+    email VARCHAR(256) NOT NULL,
+    first_name VARCHAR(256),
+    last_name VARCHAR(256),
+    type BIGINT,
+    pmi BIGINT,
+    timezone VARCHAR(100),
+    verified BIGINT,
+    created_at BIGINT,
+    last_login_time BIGINT,
+    language VARCHAR(100),
+    status BIGINT,
+    role_id BIGINT,
+    PRIMARY KEY (email)
+);
+
+CREATE INDEX grouper_zoom_user_config_id_idx ON grouper_prod_zoom_user (config_id);
+
+CREATE UNIQUE INDEX grouper_zoom_user_email_idx ON grouper_prod_zoom_user (email, config_id);
+
+CREATE UNIQUE INDEX grouper_zoom_user_id_idx ON grouper_prod_zoom_user (id, config_id);
+
+CREATE INDEX grouper_zoom_user_member_id_idx ON grouper_prod_zoom_user (member_id, config_id);
 
 ALTER TABLE grouper_composites
     ADD CONSTRAINT fk_composites_owner FOREIGN KEY (owner) REFERENCES grouper_groups (id);
