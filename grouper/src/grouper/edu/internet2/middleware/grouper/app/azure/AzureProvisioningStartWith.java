8c65b7615cffdd894cf52a9377bd3124c387efe5
Vivek Sachdeva <erviveksachdeva@gmail.com>
2022-06-06 22:37:42 -0700

    Add provisioning start with and remove duplicate configs

--- grouper/src/grouper/edu/internet2/middleware/grouper/app/azure/AzureProvisioningStartWith.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/app/azure/AzureProvisioningStartWith.java
@@ -11,6 +11,7 @@ import java.util.Set;
 import edu.internet2.middleware.grouper.app.config.GrouperConfigurationModuleAttribute;
 import edu.internet2.middleware.grouper.app.provisioning.ProvisionerStartWithBase;
 import edu.internet2.middleware.grouper.cfg.text.GrouperTextContainer;
+import edu.internet2.middleware.grouper.util.GrouperUtil;
 import edu.internet2.middleware.grouperClientExt.org.apache.commons.lang3.StringUtils;
 
 /**
@@ -27,7 +28,93 @@ public class AzureProvisioningStartWith extends ProvisionerStartWithBase {
   public void populateProvisionerConfigurationValuesFromStartWith(
       Map<String, String> startWithSuffixToValue,
       Map<String, Object> provisionerSuffixToValue) {
-    // TODO Auto-generated method stub
+    
+    provisionerSuffixToValue.put("azureExternalSystemConfigId", startWithSuffixToValue.get("azureExternalSystemConfigId"));
+    
+    if ( StringUtils.isNotBlank(startWithSuffixToValue.get("userAttributesType")) && !StringUtils.equals(startWithSuffixToValue.get("userAttributesType"), "core")) {
+      provisionerSuffixToValue.put("entityResolver.entityAttributesNotInSubjectSource", "true");
+    }
+    
+    {
+      int numberOfGroupAttributes = 0;
+      
+      provisionerSuffixToValue.put("operateOnGrouperGroups", "true");
+      provisionerSuffixToValue.put("targetGroupAttribute."+numberOfGroupAttributes+".name", startWithSuffixToValue.get("groupDisplayNameAttributeValue"));
+      numberOfGroupAttributes++;
+      
+      if (GrouperUtil.booleanValue(startWithSuffixToValue.get("useGroupDescription"), true)) {
+        provisionerSuffixToValue.put("targetGroupAttribute."+numberOfGroupAttributes+".name", "description");
+        numberOfGroupAttributes++;
+      }
+      
+      provisionerSuffixToValue.put("targetGroupAttribute."+numberOfGroupAttributes+".name", startWithSuffixToValue.get("mailNicknameAttributeValue"));
+      numberOfGroupAttributes++;
+      
+      provisionerSuffixToValue.put("numberOfGroupAttributes", numberOfGroupAttributes);
+    }
+    
+    {
+      int numberOfMetadataItems = 0;
+      
+      if (GrouperUtil.booleanValue(startWithSuffixToValue.get("hasMetadataForGroupType"), true)) {
+        numberOfMetadataItems++;
+        provisionerSuffixToValue.put("metadata.0.name", "md_groupType");
+        provisionerSuffixToValue.put("metadata.0.showForGroup", "true");
+      }
+      
+      if (GrouperUtil.booleanValue(startWithSuffixToValue.get("hasMetadataForAllowOnlyMembersToPost"), true)) {
+        numberOfMetadataItems++;
+        provisionerSuffixToValue.put("metadata.1.name", "md_allowOnlyMembersToPost");
+        provisionerSuffixToValue.put("metadata.1.showForGroup", "true");
+      }
+      
+      if (GrouperUtil.booleanValue(startWithSuffixToValue.get("hasMetadataForHideGroupInOutlook"), true)) {
+        numberOfMetadataItems++;
+        provisionerSuffixToValue.put("metadata.2.name", "md_hideGroupInOutlook");
+        provisionerSuffixToValue.put("metadata.2.showForGroup", "true");
+      }
+      
+      if (GrouperUtil.booleanValue(startWithSuffixToValue.get("hasMetadataForSubscribeNewGroupMembers"), true)) {
+        numberOfMetadataItems++;
+        provisionerSuffixToValue.put("metadata.3.name", "md_subscribeNewGroupMembers");
+        provisionerSuffixToValue.put("metadata.3.showForGroup", "true");
+      }
+      
+      if (GrouperUtil.booleanValue(startWithSuffixToValue.get("hasMetadataForWelcomeEmailDisabled"), true)) {
+        numberOfMetadataItems++;
+        provisionerSuffixToValue.put("metadata.4.name", "md_welcomeEmailDisabled");
+        provisionerSuffixToValue.put("metadata.4.showForGroup", "true");
+      }
+      
+      if (GrouperUtil.booleanValue(startWithSuffixToValue.get("hasMetadataForResourceProvisioningOptionsTeams"), true)) {
+        numberOfMetadataItems++;
+        provisionerSuffixToValue.put("metadata.5.name", "md_resourceProvisioningOptionsTeams");
+        provisionerSuffixToValue.put("metadata.5.showForGroup", "true");
+      }
+      
+      if (numberOfMetadataItems > 0) {
+        provisionerSuffixToValue.put("configureMetadata", "true");
+      }
+    }
+    
+    {
+      int numberOfEntityAttributes = 0;
+      
+      provisionerSuffixToValue.put("operateOnGrouperEntities", "true");
+      provisionerSuffixToValue.put("targetEntityAttribute."+numberOfEntityAttributes+".name", startWithSuffixToValue.get("entityUserPrincipalName"));
+      numberOfEntityAttributes++;
+      
+      if (GrouperUtil.booleanValue(startWithSuffixToValue.get("useGroupDescription"), true)) {
+        provisionerSuffixToValue.put("targetGroupAttribute."+numberOfEntityAttributes+".name", "description");
+        numberOfEntityAttributes++;
+      }
+      
+      provisionerSuffixToValue.put("targetGroupAttribute."+numberOfEntityAttributes+".name", startWithSuffixToValue.get("mailNicknameAttributeValue"));
+      numberOfEntityAttributes++;
+      
+      provisionerSuffixToValue.put("numberOfGroupAttributes", numberOfEntityAttributes);
+    }
+    
     
   }
 
--- grouper/src/grouper/edu/internet2/middleware/grouper/app/duo/DuoProvisioningStartWith.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/app/duo/DuoProvisioningStartWith.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.Set;
 
 import edu.internet2.middleware.grouper.app.provisioning.ProvisionerStartWithBase;
+import edu.internet2.middleware.grouper.util.GrouperUtil;
 import edu.internet2.middleware.grouperClientExt.org.apache.commons.lang3.StringUtils;
 
 public class DuoProvisioningStartWith extends ProvisionerStartWithBase {
@@ -19,7 +20,54 @@ public class DuoProvisioningStartWith extends ProvisionerStartWithBase {
   public void populateProvisionerConfigurationValuesFromStartWith(
       Map<String, String> startWithSuffixToValue,
       Map<String, Object> provisionerSuffixToValue) {
-    // TODO Auto-generated method stub
+    
+    provisionerSuffixToValue.put("duoExternalSystemConfigId", startWithSuffixToValue.get("duoExternalSystemConfigId"));
+    
+    if ( StringUtils.isNotBlank(startWithSuffixToValue.get("userAttributesType")) && !StringUtils.equals(startWithSuffixToValue.get("userAttributesType"), "core")) {
+      provisionerSuffixToValue.put("entityResolver.entityAttributesNotInSubjectSource", "true");
+    }
+    
+    if (GrouperUtil.booleanValue(startWithSuffixToValue.get("manageGroups"), false)) {
+      
+      provisionerSuffixToValue.put("operateOnGrouperGroups", "true");
+      provisionerSuffixToValue.put("numberOfGroupAttributes", 1);
+      
+      provisionerSuffixToValue.put("targetGroupAttribute.0.name", startWithSuffixToValue.get("groupNameAttributeValue"));
+      
+      if (GrouperUtil.booleanValue(startWithSuffixToValue.get("useGroupDescription"), true)) {
+        provisionerSuffixToValue.put("numberOfGroupAttributes", 2);
+        provisionerSuffixToValue.put("targetGroupAttribute.1.name", "description");
+      }
+      
+    }
+    
+    if (GrouperUtil.booleanValue(startWithSuffixToValue.get("manageEntities"), false)) {
+      
+      provisionerSuffixToValue.put("operateOnGrouperEntities", "true");
+      int numberOfEntityAttributes = 1;
+      
+      provisionerSuffixToValue.put("targetEntityAttribute.0.name", startWithSuffixToValue.get("entityUserName"));
+      
+      if (StringUtils.isNotBlank(startWithSuffixToValue.get("entityNameSubjectAttribute"))) {
+        numberOfEntityAttributes++;
+        provisionerSuffixToValue.put("targetEntityAttribute.1.name", startWithSuffixToValue.get("entityNameSubjectAttribute"));
+      }
+      
+      if (StringUtils.isNotBlank(startWithSuffixToValue.get("entityFirstNameSubjectAttribute"))) {
+        numberOfEntityAttributes++;
+        provisionerSuffixToValue.put("targetEntityAttribute.2.name", startWithSuffixToValue.get("entityFirstNameSubjectAttribute"));
+      }
+      
+      if (StringUtils.isNotBlank(startWithSuffixToValue.get("entityEmailSubjectAttribute"))) {
+        numberOfEntityAttributes++;
+        provisionerSuffixToValue.put("targetEntityAttribute.3.name", startWithSuffixToValue.get("entityEmailSubjectAttribute"));
+      }
+      
+      provisionerSuffixToValue.put("numberOfEntityAttributes", numberOfEntityAttributes);
+      
+    }
+    
+    
     
   }
 
