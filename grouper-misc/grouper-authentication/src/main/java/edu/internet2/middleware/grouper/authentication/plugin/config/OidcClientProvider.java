7fcc4b37ed20eea3dfbb56399f70b5b563ca072c
Jj! <jj@unicon.net>
2022-06-07 11:16:30 -0500

    [WIP] update logger

--- grouper-misc/grouper-authentication/src/main/java/edu/internet2/middleware/grouper/authentication/plugin/config/OidcClientProvider.java
+++ grouper-misc/grouper-authentication/src/main/java/edu/internet2/middleware/grouper/authentication/plugin/config/OidcClientProvider.java
@@ -1,15 +1,33 @@
 package edu.internet2.middleware.grouper.authentication.plugin.config;
 
 import edu.internet2.middleware.grouper.authentication.plugin.ConfigUtils;
+import edu.internet2.middleware.grouper.authentication.plugin.ExternalAuthenticationServletContainerInitializer;
+import edu.internet2.middleware.grouper.authentication.plugin.Pac4jConfigFactory;
 import edu.internet2.middleware.grouper.authentication.plugin.oidc.client.ClaimAsUsernameOidcClient;
 import edu.internet2.middleware.grouper.authentication.plugin.oidc.config.ClaimAsUsernameOidcConfiguration;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.log4j.Logger;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
 import org.pac4j.core.client.Client;
 import org.pac4j.oidc.client.OidcClient;
 import org.pac4j.oidc.config.OidcConfiguration;
 
 public class OidcClientProvider implements ClientProvider {
-    private static Logger LOGGER = Logger.getLogger(OidcClientProvider.class);
+    private static final Log LOGGER;
+    static {
+        try {
+            BundleContext bundleContext = FrameworkUtil.getBundle(Pac4jConfigFactory.class).getBundleContext();
+            //TODO: figure out why this is weird
+            ServiceReference<LogFactory> logfactoryReference = (ServiceReference<LogFactory>) bundleContext.getAllServiceReferences("org.apache.commons.logging.LogFactory", null)[0];
+            LOGGER = bundleContext.getService(logfactoryReference).getInstance(ExternalAuthenticationServletContainerInitializer.class);
+        } catch (InvalidSyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
     public boolean supports(String type) {
