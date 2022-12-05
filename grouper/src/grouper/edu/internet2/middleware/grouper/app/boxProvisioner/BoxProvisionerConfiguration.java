94cabb1327db6d9535676834c4f44b5be2b8d81c
Vivek Sachdeva <erviveksachdeva@gmail.com>
2022-12-04 19:34:02 -0800

    Box provisioner validation to require name, id, login for entity and name,id for groups

--- grouper/src/grouper/edu/internet2/middleware/grouper/app/boxProvisioner/BoxProvisionerConfiguration.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/app/boxProvisioner/BoxProvisionerConfiguration.java
@@ -5,8 +5,11 @@ import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 
+import edu.internet2.middleware.grouper.app.config.GrouperConfigurationModuleAttribute;
 import edu.internet2.middleware.grouper.app.provisioning.ProvisioningConfiguration;
 import edu.internet2.middleware.grouper.cfg.dbConfig.ConfigFileName;
+import edu.internet2.middleware.grouper.cfg.text.GrouperTextContainer;
+import edu.internet2.middleware.grouper.util.GrouperUtil;
 
 public class BoxProvisionerConfiguration extends ProvisioningConfiguration {
 
@@ -56,4 +59,96 @@ public class BoxProvisionerConfiguration extends ProvisioningConfiguration {
     super.editConfig(fromUi, message, errorsToDisplay, validationErrorsToDisplay, actionsPerformed);
   }
 
+  
+  @Override
+  public void validatePreSave(boolean isInsert, List<String> errorsToDisplay,
+      Map<String, String> validationErrorsToDisplay) {
+    
+    super.validatePreSave(isInsert, errorsToDisplay, validationErrorsToDisplay);
+    
+    if (errorsToDisplay.size() > 0 || validationErrorsToDisplay.size() > 0) {
+      return;
+    } 
+    
+    GrouperConfigurationModuleAttribute numberOfGroupAttributes = this.retrieveAttributes().get("numberOfGroupAttributes");
+    
+    int numberOfGroupAttributesLength = 0;
+    
+    boolean groupNameThere = false;
+    boolean groupIdThere = false;
+    
+    if (numberOfGroupAttributes != null) {
+      
+      numberOfGroupAttributesLength = GrouperUtil.intValue(numberOfGroupAttributes.getValueOrExpressionEvaluation(), 0);
+      
+      for (int i=0; i<numberOfGroupAttributesLength; i++) {
+        
+        GrouperConfigurationModuleAttribute attributeName = this.retrieveAttributes().get("targetGroupAttribute."+i+".name");
+        String value = attributeName.getValueOrExpressionEvaluation();
+        
+        if (StringUtils.equals("name", value)) {
+          groupNameThere = true;
+        }
+        if (StringUtils.equals("id", value)) {
+          groupIdThere = true;
+        } 
+      }
+    }
+    
+    if (!groupIdThere) {
+      String errorMessage = GrouperTextContainer.textOrNull("grouperBoxProvisionerConfiugrationGroupIdRequired");
+      errorsToDisplay.add(errorMessage);
+    }
+
+    if (!groupNameThere) {
+      String errorMessage = GrouperTextContainer.textOrNull("grouperBoxProvisionerConfiugrationGroupNameRequired");
+      errorsToDisplay.add(errorMessage);
+    }
+    
+    
+    GrouperConfigurationModuleAttribute numberOfEntityAttributes = this.retrieveAttributes().get("numberOfEntityAttributes");
+    
+    int numberOfEntityAttributesLength = 0;
+    
+    boolean entityNameThere = false;
+    boolean entityLoginThere = false;
+    boolean entityIdThere = false;
+    
+    if (numberOfEntityAttributes != null) {
+      
+      numberOfEntityAttributesLength = GrouperUtil.intValue(numberOfEntityAttributes.getValueOrExpressionEvaluation(), 0);
+      
+      for (int i=0; i<numberOfEntityAttributesLength; i++) {
+        
+        GrouperConfigurationModuleAttribute attributeName = this.retrieveAttributes().get("targetEntityAttribute."+i+".name");
+        String value = attributeName.getValueOrExpressionEvaluation();
+        
+        if (StringUtils.equals("name", value)) {
+          entityNameThere = true;
+        }
+        if (StringUtils.equals("id", value)) {
+          entityIdThere = true;
+        } 
+        if (StringUtils.equals("login", value)) {
+          entityLoginThere = true;
+        } 
+      }
+    }
+    
+    if (!entityIdThere) {
+      String errorMessage = GrouperTextContainer.textOrNull("grouperBoxProvisionerConfiugrationEntityIdRequired");
+      errorsToDisplay.add(errorMessage);
+    }
+
+    if (!entityNameThere) {
+      String errorMessage = GrouperTextContainer.textOrNull("grouperBoxProvisionerConfiugrationEntityNameRequired");
+      errorsToDisplay.add(errorMessage);
+    }
+    
+    if (!entityLoginThere) {
+      String errorMessage = GrouperTextContainer.textOrNull("grouperBoxProvisionerConfiugrationEntityNameRequired");
+      errorsToDisplay.add(errorMessage);
+    }
+    
+  }
 }
