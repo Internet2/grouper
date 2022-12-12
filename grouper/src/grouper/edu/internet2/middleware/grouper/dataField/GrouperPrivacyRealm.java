3ca735e8f5d5065301d95f87dcb815c3199f3bd4
mchyzer <mchyzer@isc.upenn.edu>
2022-12-12 13:46:31 -0500

    add more data field settings

---
+++ grouper/src/grouper/edu/internet2/middleware/grouper/dataField/GrouperPrivacyRealm.java
@@ -0,0 +1,32 @@
+package edu.internet2.middleware.grouper.dataField;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import edu.internet2.middleware.grouper.cfg.GrouperConfig;
+import edu.internet2.middleware.grouper.cfg.dbConfig.OptionValueDriver;
+import edu.internet2.middleware.grouper.util.GrouperUtil;
+import edu.internet2.middleware.grouperClient.collections.MultiKey;
+
+public class GrouperPrivacyRealm implements OptionValueDriver {
+
+
+  /**
+   * some required config to see what the privacy realms
+   */
+  private static Pattern fieldConfigIds = Pattern.compile("^grouperPrivacyRealm\\.[^.]+\\.privacyRealmName$");
+  
+  @Override
+  public List<MultiKey> retrieveKeysAndLabels() {
+        
+    Set<String> configIds = GrouperConfig.retrieveConfig().propertyConfigIds(fieldConfigIds);
+    List<MultiKey> results = new ArrayList<>();
+    for (String theConfigId : GrouperUtil.nonNull(configIds)) {
+      results.add(new MultiKey(theConfigId, theConfigId));
+    }
+    return results;
+  }
+
+}
