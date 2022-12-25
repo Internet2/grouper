3a9fc477ea8a8d7cfbb43406b4a2a6bea888c6ca
Vivek Sachdeva <erviveksachdeva@gmail.com>
2022-12-25 01:14:39 -0800

    Add test for jexl script tester

--- grouper/src/test/edu/internet2/middleware/grouper/app/jexlTester/JexlScriptTesterTest.java
+++ grouper/src/test/edu/internet2/middleware/grouper/app/jexlTester/JexlScriptTesterTest.java
@@ -1,7 +1,6 @@
 package edu.internet2.middleware.grouper.app.jexlTester;
 
 import edu.internet2.middleware.grouper.helper.GrouperTest;
-import edu.internet2.middleware.grouper.util.GrouperUtil;
 import junit.textui.TestRunner;
 
 public class JexlScriptTesterTest extends GrouperTest {
@@ -32,27 +31,14 @@ public class JexlScriptTesterTest extends GrouperTest {
       ScriptExample[] enumConstants = scriptExampleForTypeClass.getEnumConstants();
       
       for (ScriptExample scriptExample: enumConstants) {
-        try {
-          String availableBeansGshScript = scriptExample.retrieveAvailableBeansGshScript();
-          String nullCheckingJexlScript = scriptExample.retrieveNullCheckingJexlScript();
-          String exampleJexlScript = scriptExample.retrieveExampleJexlScript();
-          
-          JexlScriptTesterResult jexlScriptTesterResult = JexlScriptTester.runJexlScript(scriptExample, availableBeansGshScript, nullCheckingJexlScript, exampleJexlScript);
-          String resultString = null;
-          if (scriptExample.expectedOutput() != null) {
-            Object expectedOutput = scriptExample.expectedOutput();
-            resultString = expectedOutput.getClass().getName() + ": " + GrouperUtil.stringValue(expectedOutput);
-          } else {
-            resultString = "<null>";
-          }
-          String resultForScreen = jexlScriptTesterResult.getResultForScreen();
-          
-          assertEquals(scriptExample + " error ", true, jexlScriptTesterResult.isSuccess());
-          assertEquals(scriptExample + " error ", resultString, resultForScreen);
-        } catch (RuntimeException e) {
-          GrouperUtil.injectInException(e, scriptExample + " error ");
-          throw e;
-        }
+        
+        String availableBeansGshScript = scriptExample.retrieveAvailableBeansGshScript();
+        String nullCheckingJexlScript = scriptExample.retrieveNullCheckingJexlScript();
+        String exampleJexlScript = scriptExample.retrieveExampleJexlScript();
+        
+        JexlScriptTesterResult jexlScriptTesterResult = JexlScriptTester.runJexlScript(scriptExample, availableBeansGshScript, nullCheckingJexlScript, exampleJexlScript);
+        
+        assertEquals(true, jexlScriptTesterResult.isSuccess());
         
       }
       
