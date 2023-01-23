0dbd5a75e73680fd4c88a7023d2e5930b3325838
Shilen Patel <sipatel2@gmail.com>
2023-01-23 08:21:01 -0500

    GRP-4567: Upgrade Java and Tomcat and remove PSU SCIM (commit 7)

--- grouper/misc/ci-test/ci-test-pgsql.sh
+++ grouper/misc/ci-test/ci-test-pgsql.sh
@@ -173,6 +173,9 @@ $JAVA -classpath "$CP" \
   -Dgrouper.allow.db.changes=true \
   -Dgrouper.home=./ \
   -Xms80m -Xmx640m \
+  --add-opens java.base/java.lang=ALL-UNNAMED \
+  --add-opens java.base/java.util=ALL-UNNAMED \
+  --add-opens java.sql/java.sql=ALL-UNNAMED \
   edu.internet2.middleware.grouper.AllTests \
   -all -noprompt \
   >>$TESTLOG 2>&1
@@ -206,6 +209,9 @@ $JAVA -classpath "$CP" \
   -Dgrouper.allow.db.changes=true \
   -Dgrouper.home=grouper-misc/grouper-pspng \
   -Xms80m -Xmx640m \
+  --add-opens java.base/java.lang=ALL-UNNAMED \
+  --add-opens java.base/java.util=ALL-UNNAMED \
+  --add-opens java.sql/java.sql=ALL-UNNAMED \
   edu.internet2.middleware.grouper.AllTests pspng.AllPspngTests \
   -noprompt \
   >>$TESTLOG 2>&1
