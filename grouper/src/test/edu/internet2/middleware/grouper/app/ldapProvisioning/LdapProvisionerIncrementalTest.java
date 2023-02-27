b7220736daab68c33ca620d942b9c49088798fa7
mchyzer <mchyzer@isc.upenn.edu>
2023-02-27 13:03:36 -0500

    fix test

--- grouper/src/test/edu/internet2/middleware/grouper/app/ldapProvisioning/LdapProvisionerIncrementalTest.java
+++ grouper/src/test/edu/internet2/middleware/grouper/app/ldapProvisioning/LdapProvisionerIncrementalTest.java
@@ -63,7 +63,7 @@ public class LdapProvisionerIncrementalTest extends GrouperProvisioningBaseTest 
    * @param args
    */
   public static void main(String[] args) {
-    TestRunner.run(new LdapProvisionerIncrementalTest("testDoNotAddIfNotExistIncremental"));    
+    TestRunner.run(new LdapProvisionerIncrementalTest("testDoNotAddIfNotExistFull"));    
   }
   
   @Override
