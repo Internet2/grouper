02239e478e7b62a9d006b71009ab1a1232cbf03a
Shilen Patel <sipatel2@gmail.com>
2023-09-01 21:05:17 -0400

    GRP-4928: ldap provisioner wants to update dn based on case

--- grouper/src/grouper/edu/internet2/middleware/grouper/app/ldapProvisioning/LdapSyncCompare.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/app/ldapProvisioning/LdapSyncCompare.java
@@ -1,5 +1,10 @@
 package edu.internet2.middleware.grouper.app.ldapProvisioning;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 
 import com.unboundid.ldap.sdk.DN;
@@ -39,15 +44,22 @@ public class LdapSyncCompare extends GrouperProvisioningCompare {
     String dn1;
     String dn2;
     
+    String baseDnString = null;
+    if ("group".equals(grouperTargetUpdatable.objectTypeName())) {
+      baseDnString = ((LdapSyncConfiguration)this.getGrouperProvisioner().retrieveGrouperProvisioningConfiguration()).getGroupSearchBaseDn();
+    } else if ("entity".equals(grouperTargetUpdatable.objectTypeName())) {
+      baseDnString = ((LdapSyncConfiguration)this.getGrouperProvisioner().retrieveGrouperProvisioningConfiguration()).getUserSearchBaseDn();
+    }
+    
     try {
-      dn1 = getMinimallyEncodedDNStringWithLowercaseAttributeNames((String)grouperValue);
+      dn1 = getMinimallyEncodedDNStringWithLowercaseAttributeNamesIgnoreBase((String)grouperValue, baseDnString);
     } catch (LDAPException e) {
       LOG.warn("Failed to parse DN: " + grouperValue, e);
       return originalCheck;
     }
     
     try {
-      dn2 = getMinimallyEncodedDNStringWithLowercaseAttributeNames((String)targetValue);
+      dn2 = getMinimallyEncodedDNStringWithLowercaseAttributeNamesIgnoreBase((String)targetValue, baseDnString);
     } catch (LDAPException e) {
       LOG.warn("Failed to parse DN: " + targetValue, e);
       return originalCheck;
@@ -56,19 +68,37 @@ public class LdapSyncCompare extends GrouperProvisioningCompare {
     return GrouperUtil.equals(dn1, dn2);
   }
   
-  private String getMinimallyEncodedDNStringWithLowercaseAttributeNames(String dnString) throws LDAPException {
+  private static String getMinimallyEncodedDNStringWithLowercaseAttributeNamesIgnoreBase(String dnString, String baseDnString) throws LDAPException {
     DN dn = new DN(dnString);
     
-    for (RDN rdn : dn.getRDNs()) {
+    List<RDN> rdns;
+    
+    if (StringUtils.isEmpty(baseDnString)) {
+      rdns = Arrays.asList(dn.getRDNs());
+    } else {
+      rdns = new ArrayList<RDN>();
+      List<RDN> remainingRdns = new ArrayList<>(Arrays.asList(dn.getRDNs()));
+      String baseDnNormalizedString = new DN(baseDnString).toNormalizedString();
+      
+      while (remainingRdns.size() > 0) {
+        if (new DN(remainingRdns.toArray(new RDN[0])).toNormalizedString().equals(baseDnNormalizedString)) {
+          // the rest is the base
+          break;
+        }
+        rdns.add(remainingRdns.remove(0));
+      }
+    }
+    
+    for (RDN rdn : rdns) {
       String[] oldAttributeNames = rdn.getAttributeNames();
       String[] newAttributeNames = new String[oldAttributeNames.length];
       for (int i = 0; i < oldAttributeNames.length; i++) {
         newAttributeNames[i] = oldAttributeNames[i].toLowerCase();
       }
       
-      GrouperUtil.assignField(rdn, "attributeNames", newAttributeNames);
+      GrouperUtil.assignField(rdn, "attributeNames", newAttributeNames);      
     }
     
-    return dn.toMinimallyEncodedString();
+    return new DN(rdns).toMinimallyEncodedString();
   }
 }
