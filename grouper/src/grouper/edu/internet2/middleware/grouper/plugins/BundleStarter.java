97ee2520078f9ce19beefa995968a295bb6877ec
Jj! <jj@scaldingspoon.com>
2023-10-09 17:15:10 -0500

    refactor deprecate old namespace use full location for bundle rather than just the file name

--- grouper/src/grouper/edu/internet2/middleware/grouper/plugins/BundleStarter.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/plugins/BundleStarter.java
@@ -1,8 +1,10 @@
 package edu.internet2.middleware.grouper.plugins;
 
+import edu.internet2.middleware.grouper.Group;
 import edu.internet2.middleware.grouper.cfg.GrouperConfig;
 import edu.internet2.middleware.grouper.util.GrouperUtil;
 import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
@@ -22,12 +24,15 @@ public class BundleStarter {
 
     private final String bundleDirWithLastSlash;
 
+    private final Map<String, Bundle> installedBundles;
+
     public BundleStarter(BundleContext bundleContext) {
         this.bundleContext = bundleContext;
+        this.installedBundles = new HashMap<>();
 
         try {
             Path tmpDir = Files.createTempDirectory("grouper");
-            bundleDirWithLastSlash = GrouperUtil.stripLastSlashIfExists(GrouperConfig.retrieveConfig().propertyValueString("grouper.osgi.jar.dir", tmpDir.toString())) + File.separator;
+            bundleDirWithLastSlash = GrouperUtil.stripLastSlashIfExists(GrouperConfig.retrieveConfig().propertyValueString("grouper.osgi.jar.dir", tmpDir.toString())) + "/";
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -36,16 +41,37 @@ public class BundleStarter {
     public void start() {
         Map<String, Map<Class<?>, Set<String>>> pluginJarNameToInterfaceToImplementationClasses = new HashMap<String, Map<Class<?>, Set<String>>>();
 
-        Pattern pattern = Pattern.compile("^grouperOsgiPlugin\\.([^.]+)\\.jarName$");
+        // TODO: deprecate
+        {
+            Pattern pattern = Pattern.compile("^grouperOsgiPlugin\\.([^.]+)\\.jarName$");
+            Set<String> configIds = GrouperConfig.retrieveConfig().propertyConfigIds(pattern);
+            if (GrouperUtil.length(configIds) > 0) {
+                for (String configId: configIds) {
+                    LOG.warn("DEPRECATION WARNING: you are using older configuration in the namespace `grouperOsgiPlugin`; this will be removed in future versions. Update configuration to use `grouper.osgi.plugin`");
+                    String jarName = GrouperConfig.retrieveConfig().propertyValueString("grouperOsgiPlugin." + configId + ".jarName");
+                    Bundle bundle = null;
+                    try {
+                        bundle = bundleContext.installBundle("file:" + bundleDirWithLastSlash + jarName);
+                        bundle.start();
+                        installedBundles.put(configId, bundle);
+                    } catch (BundleException e) {
+                        LOG.error("Problem installing plugin: " + jarName, e);
+                    }
+                }
+            }
+        }
+
+        Pattern pattern = Pattern.compile("^grouper\\.osgi\\.plugin\\.([^.]+)\\.location$");
         Set<String> configIds = GrouperConfig.retrieveConfig().propertyConfigIds(pattern);
         if (GrouperUtil.length(configIds) > 0) {
             for (String configId: configIds) {
-                String jarName = GrouperConfig.retrieveConfig().propertyValueString("grouperOsgiPlugin." + configId + ".jarName");
+                String location = GrouperConfig.retrieveConfig().propertyValueString("grouper.osgi.plugin." + configId + ".location");
                 try {
-                    Bundle bundle = bundleContext.installBundle("file:" + bundleDirWithLastSlash + jarName);
+                    Bundle bundle = bundleContext.installBundle(location);
                     bundle.start();
+                    installedBundles.put(configId, bundle);
                 } catch (BundleException e) {
-                    LOG.error("Problem installing plugin: " + jarName, e);
+                    LOG.error("Problem installing plugin: " + location, e);
                 }
             }
         }
