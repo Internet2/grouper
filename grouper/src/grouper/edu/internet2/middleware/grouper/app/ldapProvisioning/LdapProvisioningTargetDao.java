efdf88d5dd70169a579824400547f69d50756849
Vivek Sachdeva <erviveksachdeva@gmail.com>
2024-03-26 23:36:19 -0700

    GRP-5308 - Provisioning entities not filtering objectClass when Select All Entities is false

--- grouper/src/grouper/edu/internet2/middleware/grouper/app/ldapProvisioning/LdapProvisioningTargetDao.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/app/ldapProvisioning/LdapProvisioningTargetDao.java
@@ -894,7 +894,7 @@ public class LdapProvisioningTargetDao extends GrouperProvisionerTargetDaoBase {
       String userSearchAllFilter = ldapSyncConfiguration.getEntitySearchAllFilter();
       
       if (StringUtils.isEmpty(userSearchAllFilter)) {
-        userSearchAllFilter = this.generateUserSearchAllFilter();
+        userSearchAllFilter = this.generateUserSearchFilter(null);
       }
 
       if (StringUtils.isEmpty(userSearchAllFilter)) {
@@ -1019,6 +1019,8 @@ public class LdapProvisioningTargetDao extends GrouperProvisionerTargetDaoBase {
             if (GrouperUtil.length(batchValues) > 1) {
               filter = "(|" + filter + ")";
             }
+            
+            filter = generateUserSearchFilter(filter);
             
             LdapSyncDaoForLdap ldapSyncDaoForLdap = new LdapSyncDaoForLdap();
             List<LdapEntry> ldapEntries = ldapSyncDaoForLdap.search(ldapConfigId, entitySearchBaseDn, filter, LdapSearchScope.SUBTREE_SCOPE, new ArrayList<String>(entitySearchAttributeNames));
@@ -1542,9 +1544,12 @@ public class LdapProvisioningTargetDao extends GrouperProvisionerTargetDaoBase {
 
   /**
    * look at object classes and the search attribute
+   * @param individualFilter - leave blank to get all or pass in a filter for an individual filter
    * @return the filter
    */
-  public String generateUserSearchAllFilter() {
+  public String generateUserSearchFilter(String individualFilter) {
+    
+    boolean useAllFilter = StringUtils.isBlank(individualFilter);
     
     LdapSyncConfiguration ldapSyncConfiguration = (LdapSyncConfiguration) this.getGrouperProvisioner().retrieveGrouperProvisioningConfiguration();
 
@@ -1553,12 +1558,14 @@ public class LdapProvisioningTargetDao extends GrouperProvisionerTargetDaoBase {
     // get the search attribute
     List<GrouperProvisioningConfigurationAttribute> grouperProvisioningConfigurationAttributes = new ArrayList<GrouperProvisioningConfigurationAttribute>(ldapSyncConfiguration.getEntitySearchAttributes());
     
-    // exclude ldap_dn as a search attribute
-    Iterator<GrouperProvisioningConfigurationAttribute> grouperProvisioningConfigurationAttributesIter = grouperProvisioningConfigurationAttributes.iterator();
-    while (grouperProvisioningConfigurationAttributesIter.hasNext()) {
-      GrouperProvisioningConfigurationAttribute grouperProvisioningConfigurationAttribute = grouperProvisioningConfigurationAttributesIter.next();
-      if (StringUtils.equals(grouperProvisioningConfigurationAttribute.getName(), ldap_dn)) {
-        grouperProvisioningConfigurationAttributesIter.remove();
+    if (useAllFilter) {
+      // exclude ldap_dn as a search attribute
+      Iterator<GrouperProvisioningConfigurationAttribute> grouperProvisioningConfigurationAttributesIter = grouperProvisioningConfigurationAttributes.iterator();
+      while (grouperProvisioningConfigurationAttributesIter.hasNext()) {
+        GrouperProvisioningConfigurationAttribute grouperProvisioningConfigurationAttribute = grouperProvisioningConfigurationAttributesIter.next();
+        if (StringUtils.equals(grouperProvisioningConfigurationAttribute.getName(), ldap_dn)) {
+          grouperProvisioningConfigurationAttributesIter.remove();
+        }
       }
     }
     
@@ -1583,21 +1590,32 @@ public class LdapProvisioningTargetDao extends GrouperProvisionerTargetDaoBase {
       }
     }
     
-    int numberOfConditions = GrouperUtil.length(objectClasses) + (grouperProvisioningConfigurationAttributes.size() > 0 ? 1 : 0);
+    int numberOfConditions = 0;
+    if (useAllFilter) {
+      numberOfConditions = GrouperUtil.length(objectClasses) + (grouperProvisioningConfigurationAttributes.size() > 0 ? 1 : 0);
+    } else {
+      numberOfConditions = GrouperUtil.length(objectClasses) + 1;
+    }
+    
     
     if (numberOfConditions > 1) {
       filterBuilder.append("(&");
     }
-
-    if (grouperProvisioningConfigurationAttributes.size() > 1) {
-      filterBuilder.append("(|");
+    
+    if (useAllFilter) {      
+      if (grouperProvisioningConfigurationAttributes.size() > 1) {
+        filterBuilder.append("(|");
+      }
+      for (GrouperProvisioningConfigurationAttribute grouperProvisioningConfigurationAttribute : grouperProvisioningConfigurationAttributes) {
+        filterBuilder.append("(" + GrouperUtil.ldapFilterEscape(grouperProvisioningConfigurationAttribute.getName()) + "=*)");
+      }
+      if (grouperProvisioningConfigurationAttributes.size() > 1) {
+        filterBuilder.append(")");
+      }
+    } else {
+      filterBuilder.append(individualFilter);
     }
-    for (GrouperProvisioningConfigurationAttribute grouperProvisioningConfigurationAttribute : grouperProvisioningConfigurationAttributes) {
-      filterBuilder.append("(" + GrouperUtil.ldapFilterEscape(grouperProvisioningConfigurationAttribute.getName()) + "=*)");
-    }
-    if (grouperProvisioningConfigurationAttributes.size() > 1) {
-      filterBuilder.append(")");
-    }
+    
     for (String objectClass : GrouperUtil.nonNull(objectClasses)) {
       filterBuilder.append("(objectclass=").append(GrouperUtil.ldapFilterEscape(objectClass)).append(")");
     }
