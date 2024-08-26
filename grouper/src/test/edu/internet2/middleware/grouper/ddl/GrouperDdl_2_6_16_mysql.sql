fb8bf5241ed874f5daffce9ad26b75df81047b82
mchyzer <mchyzer@isc.upenn.edu>
2024-08-26 02:34:52 -0400

    GRP-5641: add grouper_ddl_worker table to ddl unit test

--- grouper/src/test/edu/internet2/middleware/grouper/ddl/GrouperDdl_2_6_16_mysql.sql
+++ grouper/src/test/edu/internet2/middleware/grouper/ddl/GrouperDdl_2_6_16_mysql.sql
@@ -1,3 +1,15 @@
+CREATE TABLE grouper_ddl_worker
+(
+    id VARCHAR(40) NOT NULL,
+    grouper VARCHAR(40) NOT NULL,
+    worker_uuid VARCHAR(40) NOT NULL,
+    heartbeat DATETIME,
+    last_updated DATETIME NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE UNIQUE INDEX grouper_ddl_worker_grp_idx ON grouper_ddl_worker (grouper);
+
 CREATE TABLE grouper_ddl
 (
     id VARCHAR(40) NOT NULL,
