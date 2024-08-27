aa65d08e9ef3c5268ffb7d9fd47b57cfa5dad64d
mchyzer <mchyzer@isc.upenn.edu>
2024-08-27 14:15:31 -0400

    GRP-5656: grouper should set daemon start dates by default if job doesnt set them

--- grouper/src/grouper/edu/internet2/middleware/grouper/app/loader/db/Hib3GrouperLoaderLog.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/app/loader/db/Hib3GrouperLoaderLog.java
@@ -114,7 +114,7 @@ public class Hib3GrouperLoaderLog implements HibGrouperLifecycle {
   private String status;
   
   /** when the job started */
-  private Timestamp startedTime = null; // new Timestamp(System.currentTimeMillis());
+  private Timestamp startedTime = new Timestamp(System.currentTimeMillis());
 
   /** when the record was last updated */
   private Timestamp lastUpdated;
