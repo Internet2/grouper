271a8903316c9410beea2956323bcc5936c59d09
mchyzer <mchyzer@isc.upenn.edu>
2025-01-26 08:28:10 -0500

    GRP-5963: batch scim insert memberships and retry each if batch fails

--- grouper/src/grouper/edu/internet2/middleware/grouper/app/loader/GrouperDaemonDeleteOldRecords.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/app/loader/GrouperDaemonDeleteOldRecords.java
@@ -222,7 +222,10 @@ public class GrouperDaemonDeleteOldRecords extends OtherJobBase {
       GrouperDaemonUtils.stopProcessingIfJobPaused();
 
       try {
-        verifyTableIdIndexes(jobMessage, hib3GrouploaderLog);
+        boolean everythingOk = verifyTableIdIndexes(jobMessage, hib3GrouploaderLog);
+        if (!everythingOk) {
+          error = true;
+        }
       } catch (Exception e) {
         LOG.error("Error in verifyTableIdIndexes", e);
         GrouperLoaderLogger.addLogEntry(LOG_LABEL, "errorInVerifyTableIdIndexes", ExceptionUtils.getFullStackTrace(e));
@@ -261,7 +264,7 @@ public class GrouperDaemonDeleteOldRecords extends OtherJobBase {
   
       GrouperDaemonUtils.stopProcessingIfJobPaused();
 
-      if (error) {
+      if (error || StringUtils.equals(GrouperLoaderStatus.ERROR.name(), hib3GrouploaderLog.getStatus())) {
         hib3GrouploaderLog.setStatus(GrouperLoaderStatus.ERROR.name());
         GrouperLoaderLogger.addLogEntry(LOG_LABEL, "status", "error");
       } else {
@@ -1090,9 +1093,12 @@ public class GrouperDaemonDeleteOldRecords extends OtherJobBase {
 
   /**
    * verify that table id indexes
+   * @return true if of, and false if there was an error
    */
-  public static void verifyTableIdIndexes(StringBuilder jobMessage, Hib3GrouperLoaderLog hib3GrouploaderLog) {
+  public static boolean verifyTableIdIndexes(StringBuilder jobMessage, Hib3GrouperLoaderLog hib3GrouploaderLog) {
 
+    boolean everythingOk = true;
+    
     //lets see if there are any nulls
     for (TableIndexType tableIndexType : TableIndexType.values()) {
       
@@ -1103,7 +1109,22 @@ public class GrouperDaemonDeleteOldRecords extends OtherJobBase {
       try {
 
         // maybe this table doesnt have an id col
-        GcTableSyncTableMetadata gcTableSyncTableMetadata = GcTableSyncTableMetadata.retrieveTableMetadataFromCacheOrDatabase("grouper", tableIndexType.tableName());
+      GcTableSyncTableMetadata gcTableSyncTableMetadata = null;
+      
+      try {
+        gcTableSyncTableMetadata = GcTableSyncTableMetadata.retrieveTableMetadataFromCacheOrDatabase("grouper", tableIndexType.tableName());
+      } catch (RuntimeException e) {
+        String errorMessage = "Error finding table metadata for table '" + tableIndexType.tableName() + "'.  Note; this is expected if you are in the middle of an upgrade";
+        LOG.error(errorMessage, e);
+        if (jobMessage != null) {
+          jobMessage.append("\n\n" + errorMessage + "\n" + GrouperUtil.getFullStackTrace(e) + "\n\n");
+        }
+        if (hib3GrouploaderLog != null) {
+          hib3GrouploaderLog.setStatus(GrouperLoaderStatus.ERROR.name());
+        }
+        everythingOk = false;
+        continue;
+      }
         if (gcTableSyncTableMetadata != null) {
           GcTableSyncColumnMetadata gcTableSyncColumnMetadata = gcTableSyncTableMetadata.lookupColumn("id", false);
           if (gcTableSyncColumnMetadata == null) {
@@ -1149,5 +1170,6 @@ public class GrouperDaemonDeleteOldRecords extends OtherJobBase {
       }
 
     }
+    return everythingOk;
   }
 }
