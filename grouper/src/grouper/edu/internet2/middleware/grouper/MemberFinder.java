5890bc6760fb72bf8fd19f9836f7a5512781062c
mchyzer <mchyzer@isc.upenn.edu>
2025-03-01 13:34:05 -0500

    fix compile

--- grouper/src/grouper/edu/internet2/middleware/grouper/MemberFinder.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/MemberFinder.java
@@ -33,10 +33,12 @@
 package edu.internet2.middleware.grouper;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.commons.lang.StringUtils;
