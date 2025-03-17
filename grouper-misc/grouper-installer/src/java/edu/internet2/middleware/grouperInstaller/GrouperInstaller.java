03ef354e3a97a9273570463a53f085477ae6e409
mchyzer <mchyzer@isc.upenn.edu>
2025-03-17 19:10:11 -0400

    GRP-6060: upgrade tomcat to 9.0.102 which addresses some security vulnerabilities

--- grouper-misc/grouper-installer/src/java/edu/internet2/middleware/grouperInstaller/GrouperInstaller.java
+++ grouper-misc/grouper-installer/src/java/edu/internet2/middleware/grouperInstaller/GrouperInstaller.java
@@ -90,7 +90,7 @@ import org.apache.commons.httpclient.methods.GetMethod;
  */
 public class GrouperInstaller {
 
-  public static final String TOMCAT_VERSION = "9.0.98";
+  public static final String TOMCAT_VERSION = "9.0.102";
 
   /**
    * tomcat version
