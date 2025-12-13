53f5f204ddadc537adc4caca5ebab4010787acf0
mchyzer <mchyzer@isc.upenn.edu>
2025-12-13 18:18:52 -0500

    GRP-6463: Add ability for generic provisioners to load data back to Grouper

--- grouper/src/grouper/edu/internet2/middleware/grouper/app/genericProvisioner/GrouperGenericConfiguration.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/app/genericProvisioner/GrouperGenericConfiguration.java
@@ -5,14 +5,22 @@ import edu.internet2.middleware.grouper.app.provisioning.GrouperProvisioningConf
 public class GrouperGenericConfiguration extends GrouperProvisioningConfiguration {
 
   private String genericProvisionerDaoClassName;
+  private String genericLoaderClassName;
   
-    
+  public String getGenericLoaderClassName() {
+    return genericLoaderClassName;
+  }
+  
+  public void setGenericLoaderClassName(String genericLoaderClassName) {
+    this.genericLoaderClassName = genericLoaderClassName;
+  }
 
   @Override
   public void configureSpecificSettings() {
     
     this.genericProvisionerDaoClassName = this.retrieveConfigString("genericProvisionerDaoClassName", true);
     
+    this.genericLoaderClassName = this.retrieveConfigString("genericLoaderClassName", false);
   }
   
   public String getGenericProvisionerDaoClassName() {
