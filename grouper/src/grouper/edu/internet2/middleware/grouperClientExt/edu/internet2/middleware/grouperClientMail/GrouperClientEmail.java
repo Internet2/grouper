8e96e98802b3767b3112a9759564049b3672816a
Chad Redman <credman@unicon.net>
2025-12-22 16:39:20 -0500

    V6 library updates: migrate javax.activation and javax.mail to angus-mail (Jakarta-compatible)

--- grouper/src/grouper/edu/internet2/middleware/grouperClientExt/edu/internet2/middleware/grouperClientMail/GrouperClientEmail.java
+++ grouper/src/grouper/edu/internet2/middleware/grouperClientExt/edu/internet2/middleware/grouperClientMail/GrouperClientEmail.java
@@ -21,14 +21,14 @@ package edu.internet2.middleware.grouperClientExt.edu.internet2.middleware.group
 
 import java.util.Properties;
 
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.Message.RecipientType;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.Message.RecipientType;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 
 import edu.internet2.middleware.grouperClient.util.GrouperClientConfig;
 import edu.internet2.middleware.grouperClient.util.GrouperClientUtils;
