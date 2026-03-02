684094a537c82d438a804d57087e45f37b6686e7
mchyzer <mchyzer@isc.upenn.edu>
2026-03-02 18:13:15 -0500

    GRP-6641: (commit 4) do not allow a delete of data field if it is used somewhere else

--- grouper/src/grouper/edu/internet2/middleware/grouper/dataField/GrouperPrivacyRealmConfiguration.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/dataField/GrouperPrivacyRealmConfiguration.java
@@ -1,5 +1,6 @@
 package edu.internet2.middleware.grouper.dataField;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -9,6 +10,8 @@ import org.apache.commons.lang3.StringUtils;
 
 import edu.internet2.middleware.grouper.app.config.GrouperConfigurationModuleBase;
 import edu.internet2.middleware.grouper.cfg.dbConfig.ConfigFileName;
+import edu.internet2.middleware.grouper.exception.GrouperReferentialIntegrityException;
+import edu.internet2.middleware.grouper.util.GrouperUtil;
 
 public class GrouperPrivacyRealmConfiguration extends GrouperConfigurationModuleBase {
   
@@ -48,6 +51,37 @@ public class GrouperPrivacyRealmConfiguration extends GrouperConfigurationModule
    Set<String> classNames = new HashSet<String>();
    classNames.add(GrouperPrivacyRealmConfiguration.class.getName());
    return (List<GrouperPrivacyRealmConfiguration>) (Object) retrieveAllConfigurations(classNames);
+  }
+
+  @Override
+  public void deleteConfig(boolean fromUi) {
+
+    String configId = this.getConfigId();
+
+    List<String> referencingConfigs = new ArrayList<>();
+
+    // check if this privacy realm is referenced by any data fields or data rows
+    GrouperDataEngine grouperDataEngine = new GrouperDataEngine();
+    grouperDataEngine.loadFieldsAndRows(null);
+
+    for (Map.Entry<String, GrouperDataFieldConfig> fieldEntry : grouperDataEngine.getFieldConfigByConfigId().entrySet()) {
+      if (StringUtils.equals(configId, fieldEntry.getValue().getGrouperPrivacyRealmConfigId())) {
+        referencingConfigs.add("data field '" + fieldEntry.getKey() + "'");
+      }
+    }
+
+    for (Map.Entry<String, GrouperDataRowConfig> rowEntry : grouperDataEngine.getRowConfigByConfigId().entrySet()) {
+      if (StringUtils.equals(configId, rowEntry.getValue().getPrivacyRealmName())) {
+        referencingConfigs.add("data row '" + rowEntry.getKey() + "'");
+      }
+    }
+
+    if (referencingConfigs.size() > 0) {
+      String referencingConfigsString = StringUtils.join(referencingConfigs, ", ");
+      throw new GrouperReferentialIntegrityException("Error: cannot delete this privacy realm because it is referenced by: " + referencingConfigsString);
+    }
+
+    super.deleteConfig(fromUi);
   }
 
 }
