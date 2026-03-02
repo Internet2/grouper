be67d7e2d8315ed48e976be219fb45b17963458a
mchyzer <mchyzer@isc.upenn.edu>
2026-03-02 18:13:15 -0500

    GRP-6641: (commit 4) do not allow a delete of data field if it is used somewhere else

--- grouper-ui/java/src/edu/internet2/middleware/grouper/grouperUi/serviceLogic/UiV2EntityDataFields.java
+++ grouper-ui/java/src/edu/internet2/middleware/grouper/grouperUi/serviceLogic/UiV2EntityDataFields.java
@@ -11,13 +11,10 @@ import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.util.regex.Pattern;
-
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 
 import java.util.Collections;
-import edu.internet2.middleware.grouper.app.loader.GrouperLoaderConfig;
 import edu.internet2.middleware.grouper.GrouperSession;
 import edu.internet2.middleware.grouper.cfg.GrouperConfig;
 import edu.internet2.middleware.grouper.cfg.text.GrouperTextContainer;
@@ -50,7 +47,6 @@ import edu.internet2.middleware.grouper.grouperUi.beans.ui.TextContainer;
 import edu.internet2.middleware.grouper.ui.GrouperUiFilter;
 import edu.internet2.middleware.grouper.util.GrouperUtil;
 import edu.internet2.middleware.grouperClient.collections.MultiKey;
-import edu.internet2.middleware.grouperClient.jdbc.GcDbAccess;
 import edu.internet2.middleware.subject.Subject;
 
 public class UiV2EntityDataFields {
@@ -1318,43 +1314,6 @@ public class UiV2EntityDataFields {
         throw new RuntimeException("ConfigId cannot be blank");
       }
       
-      // check if this data provider is referenced by any queries or change log queries
-      List<String> referencingConfigs = new ArrayList<>();
-
-      List<GrouperDataProviderQueryConfiguration> allQueryConfigs = GrouperDataProviderQueryConfiguration.retrieveAllDataProviderQueryConfigurations();
-      for (GrouperDataProviderQueryConfiguration queryConfig : GrouperUtil.nonNull(allQueryConfigs)) {
-        String providerConfigId = queryConfig.retrieveAttributeValueFromConfig("providerConfigId", false);
-        if (StringUtils.equals(configId, providerConfigId)) {
-          referencingConfigs.add("data provider query '" + queryConfig.getConfigId() + "'");
-        }
-      }
-
-      List<GrouperDataProviderChangeLogQueryConfiguration> allChangeLogQueryConfigs = GrouperDataProviderChangeLogQueryConfiguration.retrieveAllDataProviderChangeLogQueryConfigurations();
-      for (GrouperDataProviderChangeLogQueryConfiguration changeLogQueryConfig : GrouperUtil.nonNull(allChangeLogQueryConfigs)) {
-        String providerConfigId = changeLogQueryConfig.retrieveAttributeValueFromConfig("providerConfigId", false);
-        if (StringUtils.equals(configId, providerConfigId)) {
-          referencingConfigs.add("data provider change log query '" + changeLogQueryConfig.getConfigId() + "'");
-        }
-      }
-
-      // check if this data provider is referenced by any daemon jobs
-      Pattern daemonPattern = Pattern.compile("^otherJob\\.(.*)\\.dataProviderConfigId$");
-      Set<String> daemonConfigIds = GrouperLoaderConfig.retrieveConfig().propertyConfigIds(daemonPattern);
-      for (String daemonConfigId : GrouperUtil.nonNull(daemonConfigIds)) {
-        String daemonProviderConfigId = GrouperLoaderConfig.retrieveConfig().propertyValueString("otherJob." + daemonConfigId + ".dataProviderConfigId");
-        if (StringUtils.equals(configId, daemonProviderConfigId)) {
-          referencingConfigs.add("daemon '" + daemonConfigId + "'");
-        }
-      }
-
-      if (referencingConfigs.size() > 0) {
-        String referencingConfigsString = StringUtils.join(referencingConfigs, ", ");
-        guiResponseJs.addAction(GuiScreenAction.newMessage(GuiMessageType.error,
-            TextContainer.retrieveFromRequest().getText().get("dataProviderConfigDeleteErrorInUse")
-            + " " + referencingConfigsString));
-        return;
-      }
-
       GrouperDataProviderConfiguration dataProviderConfiguration = new GrouperDataProviderConfiguration();
 
       dataProviderConfiguration.setConfigId(configId);
