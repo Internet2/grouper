f056ae11a0692821b2e2797ed48360115eb96894
mchyzer <mchyzer@isc.upenn.edu>
2026-03-02 20:53:05 -0500

    GRP-6672: memory issues with httpclient command logging

--- grouper/src/grouper/edu/internet2/middleware/grouper/util/GrouperHttpClientLog.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/util/GrouperHttpClientLog.java
@@ -6,18 +6,41 @@ import java.util.Set;
 public class GrouperHttpClientLog {
 
   private StringBuilder log = new StringBuilder();
-  
-  
+
+  private boolean disabled = false;
+
+  public boolean isDisabled() {
+    return disabled;
+  }
+
+  public void setDisabled(boolean disabled) {
+    this.disabled = disabled;
+  }
+
   public StringBuilder getLog() {
     return log;
   }
 
-  
+
   public void setLog(StringBuilder log) {
     this.log = log;
   }
 
+  /**
+   * append to the log in a thread-safe way, respecting the max size
+   * @param text
+   * @param maxLogSize
+   */
+  public synchronized void appendIfRoom(CharSequence text, long maxLogSize) {
+    if (this.disabled) {
+      return;
+    }
+    if (this.log.length() + text.length() < maxLogSize) {
+      this.log.append(text);
+    }
+  }
+
   public GrouperHttpClientLog() {
   }
-  
+
 }
