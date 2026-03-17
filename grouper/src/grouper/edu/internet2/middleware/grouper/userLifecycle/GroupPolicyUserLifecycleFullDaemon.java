f10b7d954595c7da8b65e19bb04cd0d791b07399
mchyzer <mchyzer@isc.upenn.edu>
2026-03-17 02:43:09 -0400

    GRP-6792: make MultiKey hashing faster

--- grouper/src/grouper/edu/internet2/middleware/grouper/userLifecycle/GroupPolicyUserLifecycleFullDaemon.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/userLifecycle/GroupPolicyUserLifecycleFullDaemon.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.commons.collections.keyvalue.MultiKey;
+import edu.internet2.middleware.grouperClient.collections.MultiKey;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Strings;
 import org.apache.commons.lang3.builder.EqualsBuilder;
