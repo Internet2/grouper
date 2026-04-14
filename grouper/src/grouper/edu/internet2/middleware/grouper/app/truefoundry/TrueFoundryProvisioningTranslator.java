425a63048990da8577b92463b830527514011168
mchyzer <mchyzer@isc.upenn.edu>
2026-04-14 18:30:12 -0400

    GRP-6911: TrueFoundry provisioner: team managers not provisioned correctly

--- grouper/src/grouper/edu/internet2/middleware/grouper/app/truefoundry/TrueFoundryProvisioningTranslator.java
+++ grouper/src/grouper/edu/internet2/middleware/grouper/app/truefoundry/TrueFoundryProvisioningTranslator.java
@@ -1,12 +1,16 @@
 package edu.internet2.middleware.grouper.app.truefoundry;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
 
 import edu.internet2.middleware.grouper.app.provisioning.GrouperProvisioningConfigurationAttribute;
 import edu.internet2.middleware.grouper.app.provisioning.GrouperProvisioningTranslator;
+import edu.internet2.middleware.grouper.app.provisioning.ProvisioningEntityWrapper;
 import edu.internet2.middleware.grouper.app.provisioning.ProvisioningGroup;
 import edu.internet2.middleware.grouper.app.provisioning.ProvisioningGroupWrapper;
 import edu.internet2.middleware.grouper.util.GrouperUtil;
@@ -66,6 +70,37 @@ public class TrueFoundryProvisioningTranslator extends GrouperProvisioningTransl
       }
 
       Set<String> managerEntityIds = provisioningGroupWrapper.groupMembers(managerGroupName, "entityAttributeValueCache0");
+
+      // On the first provision the entity attribute cache may not be populated yet
+      // (it gets written after translation). Fall back to resolving entity IDs by
+      // matching manager group memberIds to the provisioner's in-memory entity wrappers
+      // and computing the ID from the grouperProvisioningEntity email field.
+      if (GrouperUtil.length(managerEntityIds) == 0) {
+        Set<String> managerMemberIds = provisioningGroupWrapper.groupMembers(managerGroupName, "memberId");
+        if (GrouperUtil.length(managerMemberIds) > 0) {
+          Map<String, ProvisioningEntityWrapper> memberIdToEntityWrapper = new HashMap<String, ProvisioningEntityWrapper>();
+          for (ProvisioningEntityWrapper entityWrapper : GrouperUtil.nonNull(
+              this.getGrouperProvisioner().retrieveGrouperProvisioningData().getProvisioningEntityWrappers())) {
+            if (entityWrapper.getMemberId() != null) {
+              memberIdToEntityWrapper.put(entityWrapper.getMemberId(), entityWrapper);
+            }
+          }
+          managerEntityIds = new HashSet<String>();
+          for (String managerMemberId : managerMemberIds) {
+            ProvisioningEntityWrapper entityWrapper = memberIdToEntityWrapper.get(managerMemberId);
+            if (entityWrapper != null) {
+              // Use the same field that entityAttributeValueCache0 is configured from:
+              // the entity "id" which in TrueFoundry is derived from the email field.
+              Object entityId = this.translateFromGrouperProvisioningEntityField(
+                  entityWrapper, "email");
+              if (entityId != null && !StringUtils.isBlank(entityId.toString())) {
+                managerEntityIds.add(entityId.toString());
+              }
+            }
+          }
+        }
+      }
+
       grouperTargetGroup.assignAttributeValue("managers", managerEntityIds);
     }
 
