ef440a879d100c7e1b1f3697ff8e82399b2af979
Vivek Sachdeva <erviveksachdeva@gmail.com>
2026-07-30 13:39:22 -0700

    GRP-7174 - add ability to order lifecycle radios on group page, alpha is not ideal here

--- grouper-ui/java/src/edu/internet2/middleware/grouper/grouperUi/beans/ui/GuiUserLifecyclePolicy.java
+++ grouper-ui/java/src/edu/internet2/middleware/grouper/grouperUi/beans/ui/GuiUserLifecyclePolicy.java
@@ -1,6 +1,7 @@
 package edu.internet2.middleware.grouper.grouperUi.beans.ui;
 
 import edu.internet2.middleware.grouper.userLifecycle.UserLifecyclePolicyConfiguration;
+import edu.internet2.middleware.grouper.util.GrouperUtil;
 
 public class GuiUserLifecyclePolicy {
   
@@ -9,6 +10,8 @@ public class GuiUserLifecyclePolicy {
   private String policyName;
   
   private String description;
+  
+  private Integer index;
 
   public String getConfigId() {
     return configId;
@@ -38,12 +41,23 @@ public class GuiUserLifecyclePolicy {
     this.description = description;
   }
 
+  
+  public Integer getIndex() {
+    return index;
+  }
+
+  
+  public void setIndex(Integer index) {
+    this.index = index;
+  }
+
   public static GuiUserLifecyclePolicy convertFromUserLifecyclePolicyConfiguration(UserLifecyclePolicyConfiguration userLifecyclePolicyConfiguration) {
     
     GuiUserLifecyclePolicy guiUserLifecyclePolicy = new GuiUserLifecyclePolicy();
     guiUserLifecyclePolicy.setConfigId(userLifecyclePolicyConfiguration.getConfigId());
     guiUserLifecyclePolicy.setPolicyName(userLifecyclePolicyConfiguration.retrieveAttributeValueFromConfig("name", true));
     guiUserLifecyclePolicy.setDescription(userLifecyclePolicyConfiguration.retrieveAttributeValueFromConfig("description", true));
+    guiUserLifecyclePolicy.setIndex(GrouperUtil.intObjectValue(userLifecyclePolicyConfiguration.retrieveAttributeValueFromConfig("index", true), true));
     
     return guiUserLifecyclePolicy;
   }
