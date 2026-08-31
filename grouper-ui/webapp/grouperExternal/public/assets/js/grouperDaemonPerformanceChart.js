9fdfc6e237a4c0ef891e63cb9f3cacb72356d5ed
mchyzer <mchyzer@isc.upenn.edu>
2026-08-31 00:22:12 -0400

    Revert "GRP-7130 - show seconds and minutes when the max runtime of millis is in seconds and minutes"
    This reverts commit 1bcbc8f94aa95aeab0449bb3aa84c56d48d03c64.

--- grouper-ui/webapp/grouperExternal/public/assets/js/grouperDaemonPerformanceChart.js
+++ grouper-ui/webapp/grouperExternal/public/assets/js/grouperDaemonPerformanceChart.js
@@ -1,28 +1,17 @@
 //let daemonPerformanceData = {}; // returned from UiV2Admin.viewPerformanceChartResults
 
 function drawDaemonPerformanceChartD3() {
-    const millisValues = daemonPerformanceData['millis'] || [];
-    const maxMillis = millisValues.length > 0 ? Math.max(...millisValues) : 0;
-
-    // pick a single unit for the whole graph based on the largest value
-    let divisor = 1;
-    let unitLabel = 'millis';
-    if (maxMillis >= 60000) {
-        divisor = 60000;
-        unitLabel = 'minutes';
-    } else if (maxMillis > 10000) {
-        divisor = 1000;
-        unitLabel = 'seconds';
-    }
-
-    const convertedValues = millisValues.map(v => v / divisor);
-
     const c3_daemonPerformanceData = {
         x: 'date',
         columns: [
-            ['date', ...daemonPerformanceData['date'].map(dt => new Date(dt*1000))],
-            [unitLabel, ...convertedValues]
+            ['date', ...daemonPerformanceData['date'].map(dt => new Date(dt*1000))]
         ]
+    }
+
+    for (const [key, value] of Object.entries(daemonPerformanceData)) {
+        if (key != 'date') {
+            c3_daemonPerformanceData.columns.push([key, ...value],)
+        }
     }
 
     const chartPerformance = c3.generate({
@@ -39,16 +28,8 @@ function drawDaemonPerformanceChartD3() {
             },
             y: {
                 label: {
-                    text: unitLabel,
+                    text: 'millis',
                     position: 'outer-middle'
-                }
-            }
-        },
-        tooltip: {
-            format: {
-                value: function (value) {
-                    const rounded = divisor === 1 ? value : Math.round(value * 100) / 100;
-                    return rounded + ' ' + unitLabel;
                 }
             }
         }
